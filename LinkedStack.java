3588ce113fc42adb3717ab19d3fc7783a73b4c7d
contactaditya <contactadityag@gmail.com>
2016-08-11 20:04:45 -0400

    This is the code for implementing a stack in java using singly linked nodes.

---
+++ LinkedStack.java
@@ -0,0 +1,150 @@
+import java.io.*;
+import java.util.*;
+
+public class LinkedStack {
+	
+private static class Node {
+  public Object data;
+  public Node next;
+        
+  public Node(Object data, Node next) {
+    this.data = data;
+    this.next = next;
+    }
+  }	
+	
+public static Node top = null;
+
+public static void push(Object object) {
+  /**
+   * Add an item to back of stack.
+   */
+		   
+   top = new Node(object, top);
+}
+
+public static Object pop() {
+  /**
+   * Removes the next item from the front of the stack.	 
+   */	 
+	
+  if(top == null) {
+  System.out.println("The stack is already empty. No element can be removed from the stack.");  
+  return -1;
+  }	
+	
+  Object item = top.data;
+  top = top.next;
+  return item;	
+  
+  }
+
+public static boolean isEmpty() {
+  /**
+   * Test if the stack is empty.
+   */
+	
+ System.out.println();	
+	
+ if(top == null) {	 
+    System.out.println("The stack is currently empty and has no elements.");	 	  	   
+  }
+		   
+ else {	
+	 System.out.println("The stack is currently not empty.");	 		   
+  }
+ return top == null;
+ }
+		 
+public static int size() {
+  /**
+   * Returns the number of items currently in the stack.
+   */
+	
+  int count = 0;
+  for (Node node = top; node != null; node = node.next) {
+  count++;
+  }
+  
+  System.out.println();		    
+  System.out.println("The size of the stack is: " + count);	 
+  return count;	
+ }
+		
+public static void display() {
+  /**
+   * Displays all the contents of the stack.
+   */
+	
+  System.out.println();		
+	
+  System.out.print("Contents of the stack are: ");   	
+	
+  for (Node node = top; node != null; node = node.next) {
+    System.out.print(node.data + " ");
+  }
+  
+  System.out.println();		 
+ }
+		 
+public static Object peek() {
+  /**
+   * Returns the front item from the stack without popping it.
+   */	 
+	
+  System.out.println();	
+	
+ if(top == null) {  	 
+   System.out.print("The stack is empty so we can't see the last item of the stack.");  
+   return -1;
+ }	 
+		  
+ else { 
+   System.out.print("The following element is the last element of the stack: " + top.data);  	  
+  }
+ System.out.println();	
+ 
+ return top.data;
+ }
+
+public static void main(String[] args) throws NumberFormatException, IOException {
+	 // TODO Auto-generated method stub
+		  
+  int choice = 0;
+	    
+	do{	
+	    System.out.println("1. For inserting an element in the stack.");
+	    System.out.println("2. For deleting an element from the stack.");
+	    System.out.println("3. For displaying the contents of the stack.");
+	    System.out.println("4. For checking the contents of the stack are empty or not");
+	    System.out.println("5. For checking the size of the stack");
+	    System.out.println("6. For returning the last element from the stack without popping it.");
+	    System.out.println("7. For exiting from the program.");
+	    
+	    System.out.println();	
+	    	
+	    System.out.print("Enter your choice: ");
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+	    choice = Integer.parseInt(br.readLine());
+	    switch (choice) {
+	      case 1:  System.out.println();	  
+	    	  	   System.out.print("Enter a element to insert in the stack: ");
+	      		   BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));  	
+	      		   String element = br1.readLine();
+	    	  	   push(element);
+	    	  	   break;
+	      case 2:  pop();
+	               break;
+	      case 3:  display();
+	               break;
+	      case 4:  isEmpty();
+	               break;
+	      case 5:  size();
+	      		   break;  
+	      case 6:  peek();
+			   	   break;     
+	      }
+	    System.out.print("\n");
+		}while(choice!=7);
+	}
+}
