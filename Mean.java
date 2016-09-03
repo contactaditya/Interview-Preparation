b507fa78c2380511cc0789df468cae0a1bf1e774
Aditya Gupta <contactadityag@gmail.com>
2016-09-03 04:37:42 -0400

    This is the program for calculating the mean of an array.

--- Mean.java
+++ Mean.java
@@ -7,12 +7,12 @@ public class Mean {
 	
   public static long calculateMean(long[] array) {
 
-	long n = array.length;  
+    long n = array.length;  
     long sum = 0;
     long mean = 0;
      
     for(int i=0; i<n; i++) {
-    sum = sum + array[i];
+      sum = sum + array[i];
     }
 
     mean = sum/n;
@@ -22,22 +22,22 @@ public class Mean {
 
   public static void main(String[] args) throws NumberFormatException, IOException {
 		
-	Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     System.out.print("Enter number of elements in the array: ");
     int numbers = input.nextInt();   
     long array[] = new long[numbers];
     
     System.out.print("\n");
-	System.out.print("Please enter the actual values in the array: ");
-	for (int i = 0; i < numbers; i++) {
-	  array[i] = input.nextInt();
-	}
+    System.out.print("Please enter the actual values in the array: ");
+    for (int i = 0; i < numbers; i++) {
+      array[i] = input.nextInt();
+    }
 	
-	System.out.print("\n");
+    System.out.print("\n");
 	
   System.out.print("The actual values in the array are: ");
-    for (int i = 0; i < numbers; i++) {
-	System.out.print(array[i] + " ");
+  for (int i = 0; i < numbers; i++) {
+     System.out.print(array[i] + " ");
   }  
 	
   System.out.print("\n");
