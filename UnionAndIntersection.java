3d38a4a37cd178ca598d692e9ced87996bc5e5ed
contactaditya <contactadityag@gmail.com>
2016-09-03 04:22:30 -0400

    This is a program to calculate union and intersection of two sorted arrays in java.

--- UnionAndIntersection.java
+++ UnionAndIntersection.java
@@ -16,7 +16,7 @@ public class UnionAndIntersection {
   
   while(i < x && j < y) {
    if(array[i] < array1[j]) {
-     i++;  
+	 i++;  
    }
    
    else if(array[i] > array1[j]) {
@@ -24,9 +24,9 @@ public class UnionAndIntersection {
    }
    
    else {
-     list.add(array[i]);
-     i++;
-     j++;
+	list.add(array[i]);
+	i++;
+	j++;
    }
   }
   
@@ -47,21 +47,21 @@ public class UnionAndIntersection {
   System.out.print("The value of union of the sorted arrays is: ");
   
   while(i < x && j < y) {
-    if(array[i] < array1[j]) {
-      list.add(array[i]);	
-      i++;  
-    }
+	if(array[i] < array1[j]) {
+	  list.add(array[i]);	
+	  i++;  
+	}
 	   
-    else if(array[i] > array1[j]) {
-      list.add(array1[j]);		
-      j++;  
-    }
+	else if(array[i] > array1[j]) {
+	  list.add(array1[j]);		
+	  j++;  
+	}
 	   
-    else {
-      list.add(array[i]);
-      i++;
-      j++;
-    }
+	else {
+	list.add(array[i]);
+	i++;
+	j++;
+	}
   }
   
   
@@ -71,8 +71,8 @@ public class UnionAndIntersection {
   }
   
   while (j < array1.length) {
-    list.add(array1[j]);
-    j++;
+	list.add(array1[j]);
+	j++;
   }
   
   Set<Integer> uniqueSet = new TreeSet<Integer>(list);
@@ -87,14 +87,13 @@ public class UnionAndIntersection {
 	  
   Scanner input = new Scanner(System.in);
   System.out.print("Enter number of elements in the first array: ");
-  int number = Integer.parseInt(input.nextLine());  
+  int number = input.nextInt();  
   int array[] = new int[number];
   
   System.out.println();	
   System.out.print("Please enter number: ");
-  System.out.println();	
   for (int i = 0; i < array.length; i++) {
-    array[i] = Integer.parseInt(input.nextLine());
+    array[i] = input.nextInt();
   }
     
   Arrays.sort(array);
@@ -102,14 +101,13 @@ public class UnionAndIntersection {
   Scanner input1 = new Scanner(System.in);
   System.out.println();	
   System.out.print("Enter number of elements in the second array: ");
-  int number1 = Integer.parseInt(input1.nextLine());  
+  int number1 = input.nextInt();  
   int array1[] = new int[number1];
   
   System.out.println();	
   System.out.print("Please enter number: ");
-  System.out.println();	
   for (int i = 0; i < array1.length; i++) {
-    array1[i] = Integer.parseInt(input1.nextLine());
+	array1[i] = input.nextInt();
   }
   
   Arrays.sort(array1);
@@ -118,22 +116,22 @@ public class UnionAndIntersection {
   System.out.println();	
 	    
   do{	
-  System.out.println("1. To calculate the interection of the sorted arrays.");
-  System.out.println("2. To calculate the union of the sorted arrays.");
-  System.out.println("3. For exiting from the program.");
+	 System.out.println("1. To calculate the interection of the sorted arrays.");
+	 System.out.println("2. To calculate the union of the sorted arrays.");
+	 System.out.println("3. For exiting from the program.");
 	   
-  System.out.println();	
+	 System.out.println();	
 	    	
-  System.out.print("Enter your choice: ");
-  BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));  
-  choice = Integer.parseInt(br1.readLine());
-  switch (choice) {
-    case 1:  getIntersection(array, array1);
-	     break;
-    case 2:  getUnion(array, array1);
-	     break;    
-  }
- System.out.print("\n");
+	 System.out.print("Enter your choice: ");
+	 BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));  
+	 choice = Integer.parseInt(br1.readLine());
+	 switch (choice) {
+	 case 1:  getIntersection(array, array1);
+	    	  break;
+	 case 2:  getUnion(array, array1);
+	          break;    
+	 }
+	 System.out.print("\n");
    }while(choice!=3);
  }     
 }
