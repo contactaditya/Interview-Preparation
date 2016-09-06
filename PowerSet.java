dfe73c215792d004444d9375740b0ae9adf79af3
contactaditya <contactadityag@gmail.com>
2016-09-06 05:25:49 -0400

    This is the program for displaying all the subsets of a set including the empty set and the set itself. This is also known as Power Set.

---
+++ PowerSet.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class PowerSet {   
+	
+ public static void printPowerSet(char array[]) { 
+   int n = array.length;
+   
+   for (int i = 0; i < (1<<n); i++)
+   {
+     System.out.print("{");
+       
+   for (int j = 0; j < n; j++) {
+	if ((i & (1 << j)) > 0) {
+       System.out.print(array[j] + "");  
+	}   
+   }  
+     System.out.println("}");
+   }
+ }
+
+  public static void main(String[] args) { 	
+	Scanner input = new Scanner(System.in);
+    System.out.print("Enter number of characters in the set: ");
+    int number = input.nextInt();  
+    char array[] = new char[number];
+  
+    System.out.println();	
+    System.out.print("Please enter the actual characters: ");
+    for (int i = 0; i < array.length; i++) {
+      array[i] = input.next().charAt(0);
+    }
+    	   	
+    printPowerSet(array);
+  }
+}
