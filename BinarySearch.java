214880dabc29fb595648dad6479aa4b283c1db6c
Aditya Gupta <contactadityag@gmail.com>
2016-09-06 23:21:18 -0400

    This is the program for Binary Search.

--- BinarySearch.java
+++ BinarySearch.java
@@ -21,33 +21,33 @@ public static int binarySearch(int[] a, int key) {
 return -1;
 }
 	
- public static void main(String[] args) throws NumberFormatException, IOException {
-   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
-   System.out.println("Enter number of elements in the array:");
-   int number = Integer.parseInt(br.readLine());
-   int array[] = new int[number];
-   System.out.println("Please enter the actual values in the array:");
-   for (int i = 0; i < number; i++) {
-     array[i] = Integer.parseInt(br.readLine());
-   }
-   System.out.print("The actual values in the array are: ");
-   for (int i = 0; i < number; i++) {
-     System.out.print(array[i] + " ");
-   }
-   Arrays.sort(array);
-   System.out.print("\n");
-   System.out.print("The sorted array is: ");
-   for (int i=0; i < array.length; i++) {
-     System.out.print(array[i] + " ");
-   }
-   System.out.print("\n");
-   System.out.println("Enter the element to search:");
-   int key = Integer.parseInt(br.readLine());
-   int index = binarySearch(array,key);
-   if (index != -1) {
-    System.out.println("Element found at index position: " + index);
-   } else {
-    System.out.println("Element has not been found");
-    }
+public static void main(String[] args) throws NumberFormatException, IOException {
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+  System.out.println("Enter number of elements in the array:");
+  int number = Integer.parseInt(br.readLine());
+  int array[] = new int[number];
+  System.out.println("Please enter the actual values in the array:");
+  for (int i = 0; i < number; i++) {
+    array[i] = Integer.parseInt(br.readLine());
   }
+  System.out.print("The actual values in the array are: ");
+  for (int i = 0; i < number; i++) {
+    System.out.print(array[i] + " ");
+  }
+  Arrays.sort(array);
+  System.out.print("\n");
+  System.out.print("The sorted array is: ");
+  for (int i=0; i < array.length; i++) {
+    System.out.print(array[i] + " ");
+  }
+  System.out.print("\n");
+  System.out.println("Enter the element to search:");
+  int key = Integer.parseInt(br.readLine());
+  int index = binarySearch(array,key);
+  if (index != -1) {
+    System.out.println("Element found at index position: " + index);
+  } else {
+    System.out.println("Element has not been found");
+  }
+ }
 }	
