4e00648934db77683264611f6026ab8aff327c8c
Aditya Gupta <contactadityag@gmail.com>
2016-09-09 07:53:30 -0400

    This is the program for creating an output array such that output[i] is the product of all input elements except input[i].

--- findProductWithoutCurrentElement.java
+++ findProductWithoutCurrentElement.java
@@ -10,7 +10,7 @@ public class findProductWithoutCurrentElement {
   // Calculating the product of all the elements to the left of the current element 
    
    for(int i = 0; i < n; i++) {
-	 product[i] = multiplication;  
+     product[i] = multiplication;  
      multiplication = multiplication * array[i];
    }
    
@@ -19,15 +19,15 @@ public class findProductWithoutCurrentElement {
   // Calculating the product of all the elements to the right of the current element 
    
    for(int i = n-1; i >= 0; i--) {
-	 product[i] = product[i] * multiplication;  
-	 multiplication = multiplication * array[i];
+     product[i] = product[i] * multiplication;  
+     multiplication = multiplication * array[i];
    }
    
    System.out.println();	
    System.out.print("The content of the output array is: ");
    
    for(int i = 0; i < n; i++) {
-	 System.out.print(product[i] + " ");
+     System.out.print(product[i] + " ");
    }
  }
  
@@ -37,11 +37,11 @@ public class findProductWithoutCurrentElement {
    int multiplication = 1;
 
    for(int i = 0; i < n; i++) {
-	 multiplication = multiplication * array[i];
+     multiplication = multiplication * array[i];
    }
 	   
    for(int j = 0; j < n; j++) {
-	 product[j] = multiplication/array[j];
+     product[j] = multiplication/array[j];
    }
 	   
    System.out.println();	
@@ -61,7 +61,7 @@ public class findProductWithoutCurrentElement {
   System.out.println();	
   System.out.print("Please enter the elements in the array: ");
   for (int i = 0; i < array.length; i++) {
-	array[i] = input.nextInt();
+     array[i] = input.nextInt();
   }
 	  
   int firstElement = array[0];
