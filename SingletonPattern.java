36c8792b25b687caa1dabe0a84f5fb3bd882b7d5
contactaditya <contactadityag@gmail.com>
2016-10-13 01:47:31 -0400

    This is the program which is an example of a singleton implementation.

---
+++ SingletonPattern.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class SingletonPattern {
+	
+ private static SingletonPattern instance = null;
+ private SingletonPattern() {}
+    
+ public static synchronized SingletonPattern getInstance() {
+   if (instance == null) {
+	instance = new SingletonPattern();
+   }
+  return instance;
+ }
+ 
+ public void getMessage(){
+   System.out.println("This is an example of Singleton Pattern."); 
+ }
+    
+ public static void main(String[] args) {
+   SingletonPattern singleton = SingletonPattern.getInstance();
+   singleton.getMessage();
+ }
+}
