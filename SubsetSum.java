e306330bec4d20bff23e49d3c73430adad46c907
Aditya Gupta <contactadityag@gmail.com>
2016-10-27 22:59:46 -0400

    This is the code for solving the Subset Sum Problem which determines if there is a subset of the given set with sum equal to given sum.
    In this solution we use dynamic programming to create a 2D table subset[][] and fill it in bottom up manner.

--- SubsetSum.java
+++ SubsetSum.java
@@ -18,7 +18,7 @@ public class SubsetSum {
   // We can't find a sum > 0 in an empty array.
   
   for(int s = 1; s <= sum; s++) {
-	subset[s][0] = false;
+    subset[s][0] = false;
   }
   
   // Fill the subset table in a bottom up manner.
@@ -28,40 +28,40 @@ public class SubsetSum {
 	   
     subset[i][j] = subset[i][j-1];
     
-     if(i >= array[j-1]) {
+    if(i >= array[j-1]) {
       subset[i][j] = subset[i][j] || subset[i - array[j-1]][j-1];
-     }    
-    }
+    }    
+   }
   }
   return subset[sum][n];
  }	
 		
   public static void main(String[] args) throws NumberFormatException, IOException {
 	  
-	Scanner input = new Scanner(System.in);
-	System.out.print("Enter number of elements in the array: ");
-	int number = input.nextInt();  
-	int array[] = new int[number];
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter number of elements in the array: ");
+    int number = input.nextInt();  
+    int array[] = new int[number];
+	 
+    System.out.println();	
+    System.out.print("Please enter number: ");
+    for (int i = 0; i < array.length; i++) {
+       array[i] = input.nextInt();
+    }
 	
-	System.out.println();	
-	System.out.print("Please enter number: ");
-	for (int i = 0; i < array.length; i++) {
-	  array[i] = input.nextInt();
-	}
-	
-	Scanner input1 = new Scanner(System.in);
-	System.out.println();	
-	System.out.print("Enter the sum of subset that you want to check whether it is present in the array or not: ");
-	int number1 = input1.nextInt();  
+    Scanner input1 = new Scanner(System.in);
+    System.out.println();	
+    System.out.print("Enter the sum of subset that you want to check whether it is present in the array or not: ");
+    int number1 = input1.nextInt();  
 		
-	boolean max = SubsetSum(array, number1);
-	System.out.print("\n");
+    boolean max = SubsetSum(array, number1);
+    System.out.print("\n");
 	
-	if(max) {
+    if(max) {
       System.out.println("The array contains a subset with the sum: " + number1);	
-	}
-	else {
-	  System.out.println("The array does not contain a subset with the sum: " + number1);	
-	}
+    }
+    else { 
+      System.out.println("The array does not contain a subset with the sum: " + number1);	
+    }
   }     
 }
