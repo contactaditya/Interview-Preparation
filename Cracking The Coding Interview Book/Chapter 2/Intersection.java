411f999c1d4fe799957c88075c18f07e20741812
Aditya Gupta <contactadityag@gmail.com>
2016-12-16 17:31:31 -0500

    This is the solution to the Intersection Problem.

--- Cracking The Coding Interview Book/Chapter 2/Intersection.java
+++ Cracking The Coding Interview Book/Chapter 2/Intersection.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
  public class Intersection { 
-	private static LinkedListNode head1;
-	private static LinkedListNode head2;
-	private static LinkedListNode head;
-	private static int size;  
+     private static LinkedListNode head1;
+     private static LinkedListNode head2;
+     private static LinkedListNode head;
+     private static int size;  
 
-	public Intersection() {
+     public Intersection() {
     // this is an empty list, so the reference to the head node is set to a new node with no data				  
-	  head = new LinkedListNode(null);
-	  size = 0;
-	}
+        head = new LinkedListNode(null);
+	size = 0; 
+     }
 	
-	public static int getCount(LinkedListNode node) {
+     public static int getCount(LinkedListNode node) {
     	LinkedListNode current = node;
         int count = 0;
  
@@ -22,47 +22,47 @@ import java.util.*;
         }
  
       return count;
-    }
+     }
 	
-	public static int getNode(int d, LinkedListNode headA, LinkedListNode headB) {
-		LinkedListNode current1 = headA;
-		LinkedListNode current2 = headB;
+     public static int getNode(int d, LinkedListNode headA, LinkedListNode headB) {
+	LinkedListNode current1 = headA;
+	LinkedListNode current2 = headB;
 	 	         		     
-		for(int i = 0; i < d; i++) {
-		   if (current1 == null) {
-	          return -1;
-	       }
-		  current1 = current1.next;
-	    }
-				     
-		while (current1 != null && current2 != null) {
-	      if (current1.data == current2.data) {
-	         return (int) current1.data;
-	      }
-	      current1 = current1.next;
-	      current2 = current2.next;
-	    } 
-	  return -1;   
+	for(int i = 0; i < d; i++) {
+	   if (current1 == null) {
+	      return -1;
+	   }
+          current1 = current1.next;
 	}
+				     
+	while (current1 != null && current2 != null) {
+	   if (current1.data == current2.data) {
+	      return (int) current1.data;
+	   }
+	   current1 = current1.next;
+	   current2 = current2.next;
+	 } 
+       return -1;   
+     }
 	
-	public static int findIntersection(LinkedListNode headA, LinkedListNode headB) {
-	   if(headA == null || headB == null) {
-		   return -1;
-	   }
+     public static int findIntersection(LinkedListNode headA, LinkedListNode headB) {
+        if(headA == null || headB == null) {
+           return -1;
+        }
 	   
-	   // Get the sizes of the two linked lists.
+       // Get the sizes of the two linked lists.
 		
-	   int current1 = getCount(headA);
-	   int current2 = getCount(headB);
-	   int d;
+	int current1 = getCount(headA);
+	int current2 = getCount(headB);
+	int d;
 	   
-	   if(current1 > current2) {
-		  d = current1 - current2;
-		  return getNode(d, headA, headB); 
-	   } else {
-		  d = current2 - current1;
-		  return getNode(d, headA, headB); 
-	   }
+	if(current1 > current2) {
+           d = current1 - current2;
+           return getNode(d, headA, headB); 
+	} else {
+	    d = current2 - current1;
+            return getNode(d, headA, headB); 
+	  }
      }
 
 	public static void main(String[] args) {  
@@ -83,7 +83,7 @@ import java.util.*;
 	    System.out.println("The value of the node where the two lists merge is: " + intersection);
 	  }
 	  else {
-		System.out.println("The linkedlist do not intersect.");  
+	    System.out.println("The linkedlist do not intersect.");  
 	  }
 	}
   }
