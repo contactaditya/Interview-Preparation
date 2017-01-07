5f1f70dc9fc77fbd8d3406e4a692a11c30c2c4c6
contactaditya <contactadityag@gmail.com>
2017-01-06 23:56:14 -0500

    This is a program to print Left View of a Binary Tree.

---
+++ PrintLeftViewOfABinaryTree.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+ public class PrintLeftViewOfABinaryTree { 	
+	static Node root;	    
+	static Node previous;
+	static int  maxLevel = 0;	
+	
+	public static void leftView() {
+       leftViewUtil(root, 1);
+    }
+	
+	public static void leftViewUtil(Node node, int level) {
+	   
+	   if(node == null) {
+		  return;
+	   }	
+	   
+	// If this is the first node of its level
+	   
+	   if (maxLevel < level) {
+         System.out.print(node.data + " ");
+         maxLevel = level;
+       }
+	   
+	   leftViewUtil(node.left, level+1);
+       leftViewUtil(node.right, level+1);
+	}
+
+	public static void main(String[] args) { 
+	  PrintLeftViewOfABinaryTree tree = new PrintLeftViewOfABinaryTree();
+	  tree.root = new Node(12);
+	  tree.root.left = new Node(10);
+	  tree.root.right = new Node(30);
+	  tree.root.right.left = new Node(25);
+	  tree.root.right.right = new Node(40);
+	
+	     
+	  System.out.print("Left view of the Binary Tree is: ");
+	  tree.leftView();		
+	}
+}
