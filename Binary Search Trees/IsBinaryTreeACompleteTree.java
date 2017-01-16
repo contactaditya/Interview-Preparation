ce9d5096440bc16a05537edee9f9571a247a809c
contactaditya <contactadityag@gmail.com>
2017-01-16 10:48:37 -0500

    This is a program to check whether a binary tree is a complete tree or not.

---
+++ Binary Search Trees/IsBinaryTreeACompleteTree.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+ public class IsBinaryTreeACompleteTree { 
+	Node root;	    
+	Node previous;
+	
+	public int getNodeCount(Node node) {
+		 
+	  if (node == null) {
+	   	return 0;
+	  }
+	 
+	  return (1 + getNodeCount(node.left) + getNodeCount(node.right));   	
+	}
+	
+	public boolean isComplete(Node node, int index, int nodeCount) {
+		 
+       if (node == null) {
+		 return true;
+	   }
+       
+       // If index assigned to current node is more than number of nodes in tree, then tree is not complete
+       if (index >= nodeCount) {
+          return false;
+       }
+        
+	   return (isComplete(root.left, 2 * index + 1, nodeCount) && isComplete(root.right, 2 * index + 2, nodeCount));  	
+    }
+
+	public static void main(String[] args) {   
+	  IsBinaryTreeACompleteTree tree = new IsBinaryTreeACompleteTree();
+	  tree.root = new Node(1);
+      tree.root.left = new Node(2);
+      tree.root.right = new Node(3);
+      tree.root.left.right = new Node(5);
+      tree.root.left.left = new Node(4);
+      tree.root.right.right = new Node(6);
+      
+      int index = 0;  
+      int nodeCount = tree.getNodeCount(tree.root);
+      
+      if(tree.isComplete(tree.root, index, nodeCount)) {
+    	System.out.println("The binary tree is complete.");  
+      }
+      else {
+    	System.out.println("The binary tree is not complete.");    
+      }
+      
+    }
+  }
