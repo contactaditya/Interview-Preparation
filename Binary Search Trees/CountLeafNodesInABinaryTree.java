ac03f3341851e5695b1031ae961cd895df37109f
contactaditya <contactadityag@gmail.com>
2017-01-16 10:54:10 -0500

    This program counts the number of leaf nodes in a binary tree.

---
+++ Binary Search Trees/CountLeafNodesInABinaryTree.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+ public class CountLeafNodesInABinaryTree { 
+	Node root;	    
+    Node previous;
+    
+    public int getLeafCount(Node node) {
+		 
+   	 if (node == null) {
+   		return 0;
+   	 }
+   	
+   	 if (node.left == null && node.right == null) {    
+        return 1;
+     }	 
+     else {
+       return getLeafCount(node.left) + getLeafCount(node.right);
+     }   	
+   }
+
+	public static void main(String[] args) {
+	  CountLeafNodesInABinaryTree tree = new CountLeafNodesInABinaryTree();
+	  tree.root = new Node(1);
+	  tree.root.left = new Node(2);
+	  tree.root.right = new Node(3);
+	  tree.root.left.left = new Node(4);
+	  tree.root.left.right = new Node(5);
+	         
+	  System.out.println("The leaf count of binary tree is : " + tree.getLeafCount(tree.root));
+
+	}
+
+}
