fc4401224af5d64d5b40fe4f9322754f8859895e
Aditya Gupta <contactadityag@gmail.com>
2017-01-26 17:43:04 -0500

    This is a program to check whether a binary tree is a complete tree or not.

--- Binary Search Trees/IsBinaryTreeACompleteTree.java
+++ Binary Search Trees/IsBinaryTreeACompleteTree.java
@@ -2,35 +2,35 @@ import java.io.*;
 import java.util.*;
 
  public class IsBinaryTreeACompleteTree { 
-	Node root;	    
-	Node previous;
+    Node root;	    
+    Node previous;
 	
-	public int getNodeCount(Node node) {
-		 
-	  if (node == null) {
-	   	return 0;
-	  }
-	 
-	  return (1 + getNodeCount(node.left) + getNodeCount(node.right));   	
-	}
-	
-	public boolean isComplete(Node node, int index, int nodeCount) {
+    public int getNodeCount(Node node) {
 		 
        if (node == null) {
-		 return true;
-	   }
+	  return 0;
+       }
+	 
+       return (1 + getNodeCount(node.left) + getNodeCount(node.right));   	
+    }
+	
+    public boolean isComplete(Node node, int index, int nodeCount) {
+		 
+       if (node == null) {
+	  return true;
+       }
        
        // If index assigned to current node is more than number of nodes in tree, then tree is not complete
        if (index >= nodeCount) {
           return false;
        }
         
-	   return (isComplete(root.left, 2 * index + 1, nodeCount) && isComplete(root.right, 2 * index + 2, nodeCount));  	
+      return (isComplete(root.left, 2 * index + 1, nodeCount) && isComplete(root.right, 2 * index + 2, nodeCount));  	
     }
 
-	public static void main(String[] args) {   
-	  IsBinaryTreeACompleteTree tree = new IsBinaryTreeACompleteTree();
-	  tree.root = new Node(1);
+ public static void main(String[] args) {   
+      IsBinaryTreeACompleteTree tree = new IsBinaryTreeACompleteTree();
+      tree.root = new Node(1);
       tree.root.left = new Node(2);
       tree.root.right = new Node(3);
       tree.root.left.right = new Node(5);
@@ -47,5 +47,5 @@ import java.util.*;
     	System.out.println("The binary tree is not complete.");    
       }
       
-    }
-  }
+   }
+ }
