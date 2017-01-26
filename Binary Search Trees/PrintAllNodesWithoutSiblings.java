9d0989e631310228a5345f7a08c1751a7590976b
Aditya Gupta <contactadityag@gmail.com>
2017-01-26 17:59:51 -0500

    This is the program for printing all the nodes that don't have a sibling in a Binary Tree.

--- Binary Search Trees/PrintAllNodesWithoutSiblings.java
+++ Binary Search Trees/PrintAllNodesWithoutSiblings.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
  public class PrintAllNodesWithoutSiblings {    
-	Node root;	    
-	Node previous;
+     Node root;	    
+     Node previous;
 
  public void printSingles(Node node) {	
 		 
-	if(node == null) {
-	  return;
-	}
+    if (node == null) {
+       return;
+    }
 	
-	if (node.left != null && node.right != null) {
-	   printSingles(node.left);
-	   printSingles(node.right);
+    if (node.left != null && node.right != null) {
+        printSingles(node.left);
+	printSingles(node.right);
     }
 	
     else if (node.right != null) {
-	   System.out.print(node.right.data + " ");
-	   printSingles(node.right);
-	}
+        System.out.print(node.right.data + " ");
+        printSingles(node.right);
+    }
 	
     else if (node.left != null) {
        System.out.print( node.left.data + " ");
@@ -27,7 +27,7 @@ import java.util.*;
  }
 
   public static void main(String[] args) { 	
-	PrintAllNodesWithoutSiblings tree = new PrintAllNodesWithoutSiblings();
+    PrintAllNodesWithoutSiblings tree = new PrintAllNodesWithoutSiblings();
     tree.root = new Node(1);
     tree.root.left = new Node(2);
     tree.root.right = new Node(3);
@@ -35,7 +35,7 @@ import java.util.*;
     tree.root.right.left = new Node(5);
     tree.root.right.left.right = new Node(6);
 	     
-	System.out.print("The nodes that don't have siblings: ");
-	tree.printSingles(tree.root);		
+    System.out.print("The nodes that don't have siblings: ");
+    tree.printSingles(tree.root);		
   }
 }
