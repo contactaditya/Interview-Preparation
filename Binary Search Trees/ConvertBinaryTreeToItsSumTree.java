aecf551d8c05035bc8e051bcd2e235e454fbbb2f
Aditya Gupta <contactadityag@gmail.com>
2017-01-26 16:49:34 -0500

    This is the program where each node contains the sum of the left and the right sub trees in the original tree.

--- Binary Search Trees/ConvertBinaryTreeToItsSumTree.java
+++ Binary Search Trees/ConvertBinaryTreeToItsSumTree.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
  public class ConvertBinaryTreeToItsSumTree {  
-	Node root;	    
-	Node previous;
+    Node root;	    
+    Node previous;
 
   public int toSumTree(Node node) {
 		 
      if (node == null) {
-	   return 0;
+	return 0;
      }
      
      int oldValue = node.data;
@@ -17,29 +17,29 @@ import java.util.*;
   
   public void printInorderTraversal(Node node) {
 		 
-	   if (node == null) {
-		 return;
-	   }
+     if (node == null) {
+	return;
+     }
 	 
-	   printInorderTraversal(node.left);
-	   System.out.print(node.data + " ");	
-	   printInorderTraversal(node.right);
+     printInorderTraversal(node.left);
+     System.out.print(node.data + " ");	
+     printInorderTraversal(node.right);
   }
  
-	public static void main(String[] args) { 
-	  ConvertBinaryTreeToItsSumTree tree = new ConvertBinaryTreeToItsSumTree();
-      tree.root = new Node(10);
-      tree.root.left = new Node(-2);
-      tree.root.right = new Node(6);
-      tree.root.left.left = new Node(8);
-      tree.root.left.right = new Node(-4);
-      tree.root.right.left = new Node(7);
-      tree.root.right.right = new Node(5);
+ public static void main(String[] args) { 
+    ConvertBinaryTreeToItsSumTree tree = new ConvertBinaryTreeToItsSumTree();
+    tree.root = new Node(10);
+    tree.root.left = new Node(-2);
+    tree.root.right = new Node(6);
+    tree.root.left.left = new Node(8);
+    tree.root.left.right = new Node(-4);
+    tree.root.right.left = new Node(7);
+    tree.root.right.right = new Node(5);
       
-      tree.toSumTree(tree.root);
+    tree.toSumTree(tree.root);
 	     
-	  System.out.print("Inorder Traversal of the resultant tree is: ");
-	  tree.printInorderTraversal(tree.root);	
+    System.out.print("Inorder Traversal of the resultant tree is: ");
+    tree.printInorderTraversal(tree.root);	
 	 
-	}
+  }
 }
