f67a8ba1b3e6a46aa86321cc3fde0f2b4006f10b
Aditya Gupta <contactadityag@gmail.com>
2017-01-26 16:39:09 -0500

    This is the program for constructing the Binary Tree from Inorder and Preorder Traversals.

--- Binary Search Trees/ConstructBinaryTreeFromPreorderAndInorder.java
+++ Binary Search Trees/ConstructBinaryTreeFromPreorderAndInorder.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
  public class ConstructBinaryTreeFromPreorderAndInorder {   
-	Node root;	    
-	static int preIndex = 0;
+    Node root;	    
+    static int preIndex = 0;
 
  public Node buildTree(int inOrder[], int preOrder[], int inStart, int inEnd) {
 	
@@ -17,7 +17,7 @@ import java.util.*;
    // If this node has no children then return 
    
    if (inStart == inEnd) {
-	  return node;  
+      return node;  
    } 
    
    // Else find the index of this node in Inorder traversal 
@@ -29,40 +29,40 @@ import java.util.*;
    node.left = buildTree(inOrder, preOrder, inStart, inOrderIndex - 1);
    node.right = buildTree(inOrder, preOrder, inOrderIndex + 1, inEnd);
     
-	return node;
+   return node;
  }
  
  public int search(int [] inOrder, int start, int end, int data) {
 	 
-	for(int i = start; i <= end; i++) {
-	  if (inOrder[i] == data) {
-         return i;  
-	  }
-	}
-   return -1;
-  }
+   for(int i = start; i <= end; i++) {
+     if (inOrder[i] == data) {
+       return i;  
+     }
+   }
+  return -1;
+ }
 
  public void printInorderTraversal(Node node) {
 	 
     if (node == null) {
-	  return;
-	}
+      return;
+    }
 	 
-	printInorderTraversal(node.left);
-	System.out.print(node.data + " ");	
-	printInorderTraversal(node.right);
+    printInorderTraversal(node.left);
+    System.out.print(node.data + " ");	
+    printInorderTraversal(node.right);
  }
 
-	public static void main(String[] args) {
-	  ConstructBinaryTreeFromPreorderAndInorder tree = new ConstructBinaryTreeFromPreorderAndInorder();	
-	  int [] inOrder = {2,5,6,10,12,14,15};
-	  int [] preOrder = {10,5,2,6,14,12,15};
-	  int length = inOrder.length;
+ public static void main(String[] args) {
+   ConstructBinaryTreeFromPreorderAndInorder tree = new ConstructBinaryTreeFromPreorderAndInorder();	 
+   int [] inOrder = {2,5,6,10,12,14,15};
+   int [] preOrder = {10,5,2,6,14,12,15};
+   int length = inOrder.length;
 	  
-	  Node node = tree.buildTree(inOrder, preOrder, 0, length-1);
+   Node node = tree.buildTree(inOrder, preOrder, 0, length-1);
 	  
-	  System.out.print("Inorder traversal of constructed tree is: ");
-	  tree.printInorderTraversal(node);		
+   System.out.print("Inorder traversal of constructed tree is: ");
+   tree.printInorderTraversal(node);		
 	  
-	}
+  }
 }
