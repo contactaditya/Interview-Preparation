14478f789d9758709b2d197893379df6c352c94b
Aditya Gupta <contactadityag@gmail.com>
2017-01-26 23:45:28 -0500

    This is the program for creating a Balanced Binary Search Tree which has the same data members as the given Linked List.

--- Binary Search Trees/SortedLinkedListToBalancedBST.java
+++ Binary Search Trees/SortedLinkedListToBalancedBST.java
@@ -2,92 +2,92 @@ import java.io.*;
 import java.util.*;
 
  public class SortedLinkedListToBalancedBST { 
-	private static DoublyLinkedListNode head;
-	private static int size;  
-	private static Node root;	    
-	private static Node previous;
+    private static DoublyLinkedListNode head;
+    private static int size;  
+    private static Node root;	    
+    private static Node previous;
 	
-	public SortedLinkedListToBalancedBST() {
-	// this is an empty list, so the reference to the head node is set to a new node with no data				  
-	   head = new DoublyLinkedListNode(null);
-	   size = 0;
-	}
+ public SortedLinkedListToBalancedBST() {
+ // this is an empty list, so the reference to the head node is set to a new node with no data				  
+     head = new DoublyLinkedListNode(null);
+     size = 0;
+ }
 	
-	public static Node sortedLinkedListToBST() {
-		int number = getCount(head); 
-		return sortedLinkedListToBSTRecurion(number);
-	}
+ public static Node sortedLinkedListToBST() {
+    int number = getCount(head); 
+    return sortedLinkedListToBSTRecurion(number);
+ }
 	
-	public static Node sortedLinkedListToBSTRecurion(int number) {
-	  if (number <= 0) {
-	    return null;
-	  }
+ public static Node sortedLinkedListToBSTRecurion(int number) {
+    if (number <= 0) {
+       return null;
+    }
 	  
-	  /* Recursively construct the left subtree */
-      Node left = sortedLinkedListToBSTRecurion(number / 2);
+    /* Recursively construct the left subtree */
+    Node left = sortedLinkedListToBSTRecurion(number / 2);
       
-	  /* Make middle node as root of BST */
-      Node root = new Node((int)head.data);
+    /* Make middle node as root of BST */
+    Node root = new Node((int)head.data);
       
-      // Set pointer to left subtree
-      root.left = left;
+    // Set pointer to left subtree
+    root.left = left;
       
-      head = head.next;
-      root.right = sortedLinkedListToBSTRecurion(number - number/2 - 1);
+    head = head.next;
+    root.right = sortedLinkedListToBSTRecurion(number - number/2 - 1);
 	  
-	   return root;
-	}
+    return root;
+ }
 
-	public static int getCount(DoublyLinkedListNode node) {
-	  DoublyLinkedListNode current = node;
-      int count = 0;
+ public static int getCount(DoublyLinkedListNode node) {
+     DoublyLinkedListNode current = node;
+     int count = 0;
  
-      while (current != null) {
+     while (current != null) {
         count++;
         current = current.next;
-      }
+     }
  
-      return count;
+    return count;
+ }
+			 
+ public static void PrintElementsOfALinkedList(DoublyLinkedListNode head) {	 
+    DoublyLinkedListNode current = head; 
+    System.out.print("Contents of the linked list are: ");	 
+    while(current != null) {	   
+       System.out.print(current.getData() + " ");	 	
+       current = current.getNext();
     }
-			 
-	public static void PrintElementsOfALinkedList(DoublyLinkedListNode head) {	 
-	  DoublyLinkedListNode current = head; 
-	  System.out.print("Contents of the linked list are: ");	 
-	  while(current != null) {	   
-		System.out.print(current.getData() + " ");	 	
-		current = current.getNext();
-	  }
-	} 
+ } 
 	
-	public void printPreorderTraversal(Node node) {
+public void printPreorderTraversal(Node node) {
 			 
-	  if (node == null) {
-	    return;
-	  }
+   if (node == null) {
+      return;
+   }
 		    
-	  System.out.print(node.data + " ");		  
-      printPreorderTraversal(node.left);
-	  printPreorderTraversal(node.right);
-	}
+   System.out.print(node.data + " ");		  
+   printPreorderTraversal(node.left);
+   printPreorderTraversal(node.right);
+}
 
-	public static void main(String[] args) {       
-	  SortedLinkedListToBalancedBST list = new SortedLinkedListToBalancedBST();
-	  list.head = new DoublyLinkedListNode(1);
-	  list.head.next = new DoublyLinkedListNode(2);
-	  list.head.next.next = new DoublyLinkedListNode(3);
-	  list.head.next.next.next = new DoublyLinkedListNode(4);
-	  list.head.next.next.next.next = new DoublyLinkedListNode(5);
-	  list.head.next.next.next.next.next = new DoublyLinkedListNode(6);
-	  list.head.next.next.next.next.next.next = new DoublyLinkedListNode(7);
+public static void main(String[] args) {       
+    SortedLinkedListToBalancedBST list = new SortedLinkedListToBalancedBST(); 
+    list.head = new DoublyLinkedListNode(1);
+    list.head.next = new DoublyLinkedListNode(2);
+    list.head.next.next = new DoublyLinkedListNode(3);
+    list.head.next.next.next = new DoublyLinkedListNode(4);
+    list.head.next.next.next.next = new DoublyLinkedListNode(5);
+    list.head.next.next.next.next.next = new DoublyLinkedListNode(6);
+    list.head.next.next.next.next.next.next = new DoublyLinkedListNode(7);
 	  
-	  list.PrintElementsOfALinkedList(head);
+    list.PrintElementsOfALinkedList(head);
 	  
-	  root = list.sortedLinkedListToBST();
+    root = list.sortedLinkedListToBST();
 	  
-	  System.out.println();
-	  System.out.println();
-      System.out.print("Preorder traversal of binary tree is: ");
-	  list.printPreorderTraversal(root);		
+    System.out.println();
+    System.out.println();
+    System.out.print("Preorder traversal of binary tree is: ");
+    list.printPreorderTraversal(root);		
 
   }
 }
