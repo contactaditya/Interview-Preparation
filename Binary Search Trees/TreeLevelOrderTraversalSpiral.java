7370f929704b434cb14071f913cb9f25d19d0f25
Aditya Gupta <contactadityag@gmail.com>
2017-01-26 23:58:43 -0500

    This is the program for printing spiral order traversal of a Binary Tree.

--- Binary Search Trees/TreeLevelOrderTraversalSpiral.java
+++ Binary Search Trees/TreeLevelOrderTraversalSpiral.java
@@ -7,7 +7,7 @@ import java.util.*;
  public void printLevelOrderTraversalSpiral(Node node) {
 	 
    if (node == null) {
-	  return;
+      return;
    } 
    
    Stack<Node> currentLevel = new Stack<Node>();
@@ -18,11 +18,11 @@ import java.util.*;
    while (!currentLevel.empty() || !nextLevel.empty()) {
  
    while (!currentLevel.empty()) {  
-	 Node temp = currentLevel.peek();
-	 currentLevel.pop();
-	 System.out.print(temp.data + " ");
+      Node temp = currentLevel.peek();
+      currentLevel.pop();
+      System.out.print(temp.data + " ");
 	 
-	 // Note that is right is pushed before left
+   // Note that is right is pushed before left
      if (temp.right != null) {
         nextLevel.push(temp.right);
      }
@@ -33,34 +33,34 @@ import java.util.*;
    }	
   
    while (!nextLevel.empty()) {  
-	 Node temp = nextLevel.peek();
+     Node temp = nextLevel.peek();
      nextLevel.pop();
-	 System.out.print(temp.data + " ");
+     System.out.print(temp.data + " ");
 		 
    // Note that is left is pushed before right
-	 if (temp.left != null) {
-	   currentLevel.push(temp.left);
-	 }
+     if (temp.left != null) {
+       currentLevel.push(temp.left);
+     }
 	      
-	 if (temp.right != null) {
-	   currentLevel.push(temp.right);	
-	 }
+     if (temp.right != null) {
+       currentLevel.push(temp.right);	
+     }
    }
-  }
+ }
 }
 
-	public static void main(String[] args) {  
-	 TreeLevelOrderTraversalSpiral tree = new TreeLevelOrderTraversalSpiral();
-     tree.root = new Node(1);
-     tree.root.left = new Node(2);
-     tree.root.right = new Node(3);
-     tree.root.left.left = new Node(7);
-     tree.root.left.right = new Node(6);
-     tree.root.right.left = new Node(5);
-     tree.root.right.right = new Node(4);
+public static void main(String[] args) {  
+    TreeLevelOrderTraversalSpiral tree = new TreeLevelOrderTraversalSpiral();
+    tree.root = new Node(1);
+    tree.root.left = new Node(2);
+    tree.root.right = new Node(3);
+    tree.root.left.left = new Node(7);
+    tree.root.left.right = new Node(6);
+    tree.root.right.left = new Node(5);
+    tree.root.right.right = new Node(4);
 	     
-	 System.out.print("Spiral Order traversal of Binary Tree is: ");
-	 tree.printLevelOrderTraversalSpiral(tree.root);		
+    System.out.print("Spiral Order traversal of Binary Tree is: ");
+    tree.printLevelOrderTraversalSpiral(tree.root);		
 
   }
 }
