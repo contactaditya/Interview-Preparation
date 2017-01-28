59e3a425f467b2d284a044867068c705efe8129f
Aditya Gupta <contactadityag@gmail.com>
2017-01-27 20:11:59 -0500

    This program shuffles a set of numbers without duplicates.

--- Arrays/ShuffleAnArray.java
+++ Arrays/ShuffleAnArray.java
@@ -2,17 +2,17 @@ import java.io.*;
 import java.util.*;
 
  public class ShuffleAnArray {
-	private static int array[];
-	private static int shuffle[];
-	private static Random random;
+    private static int array[];
+    private static int shuffle[];
+    private static Random random;
 	
-	public ShuffleAnArray() {
+    public ShuffleAnArray() {
        this.array = array;
        this.shuffle = shuffle;
        random = new Random();
     }
 	
-	/** Returns a random shuffling of the array. */
+    /** Returns a random shuffling of the array. */
     public static int[] shuffle(int[] array) {
       if(array == null) {
          return null;
@@ -37,26 +37,26 @@ import java.util.*;
        return array;
     }
 
-	public static void main(String[] args) { 	
-	  ShuffleAnArray arrayshuffle  = new ShuffleAnArray();	
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the first array: ");
-	  int number = input.nextInt();  
-	  array = new int[number];
+    public static void main(String[] args) { 	
+       ShuffleAnArray arrayshuffle  = new ShuffleAnArray();	
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the first array: ");
+       int number = input.nextInt();  
+       array = new int[number];
 	
-	  System.out.println();	
-	  System.out.print("Please enter the elements of the array: ");
-	  for (int i = 0; i < array.length; i++) {
-	    array[i] = input.nextInt();
-      }
+       System.out.println();	
+       System.out.print("Please enter the elements of the array: ");
+       for (int i = 0; i < array.length; i++) {
+	  array[i] = input.nextInt();
+       }
 	  
-	  int result[] = new int[array.length];
+       int result[] = new int[array.length];
 		
-	  result = shuffle(array);
-	  System.out.print("\n");  
-	  System.out.print("The array after shuffling is: ");
-	  for(int i = 0; i < result.length; i++) {
-		System.out.print(result[i] + " ");  
-	  }
+       result = shuffle(array);
+       System.out.print("\n");  
+       System.out.print("The array after shuffling is: ");
+       for(int i = 0; i < result.length; i++) {
+	  System.out.print(result[i] + " ");  
+       }
    }
 }
