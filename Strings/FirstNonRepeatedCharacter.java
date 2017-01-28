13fdd42dba484a939470ccecbb99186e2c46e7aa
Aditya Gupta <contactadityag@gmail.com>
2017-01-27 23:35:31 -0500

    This is the program for finding the First Non Repeated Character In A String.

--- Strings/FirstNonRepeatedCharacter.java
+++ Strings/FirstNonRepeatedCharacter.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 public class FirstNonRepeatedCharacter { 
 	
-   public static Character firstNonRepeatedCharacter(String string) {
-	 if(string == null) {
-	    return null;	
-	 }
+ public static Character firstNonRepeatedCharacter(String string) {
+    if(string == null) {
+       return null;	
+    }
 		
-	 LinkedHashMap<Character, Integer> hashmap = new LinkedHashMap<Character, Integer>();  
+    LinkedHashMap<Character, Integer> hashmap = new LinkedHashMap<Character, Integer>();  
 		
-	 for(int i = 0; i< string.length(); i++) {  
+    for(int i = 0; i< string.length(); i++) {  
        if(string.charAt(i) != ' '){  
          if(hashmap.get(string.charAt(i)) != null) {  
            hashmap.put(string.charAt(i), hashmap.get(string.charAt(i)) + 1);  
@@ -20,28 +20,28 @@ public class FirstNonRepeatedCharacter {
        }  
      }  
 		
-	 for(Character character : hashmap.keySet()) {		
-	   if(hashmap.get(character) == 1) {
-		 return character;
-	   }
-	 }
-	return null;
+     for(Character character : hashmap.keySet()) {		
+        if(hashmap.get(character) == 1) {
+	   return character;
 	}
+      }
+     return null;
+    }
 
-	public static void main(String[] args) { 
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.next(); 
+public static void main(String[] args) { 
+    String string = new String();	  
+    Scanner input = new Scanner(System.in); 
+    System.out.print("Enter the string: ");
+    string = input.next(); 
 	  
-	  Character character = firstNonRepeatedCharacter(string);
+    Character character = firstNonRepeatedCharacter(string);
 	  
-	  System.out.println();
+    System.out.println();
 	  
-	  if(character != null){  
-	     System.out.println("First Non Repeated Character in the string is: " + character);
-	  }	 else {
-		 System.out.println("No non repeated characters are found"); 
-	  }
-   }
+    if(character != null) {  
+       System.out.println("First Non Repeated Character in the string is: " + character);
+    } else {
+       System.out.println("No non repeated characters are found"); 
+    }
+  }
 }
