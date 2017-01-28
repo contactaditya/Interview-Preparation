a2a14b10926c8cbaf5e552cff4e467e89b0d16fe
Aditya Gupta <contactadityag@gmail.com>
2017-01-28 15:51:39 -0500

    This is the solution to the Successor Problem.

--- Cracking The Coding Interview Book/Chapter 4/Successor.java
+++ Cracking The Coding Interview Book/Chapter 4/Successor.java
@@ -12,82 +12,82 @@ import java.util.*;
   }
 
  public class Successor { 
-     static TreeNode root;	    
-     static TreeNode previous;
+    static TreeNode root;	    
+    static TreeNode previous;
 	
-     TreeNode insert(TreeNode node, int data) {
+    TreeNode insert(TreeNode node, int data) {
 		 
-      /* 1. If the tree is empty, return a new, single node */
-	if (node == null) {
-	   return (new TreeNode(data));
-	} else {
+   /* 1. If the tree is empty, return a new, single node */
+    if (node == null) {
+      return (new TreeNode(data));
+    } else {
 	 
-	  TreeNode temp = null;
+      TreeNode temp = null;
 	             
-	 /* 2. Otherwise, recurse down the tree */
-	  if (data <= node.data) {
-	     temp = insert(node.left, data);
-	     node.left = temp;
-	     temp.parent = node;
+   /* 2. Otherwise, recurse down the tree */
+     if (data <= node.data) {
+	temp = insert(node.left, data);
+	node.left = temp;
+	temp.parent = node;
 	 
-	   } else {
-	      temp = insert(node.right, data);
-	      node.right = temp;
-	      temp.parent = node;
-	   }
+      } else {
+	temp = insert(node.right, data);
+	node.right = temp;
+	temp.parent = node;
+      }
 	 
-	 /* return the (unchanged) node pointer */
-	  return node;
-	 }
-     }
+   /* return the (unchanged) node pointer */
+    return node;
+   }
+ }
 	
-	public static TreeNode inorderSuccessor(TreeNode node) {
-	   if(node == null) {
-	      return null;
-           }
-	   
-	   if(node.right != null) {
-	      return leftMostChild(node.right);
-	   } else {
-	      TreeNode x = node.parent;
+public static TreeNode inorderSuccessor(TreeNode node) {
+    if(node == null) {
+       return null;
+    }
+	    
+    if(node.right != null) {
+       return leftMostChild(node.right);
+    } else {
+       TreeNode x = node.parent;
 		   
-	      while(x != null && x.left != node) {
-		 node = x;
-		 x = x.parent;
-	      }
+    while(x != null && x.left != node) {
+       node = x;
+       x = x.parent;
+    }
 		 
-	     return x;    		   
-	   }	 	   
-	}	
+    return x;    		   
+  }	 	   
+}	
 	
-	public static TreeNode leftMostChild(TreeNode node) {
-	   if(node == null) {
-	      return null;
-	   }
+public static TreeNode leftMostChild(TreeNode node) {
+   if(node == null) {
+     return null;
+   }
 		   
-	   if(node.left != null) {
-	      node = node.left;
-	   }  
-	  return node; 
-	}
+   if(node.left != null) {
+     node = node.left;
+   }  
+ return node; 
+}
 
-	public static void main(String[] args) {  
-	  Successor tree = new Successor();
-	  TreeNode root = null, temp = null, suc = null, min = null;
-          root = tree.insert(root, 20);
-          root = tree.insert(root, 8);
-      	  root = tree.insert(root, 22);
-      	  root = tree.insert(root, 4);
-          root = tree.insert(root, 12);
-      	  root = tree.insert(root, 10);
-      	  root = tree.insert(root, 14);
-          temp = root.left.right.left;
+public static void main(String[] args) {  
+     Successor tree = new Successor();
+     TreeNode root = null, temp = null, suc = null, min = null;
+     root = tree.insert(root, 20);
+     root = tree.insert(root, 8);
+     root = tree.insert(root, 22);
+     root = tree.insert(root, 4);
+     root = tree.insert(root, 12);
+     root = tree.insert(root, 10);
+     root = tree.insert(root, 14);
+     temp = root.left.right.left;
 	  
-	  TreeNode next = inorderSuccessor(temp);	
-	  if(next != null) {
-	    System.out.println("Inorder successor of " + temp.data  + " the node is: " + next.data);
-	  } else {
-	    System.out.println("Inorder successor does not exist.");
-	  }	  
-	}
-  }
+     TreeNode next = inorderSuccessor(temp);	
+     if(next != null) {
+        System.out.println("Inorder successor of " + temp.data  + " the node is: " + next.data);
+     } else {
+	System.out.println("Inorder successor does not exist.");
+     }	  
+   }
+}
