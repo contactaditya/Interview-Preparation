085a3529a5663495f01b224ba045baf7b340d23c
Aditya Gupta <contactadityag@gmail.com>
2017-01-30 03:18:26 -0500

    This program computes the researcher's h-index given an array of citations.

--- Arrays/HIndex.java
+++ Arrays/HIndex.java
@@ -3,37 +3,37 @@ import java.util.*;
 
  public class HIndex { 	
 	 
-   public static int hIndex(int[] citations) {
-	 if(citations == null || citations.length == 0) {
-	   return 0;	 
-	 }
+  public static int hIndex(int[] citations) {
+     if(citations == null || citations.length == 0) {
+	return 0;	 
+     }
 	 
-	 Arrays.sort(citations);
-	 int result = 0;
-	 for(int i=0; i<citations.length; i++){
-	    int smaller = Math.min(citations[i], citations.length-i);
-	    result = Math.max(result, smaller);
-	 }
+     Arrays.sort(citations);
+     int result = 0;
+     for(int i=0; i<citations.length; i++){
+	int smaller = Math.min(citations[i], citations.length-i);
+	result = Math.max(result, smaller);
+     }
 	  
-	 return result;
-   }	
+    return result;
+  }	
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int citations[] = new int[number];
+public static void main(String[] args) {   
+   Scanner input = new Scanner(System.in);
+   System.out.print("Enter number of elements in the array: ");
+   int number = input.nextInt();  
+   int citations[] = new int[number];
 	
-	  System.out.println();	
-	  System.out.print("Please enter the elements in the array: ");
-	  for (int i = 0; i < citations.length; i++) {
-		 citations[i] = input.nextInt();
-	  }
+   System.out.println();	
+   System.out.print("Please enter the elements in the array: ");
+   for (int i = 0; i < citations.length; i++) {
+      citations[i] = input.nextInt();
+   }
 		  
-	  int hIndex = hIndex(citations);
+   int hIndex = hIndex(citations);
 	  
-	  System.out.println();
-	  System.out.print("The researcher's h-index is: " + hIndex);
+   System.out.println();
+   System.out.print("The researcher's h-index is: " + hIndex);
 
-	}
+   }
  }
