a509da3f3094b6d8362ea8a6672dca1d7c4b3745
Aditya Gupta <contactadityag@gmail.com>
2017-02-04 04:19:21 -0500

    This program returns the length of last word in the string.

--- Strings/LengthOfLastWord.java
+++ Strings/LengthOfLastWord.java
@@ -4,35 +4,35 @@ import java.io.*;
  public class LengthOfLastWord {   
 
   public static int lengthOfLastWord(String string) {  
-	 if (string == null || string.length() == 0) {
-		return 0;
-	 }
+     if (string == null || string.length() == 0) {
+	return 0;
+     }
 	 
-	 int count = 0;
-	 int length = string.length() - 1;
+     int count = 0;
+     int length = string.length() - 1;
 	 
-	// from end of string, eliminate ' ' character
- 	while (length >= 0 && string.charAt(length) == ' ') {
- 		length--;
- 	}
+     // from end of string, eliminate ' ' character
+     while (length >= 0 && string.charAt(length) == ' ') {
+ 	length--;
+     }
  	
-	while (length >= 0 && string.charAt(length) != ' ') {
-		count++;
-		length--;
-	}
+     while (length >= 0 && string.charAt(length) != ' ') {
+	count++;
+	length--;
+     }
 	
-	return count;
+    return count;
    }
 
-	public static void main(String[] args) { 	  
-	  String string = new String();	  	
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine(); 
+  public static void main(String[] args) { 	  
+     String string = new String();	  	 
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the string: ");
+     string = input.nextLine(); 
 	  
-	  int length = lengthOfLastWord(string);
-	  System.out.println();
-	  System.out.println("The length of the last word in the string is: " + length);
+     int length = lengthOfLastWord(string);
+     System.out.println();
+     System.out.println("The length of the last word in the string is: " + length);
 
-	}
-  }
+    }
+ }
