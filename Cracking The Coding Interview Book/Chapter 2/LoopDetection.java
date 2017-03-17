17368fa77bf4a5a76ff0d6f3d056e66070c9e4e0
Aditya Gupta <contactadityag@gmail.com>
2017-03-17 19:22:24 -0400

    This is the solution to the Loop Detection Problem.

--- Cracking The Coding Interview Book/Chapter 2/LoopDetection.java
+++ Cracking The Coding Interview Book/Chapter 2/LoopDetection.java
@@ -29,7 +29,7 @@ class LinkedListNode {
 	 }
 			
 	 public void setNext(LinkedListNode next) {
-		this.next = next;
+	    this.next = next;
 	  }
 	 }
 
@@ -41,52 +41,52 @@ class LinkedListNode {
     // this is an empty list, so the reference to the head node is set to a new node with no data				  
 	  head = new LinkedListNode(null);
 	  size = 0;
-    }
+        }
 
 	public static LinkedListNode findBeginning(LinkedListNode head) {
 	   LinkedListNode slow = head;
 	   LinkedListNode fast = head;
 		     
 	   while(fast != null && fast.next != null) {    
-		 slow = slow.next;
-		 fast = fast.next.next;
-		 if(slow == fast) {
-			break; 
-		 }
+	      slow = slow.next;
+	      fast = fast.next.next;
+	      if(slow == fast) {
+		break; 
+	      }
 	   }
 	   
 	   // If there is no meeting point then there is no loop
 	   
 	   if(fast == null || fast.next == null) {
-		   return null;
+	      return null;
 	   }
 	   
 	   slow = head;
 	   
 	   while(slow != fast) {
-		  slow = slow.next;
-		  fast = fast.next;
+	      slow = slow.next;
+	      fast = fast.next;
 	   }
 	   
 	 return fast;
 	}
 	  
-	public static void main(String[] args) {  
-	  LoopDetection list = new LoopDetection();
-	  list.head = new LinkedListNode(50);
-	  list.head.next = new LinkedListNode(20);
-	  list.head.next.next = new LinkedListNode(15);
-	  list.head.next.next.next = new LinkedListNode(4);
-	  list.head.next.next.next.next = new LinkedListNode(10);
-	  list.head.next.next.next.next.next = list.head.next;
+ public static void main(String[] args) {  
+     LoopDetection list = new LoopDetection();
+     list.head = new LinkedListNode(50);
+     list.head.next = new LinkedListNode(20);
+     list.head.next.next = new LinkedListNode(15); 
+     list.head.next.next.next = new LinkedListNode(4);
+     list.head.next.next.next.next = new LinkedListNode(10);
+     list.head.next.next.next.next.next = list.head.next;
 	     	     
-	  LinkedListNode IsALoop = list.findBeginning(head);
+     LinkedListNode IsALoop = list.findBeginning(head);
 	  
-	  if(IsALoop == null) {
-		System.out.println("There is no loop in the linkedlist.");  
-	  }
-	  else {
-	    System.out.println("There is a loop in the linkedlist and data where the loop is present is: " + IsALoop.data);    
-	  }
-	}
+     if(IsALoop == null) {
+       System.out.println("There is no loop in the linkedlist.");  
+     }
+     else {
+       System.out.println("There is a loop in the linkedlist and data where the loop is present is: " + IsALoop.data);    
+     }
+   }
  }
