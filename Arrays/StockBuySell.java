3bd2aae34c34601845b18f0145714b59c0764e36
Aditya Gupta <contactadityag@gmail.com>
2017-05-06 23:03:23 -0400

    This is a program to find the maximum profit that you can make by buying and selling a stock on each day.

--- Arrays/StockBuySell.java
+++ Arrays/StockBuySell.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
  
   class Interval {
-	 int buy = 0;
-	 int sell = 0;
+     int buy = 0;
+     int sell = 0;
   }
 
   public class StockBuySell {
@@ -12,7 +12,7 @@ import java.util.*;
 	
 	public static void stockBuySell(int[] price, int numberOfStocks) {
 	   if(numberOfStocks == 1) {
-		  return; 
+	      return; 
 	   }
 	   
 	   int count = 0;
@@ -22,54 +22,54 @@ import java.util.*;
 	   int i = 0;
 	   while (i < numberOfStocks - 1) {
 	 // Find Local Minima. Note that the limit is (n-2) as we are comparing present element to the next element.   
-		  while ((i < numberOfStocks - 1) && (price[i + 1] <= price[i])) {
-             i++; 
-		  }
+	       while ((i < numberOfStocks - 1) && (price[i + 1] <= price[i])) {
+             	  i++; 
+	       }
 		  
-		  if(i == numberOfStocks - 1) {
-			 break; 
-		  }
+	       if(i == numberOfStocks - 1) {
+		  break; 
+	       }
 		  
-		  Interval interval = new Interval();
-		  interval.buy = i++;
+	       Interval interval = new Interval();
+	       interval.buy = i++;
 		  
-     // Find Local Maxima. Note that the limit is (n-1) as we are comparing to previous element.
-		  while ((i < numberOfStocks) && (price[i] >= price[i - 1])) {
-              i++;
-		  }
+          // Find Local Maxima. Note that the limit is (n-1) as we are comparing to previous element.
+	       while ((i < numberOfStocks) && (price[i] >= price[i - 1])) {
+              	  i++;
+	       }
 		  
-		// Store the index of maxima
-		  interval.sell = i - 1;
-		  solution.add(interval);
+          // Store the index of maxima
+	       interval.sell = i - 1;
+	       solution.add(interval);
 		  
-		  count++;   
+	       count++;   
 	   }
 	   
 	   System.out.println();
 	   
 	   if (count == 0) {
-          System.out.println("There is no day when buying the stock " + "will make profit");
+              System.out.println("There is no day when buying the stock " + "will make profit");
 	   }
-       else {
-          for (int j = 0; j < count; j++) {
-             System.out.println("Buy on day: " + solution.get(j).buy + "     " + "Sell on day : " + solution.get(j).sell);
-          }
-       }
+           else {
+              for (int j = 0; j < count; j++) {
+                 System.out.println("Buy on day: " + solution.get(j).buy + "     " + "Sell on day : " + solution.get(j).sell);
+              }
+           }
       return;
     }
 	
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter number of elements in the stock price: ");
-		int numberOfStocks = input.nextInt();   
-		int price[] = new int[numberOfStocks];
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter number of elements in the stock price: ");
+	int numberOfStocks = input.nextInt();   
+	int price[] = new int[numberOfStocks];
 		    
-		System.out.print("\n");
-		System.out.print("Please enter the actual values of the cost of the stock: ");
-		for (int i = 0; i < numberOfStocks; i++) {
-		   price[i] = input.nextInt(); 
-		}
+	System.out.print("\n");
+	System.out.print("Please enter the actual values of the cost of the stock: ");
+	for (int i = 0; i < numberOfStocks; i++) {
+	   price[i] = input.nextInt(); 
+	}
 		 
-		stockBuySell(price, numberOfStocks);
-	 }  
+	stockBuySell(price, numberOfStocks);
+      }  
     }
