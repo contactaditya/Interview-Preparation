3bed3b4700ef39cdfa78c7c32c5616749d50516d
Aditya Gupta <contactadityag@gmail.com>
2017-05-10 20:56:36 -0400

    This is the solution to the One Away problem.

--- Cracking The Coding Interview Book/Chapter 1/OneAway.java
+++ Cracking The Coding Interview Book/Chapter 1/OneAway.java
@@ -3,30 +3,30 @@ import java.util.*;
 public class OneAway {  
 	
   public static boolean oneEditAway(String string1, String string2) {
-	if(string1.length() == string2.length()) {
-	  return oneEditReplace(string1, string2);
-	}
-	else if (string1.length() + 1 == string2.length()) {
-	  return oneEditInsert(string1, string2);	
-	}
-	else if (string1.length() - 1 == string2.length()) {
-	  return oneEditInsert(string2, string1);		
-	}
+      if(string1.length() == string2.length()) {
+	 return oneEditReplace(string1, string2);
+      }
+      else if (string1.length() + 1 == string2.length()) {
+	 return oneEditInsert(string1, string2);	
+      }
+      else if (string1.length() - 1 == string2.length()) {
+	 return oneEditInsert(string2, string1);		
+      }
 
-	return false;  
+    return false;  
   }
   
   public static boolean oneEditReplace(String string1, String string2) {
-	boolean foundDifference = false;  
-	for(int i = 0; i < string1.length(); i++) {
-	   if(string1.charAt(i) != string2.charAt(i)) {
-		  if(foundDifference) {
-			  return false;
-		  }
-	   foundDifference = true;
-	   }
-	}	  
-	return true;
+     boolean foundDifference = false;  
+     for(int i = 0; i < string1.length(); i++) {
+       if(string1.charAt(i) != string2.charAt(i)) {
+	 if(foundDifference) {
+	    return false;
+	 }
+        foundDifference = true;
+       }
+     }	  
+    return true;
   }
   
   public static boolean oneEditInsert(String string1, String string2) {
@@ -36,7 +36,7 @@ public class OneAway {
      while(index2 < string2.length() && index1 < string1.length()) {
         if(string1.charAt(index1) != string2.charAt(index2)) {
           if(index1 != index2) {
-        	 return false;
+             return false;
           }
         index2++;  	
         }
@@ -50,20 +50,20 @@ public class OneAway {
   }
 	
   public static void main(String[] args) { 	  
-	String string = new String();	  
+    String string = new String();	  
     Scanner input = new Scanner(System.in);
     System.out.print("Enter the first string: ");
     string = input.nextLine(); 
     
     System.out.println();
     
-	String string1 = new String();	  
+    String string1 = new String();	  
     System.out.print("Enter the second string: ");
     string1 = input.nextLine(); 
     
     boolean isOneEdit = oneEditAway(string, string1);
     System.out.println();
-	System.out.println(string + ", " + string1 + ": " + isOneEdit);
+    System.out.println(string + ", " + string1 + ": " + isOneEdit);
 	
   }
 }
