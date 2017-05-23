1bf5d51629a3653dff6ff31ba448022723788136
Aditya Gupta <contactadityag@gmail.com>
2017-05-23 16:40:37 -0400

    This is the solution to the Palindrome Permutation problem.

--- Cracking The Coding Interview Book/Chapter 1/PalindromePermutation.java
+++ Cracking The Coding Interview Book/Chapter 1/PalindromePermutation.java
@@ -3,58 +3,58 @@ import java.io.*;
 
  public class PalindromePermutation {
 	 
-	public static boolean isPermutationOfPalindrome(String phrase) {
-	   int[] table = buildCharFrequencyTable(phrase);
-	   return checkMaxOneOdd(table);
+    public static boolean isPermutationOfPalindrome(String phrase) {
+       int[] table = buildCharFrequencyTable(phrase);
+       return checkMaxOneOdd(table);
     }
     
     public static boolean checkMaxOneOdd(int[] table) {
-	  boolean foundOdd = false;
-	  for (int count : table) {
-		if (count % 2 == 1) {
-		  if (foundOdd) {
-			return false;
-		  }
-		 foundOdd = true;
-	    }
-	  }
-	  return true;
-	}
+       boolean foundOdd = false;
+       for (int count : table) {
+	 if (count % 2 == 1) {
+	   if (foundOdd) {
+	     return false;
+	   }
+	   foundOdd = true;
+	 }
+       }
+      return true;
+    }
     
-    public static int getCharNumber(Character c) {
-	  int a = Character.getNumericValue('a');
-	  int z = Character.getNumericValue('z');
-	  int value = Character.getNumericValue(c);
+  public static int getCharNumber(Character c) {
+     int a = Character.getNumericValue('a');
+     int z = Character.getNumericValue('z');
+     int value = Character.getNumericValue(c);
 	  
-	  if (a <= value && value <= z) {
-		return value - a;
-	  }
-	 return -1;
-	}
+     if (a <= value && value <= z) {
+	return value - a;
+     }
+    return -1;
+  }
     
-    public static int[] buildCharFrequencyTable(String phrase) {
-	  int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
-	  for (char c : phrase.toCharArray()) {
-		int x = getCharNumber(c);
-		if (x != -1) {
-		  table[x]++;
-		}
-	  }
-	  return table;
-	}
+ public static int[] buildCharFrequencyTable(String phrase) {
+    int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+    for (char c : phrase.toCharArray()) {
+       int x = getCharNumber(c);
+       if (x != -1) {
+	 table[x]++;
+       }
+    }
+  return table;
+ }
     	 
-	public static void main(String[] args) {
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the first string: ");
-	  string = input.nextLine(); 
+ public static void main(String[] args) {
+     String string = new String();	  
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the first string: ");
+     string = input.nextLine(); 
 		  
-	  System.out.println();
-	  if(isPermutationOfPalindrome(string)) {
-	    System.out.println("The string is a permutation of the palindrome.");
-	  }
-	  else {
-		System.out.println("The string is not a permutation of the palindrome.");  
-	  }
+     System.out.println();
+     if(isPermutationOfPalindrome(string)) {
+	System.out.println("The string is a permutation of the palindrome.");
+     }
+     else {
+        System.out.println("The string is not a permutation of the palindrome.");  
+     }
    }
  } 
