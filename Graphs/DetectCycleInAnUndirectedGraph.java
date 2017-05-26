4fd0a1d5d5e3d66c624c5250159891329a696d4c
Aditya Gupta <contactadityag@gmail.com>
2017-05-25 22:26:02 -0400

    This program is for detecting cycles in an undirected graph.

--- Graphs/DetectCycleInAnUndirectedGraph.java
+++ Graphs/DetectCycleInAnUndirectedGraph.java
@@ -2,27 +2,27 @@ import java.io.*;
 import java.util.*;
 
  public class DetectCycleInAnUndirectedGraph {  
-	private static int vertices; // Number of vertices
-	private LinkedList<Integer> adj[]; // Adjacency List for each vertex
+     private static int vertices; // Number of vertices
+     private LinkedList<Integer> adj[]; // Adjacency List for each vertex
 	
   public DetectCycleInAnUndirectedGraph(int v) {
-	vertices = v;
-	adj = new LinkedList[v];
+     vertices = v;
+     adj = new LinkedList[v];
 		  
-	for(int i=0;i<v;i++) {
-	  adj[i] = new LinkedList();  
-	}	 
+     for(int i=0;i<v;i++) {
+	adj[i] = new LinkedList();  
+     }	 
   }
   
   void addEdge(int source, int destination) {
-	 adj[source].add(destination); 
-	 adj[destination].add(source); 
+      adj[source].add(destination); 
+      adj[destination].add(source); 
   }
   
   public boolean isCyclic() {  
 	  
-	// Mark all the vertices as not visited and not part of recursion stack
-	boolean visited[] = new boolean[vertices];
+    // Mark all the vertices as not visited and not part of recursion stack
+    boolean visited[] = new boolean[vertices];
     for (int i = 0; i < vertices; i++) {
        visited[i] = false;
     }
@@ -40,9 +40,9 @@ import java.util.*;
   
   public boolean isCyclicUtil(int vertices, boolean visited[], int parent) {  
 	  
-	// Mark the current node as visited
-	visited[vertices] = true;
-	Integer i;
+     // Mark the current node as visited
+     visited[vertices] = true;
+     Integer i;
 	
     // Recur for all the vertices adjacent to this vertex
     Iterator<Integer> it = adj[vertices].iterator();
@@ -58,29 +58,29 @@ import java.util.*;
       }
     // If an adjacent is visited and not parent of current vertex, then there is a cycle.
       else if(i != parent) {
-           return true;	
-        }
+         return true;	
       }
+    }
    
     return false;
   }
 
   public static void main(String[] args) {  
-	  DetectCycleInAnUndirectedGraph g = new DetectCycleInAnUndirectedGraph(5);
-	  g.addEdge(1,0);
-	  g.addEdge(0,2);
-	  g.addEdge(2,0);
-	  g.addEdge(0,3);
-	  g.addEdge(3,4);
+      DetectCycleInAnUndirectedGraph g = new DetectCycleInAnUndirectedGraph(5);
+      g.addEdge(1,0);
+      g.addEdge(0,2);
+      g.addEdge(2,0);
+      g.addEdge(0,3);
+      g.addEdge(3,4);
 	  
-	  boolean IsACycle = g.isCyclic();
+      boolean IsACycle = g.isCyclic();
 	  	  
-	  if(IsACycle) {
-		 System.out.println("There is a cycle in the graph.");  
-	  }
-	  else {
-		 System.out.println("There is no cycle in the graph.");    
-	  }
+      if(IsACycle) {
+	 System.out.println("There is a cycle in the graph.");  
+      }
+      else {
+	 System.out.println("There is no cycle in the graph.");    
+      }
 
-	}
+   }
  }
