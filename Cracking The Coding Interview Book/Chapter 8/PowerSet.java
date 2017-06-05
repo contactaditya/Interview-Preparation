3aee50c33535af7738e7f3611930df239386ffa7
Aditya Gupta <contactadityag@gmail.com>
2017-06-04 20:10:28 -0400

    This is the solution to the Power Set Problem.

--- Cracking The Coding Interview Book/Chapter 8/PowerSet.java
+++ Cracking The Coding Interview Book/Chapter 8/PowerSet.java
@@ -11,33 +11,33 @@ public class PowerSet {
    
   // Run a loop for printing all 2^n subsets one by one
    
-  for (int i = 0; i < (1<<n); i++) {
-   System.out.print("{");
+   for (int i = 0; i < (1<<n); i++) {
+     System.out.print("{");
        
-  for (int j = 0; j < n; j++) {
+     for (int j = 0; j < n; j++) {
 	  
-  // Run a loop for printing all 2^n subsets one by one
+     // Run a loop for printing all 2^n subsets one by one
 	  
-   if ((i & (1 << j)) > 0) {
-    System.out.print(array[j] + "");  
-   }   
-  }  
-  System.out.println("}");
-  }
+       if ((i & (1 << j)) > 0) {
+         System.out.print(array[j] + "");  
+       }   
+     }  
+    System.out.println("}");
+   }
  }
 
  public static void main(String[] args) { 	
-  Scanner input = new Scanner(System.in);
-  System.out.print("Enter number of characters in the set: ");
-  int number = input.nextInt();  
-  char array[] = new char[number];
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter number of characters in the set: ");
+    int number = input.nextInt();  
+    char array[] = new char[number];
   
-  System.out.println();	
-  System.out.print("Please enter the actual characters: ");
-  for (int i = 0; i < array.length; i++) {
-    array[i] = input.next().charAt(0);
-  }
+    System.out.println();	
+    System.out.print("Please enter the actual characters: ");
+    for (int i = 0; i < array.length; i++) {
+      array[i] = input.next().charAt(0);
+    }
     	   	
- printPowerSet(array);
+    printPowerSet(array);
   }
 }
