1eb3356e0096474e2f4b6b77da877fb5bdcbec9e
Aditya Gupta <contactadityag@gmail.com>
2017-06-21 01:40:35 -0400

    This is a program to Convert a Binary Tree into its Mirror Tree.

--- Binary Search Trees/ConvertABinaryTreeToItsMirrorTree.java
+++ Binary Search Trees/ConvertABinaryTreeToItsMirrorTree.java
@@ -1,13 +1,13 @@
 import java.util.*;
 import java.io.*;
 
-  public class ConvertABinaryTreeToItsMirrorTree {   
-	 static Node root;	    
+ public class ConvertABinaryTreeToItsMirrorTree {   
+   static Node root;	    
 	 
    public Node mirror(Node node) {	  	
-	  if (node == null) {
-		return node;
-	  }
+      if (node == null) {
+	 return node;
+      }
 	   
       /* do the subtrees */
       Node left = mirror(node.left);
@@ -22,32 +22,32 @@ import java.io.*;
 	 
    public void printInorderTraversal(Node node) {
 	 
-	 if (node == null) {
-	   return;
-     }
+      if (node == null) {
+	 return;
+      }
 
-	 printInorderTraversal(node.left);
-	 System.out.print(node.data + " ");	
-	 printInorderTraversal(node.right);
+      printInorderTraversal(node.left);
+      System.out.print(node.data + " ");	
+      printInorderTraversal(node.right);
    }
 
-	public static void main(String[] args) {   
-	  ConvertABinaryTreeToItsMirrorTree tree = new ConvertABinaryTreeToItsMirrorTree();
-	  tree.root = new Node(1);
-	  tree.root.left = new Node(2);
-	  tree.root.right = new Node(3);
-	  tree.root.left.left = new Node(4);
-	  tree.root.left.right = new Node(5);
+   public static void main(String[] args) {   
+      ConvertABinaryTreeToItsMirrorTree tree = new ConvertABinaryTreeToItsMirrorTree();
+      tree.root = new Node(1);
+      tree.root.left = new Node(2);
+      tree.root.right = new Node(3);
+      tree.root.left.left = new Node(4);
+      tree.root.left.right = new Node(5);
 	  
-	  System.out.print("Inorder traversal of binary tree is: ");
-	  tree.printInorderTraversal(tree.root);		
-	  System.out.println();	
+      System.out.print("Inorder traversal of binary tree is: ");
+      tree.printInorderTraversal(tree.root);		
+      System.out.println();	
 	  
-	  tree.mirror(root);
+      tree.mirror(root);
 	  
-	  System.out.println();	
-	  System.out.print("Inorder traversal of binary tree is: ");
-	  tree.printInorderTraversal(tree.root);		
+      System.out.println();	
+      System.out.print("Inorder traversal of binary tree is: ");
+      tree.printInorderTraversal(tree.root);		
 	  
-    }
+   }
  }
