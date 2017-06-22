ce9c0a041fadb610ac7a39ac1f7b476bb135dfc8
Aditya Gupta <contactadityag@gmail.com>
2017-06-22 03:22:26 -0400

    This is a program to find the maximum profit that you can make by buying and selling a stock on each day.

--- Arrays/StockBuySell.java
+++ Arrays/StockBuySell.java
@@ -8,9 +8,9 @@ import java.util.*;
 
   public class StockBuySell {
 		
-	// This function finds the buy sell schedule for maximum profit.
+      // This function finds the buy sell schedule for maximum profit.
 	
-	public static void stockBuySell(int[] price, int numberOfStocks) {
+      public static void stockBuySell(int[] price, int numberOfStocks) {
 	   if(numberOfStocks == 1) {
 	      return; 
 	   }
@@ -21,28 +21,28 @@ import java.util.*;
 	   
 	   int i = 0;
 	   while (i < numberOfStocks - 1) {
-	 // Find Local Minima. Note that the limit is (n-2) as we are comparing present element to the next element.   
-	       while ((i < numberOfStocks - 1) && (price[i + 1] <= price[i])) {
-             	  i++; 
-	       }
+	    // Find Local Minima. Note that the limit is (n-2) as we are comparing present element to the next element.   
+	      while ((i < numberOfStocks - 1) && (price[i + 1] <= price[i])) {
+             	 i++; 
+	      }
 		  
-	       if(i == numberOfStocks - 1) {
-		  break; 
-	       }
+	      if(i == numberOfStocks - 1) {
+		 break; 
+	      }
 		  
-	       Interval interval = new Interval();
-	       interval.buy = i++;
+	      Interval interval = new Interval();
+	      interval.buy = i++;
 		  
-          // Find Local Maxima. Note that the limit is (n-1) as we are comparing to previous element.
-	       while ((i < numberOfStocks) && (price[i] >= price[i - 1])) {
-              	  i++;
-	       }
+              // Find Local Maxima. Note that the limit is (n-1) as we are comparing to previous element.
+	      while ((i < numberOfStocks) && (price[i] >= price[i - 1])) {
+              	 i++;
+	      }
 		  
-          // Store the index of maxima
-	       interval.sell = i - 1;
-	       solution.add(interval);
+              // Store the index of maxima
+	      interval.sell = i - 1;
+	      solution.add(interval);
 		  
-	       count++;   
+	      count++;   
 	   }
 	   
 	   System.out.println();
@@ -51,9 +51,9 @@ import java.util.*;
               System.out.println("There is no day when buying the stock " + "will make profit");
 	   }
            else {
-              for (int j = 0; j < count; j++) {
-                 System.out.println("Buy on day: " + solution.get(j).buy + "     " + "Sell on day : " + solution.get(j).sell);
-              }
+             for (int j = 0; j < count; j++) {
+                System.out.println("Buy on day: " + solution.get(j).buy + "     " + "Sell on day : " + solution.get(j).sell);
+             }
            }
       return;
     }
