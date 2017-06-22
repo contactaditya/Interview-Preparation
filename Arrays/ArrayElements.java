2485f730eaf13134e53015d15e7fb96a63d5927c
Aditya Gupta <contactadityag@gmail.com>
2017-06-22 02:08:10 -0400

    This is a program to find if there are two pairs (a, b) and (c, d) such that a+b = c+d.

--- Arrays/ArrayElements.java
+++ Arrays/ArrayElements.java
@@ -4,14 +4,14 @@ import java.io.*;
  public class ArrayElements {
 	 
     static class pair {
-	  int first, second;
-	  pair(int f,int s) {
-	    first = f; 
-	    second = s;
-	  }
+       int first, second;
+       pair(int f,int s) {
+	 first = f; 
+	 second = s;
+       }
     }; 
 	 
-	public static boolean findPairs(int[] array) {
+    public static boolean findPairs(int[] array) {
        // Create an empty Hash to store mapping from sum to pair indexes
        HashMap<Integer,pair> map = new HashMap<Integer,pair>();
        int n = array.length;
@@ -24,33 +24,33 @@ import java.io.*;
              map.put(sum,new pair(i,j));
            }
            else {
-        	  // Find previous pair  
-        	  pair p = map.get(sum);
-    		  // Since array elements are distinct, we don't need to check if any element is common among pairs  
+              // Find previous pair  
+              pair p = map.get(sum);
+    	      // Since array elements are distinct, we don't need to check if any element is common among pairs  
               System.out.println();
-         	  System.out.println("There are two pairs ("+ array[p.first] +","+ array[p.second] +") and ("+ array[i] +","+ array[j] +") such that "+ array[p.first] +" + "+ array[p.second] +" = "+ array[i] +" + "+ array[j] +""); 
+              System.out.println("There are two pairs ("+ array[p.first] +","+ array[p.second] +") and ("+ array[i] +","+ array[j] +") such that "+ array[p.first] +" + "+ array[p.second] +" = "+ array[i] +" + "+ array[j] +""); 
               return true;  
            }
          }
        }
       return false;
-	}	
+    }	
 	 
-	public static void main(String[] args) {
-	   Scanner input = new Scanner(System.in);
-	   System.out.print("Enter number of elements in the array: ");
-	   int numbers = input.nextInt();   
-	   int array[] = new int[numbers];
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter number of elements in the array: ");
+	int numbers = input.nextInt();   
+	int array[] = new int[numbers];
 	    
-	   System.out.print("\n");
-	   System.out.print("Please enter the actual values in the array: ");
-	   for (int i = 0; i < numbers; i++) {
-		 array[i] = input.nextInt(); 
-	   }
-	   boolean pairs = findPairs(array);
-	   if(!pairs) {
-		  System.out.println();
-		  System.out.println("No Pairs Found."); 
-	   }
+	System.out.print("\n");
+	System.out.print("Please enter the actual values in the array: ");
+	for (int i = 0; i < numbers; i++) {
+	   array[i] = input.nextInt(); 
 	}
+	boolean pairs = findPairs(array);
+	if(!pairs) {
+	   System.out.println();
+	   System.out.println("No Pairs Found."); 
+	}
+     }
   }
