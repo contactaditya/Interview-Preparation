a9b9c0134524ea4fcf42bcee8bf133b1d45668d7
Aditya Gupta <contactadityag@gmail.com>
2017-07-07 04:39:47 -0400

    This is a program to find sum of all left leaves in a given Binary Tree.

--- Binary Search Trees/SumOfAllLeftLeavesInAGivenBinaryTree.java
+++ Binary Search Trees/SumOfAllLeftLeavesInAGivenBinaryTree.java
@@ -2,42 +2,42 @@ import java.util.*;
 import java.io.*;
 
  public class SumOfAllLeftLeavesInAGivenBinaryTree {
-	Node root;	    
-	Node previous;
+    Node root;	    
+    Node previous;
 	
-	public static boolean isLeaf(Node node) {
+   public static boolean isLeaf(Node node) {
 		 
-	  if (node == null) {
-		return false;
-	  }
+      if (node == null) {
+	 return false;
+      }
 	  
-	  if (node.left == null && node.right == null) {
-		return true;
-	  }
+      if (node.left == null && node.right == null) {
+	 return true;
+      }
 
-	  return false;	
-    }
+     return false;	
+   }
 	
-	public static int leftLeavesSum(Node node) {
-	  int result = 0;
+   public static int leftLeavesSum(Node node) {
+      int result = 0;
 	  
-	  if (node != null) {
-		// If left of root is NULL, then add key of left child  
- 		if (isLeaf(node.left)) {
- 		  result += node.left.data; 
-		}
-		else {
-	      result += leftLeavesSum(node.left);
-		}
- 		result += leftLeavesSum(node.right);		  
-	  }
+      if (node != null) {
+	// If left of root is NULL, then add key of left child  
+ 	if (isLeaf(node.left)) {
+ 	  result += node.left.data; 
+	}
+	else {
+	  result += leftLeavesSum(node.left);
+	}
+ 	 result += leftLeavesSum(node.right);		  
+      }
 	
-	  return result;	 
-    }
+     return result;	 
+   }
 	
-	public static void main(String[] args) {
-	  SumOfAllLeftLeavesInAGivenBinaryTree tree = new SumOfAllLeftLeavesInAGivenBinaryTree();
-	  tree.root = new Node(20);
+   public static void main(String[] args) {
+      SumOfAllLeftLeavesInAGivenBinaryTree tree = new SumOfAllLeftLeavesInAGivenBinaryTree(); 
+      tree.root = new Node(20);
       tree.root.left = new Node(9);
       tree.root.right = new Node(49);
       tree.root.left.right = new Node(12);
@@ -49,7 +49,7 @@ import java.io.*;
       
       int sum = leftLeavesSum(tree.root);
   
-	  System.out.print("The Sum of left leaves of the Binary Tree is: " + sum);
+      System.out.print("The Sum of left leaves of the Binary Tree is: " + sum);
 
-	}
+   }
  }
