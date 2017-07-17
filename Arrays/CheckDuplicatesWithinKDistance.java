c6353a264de6c3af1b8b398e59ef3a093e1cc2dd
Aditya Gupta <contactadityag@gmail.com>
2017-07-17 01:13:18 -0400

    This is a program to check if a given array contains duplicate elements within k distance from each other.

--- Arrays/CheckDuplicatesWithinKDistance.java
+++ Arrays/CheckDuplicatesWithinKDistance.java
@@ -3,51 +3,51 @@ import java.io.*;
 
   public class CheckDuplicatesWithinKDistance { 
 	
-	public static boolean checkDuplicatesWithinKDistance(int array[], int k) {
+    public static boolean checkDuplicatesWithinKDistance(int array[], int k) {
       /** Base case: when K is greater than array size **/
       if (k > array.length || array.length == 1) {
         return false;
       }
-	  HashSet<Integer> set = new HashSet<Integer>();
-	  for (int i = 0; i < array.length; i++) {
-	    // If already present n hash, then we found a duplicate within k distance.
-		if (set.contains(array[i])) {
-		  System.out.println();	
-		  System.out.println("The duplicate that is present within k distance is: " + array[i]);	
+      HashSet<Integer> set = new HashSet<Integer>();
+      for (int i = 0; i < array.length; i++) {
+       // If already present n hash, then we found a duplicate within k distance.
+	if (set.contains(array[i])) {
+	  System.out.println();	
+	  System.out.println("The duplicate that is present within k distance is: " + array[i]);	
           return true;  
-		}
-		set.add(array[i]);
+	}
+	set.add(array[i]);
 	    
-		// Remove the k+1 distant item
+	// Remove the k+1 distant item
         if (i >= k) {
           set.remove(array[i-k]);
         }   
       }
-	  return false;
-	}
+      return false;
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int numbers = input.nextInt();   
+       int array[] = new int[numbers];
 	    
-	  System.out.print("\n");
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+       System.out.print("\n");
+       System.out.print("Please enter the actual values in the array: ");
+       for (int i = 0; i < numbers; i++) {
+	 array[i] = input.nextInt(); 
+       }
 	  
-	  System.out.println();	
-	  System.out.print("Please enter a number k which is smaller than the size of the array: ");
-	  int k = input.nextInt();   
-	  boolean checkDuplicates = checkDuplicatesWithinKDistance(array, k);
-	  System.out.println();
-	  if(checkDuplicates) {
-		System.out.println("There are duplicates which are within k distance.");
-	  }
-	  else {
-		System.out.println("All duplicates are more than k distance away.");  
-	  }
-	}
+       System.out.println();	
+       System.out.print("Please enter a number k which is smaller than the size of the array: ");
+       int k = input.nextInt();   
+       boolean checkDuplicates = checkDuplicatesWithinKDistance(array, k);
+       System.out.println();
+       if(checkDuplicates) {
+	 System.out.println("There are duplicates which are within k distance.");
+       }
+       else {
+	 System.out.println("All duplicates are more than k distance away.");  
+       }
+    }
   }
