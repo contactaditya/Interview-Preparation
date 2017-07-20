98f899080b115d3e7b4ccb5703509db92d65f124
contactaditya <contactadityag@gmail.com>
2017-07-20 01:50:50 -0400

    This is a program to read directly from a URL. Everything read is copied to the standard output stream.

---
+++ URLReader.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+  public class URLReader {
+
+	public static void main(String[] args) throws IOException {
+	  URL oracle = new URL("https://www.oracle.com/");
+	  BufferedReader input = new BufferedReader(new InputStreamReader(oracle.openStream()));
+
+	  String inputLine;
+	  while ((inputLine = input.readLine()) != null) {
+	    System.out.println(inputLine);
+	  }
+	  input.close();
+	}
+  }
