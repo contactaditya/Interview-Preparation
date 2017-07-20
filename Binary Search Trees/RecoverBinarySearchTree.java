f02ac246e947f19a361cafa3605ae7ab5f8d4e3d
Aditya Gupta <contactadityag@gmail.com>
2017-07-19 20:36:53 -0400

    This is a program to fix the nodes of the Binary Search Tree which have been swapped by mistake.

--- Binary Search Trees/RecoverBinarySearchTree.java
+++ Binary Search Trees/RecoverBinarySearchTree.java
@@ -2,74 +2,74 @@ import java.util.*;
 import java.io.*;
 
  public class RecoverBinarySearchTree { 	
-	Node root;	    
-	Node previous;
-	Node first;
-	Node second;
+    Node root;	    
+    Node previous;
+    Node first;
+    Node second;
 	
-	public void findSegments(Node node) {
+    public void findSegments(Node node) {
 		 
-	  if (node == null) {
-		return;
-	  }
+      if (node == null) {
+	return;
+      }
 	  
-	  findSegments(node.left);
+      findSegments(node.left);
 	  
-	  if (previous != null && previous.data > node.data) {
-		if(first == null){
-          first = previous;
-		  second = node;
-	 	}
-		else {
-		  second = node;
-		}
-	  }
-	  previous = node;
-	  findSegments(node.right);
-	}
+      if (previous != null && previous.data > node.data) {
+	 if(first == null){
+            first = previous;
+	    second = node;
+	 }
+	 else {
+	    second = node;
+	 }
+      }
+       previous = node;
+       findSegments(node.right);
+    }
 	
-	public void recoverTree(Node node) {
-	   if (node == null) {
-		 return;
-	   }	
-	   findSegments(node);	
-	   if (first !=null && second!=null) {
-	     int x = first.data;
-	     first.data = second.data;
-	     second.data = x;
-	   }
-	}
+  public void recoverTree(Node node) {
+      if (node == null) {
+	return;
+      }	
+      findSegments(node);	
+      if (first !=null && second!=null) {
+	 int x = first.data;
+	 first.data = second.data;
+	 second.data = x;
+      }
+  }
 		
-	public void printInorderTraversal(Node node) {
+  public void printInorderTraversal(Node node) {
 			 
-	   if (node == null) {
-	     return;
-	   }
+     if (node == null) {
+	return;
+     }
 		 
-	   printInorderTraversal(node.left);
-	   System.out.print(node.data + " ");	
-	   printInorderTraversal(node.right);
-	}
+     printInorderTraversal(node.left); 
+     System.out.print(node.data + " ");	
+     printInorderTraversal(node.right);
+  }
 
-	public static void main(String[] args) {   
+  public static void main(String[] args) {   
       RecoverBinarySearchTree tree = new RecoverBinarySearchTree();
-	  tree.root = new Node(6);
-	  tree.root.left = new Node(10);
-	  tree.root.right = new Node(2);
-	  tree.root.left.left = new Node(1);
-	  tree.root.left.right = new Node(3);
-	  tree.root.right.left = new Node(7);
-	  tree.root.right.right = new Node(12);
+      tree.root = new Node(6);
+      tree.root.left = new Node(10);
+      tree.root.right = new Node(2);
+      tree.root.left.left = new Node(1);
+      tree.root.left.right = new Node(3);
+      tree.root.right.left = new Node(7);
+      tree.root.right.right = new Node(12);
     	     
-	  System.out.print("Inorder Traversal of the original tree is: ");
-	  tree.printInorderTraversal(tree.root);	
+      System.out.print("Inorder Traversal of the original tree is: ");
+      tree.printInorderTraversal(tree.root);	
 	  
-	  tree.recoverTree(tree.root);
+      tree.recoverTree(tree.root);
 	  
-	  System.out.println();
-	  System.out.println();
-	  System.out.print("Inorder Traversal of the fixed tree is: ");
-	  tree.printInorderTraversal(tree.root);	
+      System.out.println();
+      System.out.println();
+      System.out.print("Inorder Traversal of the fixed tree is: ");
+      tree.printInorderTraversal(tree.root);	
 
-	}
+  }
  }
