c51ef4ff2125226b4efb7d787aa399af7ef3d833
Aditya Gupta <contactadityag@gmail.com>
2017-07-20 02:57:46 -0400

    This is a program to find a continuous subarray which adds to a given number.

--- Arrays/FindSubArrayWithGivenSum.java
+++ Arrays/FindSubArrayWithGivenSum.java
@@ -8,28 +8,28 @@ import java.util.*;
        int start = 0;
 	  
        for (int i = 1; i <= number; i++) {
-     // If current sum exceeds the sum, then remove the starting elements
-        while (currentSum > sum && start < i-1) {
-          currentSum = currentSum - array[start];
-          start++;
-        }  
-     // If current sum becomes equal to sum, then return true
-        if (currentSum == sum) {
-          int p = i-1;
-          System.out.println("Sum found between indexes " + start + " and " + p);
-          return 1;
-        }
+         // If current sum exceeds the sum, then remove the starting elements
+         while (currentSum > sum && start < i-1) {
+            currentSum = currentSum - array[start];
+            start++;
+         }  
+         // If current sum becomes equal to sum, then return true
+         if (currentSum == sum) {
+            int p = i-1;
+            System.out.println("Sum found between indexes " + start + " and " + p);
+            return 1;
+         }
         
-     // Add this element to current sum
-        if (i < number) {
-          currentSum = currentSum + array[i];
-        }      
-     }
-   System.out.println("No subarray found");
-   return 0;
-  }	
+         // Add this element to current sum
+         if (i < number) {
+            currentSum = currentSum + array[i];
+         }      
+       }
+       System.out.println("No subarray found");
+       return 0;
+   }	
 
-  public static void main(String[] args) { 	
+   public static void main(String[] args) { 	
       Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int number = input.nextInt();  
@@ -47,5 +47,5 @@ import java.util.*;
 	  
       System.out.println();
       subArrayWithGivenSum(array, number, sum);
-  }
+   }
 }
