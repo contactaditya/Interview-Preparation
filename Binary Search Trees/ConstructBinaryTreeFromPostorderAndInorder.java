8b01984a52c53f06a87e29237a2225073eb12083
Aditya Gupta <contactadityag@gmail.com>
2017-07-27 23:46:27 -0400

    This is the program for constructing the Binary Tree from Inorder and Postorder Traversals.

--- Binary Search Trees/ConstructBinaryTreeFromPostorderAndInorder.java
+++ Binary Search Trees/ConstructBinaryTreeFromPostorderAndInorder.java
@@ -2,36 +2,36 @@ import java.util.*;
 import java.io.*;
 
   public class ConstructBinaryTreeFromPostorderAndInorder { 
-	 Node root;	    
-	 Node previous;
+     Node root;	    
+     Node previous;
 	 
-	 public Node buildTree(int inOrder[], int postOrder[], int inStart, int inEnd, int postStart, int postEnd) {
+     public Node buildTree(int inOrder[], int postOrder[], int inStart, int inEnd, int postStart, int postEnd) {
 		
-	    if (inStart > inEnd || postStart > postEnd) {
-	      return null;  
-	    }
-	    	    	    
-	    // Pick current node from Preorder traversal using postIndex and decrement postIndex 
-	   
-	    Node node = new Node(postOrder[postEnd]);
-		 
-	    // If this node has no children then return 
-	   
-	    if (inStart == inEnd) {
-		  return node;  
-	    } 
-	   
-	    // Else find the index of this node in Inorder traversal 
-	   
-	    int inOrderIndex = search(inOrder, inStart, inEnd, node.data);
-	   
-	    // Using index in Inorder traversal, construct left and right subtress 
-	   
-	    node.left = buildTree(inOrder, postOrder, inStart, inOrderIndex - 1, postStart, postStart + inOrderIndex - (inStart + 1));
-	    node.right = buildTree(inOrder, postOrder, inOrderIndex + 1, inEnd, postStart + inOrderIndex- inStart, postEnd - 1);
-	    
-		return node;
+	 if (inStart > inEnd || postStart > postEnd) {
+	    return null;  
 	 }
+	    	    	    
+	 // Pick current node from Preorder traversal using postIndex and decrement postIndex 
+	   
+	 Node node = new Node(postOrder[postEnd]);
+		 
+	 // If this node has no children then return 
+	   
+	 if (inStart == inEnd) {
+            return node;  
+	 } 
+	   
+	 // Else find the index of this node in Inorder traversal 
+	   
+	 int inOrderIndex = search(inOrder, inStart, inEnd, node.data);
+	   
+	 // Using index in Inorder traversal, construct left and right subtress 
+	   
+	 node.left = buildTree(inOrder, postOrder, inStart, inOrderIndex - 1, postStart, postStart + inOrderIndex - (inStart + 1));
+	 node.right = buildTree(inOrder, postOrder, inOrderIndex + 1, inEnd, postStart + inOrderIndex- inStart, postEnd - 1);
+	    
+	 return node;
+     }
 	 
      public int search(int array[], int start, int end, int data) {
        int i;
@@ -54,15 +54,15 @@ import java.io.*;
        printPreorderTraversal(node.right);
      }
 
-	public static void main(String[] args) {   
-	  ConstructBinaryTreeFromPostorderAndInorder tree = new ConstructBinaryTreeFromPostorderAndInorder();	
-	  int inOrder[] = {4, 8, 2, 5, 1, 6, 3, 7};
-	  int postOrder[] = {8, 4, 5, 2, 6, 7, 3, 1};
+     public static void main(String[] args) {   
+	ConstructBinaryTreeFromPostorderAndInorder tree = new ConstructBinaryTreeFromPostorderAndInorder();	
+	int inOrder[] = {4, 8, 2, 5, 1, 6, 3, 7};
+	int postOrder[] = {8, 4, 5, 2, 6, 7, 3, 1};
 	        
-	  Node node = tree.buildTree(inOrder, postOrder, 0, inOrder.length-1, 0, postOrder.length-1);
+	Node node = tree.buildTree(inOrder, postOrder, 0, inOrder.length-1, 0, postOrder.length-1);
 	  
-	  System.out.print("Preorder traversal of constructed tree is: ");
-	  tree.printPreorderTraversal(node);		
+	System.out.print("Preorder traversal of constructed tree is: ");
+	tree.printPreorderTraversal(node);		
 
-	}
+     }
   }
