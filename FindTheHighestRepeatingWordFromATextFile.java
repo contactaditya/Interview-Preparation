3e176907aff7acaa89c7df570b3f0f8912673e57
Aditya Gupta <contactadityag@gmail.com>
2017-08-09 21:53:10 -0400

    This is a program to find highest repeating word from a text file.

--- FindTheHighestRepeatingWordFromATextFile.java
+++ FindTheHighestRepeatingWordFromATextFile.java
@@ -5,26 +5,26 @@ import java.io.*;
  public class FindTheHighestRepeatingWordFromATextFile {
 	
     public static Map<String, Integer> buildWordMap(String fileName) { 	 
-	   Map<String, Integer> wordMap = new HashMap<>(); 
-	   try {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
-		 String line = null; 
-		 while ((line = br.readLine()) != null) { 
-		   StringTokenizer st = new StringTokenizer(line, " ");		
-		   while(st.hasMoreTokens()) {
-		     String word = st.nextToken().toLowerCase();
-		     if (wordMap.containsKey(word)) { 
-			   wordMap.put(word, (wordMap.get(word) + 1)); 
-		     } else { 
-			   wordMap.put(word, 1); 
-		     }   
-		   }
-         }
-	   }
-	   catch (IOException e) { 
-		 System.err.println("Caught IOException: " + e.getMessage()); 
-	   } 
-	  return wordMap;    
+	Map<String, Integer> wordMap = new HashMap<>(); 
+	try {
+	      BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+	      String line = null; 
+	      while ((line = br.readLine()) != null) { 
+		StringTokenizer st = new StringTokenizer(line, " ");		
+		while(st.hasMoreTokens()) {
+		  String word = st.nextToken().toLowerCase();
+		  if (wordMap.containsKey(word)) { 
+		     wordMap.put(word, (wordMap.get(word) + 1)); 
+		  } else { 
+	             wordMap.put(word, 1); 
+		  }   
+	       }
+            }
+	}
+	catch (IOException e) { 
+          System.err.println("Caught IOException: " + e.getMessage()); 
+	} 
+       return wordMap;    
     }
     
     public static List<Entry<String, Integer>> sortByValue(Map<String, Integer> wordMap) {
@@ -40,19 +40,19 @@ import java.io.*;
       return list;
     }
 		
-	public static void main(String[] args) {
-	  Scanner input = new Scanner(System.in);
-	  String fileName = new String();	  
-	  System.out.print("Please enter the path of the file that you want to read: ");	
-	  fileName = input.next();  	
-	  Map<String, Integer> wordMap = buildWordMap(fileName); 
-	  List<Entry<String, Integer>> list = sortByValue(wordMap);
-	  System.out.println();
-	  System.out.println("List of repeated word from file and their count:");
-	  for (Map.Entry<String, Integer> entry : list) { 
-		if (entry.getValue() > 1) { 
-		  System.out.println(entry.getKey() + " " + entry.getValue()); 
-		} 
-	  }		
-	}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+	String fileName = new String();	  
+	System.out.print("Please enter the path of the file that you want to read: ");	
+	fileName = input.next();  	
+	Map<String, Integer> wordMap = buildWordMap(fileName); 
+	List<Entry<String, Integer>> list = sortByValue(wordMap);
+	System.out.println();
+	System.out.println("List of repeated word from file and their count:");
+        for (Map.Entry<String, Integer> entry : list) { 
+	  if (entry.getValue() > 1) { 
+	     System.out.println(entry.getKey() + " " + entry.getValue()); 
+	  } 
+	}		
+    }
  }
