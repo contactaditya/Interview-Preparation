b41693d405eee8b6d37004f91c9427f286e60a35
Aditya Gupta <contactadityag@gmail.com>
2017-08-17 17:11:37 -0400

    This is a program to find if it is a power of other numbers or not given a positive integer.

--- IsPower.java
+++ IsPower.java
@@ -3,58 +3,58 @@ import java.io.*;
 
  public class IsPower {
 	 
-	public static boolean isPower(int number) {
-	   double x = 2;
+    public static boolean isPower(int number) {
+       double x = 2;
 		
-	   if (number <= 1) {
-		 return true;	
-	   }
+       if (number <= 1) {
+	  return true;	
+       }
 	  
-	   for(double i=2; (i < 33) && (x<=(double)(Math.pow(2, 16))); i++) {
-		 if(Math.pow(x,i)==(double)number) {
-		   return true;
-		 }
-		 if(Math.pow(x,i) >= (Math.pow(2,32))) {
-			i=1;
-			x++;
-		 }
-	   }
+       for(double i=2; (i < 33) && (x<=(double)(Math.pow(2, 16))); i++) {
+	 if(Math.pow(x,i)==(double)number) {
+	   return true;
+	 }
+	 if(Math.pow(x,i) >= (Math.pow(2,32))) {
+	    i=1;
+	    x++;
+	 }
+       }
 
-	  return false;     
+      return false;     
     } 
 	
     public static boolean isPower1(int number) {
-        double p;
+       double p;
         
-        if (number <= 1) {
- 		 return true;	
- 	   }
+       if (number <= 1) {
+ 	 return true;	
+       }
  	   
- 	   for(int x=2; x<=Math.sqrt(number); x++) {
+       for(int x=2; x<=Math.sqrt(number); x++) {
  	       
- 		 p = Math.log(number) / Math.log(x);
- 		 if(p - (int)p < 0.000000001) {
-           return true;
-         }
- 	   }
+          p = Math.log(number) / Math.log(x);
+          if(p - (int)p < 0.000000001) {
+            return true;
+          }
+       }
  	
- 	   return false;     
-     }
+      return false;     
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter a positive integer: ");
-	  int number = input.nextInt();  
+    public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter a positive integer: ");
+       int number = input.nextInt();  
 	 		
-	  boolean result = isPower(number);
+       boolean result = isPower(number);
 
-	  System.out.println();	
-	  if(result) {
-	    System.out.print("Yes the given number can be expressed as a power of two numbers.");
-	  }
-	  else {
-		System.out.print("No the given number cannot be expressed as a power of two numbers.");  
-	  }
+       System.out.println();	
+       if(result) {
+	 System.out.print("Yes the given number can be expressed as a power of two numbers.");
+       }
+       else {
+	 System.out.print("No the given number cannot be expressed as a power of two numbers.");  
+       }
 
-	}
+    }
  }
