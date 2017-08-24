90f6597009c75e02293a7c83872a7d596d0f14b3
Aditya Gupta <contactadityag@gmail.com>
2017-08-23 21:31:47 -0400

    This is a program to find the minimum number of switches you have to press to turn on all the bulbs.

--- Arrays/MinimumNumberOfSwitchesYouHaveToPressToTurnOnAllTheBulbs.java
+++ Arrays/MinimumNumberOfSwitchesYouHaveToPressToTurnOnAllTheBulbs.java
@@ -4,30 +4,30 @@ import java.io.*;
  public class MinimumNumberOfSwitchesYouHaveToPressToTurnOnAllTheBulbs {
    
     public static int bulbs(ArrayList<Integer> a) {
-	   int state = 0;
-	   int switches = 0;
-	   for (int i=0; i < a.size(); i++) {
-		 if(state == a.get(i)) {
-		   state = 1 - state;
+       int state = 0;
+       int switches = 0;
+       for (int i=0; i < a.size(); i++) {
+	 if(state == a.get(i)) {
+	   state = 1 - state;
            switches++; 
-		 }
-	   }
+	 }
+       }
        return switches;
     }	 
 	 
-	public static void main(String[] args) {   
-	   Scanner input = new Scanner(System.in);
-	   System.out.print("Enter the number of light bulbs connected by the wire: ");
-	   int numberOfLightBulbs = input.nextInt();
-	   System.out.println();
-	   System.out.print("Please enter the initial state of all the bulbs: ");
-	   ArrayList<Integer> X = new ArrayList<Integer>();
-	   for (int i = 0; i < numberOfLightBulbs; i++) {
-	     X.add(input.nextInt());
-	   }
-	   System.out.println();
-	   System.out.println("The minimum number of switches you have to press to turn on all the bulbs are: " + bulbs(X));
+    public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number of light bulbs connected by the wire: ");
+       int numberOfLightBulbs = input.nextInt();
+       System.out.println();
+       System.out.print("Please enter the initial state of all the bulbs: ");
+       ArrayList<Integer> X = new ArrayList<Integer>();
+       for (int i = 0; i < numberOfLightBulbs; i++) {
+	  X.add(input.nextInt());
+       }
+       System.out.println();
+       System.out.println("The minimum number of switches you have to press to turn on all the bulbs are: " + bulbs(X));
 		  
-	   input.close();
-	}
+       input.close();
+    }
  }
