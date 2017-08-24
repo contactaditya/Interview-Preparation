e0f96702ff518decd4816e923c36cd04eda30424
Aditya Gupta <contactadityag@gmail.com>
2017-08-23 21:13:52 -0400

    This is a program to find shortest unique prefix to represent each word in the list.

--- Strings/ShortestUniquePrefixToRepresentEachWord.java
+++ Strings/ShortestUniquePrefixToRepresentEachWord.java
@@ -2,67 +2,67 @@ import java.util.*;
 import java.io.*;
   
   class TrieNode {
-	Character c;
+    Character c;
     int frequency;
     HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
     	   
-	public TrieNode(Character c) {
-	   this.c = c;
+    public TrieNode(Character c) {
+       this.c = c;
        frequency = 1;
-	}
+    }
 	
-	 public TrieNode() {
+    public TrieNode() {
 		 
-	 }
+    }
   }  
 	  
   public class ShortestUniquePrefixToRepresentEachWord {
-	TrieNode root;
+     TrieNode root;
 	
-	public ShortestUniquePrefixToRepresentEachWord() {
-	  root = new TrieNode();
-	}  
+     public ShortestUniquePrefixToRepresentEachWord() {
+        root = new TrieNode();
+     }  
 	    
-	public void insertWord(String word) {
-	  HashMap<Character, TrieNode> children = root.children;
-	  for (int i = 0; i < word.length(); i++) {
-        char c = word.charAt(i);
-        TrieNode t;
-        if (children.containsKey(c)) {
-          t = children.get(c);
-          t.frequency++;
-        }
-        else {
-          t = new TrieNode(c);
-          children.put(c, t);
-        }
+     public void insertWord(String word) {
+	HashMap<Character, TrieNode> children = root.children;
+	for (int i = 0; i < word.length(); i++) {
+          char c = word.charAt(i);
+          TrieNode t;
+          if (children.containsKey(c)) {
+            t = children.get(c);
+            t.frequency++;
+          }
+          else {
+            t = new TrieNode(c);
+            children.put(c, t);
+          }
         children = t.children;
       }
-	}
+     }
 	
-	public String findPrefix(String word) {
-	   if (word == null || word.length() <= 0) {
-	     return null;
-	   }
-	   StringBuilder prefix = new StringBuilder();
-	   HashMap<Character, TrieNode> children = root.children;
-	   for (int i = 0; i < word.length(); i++) {
-         char c = word.charAt(i);
-         TrieNode t;
-         if (children.containsKey(c)) {
-           t = children.get(c);
-           children = t.children;
-           prefix.append(c);
-           if (t.frequency == 1) {
-             return prefix.toString();
-           }
-         }
-         else {
-           return null;
-         }
-       } 	 	
-	   return prefix.toString();
-	}	  
+     public String findPrefix(String word) {
+	if (word == null || word.length() <= 0) {
+	  return null;
+	}
+	StringBuilder prefix = new StringBuilder();
+	HashMap<Character, TrieNode> children = root.children;
+	for (int i = 0; i < word.length(); i++) {
+          char c = word.charAt(i);
+          TrieNode t;
+          if (children.containsKey(c)) {
+            t = children.get(c);
+            children = t.children;
+            prefix.append(c);
+            if (t.frequency == 1) {
+              return prefix.toString();
+            }
+          }
+          else {
+            return null;
+          }
+        } 	 	
+      return prefix.toString();
+    }	  
 	 
     public static ArrayList<String> prefix(ArrayList<String> a) {
        if (a == null || a.size() <= 0) {
@@ -80,20 +80,20 @@ import java.io.*;
        return result;
     }	  
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number of words in the list: ");
-	  int numberOfWords = input.nextInt();
-	  System.out.println();
-	  input.nextLine();
-	  System.out.println("Please enter the actual words in the list: ");
-	  ArrayList<String> X = new ArrayList<String>();
-	  for (int i = 0; i < numberOfWords; i++) {
-		X.add(input.nextLine());
-	  }		  
-	  List<String> result = new ArrayList<String>();
-	  result = prefix(X);
-	  System.out.println();
-	  System.out.println("The solution set with unique prefix is: " + result);
-	}
+    public static void main(String[] args) {  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number of words in the list: ");
+       int numberOfWords = input.nextInt();
+       System.out.println();
+       input.nextLine();
+       System.out.println("Please enter the actual words in the list: ");
+       ArrayList<String> X = new ArrayList<String>();
+       for (int i = 0; i < numberOfWords; i++) {
+	 X.add(input.nextLine());
+       }		  
+       List<String> result = new ArrayList<String>();
+       result = prefix(X);
+       System.out.println();
+       System.out.println("The solution set with unique prefix is: " + result);
+    }
   }
