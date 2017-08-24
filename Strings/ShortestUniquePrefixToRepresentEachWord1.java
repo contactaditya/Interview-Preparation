719181e5e33894a8b70e4f2f32d40c7dbebf2710
Aditya Gupta <contactadityag@gmail.com>
2017-08-23 21:23:41 -0400

    This is a program to find shortest unique prefix to represent each word in the list.

--- Strings/ShortestUniquePrefixToRepresentEachWord1.java
+++ Strings/ShortestUniquePrefixToRepresentEachWord1.java
@@ -14,89 +14,89 @@ import java.io.*;
     } 
  }
 
- public class ShortestUniquePrefixToRepresentEachWord1 {
-	private static Node1 root;
+  public class ShortestUniquePrefixToRepresentEachWord1 {
+    private static Node1 root;
 	 
-	public static ArrayList<String> prefix(ArrayList<String> a) {
-	  if (a == null || a.size() <= 0) {
-	    return null;    
-	  }	
-	  ArrayList<String> result = new ArrayList<>();
-	  for (String word : a) {
-	    put(word);
-	  }
-	  for (String word : a) {
-	    String prefix = getPrefix(word);
-	    result.add(prefix);
-	  }
-	  return result;
-	}	
+    public static ArrayList<String> prefix(ArrayList<String> a) {
+       if (a == null || a.size() <= 0) {
+	  return null;    
+       }	
+       ArrayList<String> result = new ArrayList<>();
+       for (String word : a) {
+	 put(word);
+       }
+       for (String word : a) {
+	  String prefix = getPrefix(word);
+	  result.add(prefix);
+       }
+      return result;
+    }	
 	
-	public static String getPrefix(String string) {
-	   StringBuilder prefix = new StringBuilder();
-	   return get(root, string, 0, prefix);
-	}
+   public static String getPrefix(String string) {
+      StringBuilder prefix = new StringBuilder();
+      return get(root, string, 0, prefix);
+   }
 	
-	public static String get(Node1 node, String string, int index, StringBuilder prefix) {
+   public static String get(Node1 node, String string, int index, StringBuilder prefix) {
 	    
-	   char c = string.charAt(index);
+      char c = string.charAt(index);
 	    
-	   if (c < node.character) {
-	     return get(node.left, string, index, prefix);
-	   } else if (c > node.character) {
-	     return get(node.right, string, index, prefix);
-	   } else if (node.value == 1) {
-	     prefix.append(node.character);
-	     return prefix.toString();
-	   } else {
-	     prefix.append(node.character);
-	     return get(node.mid, string, index + 1, prefix);
-	   }
-	}
+      if (c < node.character) {
+	 return get(node.left, string, index, prefix);
+      } else if (c > node.character) {
+	 return get(node.right, string, index, prefix);
+      } else if (node.value == 1) {
+	 prefix.append(node.character);
+	 return prefix.toString();
+      } else {
+	 prefix.append(node.character);
+	 return get(node.mid, string, index + 1, prefix);
+      }
+   }
 	
-	public static void put(String string) {
-	   root = put(root, string, 0);
-	}
+   public static void put(String string) {
+      root = put(root, string, 0);
+   } 
 	
-	public static Node1 put(Node1 node, String string, int index) {
+   public static Node1 put(Node1 node, String string, int index) {
 	    
-	  if (index == string.length()) {
-	    return node;
-	  }
+      if (index == string.length()) {
+	 return node;
+      }
 	    
-	  if (node == null) {
-	    node = new Node1(string.charAt(index));
-	  }
+      if (node == null) {
+	 node = new Node1(string.charAt(index));
+      }
 	    
-	  char c = string.charAt(index);
+      char c = string.charAt(index);
 	    
-	  if (c < node.character) {
-	     node.left = put(node.left, string, index);
-	  } else if (c > node.character) {
-	     node.right = put(node.right, string, index);
-	  } else {
-	     node.mid = put(node.mid, string, index + 1);
-	     node.value += 1;
-	  }
+      if (c < node.character) {
+	 node.left = put(node.left, string, index);
+      } else if (c > node.character) {
+	 node.right = put(node.right, string, index);
+      } else {
+	 node.mid = put(node.mid, string, index + 1);
+	 node.value += 1;
+      }
 	    
-	   return node;
-	}
+      return node;
+   }
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number of words in the list: ");
-	  int numberOfWords = input.nextInt();
-	  System.out.println();
-	  input.nextLine();
-	  System.out.println("Please enter the actual words in the list: ");
-	  ArrayList<String> X = new ArrayList<String>();
-	  for (int i = 0; i < numberOfWords; i++) {
-		X.add(input.nextLine());
-	  }		  
-	  List<String> result = new ArrayList<String>();
-	  result = prefix(X);
-	  System.out.println();
-	  System.out.println("The solution set with unique prefix is: " + result);
+   public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number of words in the list: ");
+      int numberOfWords = input.nextInt();
+      System.out.println();
+      input.nextLine();
+      System.out.println("Please enter the actual words in the list: ");
+      ArrayList<String> X = new ArrayList<String>();
+      for (int i = 0; i < numberOfWords; i++) {
+	 X.add(input.nextLine());
+      }		  
+      List<String> result = new ArrayList<String>();
+      result = prefix(X);
+      System.out.println();
+      System.out.println("The solution set with unique prefix is: " + result);
 	  
-	}
+   }
  }
