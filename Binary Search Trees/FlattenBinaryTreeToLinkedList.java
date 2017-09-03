ea9e6fe852f293ecbfe92fc04cba3263b357f14b
Aditya Gupta <contactadityag@gmail.com>
2017-09-02 20:00:00 -0400

    This is a program to flatten a binary tree to a linked list in-place.

--- Binary Search Trees/FlattenBinaryTreeToLinkedList.java
+++ Binary Search Trees/FlattenBinaryTreeToLinkedList.java
@@ -2,36 +2,36 @@ import java.util.*;
 import java.io.*;
 
   public class FlattenBinaryTreeToLinkedList { 
-	 static Node root;	    
-	 static Node previous;
+     static Node root;	    
+     static Node previous;
 	 
-	 public static void getFlatten(Node root) {
-		if(root == null) {
-		  return; 
-	    }
+     public static void getFlatten(Node root) {
+       if(root == null) {
+	 return; 
+       }
 		  
-		Node left = root.left;
-        Node right = root.right;
+       Node left = root.left;
+       Node right = root.right;
         
-        root.left = null;
-        getFlatten(left);
-        getFlatten(right);
+       root.left = null;
+       getFlatten(left);
+       getFlatten(right);
         
-        root.right=left;
-        Node current = root;
-        while(current.right !=null) {
-          current = current.right;
-        }
+       root.right=left;
+       Node current = root;
+       while(current.right !=null) {
+         current = current.right;
+       }
         
-        current.right = right;
-	 }
+       current.right = right;
+     }
 	 
-	 public static void getFlatten1(Node root) {
-		if(root == null) {
-		  return; 
-		}
+     public static void getFlatten1(Node root) {
+	if(root == null) {
+	  return; 
+	}
 			  
-		Stack<Node> stack = new Stack<Node>();
+	Stack<Node> stack = new Stack<Node>();
         stack.push(root);
         
         while (!stack.empty()) {
@@ -52,29 +52,29 @@ import java.io.*;
         }
      }
 	 
-	 public void printInorderTraversal(Node node) {
+    public void printInorderTraversal(Node node) {
 		 
-	   if (node == null) {
-	     return;
-	   }
+       if (node == null) {
+	  return;
+       }
 		 
-	   printInorderTraversal(node.left);
-	   System.out.print(node.data + " ");	
-	   printInorderTraversal(node.right);
-	 }
-
-	 public static void main(String[] args) { 
-	   FlattenBinaryTreeToLinkedList tree = new FlattenBinaryTreeToLinkedList();
-	   tree.root = new Node(1);
-	   tree.root.left = new Node(2);
-	   tree.root.right = new Node(5);
-	   tree.root.left.left = new Node(3);
-	   tree.root.left.right = new Node(4);
-	   tree.root.right.right = new Node(6);
+       printInorderTraversal(node.left);
+       System.out.print(node.data + " ");	
+       printInorderTraversal(node.right); 
+    }
+	  
+    public static void main(String[] args) { 
+       FlattenBinaryTreeToLinkedList tree = new FlattenBinaryTreeToLinkedList();
+       tree.root = new Node(1);
+       tree.root.left = new Node(2);
+       tree.root.right = new Node(5);
+       tree.root.left.left = new Node(3);
+       tree.root.left.right = new Node(4);
+       tree.root.right.right = new Node(6);
 		     
-	   tree.getFlatten(tree.root);	
-	   System.out.print("Inorder traversal of binary tree is: ");
-	   tree.printInorderTraversal(tree.root);		
+       tree.getFlatten(tree.root);	
+       System.out.print("Inorder traversal of binary tree is: ");
+       tree.printInorderTraversal(tree.root);		
 
-	 }
+    }
   }
