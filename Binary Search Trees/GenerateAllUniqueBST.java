f9f7a17f7c3d09a6b824265bc4606262fb2b4f83
Aditya Gupta <contactadityag@gmail.com>
2017-09-05 21:29:54 -0400

    This is a program to generate all structurally unique BST's (binary search trees) that store values 1...n.

--- Binary Search Trees/GenerateAllUniqueBST.java
+++ Binary Search Trees/GenerateAllUniqueBST.java
@@ -7,16 +7,16 @@ import java.io.*;
       if(n==0) {
         return new ArrayList<Node>();
       }  
-	  return generate(1, n);
-	}   
+      return generate(1, n);
+    }   
     
     public static List<Node> generate(int min, int max) { 
-   	  List<Node> result = new ArrayList<Node>();
-   	  if(min > max) {
-   		result.add(null);
-        return result;    
-   	  }
-   	  for(int i=min; i<=max; i++) {
+       List<Node> result = new ArrayList<Node>();
+       if(min > max) {
+   	 result.add(null);
+         return result;    
+       }
+       for(int i=min; i<=max; i++) {
         List<Node> left = generate(min, i-1);
         List<Node> right = generate(i+1, max);
         for(Node l: left) {
@@ -27,9 +27,9 @@ import java.io.*;
            result.add(root);
          }
         }
-      }
-	  return result;
-	}
+       }
+      return result;
+    }
     
     public static void printPreorderTraversal(Node node) {
 		 
@@ -42,17 +42,17 @@ import java.io.*;
       printPreorderTraversal(node.right);
     }
 
-	public static void main(String[] args) { 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int n = input.nextInt();
-	  List<Node> result = new ArrayList<Node>();
-	  result = generateTrees(n);
-	  System.out.println();  
-	  System.out.println("The unique BST's which are possible are: ");
-	  for(int i = 0; i < result.size(); i++) {
-		printPreorderTraversal(result.get(i));
-		System.out.println();
-	  }
-	}
+    public static void main(String[] args) { 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int n = input.nextInt();
+      List<Node> result = new ArrayList<Node>();
+      result = generateTrees(n);
+      System.out.println();  
+      System.out.println("The unique BST's which are possible are: ");
+      for(int i = 0; i < result.size(); i++) {
+	printPreorderTraversal(result.get(i));
+	System.out.println();
+      }
+    }
   }
