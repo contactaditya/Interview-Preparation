1618d79b0a31ece50e696a6b950ded4aa1aab842
Aditya Gupta <contactadityag@gmail.com>
2017-09-09 21:44:59 -0400

    This is program to remove duplicates in place in a sorted array such that each element appear only once and return the new length.

--- Arrays/RemoveDuplicatesFromSortedArray1.java
+++ Arrays/RemoveDuplicatesFromSortedArray1.java
@@ -3,7 +3,7 @@ import java.io.*;
 
  public class RemoveDuplicatesFromSortedArray1 { 
 	 
-    public static int removeDuplicates(int array[]) {
+   public static int removeDuplicates(int array[]) {
       if (array.length == 0) {
         return 0;  
       }
@@ -15,24 +15,24 @@ import java.io.*;
        }  
       }  
       return i+1;  
-	}
+   }
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+   public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 
-	  System.out.print("\n");
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt();
-	  }
+      System.out.print("\n");
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt();
+      }
 	  
-	  Arrays.sort(array);
+      Arrays.sort(array);
 	  
-	  int length = removeDuplicates(array); 
-	  System.out.println();	
-	  System.out.print("The new length of the array without any duplicates is: " + length);
-	}
- }
+      int length = removeDuplicates(array); 
+      System.out.println();	
+      System.out.print("The new length of the array without any duplicates is: " + length);
+   }
+}
