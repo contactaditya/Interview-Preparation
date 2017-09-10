6b4c69c5b8a0a74be538b1d88706663c60b14858
Aditya Gupta <contactadityag@gmail.com>
2017-09-09 22:25:57 -0400

    This is a program to reverse the first K characters in a string for every 2k characters counting from the start of the string.

--- Strings/ReverseFirstKCharactersInAString.java
+++ Strings/ReverseFirstKCharactersInAString.java
@@ -3,12 +3,12 @@ import java.io.*;
 
   public class ReverseFirstKCharactersInAString {
 	  
-	public static String reverseWords(String string, int k) {  
-	  if (string == null || string.length() == 0) {
-		return "";
+    public static String reverseWords(String string, int k) {  
+      if (string == null || string.length() == 0) {
+	 return "";
       }
 			 
-	  char[] array = string.toCharArray();
+      char[] array = string.toCharArray();
       int n = array.length;
       int i = 0;
       while(i < n) {
@@ -16,10 +16,10 @@ import java.io.*;
          swap(array, i, j);
          i += 2 * k;    
       }
-	  return string.valueOf(array);
-	}  
+      return string.valueOf(array);
+    }  
 	
-	private static void swap(char[] array, int left, int right) {
+    private static void swap(char[] array, int left, int right) {
       while (left < right) {
         char temp = array[left];
         array[left++] = array[right];
@@ -27,19 +27,18 @@ import java.io.*;
       }
     }	
 
-	public static void main(String[] args) {
-	  String string = new String();	  	
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine(); 
+    public static void main(String[] args) {
+       String string = new String();	  	
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the string: ");
+       string = input.nextLine(); 
 		  
-	  System.out.println();
-	  System.out.print("Enter how many characters you want to reverse: ");
-	  int k = input.nextInt();  
+       System.out.println();
+       System.out.print("Enter how many characters you want to reverse: ");
+       int k = input.nextInt();  
 		  
-	  String reverse = reverseWords(string,k);
-	  System.out.println();
-	  System.out.println("The string after reversing " + k + " characters is: " + reverse);	
-
-	}
+       String reverse = reverseWords(string,k);
+       System.out.println();
+       System.out.println("The string after reversing " + k + " characters is: " + reverse);	
+    }
   }
