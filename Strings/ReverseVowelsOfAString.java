6486c67db32547c37110d9631b53b229a1827800
Aditya Gupta <contactadityag@gmail.com>
2017-09-09 22:23:19 -0400

    This is a program to take a string as input and reverse only the vowels of a string.

--- Strings/ReverseVowelsOfAString.java
+++ Strings/ReverseVowelsOfAString.java
@@ -3,12 +3,12 @@ import java.io.*;
 
   public class ReverseVowelsOfAString {   
 	  
-	public static String reverseVowels(String string) {  
-	   char[] list = string.toCharArray();
-	   Set<Character> vowels = new HashSet<>(Arrays.asList(new Character[]{'a','e','i','o','u','A','E','I','O','U'}));
-	   int start = 0;
-	   int end = string.length()-1;
-	   while(start<end) {
+    public static String reverseVowels(String string) {  
+       char[] list = string.toCharArray();
+       Set<Character> vowels = new HashSet<>(Arrays.asList(new Character[]{'a','e','i','o','u','A','E','I','O','U'}));
+       int start = 0;
+       int end = string.length()-1;
+       while(start<end) {
          if (!vowels.contains(list[start])) {
            start++;
            continue;
@@ -24,17 +24,17 @@ import java.io.*;
          start++;
          end--;
        } 
-	   return string.valueOf(list);
-	}
+       return string.valueOf(list);
+    }
 
-	public static void main(String[] args) { 
-	  String string = new String();	  	
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine(); 
-	  
-	  String reverse = reverseVowels(string);
-	  System.out.println();
-	  System.out.println("The string after reversing the vowels is: " + reverse);
-	}
+    public static void main(String[] args) { 
+       String string = new String();	  	
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the string: ");
+       string = input.nextLine(); 
+	   
+       String reverse = reverseVowels(string);
+       System.out.println();
+       System.out.println("The string after reversing the vowels is: " + reverse);
+    }
   }
