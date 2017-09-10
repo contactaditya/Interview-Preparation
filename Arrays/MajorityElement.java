639dc12bd5f2d83ad4425b5b59fd6cc2f78960e9
Aditya Gupta <contactadityag@gmail.com>
2017-09-09 21:12:45 -0400

    This is a program to find the majority element in an array. The majority element is the element that appears more than n/2 times.

--- Arrays/MajorityElement.java
+++ Arrays/MajorityElement.java
@@ -21,22 +21,22 @@ import java.io.*;
       }
  
       return result;  
-  	}
+   }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+   public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int numbers = input.nextInt();   
+       int array[] = new int[numbers];
 
-	  System.out.print("\n");
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt();
-	  }
+       System.out.print("\n");
+       System.out.print("Please enter the actual values in the array: ");
+       for (int i = 0; i < numbers; i++) {
+	  array[i] = input.nextInt();
+       }
 	
-	  int majorityElement = majorityElement(array); 
-	  System.out.println();	
-	  System.out.print("The majority element that appears more than n/2 times in the array is: " + majorityElement);
-	}
-  }
+       int majorityElement = majorityElement(array); 
+       System.out.println();	
+       System.out.print("The majority element that appears more than n/2 times in the array is: " + majorityElement);
+   }
+}
