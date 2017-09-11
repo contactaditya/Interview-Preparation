c6892fe7fff7b749c68a4fcb4a0c6eda8a8cb740
Aditya Gupta <contactadityag@gmail.com>
2017-09-10 23:35:52 -0400

    This is a program to check whether an integer is a power of two.

--- Bit Manipulation/PowerOfTwo.java
+++ Bit Manipulation/PowerOfTwo.java
@@ -5,20 +5,20 @@ import java.io.*;
 	
     public static boolean isPowerOfTwo(int number) {
        return ((number & (number-1))==0 && number>0);
-	}  
+    }  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();  
+    public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number: ");
+       int number = input.nextInt();  
 	  
-	  boolean result = isPowerOfTwo(number); 
-	  System.out.println();
-	  if(result) {
-		System.out.println("The given number " + number + " can be expressed as a power of 2.");  
-	  }
-	  else {
-		System.out.println("The given number " + number + " cannot be expressed as a power of 2.");  
-	  }
-	}
+       boolean result = isPowerOfTwo(number); 
+       System.out.println();
+       if(result) {
+	 System.out.println("The given number " + number + " can be expressed as a power of 2.");  
+       }
+       else {
+	 System.out.println("The given number " + number + " cannot be expressed as a power of 2.");  
+       }
+    }
   }
