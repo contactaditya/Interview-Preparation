33db70aceeb43c7332a02d30e21fad7387caf6af
Aditya Gupta <contactadityag@gmail.com>
2017-09-12 01:36:00 -0400

    This is a program to return all possible permutations from a collection of distinct numbers.

--- Arrays/AllPossiblePermutations.java
+++ Arrays/AllPossiblePermutations.java
@@ -12,41 +12,41 @@ import java.io.*;
       return result;    
     }	 
 
-	private static void backtrack(List<List<Integer>> result, List<Integer> list, int[] array) {
-	  if(list.size() == array.length) {
-		result.add(new ArrayList<>(list));
-	  } 
-	  else{
-	    for(int i = 0; i < array.length; i++){ 
-	      if(list.contains(array[i])) {
-	    	continue; // This element already exists, skips
-	      }
-	      list.add(array[i]);
-	      backtrack(result, list, array);
-	      list.remove(list.size() - 1);
-	    }
+    private static void backtrack(List<List<Integer>> result, List<Integer> list, int[] array) {
+      if(list.size() == array.length) {
+	result.add(new ArrayList<>(list));
+      } 
+      else {
+	for(int i = 0; i < array.length; i++) { 
+	  if(list.contains(array[i])) {
+	    continue; // This element already exists, skips
 	  }
+	  list.add(array[i]);
+	  backtrack(result, list, array);
+	  list.remove(list.size() - 1);
 	}
+      }
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+      int number = input.nextInt();  
+      int array[] = new int[number];
 		  
-	  System.out.println();	
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < array.length; i++) {
-		 array[i] = input.nextInt();
-	   }
-	   List<List<Integer>> result = new ArrayList<List<Integer>>();
-	   result = permute(array);
-	   System.out.println();
-	   System.out.println("A solution set is: ");
-	   System.out.println('[');
-	   for(List<Integer> innerList : result) {
-		 System.out.println(innerList + " ");
-	   }
-	   System.out.println(']');
-	}
+      System.out.println();	
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < array.length; i++) {
+	array[i] = input.nextInt();
+      }
+      List<List<Integer>> result = new ArrayList<List<Integer>>();
+      result = permute(array);
+      System.out.println();
+      System.out.println("A solution set is: ");
+      System.out.println('[');
+      for(List<Integer> innerList : result) {
+	System.out.println(innerList + " ");
+      }
+      System.out.println(']');
+    }
   }
