2e421d74bebc0ce6c615f571841ba1b1e1f27b79
Aditya Gupta <contactadityag@gmail.com>
2017-09-13 04:08:41 -0400

    This is a program to design a data structure that supports the following two operations addWord and search.

--- Trie/WordDictionary.java
+++ Trie/WordDictionary.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.io.*;
 
  public class WordDictionary {
-	TrieNode1 root;	 
+    TrieNode1 root;	 
 	 
     /** Initialize your data structure here. */
-	public WordDictionary() {
-	  root = new TrieNode1();     
-	}
+    public WordDictionary() {
+      root = new TrieNode1();     
+    }
 	    
-	/** Adds a word into the data structure. */
+    /** Adds a word into the data structure. */
     public void addWord(String word) {
       TrieNode1 node = root;
       for(char c: word.toCharArray()) {
@@ -19,7 +19,7 @@ import java.io.*;
         node = node.children[c-'a'];
       }
       node.word = word;	     
-	}
+    }
 	    
     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
     public boolean search(String word) {
@@ -44,34 +44,32 @@ import java.io.*;
       return false;
     }
 	 
-	public static void main(String[] args) {   
-	  WordDictionary trie = new WordDictionary();
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number of words in the list: ");
-	  int numberOfWords = input.nextInt();
-	  System.out.println();
-	  input.nextLine();
-	  System.out.println("Please enter the actual words in the list: ");
-	  ArrayList<String> X = new ArrayList<String>();
-	  for (int i = 0; i < numberOfWords; i++) {
-		X.add(input.nextLine());
-	  }		  
-	  for(String string : X) {
-	    trie.addWord(string);
-	  }
-	  System.out.println();
-	  String search = new String();	
-	  System.out.print("Enter the word that you want to search: ");
-	  search = input.nextLine();  
-	  boolean searchResult = trie.search(search); 
-	  System.out.println();
-	  if(searchResult) {
-	    System.out.println("The word " + search + " is present in the trie.");
-	  }
-	  else {
-		System.out.println("The word " + search + " is not present in the trie.");
-	  }
-		
-
-	}
+    public static void main(String[] args) {   
+       WordDictionary trie = new WordDictionary();
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number of words in the list: ");
+       int numberOfWords = input.nextInt();
+       System.out.println();
+       input.nextLine();
+       System.out.println("Please enter the actual words in the list: ");
+       ArrayList<String> X = new ArrayList<String>();
+       for (int i = 0; i < numberOfWords; i++) {
+	 X.add(input.nextLine());
+       }		  
+       for(String string : X) {
+	 trie.addWord(string);
+       }
+       System.out.println();
+       String search = new String();	
+       System.out.print("Enter the word that you want to search: ");
+       search = input.nextLine();  
+       boolean searchResult = trie.search(search); 
+       System.out.println();
+       if(searchResult) {
+	 System.out.println("The word " + search + " is present in the trie.");
+       }
+       else {
+	 System.out.println("The word " + search + " is not present in the trie.");
+       }
+    }
  }
