6f6c455eeb6ac7b7bbb742c0440ab39319874e40
Aditya Gupta <contactadityag@gmail.com>
2017-09-19 05:50:55 -0400

    This is a program to find the kth largest element in an unsorted array.

--- Arrays/FindKthLargestElementInAnArray.java
+++ Arrays/FindKthLargestElementInAnArray.java
@@ -3,43 +3,43 @@ import java.io.*;
 
  public class FindKthLargestElementInAnArray {
 	
-	public static int findKthLargest(int[] array, int k) {
-	   if (array == null || array.length == 0) {
-	     return 0;
-	   }
+   public static int findKthLargest(int[] array, int k) {
+      if (array == null || array.length == 0) {
+	 return 0;
+      }
 	        
-	   java.util.PriorityQueue<Integer> maxHeap = new java.util.PriorityQueue<>(array.length, Collections.reverseOrder());	
+      java.util.PriorityQueue<Integer> maxHeap = new java.util.PriorityQueue<>(array.length, Collections.reverseOrder());	
 		
-	   for (int number : array) {
-         maxHeap.offer(number);
-       }
+      for (int number : array) {
+        maxHeap.offer(number);
+      }
 		
-	   int result = 0;
-       for (int i = 0; i < k; i++) {
-         result = maxHeap.poll();
-       }
+      int result = 0;
+      for (int i = 0; i < k; i++) {
+        result = maxHeap.poll();
+      }
 
-	   return result;     
-	}	 
+      return result;     
+   }	 
 
-	public static void main(String[] args) {   
-	   Scanner input = new Scanner(System.in);
-	   System.out.print("Enter number of elements in the array: ");
-	   int numbers = input.nextInt();   
-	   int array[] = new int[numbers];
+   public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: "); 
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 		    
-	   System.out.print("\n");
-	   System.out.print("Please enter the actual values in the array: ");
-	   for (int i = 0; i < numbers; i++) {
-		 array[i] = input.nextInt(); 
-	   }
-	   System.out.println();
-	   System.out.print("Enter the number of most frequent elements that you want the program to return: ");
-	   int k = input.nextInt();  
+      System.out.print("\n");
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt(); 
+      } 
+      System.out.println();
+      System.out.print("Enter the number of most frequent elements that you want the program to return: ");
+      int k = input.nextInt();  
 	   
-	   int result = findKthLargest(array, k);
-	   System.out.println();
-	   System.out.print("The " + k + " largest element in the sorted order is: " + result);
+      int result = findKthLargest(array, k);
+      System.out.println();
+      System.out.print("The " + k + " largest element in the sorted order is: " + result);
 
-	}
+   }
  }
