54bca243f9ffb323671808cff4b7c882bb83f45b
Aditya Gupta <contactadityag@gmail.com>
2017-09-19 06:10:03 -0400

    This is a program to reorder an unsorted array such that array[0] <= array[1] >= array[2] <= array[3]....

--- Sorting Algorithms/WiggleSort.java
+++ Sorting Algorithms/WiggleSort.java
@@ -3,39 +3,39 @@ import java.io.*;
 
  public class WiggleSort {
 	 
-	public static void wiggleSort(int[] array) {
-	  if (array == null || array.length <= 1) {
-		 return;
-	  }
+    public static void wiggleSort(int[] array) {
+       if (array == null || array.length <= 1) {
+	  return;
+       }
 	  
-	  int n = array.length;
+       int n = array.length;
 		      
-	  for (int i = 1; i < n; i++) {
-		if ((i%2 == 1 && array[i-1] > array[i]) || (i%2 == 0 && array[i-1] < array[i])) {
-		   int temp = array[i];
-		   array[i] = array[i - 1];
-		   array[i - 1] = temp;
-		}
-	  }
-	}
+       for (int i = 1; i < n; i++) {
+	 if ((i%2 == 1 && array[i-1] > array[i]) || (i%2 == 0 && array[i-1] < array[i])) {
+	   int temp = array[i];
+	   array[i] = array[i - 1];
+	   array[i - 1] = temp;
+	 }
+       }
+    }
 	
-	public static void main(String[] args) {
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int numbers = input.nextInt();   
+       int array[] = new int[numbers];
 					    
-	  System.out.print("\n");
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+       System.out.print("\n");
+       System.out.print("Please enter the actual values in the array: ");
+       for (int i = 0; i < numbers; i++) {
+	  array[i] = input.nextInt(); 
+       }
 
-	  wiggleSort(array);
-	  System.out.println();
-	  System.out.print("The array after wiggle sort is: ");
-	  for (int i=0; i < array.length; i++) {
-		System.out.print(array[i] + " ");
-	  }
-	}
+       wiggleSort(array);
+       System.out.println();
+       System.out.print("The array after wiggle sort is: ");
+       for (int i=0; i < array.length; i++) {
+	 System.out.print(array[i] + " ");
+       }
+    }
  }
