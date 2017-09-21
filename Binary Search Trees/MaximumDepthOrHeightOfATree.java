b02a925ef4488a09d6701e5b5ef59c3ffe911768
contactaditya <contactadityag@gmail.com>
2017-09-21 07:40:31 -0400

    This is a program to find the maximum depth or height of a Binary Tree.

---
+++ Binary Search Trees/MaximumDepthOrHeightOfATree.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+  public class MaximumDepthOrHeightOfATree {
+	Node root;	    
+	Node previous;  
+	  
+	public int maximumDepth(Node root) {
+	  // called on root = NULL
+	  if (root == null) {
+	    return 0;
+	  }
+	    
+	  return Math.max(maximumDepth(root.left), maximumDepth(root.right)) + 1;     	
+	}  
+	  
+	public static void main(String[] args) {
+	  MaximumDepthOrHeightOfATree tree = new MaximumDepthOrHeightOfATree();
+	  tree.root = new Node(1);
+	  tree.root.left = new Node(2);
+	  tree.root.right = new Node(3);
+	  tree.root.left.left = new Node(4);
+	  tree.root.left.right = new Node(5);
+		  
+	  System.out.println("The maximum depth of "+ "binary tree is : " + tree.maximumDepth(tree.root));
+	}
+  }
