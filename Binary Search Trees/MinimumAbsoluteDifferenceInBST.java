effc7e66aacba702704ea3566b58bb4d515d58fc
contactaditya <contactadityag@gmail.com>
2017-09-21 07:36:04 -0400

    This is a program to find the minimum absolute difference between values of any two nodes in a binary search tree.

---
+++ Binary Search Trees/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+ public class MinimumAbsoluteDifferenceInBST {
+	 Node root;	    
+	 int min = Integer.MAX_VALUE;
+	 Integer previous = null;
+
+	 public int getMinimumDifference(Node root) {
+			 
+	   if (root == null) {
+		 return min;
+	   }
+	   
+       getMinimumDifference(root.left);
+       
+       if (previous != null) {
+         min = Math.min(min, root.data - previous);
+       }
+       previous = root.data;
+       
+       getMinimumDifference(root.right);
+		
+	   return min;
+	} 
+
+	public static void main(String[] args) {
+	  MinimumAbsoluteDifferenceInBST tree = new MinimumAbsoluteDifferenceInBST();
+	  tree.root = new Node(1);
+	  tree.root.right = new Node(3);
+	  tree.root.right.left = new Node(2);
+		  
+	  System.out.println("The minimum absolute difference between values of any two nodes of the binary search tree is : " + tree.getMinimumDifference(tree.root));		
+	}
+ }
