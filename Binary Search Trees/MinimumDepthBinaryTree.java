47c0282388f88c4f708f2eae278213fc30604468
contactaditya <contactadityag@gmail.com>
2017-09-21 06:36:54 -0400

    This is the program for finding the Minimum Depth of a Binary Tree.

--- Binary Search Trees/MinimumDepthBinaryTree.java
+++ Binary Search Trees/MinimumDepthBinaryTree.java
@@ -1,51 +1,76 @@
 import java.util.*;
+import java.io.*;
 
  public class MinimumDepthBinaryTree {   
-    Node root;	    
-    Node previous;
+	Node root;	    
+	Node previous;
 
-  public int minimumDepth(Node node) {
+    public int minimumDepth(Node node) {
 		 
-    if (node == null) {
-       return 0;
-    }
+	  if (node == null) {
+	    return 0;
+	  }
 	
-    LinkedList<Node> nodes = new LinkedList<Node>();
-    LinkedList<Integer> depths = new LinkedList<Integer>();
+	  LinkedList<Node> nodes = new LinkedList<Node>();
+      LinkedList<Integer> depths = new LinkedList<Integer>();
     
-    nodes.add(node);
-    depths.add(1);
+      nodes.add(node);
+      depths.add(1);
     
-    while(!nodes.isEmpty()) {
-      Node current = nodes.remove();
-      int depth = depths.remove();	
+      while(!nodes.isEmpty()) {
+        Node current = nodes.remove();
+        int depth = depths.remove();	
       
-      if(current.left == null && current.right == null) {
-         return depth;
+        if(current.left == null && current.right == null) {
+          return depth;
+        }
+      
+        if(current.left != null){
+          nodes.add(current.left);
+          depths.add(depth + 1);
+        }
+      
+        if(current.right != null){
+          nodes.add(current.right);
+          depths.add(depth + 1);
+        }	
       }
-      
-      if(current.left != null){
-         nodes.add(current.left);
-         depths.add(depth + 1);
-      }
-      
-      if(current.right != null){
-         nodes.add(current.right);
-         depths.add(depth + 1);
-      }	
+
+	  return 0;
     }
+  
+    public int minimumDepth1(Node root) {
+	  // called on root = NULL
+      if (root == null) {
+         return 0;
+      }
 
-    return 0;
-  }
+      // Base case : Leaf Node. This accounts for height = 1.
+      if (root.left == null && root.right == null) {
+         return 1;
+      }
 
-   public static void main(String[] args) { 
-      MinimumDepthBinaryTree tree = new MinimumDepthBinaryTree();
-      tree.root = new Node(1);
-      tree.root.left = new Node(2);
-      tree.root.right = new Node(3);
-      tree.root.left.left = new Node(4);
-      tree.root.left.right = new Node(5);
+      // If left subtree is NULL, recur for right subtree
+      if (root.left == null) {
+         return minimumDepth1(root.right) + 1;
+      }
+
+      // If right subtree is NULL, recur for right subtree
+      if (root.right == null) {
+         return minimumDepth1(root.left) + 1;
+      }
+
+      return Math.min(minimumDepth1(root.left), minimumDepth1(root.right)) + 1;     	
+    }  
+
+    public static void main(String[] args) { 
+	   MinimumDepthBinaryTree tree = new MinimumDepthBinaryTree();
+	   tree.root = new Node(1);
+	   tree.root.left = new Node(2);
+	   tree.root.right = new Node(3);
+	   tree.root.left.left = new Node(4);
+	   tree.root.left.right = new Node(5);
 	  
-      System.out.println("The minimum depth of "+ "binary tree is : " + tree.minimumDepth(tree.root));
-   }
+	   System.out.println("The minimum depth of "+ "binary tree is : " + tree.minimumDepth(tree.root));
+    }
 }
---
+++ Trie/ImplementMagicDictionary.java
@@ -0,0 +1,82 @@
+import java.util.*;
+import java.io.*;
+
+ public class ImplementMagicDictionary {
+	 TrieNode1 root;	  
+	 
+	/** Initialize your data structure here. */
+	public ImplementMagicDictionary() {
+	  root = new TrieNode1();          
+	}
+	    
+	/** Build a dictionary through a list of words */
+	public void buildDictionary(String[] dictionary) {
+	  for (String string : dictionary) {	
+	    TrieNode1 node = root;
+	    for(char c: string.toCharArray()) {
+	      if(node.children[c-'a'] == null) {
+	        node.children[c-'a'] = new TrieNode1();
+	      }
+	      node = node.children[c-'a'];
+	    }
+	    node.word = string;
+	  }
+	}	
+	        
+	/** Returns if there is any word in the trie that equals to the given word after modifying exactly one character */
+	public boolean search(String word) {
+      char[] array = word.toCharArray();
+	  for(int i=0;i<array.length;i++) {
+	     char original = array[i];
+	     for(char c ='a'; c <= 'z'; c++) {
+	       if(c!=original) {
+	         array[i] = c;
+	         if(searchHelper(array,root)) {
+	           return true;
+	         }
+	       }
+	     }         
+	    array[i] = original;
+	  }
+	  return false;
+    }
+    
+	public boolean searchHelper(char[] array,TrieNode1 root) {
+	  TrieNode1 current = root;
+	  for(int i=0; i < array.length;i++) {
+		char c = array[i];  
+	    if(current.children[c-'a'] == null) {
+	       return false;  
+	    }
+	    current = current.children[c-'a'];
+	  }
+	  return current.word!=null;
+	}	
+
+	public static void main(String[] args) {
+	   ImplementMagicDictionary trie = new ImplementMagicDictionary();
+	   Scanner input = new Scanner(System.in);
+	   System.out.print("Enter the number of words in the list: ");
+	   int numberOfWords = input.nextInt();
+	   System.out.println();
+	   input.nextLine();
+	   System.out.println("Please enter the actual words in the list: ");
+	   String[] string = new String[numberOfWords];
+	   for (int i = 0; i < numberOfWords; i++) {
+		 string[i] = input.nextLine();
+	   }		  
+	   trie.buildDictionary(string);
+	   System.out.println();
+	   String search = new String();	
+	   System.out.print("Enter the word that you want to search: ");
+	   search = input.nextLine();  
+	   boolean searchResult = trie.search(search); 
+	   System.out.println();
+	   if(searchResult) {
+		 System.out.println("The word " + search + " is present in the trie.");
+	   }
+	   else {
+		 System.out.println("The word " + search + " is not present in the trie.");
+	   }
+	}
+ }
