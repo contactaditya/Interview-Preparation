489f3bea726f10dce16a80f18a982e08c24ec8d0
Aditya Gupta <contactadityag@gmail.com>
2017-09-21 23:10:44 -0400

    This is a program to return the level order traversal of its node's values. (ie, from left to right, level by level) in a binary tree.

--- Binary Search Trees/BinaryTreeLevelOrderTraversal.java
+++ Binary Search Trees/BinaryTreeLevelOrderTraversal.java
@@ -2,11 +2,11 @@ import java.util.*;
 import java.io.*;
 
  public class BinaryTreeLevelOrderTraversal {
-	Node root;	    
-	Node previous;
+     Node root;	    
+     Node previous;
 	    
-	public List<List<Integer>> printLevelOrderTraversal(Node root) {
-		Queue<Node> queue = new LinkedList<Node>();
+     public List<List<Integer>> printLevelOrderTraversal(Node root) {
+	Queue<Node> queue = new LinkedList<Node>();
         List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
         
         if(root == null) {
@@ -29,20 +29,19 @@ import java.io.*;
           wrapList.add(subList);
         }
        return wrapList;
-	}
+     }
 
-	public static void main(String[] args) {
-	   BinaryTreeLevelOrderTraversal tree = new BinaryTreeLevelOrderTraversal();
-	   tree.root = new Node(3);
-	   tree.root.left = new Node(5);
-	   tree.root.right = new Node(2);
-	   tree.root.left.left = new Node(1);
-	   tree.root.left.right = new Node(4);
-	   tree.root.right.left = new Node(6);
-	   List<List<Integer>> result = new LinkedList<List<Integer>>();
-	   result = tree.printLevelOrderTraversal(tree.root);		
+     public static void main(String[] args) {
+        BinaryTreeLevelOrderTraversal tree = new BinaryTreeLevelOrderTraversal();
+	tree.root = new Node(3);
+	tree.root.left = new Node(5);
+	tree.root.right = new Node(2);
+	tree.root.left.left = new Node(1);
+	tree.root.left.right = new Node(4);
+	tree.root.right.left = new Node(6);
+	List<List<Integer>> result = new LinkedList<List<Integer>>();
+	result = tree.printLevelOrderTraversal(tree.root);		
 		     
-	   System.out.print("Level order traversal of binary tree is: " + result);
-	  
-	}
+	System.out.print("Level order traversal of binary tree is: " + result);  
+     }
  }
