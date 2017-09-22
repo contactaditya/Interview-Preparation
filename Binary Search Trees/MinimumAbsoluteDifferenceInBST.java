2fb6f0e9882fff18c51044e02e3fdb095ef936c6
Aditya Gupta <contactadityag@gmail.com>
2017-09-21 23:08:02 -0400

    This is a program to find the minimum absolute difference between values of any two nodes in a binary search tree.

--- Binary Search Trees/MinimumAbsoluteDifferenceInBST.java
+++ Binary Search Trees/MinimumAbsoluteDifferenceInBST.java
@@ -2,34 +2,34 @@ import java.util.*;
 import java.io.*;
 
  public class MinimumAbsoluteDifferenceInBST {
-	 Node root;	    
-	 int min = Integer.MAX_VALUE;
-	 Integer previous = null;
+     Node root;	     
+     int min = Integer.MAX_VALUE;
+     integer previous = null;
 
-	 public int getMinimumDifference(Node root) {
+     public int getMinimumDifference(Node root) {
 			 
-	   if (root == null) {
-		 return min;
-	   }
-	   
-       getMinimumDifference(root.left);
-       
-       if (previous != null) {
-         min = Math.min(min, root.data - previous);
-       }
-       previous = root.data;
-       
-       getMinimumDifference(root.right);
-		
+        if (root == null) {
 	   return min;
-	} 
-
-	public static void main(String[] args) {
-	  MinimumAbsoluteDifferenceInBST tree = new MinimumAbsoluteDifferenceInBST();
-	  tree.root = new Node(1);
-	  tree.root.right = new Node(3);
-	  tree.root.right.left = new Node(2);
-		  
-	  System.out.println("The minimum absolute difference between values of any two nodes of the binary search tree is : " + tree.getMinimumDifference(tree.root));		
 	}
+	   
+        getMinimumDifference(root.left);
+       
+        if (previous != null) {
+           min = Math.min(min, root.data - previous);
+        }
+        previous = root.data;
+       
+        getMinimumDifference(root.right);
+		
+	return min;
+     } 
+
+     public static void main(String[] args) {
+	MinimumAbsoluteDifferenceInBST tree = new MinimumAbsoluteDifferenceInBST();
+	tree.root = new Node(1);
+	tree.root.right = new Node(3);
+	tree.root.right.left = new Node(2);
+		  
+	System.out.println("The minimum absolute difference between values of any two nodes of the binary search tree is : " + tree.getMinimumDifference(tree.root));		
+     }
  }
