0a066da5b5f3a674880cf83dc9bbb898b41c20b6
Aditya Gupta <contactadityag@gmail.com>
2017-09-21 21:57:30 -0400

    This is a program to find out whether there are two distinct indices i and j in the array such that array[i] = array[j].
    The absolute difference between i and j is at most k.

--- Arrays/ContainsNearByAlmostDuplicateInAnArray.java
+++ Arrays/ContainsNearByAlmostDuplicateInAnArray.java
@@ -3,57 +3,55 @@ import java.io.*;
 
 public class ContainsNearByAlmostDuplicateInAnArray {
 	
-	private static boolean containsNearbyAlmostDuplicate(int[] array, int k, int t) {
-	    if (array.length <= 1 || k < 0 || t < 0) {
-	       return false;
-	    }
+   private static boolean containsNearbyAlmostDuplicate(int[] array, int k, int t) {
+       if (array.length <= 1 || k < 0 || t < 0) {
+	  return false;
+       }
 		   
-	    TreeSet<Integer> treeSet = new TreeSet<Integer>();
+       TreeSet<Integer> treeSet = new TreeSet<Integer>();
 		   
-	    for (int i = 0; i < array.length; i++) {
-		  Integer floor = treeSet.floor(array[i] + t);
+       for (int i = 0; i < array.length; i++) {
+	  Integer floor = treeSet.floor(array[i] + t);
           Integer ceil = treeSet.ceiling(array[i] - t);
 	 
-	      if ((floor != null && floor >= array[i]) || (ceil != null && ceil <= array[i])) {
-            return true;
+	  if ((floor != null && floor >= array[i]) || (ceil != null && ceil <= array[i])) {
+             return true;
           }
 	      
-	      treeSet.add(array[i]);
-	      if (i >= k) {
+	  treeSet.add(array[i]);
+	  if (i >= k) {
             treeSet.remove(array[i-k]);
           }
-		   
-	    }
-	    return false;
-	} 	
+       }
+      return false;
+   } 	
 
-	public static void main(String[] args) {
-	   Scanner input = new Scanner(System.in);
-	   System.out.print("Enter number of elements in the array: ");
-	   int number = input.nextInt();  
-	   int array[] = new int[number];
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();  
+      int array[] = new int[number];
 					  
-	   System.out.println();	
-	   System.out.print("Please enter the actual numbers in the array: ");
-	   for (int i = 0; i < array.length; i++) {
-		  array[i] = input.nextInt();
-	   }
-	   System.out.println();	
-	   System.out.print("Enter an integer k which represents the absolute difference between the two array values: ");
-	   int k = input.nextInt();  
+      System.out.println();	
+      System.out.print("Please enter the actual numbers in the array: "); 
+      for (int i = 0; i < array.length; i++) {
+	 array[i] = input.nextInt();
+      }
+      System.out.println();	
+      System.out.print("Enter an integer k which represents the absolute difference between the two array values: ");
+      int k = input.nextInt();  
 		  
-	   System.out.println();	
-	   System.out.print("Enter an integer t which represents the absolute difference between two indexes: ");
-	   int t = input.nextInt();  
+      System.out.println();	
+      System.out.print("Enter an integer t which represents the absolute difference between two indexes: ");
+      int t = input.nextInt();  
 		  
-	   boolean duplicates = containsNearbyAlmostDuplicate(array, k, t);  
-	   System.out.println();	
-	   if(duplicates) {
-		 System.out.println("The array contains duplicates.");  
-	   } 
-	   else {
-		 System.out.println("The array does not contain any duplicates.");
-	   }
-	}
-
+      boolean duplicates = containsNearbyAlmostDuplicate(array, k, t);  
+      System.out.println();	
+      if(duplicates) {
+	System.out.println("The array contains duplicates.");  
+      } 
+      else {
+	System.out.println("The array does not contain any duplicates.");
+      }
+   }
 }
