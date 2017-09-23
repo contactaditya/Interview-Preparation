5d0e0cc730fe6e5d13f0e07ce1ba35c07849b4fe
contactaditya <contactadityag@gmail.com>
2017-09-23 19:38:13 -0400

    This is a program to find the lowest common ancestor (LCA) of two given nodes in the binary tree.

---
+++ Binary Search Trees/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+ public class LowestCommonAncestorOfABinaryTree { 	
+	Node root;
+	
+	public Node findLowestCommonAncestor(Node root, Node n1, Node n2) {
+		 
+	   if(root == null || root == n1 || root == n2) {
+		 return root;
+	   }
+		    
+       Node left = findLowestCommonAncestor(root.left, n1, n2);
+       Node right = findLowestCommonAncestor(root.right, n1, n2);
+       
+       if (left !=null && right !=null) {
+         return root;
+       }
+    
+       return (left != null) ? left : right; 
+	}
+
+	public static void main(String[] args) {     
+	  LowestCommonAncestorOfABinaryTree tree = new LowestCommonAncestorOfABinaryTree();
+      tree.root = new Node(1);
+      tree.root.left = new Node(2);
+      tree.root.right = new Node(3);
+      tree.root.left.left = new Node(4);
+      tree.root.left.right = new Node(5);
+      tree.root.right.left = new Node(6);
+      tree.root.right.right = new Node(7);
+            
+      Node n1 = tree.root.left.left;
+      Node n2 = tree.root.left.right;
+    
+      Node t = tree.findLowestCommonAncestor(tree.root, n1, n2);
+      if (t != null) {
+        System.out.println("LCA of " + n1.data + " and " + n2.data + " is " + t.data);
+      }
+      else {
+    	System.out.println("Keys are not present.");  
+      }
+	}
+ }
