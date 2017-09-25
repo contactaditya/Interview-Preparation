feeca2703f6da3d7bd64764f6ab47e43b647a909
Aditya Gupta <contactadityag@gmail.com>
2017-09-25 00:50:14 -0400

    This is a program to find the lowest and highest boundaries in a BST as L and R, trim the tree so that all its elements lies in [L, R].

--- Binary Search Trees/TrimABinarySearchTree.java
+++ Binary Search Trees/TrimABinarySearchTree.java
@@ -43,29 +43,29 @@ import java.io.*;
 	printPreorderTraversal(node.right);
      }
 	
-	public static void main(String[] args) {
-	  TrimABinarySearchTree tree = new TrimABinarySearchTree();
-	  tree.root = new Node(3);
-	  tree.root.left = new Node(0);
-	  tree.root.right = new Node(4);
-	  tree.root.left.right = new Node(2);
-	  tree.root.left.right.left = new Node(1);
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the lowest boundary of the binary search tree: ");
-	  int left = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Enter the highest boundary of the binary search tree: ");
-	  int right = input.nextInt();  
-	  Node newRoot = tree.trimBST(tree.root, left, right);		
-	  System.out.println();	     
-	  System.out.println("The new root of the trimmed binary search tree is: " + newRoot.data);	
-	  System.out.println();	  
-	  System.out.print("Inorder traversal of the trimmed binary search tree is: ");
-	  tree.printInorderTraversal(newRoot);		
-	  System.out.println();
-	  System.out.println();
-	  System.out.print("Preorder traversal of the trimmed binary search tree is: ");
-	  tree.printPreorderTraversal(newRoot);	
+     public static void main(String[] args) {
+	TrimABinarySearchTree tree = new TrimABinarySearchTree();
+	tree.root = new Node(3);
+	tree.root.left = new Node(0);
+	tree.root.right = new Node(4);
+	tree.root.left.right = new Node(2);
+	tree.root.left.right.left = new Node(1);
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter the lowest boundary of the binary search tree: ");
+	int left = input.nextInt();  
+	System.out.println();
+	System.out.print("Enter the highest boundary of the binary search tree: ");
+	int right = input.nextInt();  
+	Node newRoot = tree.trimBST(tree.root, left, right);		
+	System.out.println();	     
+	System.out.println("The new root of the trimmed binary search tree is: " + newRoot.data);	
+	System.out.println();	  
+	System.out.print("Inorder traversal of the trimmed binary search tree is: ");
+	tree.printInorderTraversal(newRoot);		
+	System.out.println();
+	System.out.println();
+	System.out.print("Preorder traversal of the trimmed binary search tree is: ");
+	tree.printPreorderTraversal(newRoot);	
 	  
-	}
+     }
   }
