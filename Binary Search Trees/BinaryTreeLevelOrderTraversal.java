d4b3120de2962a3330327fa83a076a3413c8b356
contactaditya <contactadityag@gmail.com>
2017-09-26 23:25:23 -0400

    This is a program to return the level order traversal of its node's values (ie, from left to right, level by level) in a binary tree.

--- Binary Search Trees/BinaryTreeLevelOrderTraversal.java
+++ Binary Search Trees/BinaryTreeLevelOrderTraversal.java
@@ -2,21 +2,39 @@ import java.util.*;
 import java.io.*;
 
  public class BinaryTreeLevelOrderTraversal {
-     Node root;	    
-     Node previous;
+	Node root;	    
+	Node previous;
+	
+	public List<List<Integer>> printLevelOrderTraversal(Node root) {
+       List<List<Integer>> result = new ArrayList<List<Integer>>();
+       levelHelper(result, root, 0);
+       return result;
+    }
+    
+    public void levelHelper(List<List<Integer>> result, Node root, int height) {
+       if (root == null) {
+          return;
+       }
+       if (height >= result.size()) {
+         result.add(new LinkedList<Integer>());
+       }
+       result.get(height).add(root.data);
+       levelHelper(result, root.left, height+1);
+       levelHelper(result, root.right, height+1);
+    }
 	    
-     public List<List<Integer>> printLevelOrderTraversal(Node root) {
-	Queue<Node> queue = new LinkedList<Node>();
-        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+	public List<List<Integer>> printLevelOrderTraversal1(Node root) {
+		Queue<Node> queue = new LinkedList<Node>();
+        List<List<Integer>> levelOrderTraversal = new LinkedList<List<Integer>>();
         
         if(root == null) {
-          return wrapList;
+          return levelOrderTraversal;
         }
         
         queue.offer(root);
         while(!queue.isEmpty()){
           int levelNumber = queue.size();
-          List<Integer> subList = new LinkedList<Integer>();
+          List<Integer> currentLevel = new LinkedList<Integer>();
           for(int i=0; i<levelNumber; i++) {
             if(queue.peek().left != null) {
               queue.offer(queue.peek().left);
@@ -24,24 +42,25 @@ import java.io.*;
             if(queue.peek().right != null) {
               queue.offer(queue.peek().right);
             }
-            subList.add(queue.poll().data);
+            currentLevel.add(queue.poll().data);
           }
-          wrapList.add(subList);
+          levelOrderTraversal.add(currentLevel);
         }
-       return wrapList;
-     }
+       return levelOrderTraversal;
+	}
 
-     public static void main(String[] args) {
-        BinaryTreeLevelOrderTraversal tree = new BinaryTreeLevelOrderTraversal();
-	tree.root = new Node(3);
-	tree.root.left = new Node(5);
-	tree.root.right = new Node(2);
-	tree.root.left.left = new Node(1);
-	tree.root.left.right = new Node(4);
-	tree.root.right.left = new Node(6);
-	List<List<Integer>> result = new LinkedList<List<Integer>>();
-	result = tree.printLevelOrderTraversal(tree.root);		
+	public static void main(String[] args) {
+	   BinaryTreeLevelOrderTraversal tree = new BinaryTreeLevelOrderTraversal();
+	   tree.root = new Node(3);
+	   tree.root.left = new Node(5);
+	   tree.root.right = new Node(2);
+	   tree.root.left.left = new Node(1);
+	   tree.root.left.right = new Node(4);
+	   tree.root.right.left = new Node(6);
+	   List<List<Integer>> result = new LinkedList<List<Integer>>();
+	   result = tree.printLevelOrderTraversal(tree.root);		
 		     
-	System.out.print("Level order traversal of binary tree is: " + result);  
-     }
+	   System.out.print("Level order traversal of binary tree is: " + result);
+	  
+	}
  }
