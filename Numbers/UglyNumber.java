90719c9c715f9ac705fbb91a7845931b59a475df
contactaditya <contactadityag@gmail.com>
2017-09-28 21:17:00 -0400

    This is a program to check whether a given number is an ugly number.
    Ugly numbers are positive numbers whose prime factors only include 2, 3,
    5.

---
+++ Numbers/UglyNumber.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+ public class UglyNumber {
+	
+	public static boolean isUgly(int number) {
+	  if (number <= 0) {
+		return false;
+	  }
+	  
+	  if (number == 1) {
+		return true;
+	  }	
+		
+	  int[] divisors = {2, 3, 5};
+	  for(int d : divisors) {
+        while (number % d == 0) {
+          number /= d;
+        }
+      }
+		
+	   return number == 1;
+	}	 
+
+	public static void main(String[] args) {  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the number: ");
+	  int number = input.nextInt();   
+	  boolean isTheNumberUgly = isUgly(number);
+	  System.out.println();
+	  if(isTheNumberUgly) {
+		System.out.println(number + " is an ugly number");  
+	  }
+      else {
+		System.out.println(number + " is not an ugly number");  
+	  }
+	}
+ }
