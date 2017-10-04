d57125755ce71d0c78c8699c7cbab12f95fbb5ac
contactaditya <contactadityag@gmail.com>
2017-10-04 07:57:23 -0400

    This is a program to return the bottom-up level order traversal of its node's values in a binary tree.

---
+++ Binary Search Trees/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+ public class BinaryTreeLevelOrderTraversalII { 	
+	Node root;	    
+	Node previous;
+	
+	public List<List<Integer>> printLevelOrderTraversal(Node root) {
+      List<List<Integer>> result = new ArrayList<List<Integer>>();
+      levelHelper(result, root, 0);
+      Collections.reverse(result);
+      return result;
+    }
+ 
+    public void levelHelper(List<List<Integer>> result, Node root, int height) {
+      if (root == null) {
+        return;
+      }
+      if (height >= result.size()) {
+        result.add(new LinkedList<Integer>());
+      }
+      result.get(height).add(root.data);
+      levelHelper(result, root.left, height+1);
+      levelHelper(result, root.right, height+1);
+    }
+	    
+	public List<List<Integer>> printLevelOrderTraversal1(Node root) {
+	  Queue<Node> queue = new LinkedList<Node>();
+      List<List<Integer>> levelOrderTraversal = new LinkedList<List<Integer>>();
+     
+      if(root == null) {
+        return levelOrderTraversal;
+      }
+     
+      queue.offer(root);
+      while(!queue.isEmpty()){
+        int levelNumber = queue.size();
+        List<Integer> currentLevel = new LinkedList<Integer>();
+        for(int i=0; i<levelNumber; i++) {
+          if(queue.peek().left != null) {
+            queue.offer(queue.peek().left);
+          }
+          if(queue.peek().right != null) {
+            queue.offer(queue.peek().right);
+          }
+          currentLevel.add(queue.poll().data);
+        }
+       levelOrderTraversal.add(0, currentLevel);
+      }
+      return levelOrderTraversal;
+	}
+
+	public static void main(String[] args) { 	   
+	   BinaryTreeLevelOrderTraversalII tree = new BinaryTreeLevelOrderTraversalII();
+	   tree.root = new Node(3);
+	   tree.root.left = new Node(5);
+	   tree.root.right = new Node(2);
+	   tree.root.left.left = new Node(1);
+	   tree.root.left.right = new Node(4);
+	   tree.root.right.left = new Node(6);
+	   List<List<Integer>> result = new LinkedList<List<Integer>>();
+	   result = tree.printLevelOrderTraversal(tree.root);		
+		     
+	   System.out.print("Level order traversal of binary tree is: " + result);
+	}
+ }
