58b0066f948989c70c0f835de5e1db8d7bb3b287
contactaditya <contactadityag@gmail.com>
2017-10-16 18:33:17 -0400

    This is a program to find the longest string in the dictionary that can be formed by deleting some characters of the given string.

---
+++ Strings/FindTheLongestWordInDictionaryThroughDeleting.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+  public class FindTheLongestWordInDictionaryThroughDeleting {
+	  
+	public static String findLongestWord(String string, List<String> dictionary) {
+	   String longest = "";
+	   for (String word : dictionary) {
+         if (isSubsequence(word, string)) {
+           if (word.length() > longest.length() || (word.length() == longest.length() && word.compareTo(longest) < 0)) {
+        	 longest = word;
+           }
+         }
+       }
+	   return longest;     
+	}	  
+
+	private static boolean isSubsequence(String word, String string) {
+	   int j = 0;
+	   for (int i = 0; i < string.length() && j < word.length(); i++) {
+	     if (word.charAt(j) == string.charAt(i)) {
+	        j++;
+	     }
+	   }
+	   return j == word.length();
+	}
+
+	public static void main(String[] args) {
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the number of words in the list: ");
+	  int numberOfWords = input.nextInt();
+	  System.out.println();
+	  input.nextLine();
+	  System.out.println("Please enter the actual words in the list: ");
+	  List<String> dictionary = new ArrayList<String>();
+	  for (int i = 0; i < numberOfWords; i++) {
+		dictionary.add(input.nextLine());
+	  }		  
+	  System.out.println();
+	  String string = new String();	
+      System.out.print("Enter the string: ");
+	  string = input.nextLine();  
+	  String longestWord = findLongestWord(string, dictionary);	
+	  System.out.println();
+	  System.out.println("The longest word with the smallest lexicographical order is: " + longestWord);
+	}
+  }
