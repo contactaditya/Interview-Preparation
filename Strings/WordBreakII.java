1a70dda9a85474bf71db382235a84cb8b076b272
Aditya Gupta <contactadityag@gmail.com>
2017-10-17 16:59:12 -0400

    This is a program to add spaces in a string to construct a sentence where each word is a valid dictionary word.

--- Strings/WordBreakII.java
+++ Strings/WordBreakII.java
@@ -3,49 +3,50 @@ import java.io.*;
 
   public class WordBreakII {
 	
-	public static List<String> wordBreak(String string, List<String> wordDictionary) {
-	   return DFS(string, wordDictionary, new HashMap<String, List<String>>());    
-	}  
+    public static List<String> wordBreak(String string, List<String> wordDictionary) {
+       return DFS(string, wordDictionary, new HashMap<String, List<String>>());    
+    }  
 
-	private static List<String> DFS(String string, List<String> wordDictionary, HashMap<String, List<String>> hashMap) {
-	    if (hashMap.containsKey(string)) {
-	      return hashMap.get(string);
-		}
+    private static List<String> DFS(String string, List<String> wordDictionary, HashMap<String, List<String>> hashMap) {
+       if (hashMap.containsKey(string)) {
+	 return hashMap.get(string);
+       }
 	    
-	    List<String> result = new ArrayList<String>();     
-	    if (string.length() == 0) {
-	      result.add("");
-	      return result;
-	    }
-	    for (String word : wordDictionary) {
-	      if (string.startsWith(word)) {
-	       List<String>sublist = DFS(string.substring(word.length()), wordDictionary, hashMap);
-	       for (String sub : sublist) 
-	         result.add(word + (sub.isEmpty() ? "" : " ") + sub);               
-	       }
-	    }       
-	    hashMap.put(string, result);
-	    return result;
-	}
+       List<String> result = new ArrayList<String>();     
+       if (string.length() == 0) {
+	 result.add("");
+	 return result;
+       }
+       for (String word : wordDictionary) {
+	 if (string.startsWith(word)) {
+	   List<String>sublist = DFS(string.substring(word.length()), wordDictionary, hashMap);
+	   for (String sub : sublist) {
+	     result.add(word + (sub.isEmpty() ? "" : " ") + sub); 
+	   }
+	 }  
+       }       
+       hashMap.put(string, result);
+       return result;
+    }
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number of words in the dictionary: ");
-	  int numberOfWords = input.nextInt();
-	  System.out.println();
-	  input.nextLine();
-	  System.out.println("Please enter the actual words in the dictionary: ");
-	  ArrayList<String> dictionary = new ArrayList<String>();
-	  for (int i = 0; i < numberOfWords; i++) {
-		dictionary.add(input.nextLine());
-	  }		 
-	  System.out.println();
-	  String string = new String();	
-	  System.out.print("Enter a non empty string: ");
-	  string = input.nextLine();  
-	  List<String> result = new ArrayList<String>();
-	  result = wordBreak(string, dictionary); 
-	  System.out.println();
-	  System.out.println("The sentence where every word is a valid word is: " + result);
-	}
-  }
+    public static void main(String[] args) { 	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number of words in the dictionary: ");
+       int numberOfWords = input.nextInt();
+       System.out.println();
+       input.nextLine();
+       System.out.println("Please enter the actual words in the dictionary: ");
+       ArrayList<String> dictionary = new ArrayList<String>();
+       for (int i = 0; i < numberOfWords; i++) {
+	 dictionary.add(input.nextLine());
+       }		 
+       System.out.println();
+       String string = new String();	
+       System.out.print("Enter a non empty string: ");
+       string = input.nextLine();  
+       List<String> result = new ArrayList<String>();
+       result = wordBreak(string, dictionary); 
+       System.out.println();
+       System.out.println("The sentence where every word is a valid word is: " + result);
+    }
+ }
