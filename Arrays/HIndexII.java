80487b471fc1e758972aaba6e7f5442abee6b7fb
Aditya Gupta <contactadityag@gmail.com>
2017-10-17 16:13:53 -0400

    This is a program to compute H Index if the citations array is sorted in ascending order.
    According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the
    other N − h papers have no more than h citations each."

--- Arrays/HIndexII.java
+++ Arrays/HIndexII.java
@@ -3,40 +3,40 @@ import java.util.*;
 
  public class HIndexII {
 	 
-	public static int hIndex(int[] citations) {
-	   if(citations == null || citations.length == 0) {
-		 return 0;	  
-	   }
-	   int length = citations.length;
+    public static int hIndex(int[] citations) {
+       if(citations == null || citations.length == 0) {
+	 return 0;	  
+       }
+       int length = citations.length;
        int low = 0, high = length - 1;
        while(low <= high) {
     	 int mid = low + ((high - low)/2);  
-  		 if (citations[mid] == length - mid) {
-  		   return length - mid;
-  		 } else if (citations[mid] < length - mid) {
-  		   low = mid + 1;
-  	     } else {
-  	       high = mid - 1;	 
-  	     }
+         if (citations[mid] == length - mid) {
+  	   return length - mid;
+  	 } else if (citations[mid] < length - mid) {
+           low = mid + 1;
+  	 } else {
+  	   high = mid - 1;	 
+  	 }
        }
-	   return length - low;     
+       return length - low;     
     }	 
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int citations[] = new int[number];
+    public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int number = input.nextInt();  
+       int citations[] = new int[number];
 	
-	  System.out.println();	
-	  System.out.print("Please enter the elements in the array: ");
-	  for (int i = 0; i < citations.length; i++) {
-	    citations[i] = input.nextInt();
-	  }
+       System.out.println();	
+       System.out.print("Please enter the elements in the array: ");
+       for (int i = 0; i < citations.length; i++) {
+	 citations[i] = input.nextInt();
+       }
 		  
-	  int hIndex = hIndex(citations);
+       int hIndex = hIndex(citations);
 	  
-	  System.out.println();
-	  System.out.print("The researcher's h-index is: " + hIndex);
-	}
+       System.out.println();
+       System.out.print("The researcher's h-index is: " + hIndex);
+    }
  }
