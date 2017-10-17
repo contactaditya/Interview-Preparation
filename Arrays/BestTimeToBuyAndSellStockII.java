1a7097480dcb421063dd804fe5c6f6b2907dbfc1
Aditya Gupta <contactadityag@gmail.com>
2017-10-16 21:17:10 -0400

    This is a program to find the maximum profit in an array for which the ith element is the price of a given stock on day i.
    You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).

--- Arrays/BestTimeToBuyAndSellStockII.java
+++ Arrays/BestTimeToBuyAndSellStockII.java
@@ -2,33 +2,33 @@ import java.util.*;
 import java.io.*;
 
  public class BestTimeToBuyAndSellStockII {
-	 
-	public static int maxProfit(int[] prices) {
-	   int total = 0;	
-	   for (int i=0; i< prices.length-1; i++) {
-	     if (prices[i+1]>prices[i]) {
-	       total += prices[i+1]-prices[i];
-	     }
-	   }
-	
-	   return total;     
-	}	 
-
-	public static void main(String[] args) { 		
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the stock price: ");
-	  int numberOfStocks = input.nextInt();   
-	  int price[] = new int[numberOfStocks];
-	    
-	  System.out.print("\n");
-	  System.out.print("Please enter the actual values of the cost of the stock: ");
-	  for (int i = 0; i < numberOfStocks; i++) {
-	    price[i] = input.nextInt(); 
-	  }
-	 
-	  int maximumProfit = maxProfit(price);
-	  System.out.print("\n");
-	  System.out.print("The maximum profit that can be accomplished by buying and selling stocks of ith element on day i is: " + maximumProfit);
 	  
+     public static int maxProfit(int[] prices) {
+	int total = 0;	
+	for (int i=0; i< prices.length-1; i++) {
+	  if (prices[i+1]>prices[i]) {
+	    total += prices[i+1]-prices[i];
+	  }
 	}
+	
+        return total;     
+     }	 
+
+     public static void main(String[] args) { 		
+        Scanner input = new Scanner(System.in);
+	System.out.print("Enter number of elements in the stock price: ");
+	int numberOfStocks = input.nextInt();   
+	int price[] = new int[numberOfStocks];
+	    
+	System.out.print("\n");
+	System.out.print("Please enter the actual values of the cost of the stock: ");
+	for (int i = 0; i < numberOfStocks; i++) {
+	  price[i] = input.nextInt(); 
+	}
+	 
+	int maximumProfit = maxProfit(price);
+	System.out.print("\n");
+	System.out.print("The maximum profit that can be accomplished by buying and selling stocks of ith element on day i is: " + maximumProfit);
+	  
+     }
  }
