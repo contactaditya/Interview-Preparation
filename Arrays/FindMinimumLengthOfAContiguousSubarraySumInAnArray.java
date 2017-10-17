c34b0788107f78de7eee0580390991cbc43494f6
Aditya Gupta <contactadityag@gmail.com>
2017-10-17 16:24:07 -0400

    This is a program to find the minimal length of a contiguous subarray of which the sum ≥ s.

--- Arrays/FindMinimumLengthOfAContiguousSubarraySumInAnArray.java
+++ Arrays/FindMinimumLengthOfAContiguousSubarraySumInAnArray.java
@@ -22,24 +22,24 @@ import java.io.*;
        return minimumLength == Integer.MAX_VALUE ? 0 : minimumLength;        
     }	 
 
-	public static void main(String[] args) { 	
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+    public static void main(String[] args) { 	
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int number = input.nextInt();  
+       int array[] = new int[number];
 			
-	  System.out.println();	
-	  System.out.print("Please enter the elements in the array: ");
-	  for (int i = 0; i < array.length; i++) {
-		array[i] = input.nextInt();
-	  }
+       System.out.println();	
+       System.out.print("Please enter the elements in the array: ");
+       for (int i = 0; i < array.length; i++) {
+	 array[i] = input.nextInt();
+       }
 			
-	  System.out.println();	
-	  System.out.print("Enter the sum that you want to check whether it is present in the continuos subarray or not: ");
-	  int s = input.nextInt();  
+       System.out.println();	
+       System.out.print("Enter the sum that you want to check whether it is present in the continuos subarray or not: ");
+       int s = input.nextInt();  
 		
-	  int minimumLength = minimumSubArrayLength(s, array);
-	  System.out.println(); 
-	  System.out.println("The minimal length of a contiguous subarray of which the sum is: " + minimumLength);
-	}
+       int minimumLength = minimumSubArrayLength(s, array);
+       System.out.println(); 
+       System.out.println("The minimal length of a contiguous subarray of which the sum is: " + minimumLength);
+    }
  }
