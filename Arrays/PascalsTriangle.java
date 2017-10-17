e7d430d4ab074fb676860e66c9095b452e3a755e
Aditya Gupta <contactadityag@gmail.com>
2017-10-17 16:10:09 -0400

    This is a program to generate the number of rows of Pascal's Triangle.

--- Arrays/PascalsTriangle.java
+++ Arrays/PascalsTriangle.java
@@ -3,26 +3,26 @@ import java.io.*;
 
   public class PascalsTriangle {
 	 
-	public static List<List<Integer>> generate(int numberOfRows) {
-	   List<List<Integer>> allrows = new ArrayList<List<Integer>>();
-	   ArrayList<Integer> row = new ArrayList<Integer>();
-	   for(int i=0;i<numberOfRows;i++) {
-		 row.add(0, 1);
-		 for(int j=1;j<row.size()-1;j++) {
-		   row.set(j, row.get(j)+row.get(j+1));
-		 }
-		 allrows.add(new ArrayList<Integer>(row));
-	   }
-	   return allrows;     
-	}	  
-
-	public static void main(String[] args) {
-	   Scanner input = new Scanner(System.in);
-	   System.out.print("Enter the number of rows of Pascal's Triangle: ");
-	   int numberOfRows = input.nextInt();   	
-	   List<List<Integer>> result = new ArrayList<List<Integer>>();
-	   result = generate(numberOfRows);
-	   System.out.println();
-	   System.out.println("The Pascal Triangle for " + numberOfRows + " is as follows: " + result);
+     public static List<List<Integer>> generate(int numberOfRows) {
+        List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+	ArrayList<Integer> row = new ArrayList<Integer>();
+	for(int i=0;i<numberOfRows;i++) {
+	  row.add(0, 1);
+	  for(int j=1;j<row.size()-1;j++) {
+	    row.set(j, row.get(j)+row.get(j+1));
+	  }
+	  allrows.add(new ArrayList<Integer>(row));
 	}
+        return allrows;     
+     }	  
+
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+	System.out.print("Enter the number of rows of Pascal's Triangle: ");
+	int numberOfRows = input.nextInt();   	
+	List<List<Integer>> result = new ArrayList<List<Integer>>();
+	result = generate(numberOfRows);
+	System.out.println();
+	System.out.println("The Pascal Triangle for " + numberOfRows + " is as follows: " + result);
+     }
   }
