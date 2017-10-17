43a517b434c849885dcb6a37096fee46327cd0d2
Aditya Gupta <contactadityag@gmail.com>
2017-10-17 18:54:19 -0400

    This is a program to determine if string can be segmented into a space-separated sequence of one or more dictionary words.

--- Strings/WordBreak.java
+++ Strings/WordBreak.java
@@ -3,12 +3,12 @@ import java.io.*;
 
  public class WordBreak {
 	 
-	public static boolean wordBreak(String string, List<String> wordDictionary) {
-	   if(string == null || string.length() == 0) {
-	     return true;
-	   }
-	   boolean [] dp = new boolean[string.length()+1];
-	   dp[0] = true ;
+    public static boolean wordBreak(String string, List<String> wordDictionary) {
+       if(string == null || string.length() == 0) {
+	 return true;
+       }
+       boolean [] dp = new boolean[string.length()+1];
+       dp[0] = true ;
 	               
        for(int i=1; i<=string.length(); i++) {
          dp[i] = false;
@@ -19,31 +19,31 @@ import java.io.*;
          }
        }
 	   
-	   return dp[string.length()];     
-	}	 
+       return dp[string.length()];     
+    }	 
  
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number of words in the dictionary: ");
-	  int numberOfWords = input.nextInt();
-	  System.out.println();
-	  input.nextLine();
-	  System.out.println("Please enter the actual words in the dictionary: ");
-	  ArrayList<String> dictionary = new ArrayList<String>();
-	  for (int i = 0; i < numberOfWords; i++) {
-		dictionary.add(input.nextLine());
-	  }		 
-	  System.out.println();
-	  String string = new String();	
-	  System.out.print("Enter a non empty string: ");
-	  string = input.nextLine();  
-	  boolean searchResult = wordBreak(string, dictionary); 
-	  System.out.println();
-	  if(searchResult) {
-	    System.out.println("The word " + string + " can be segmented into a space-separated sequence of one or more dictionary words.");
-	  }
-	  else {
-		System.out.println("The word " + string + " cannot be segmented into a space-separated sequence of one or more dictionary words.");
-	  }
-	}
+    public static void main(String[] args) { 	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number of words in the dictionary: ");
+       int numberOfWords = input.nextInt();
+       System.out.println();
+       input.nextLine();
+       System.out.println("Please enter the actual words in the dictionary: ");
+       ArrayList<String> dictionary = new ArrayList<String>();
+       for (int i = 0; i < numberOfWords; i++) {
+	 dictionary.add(input.nextLine());
+       }		 
+       System.out.println();
+       String string = new String();	
+       System.out.print("Enter a non empty string: ");
+       string = input.nextLine();  
+       boolean searchResult = wordBreak(string, dictionary); 
+       System.out.println();
+       if(searchResult) {
+	 System.out.println("The word " + string + " can be segmented into a space-separated sequence of one or more dictionary words.");
+       }
+       else {
+	 System.out.println("The word " + string + " cannot be segmented into a space-separated sequence of one or more dictionary words.");
+       }
+    }
  }
