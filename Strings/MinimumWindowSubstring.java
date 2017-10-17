18e26a80698a3923d821b56d7c314de19d74bda6
Aditya Gupta <contactadityag@gmail.com>
2017-10-16 21:36:00 -0400

    This is a program to find the minimum window in a string which will contain all the characters in another string.

--- Strings/MinimumWindowSubstring.java
+++ Strings/MinimumWindowSubstring.java
@@ -3,12 +3,12 @@ import java.io.*;
 
  public class MinimumWindowSubstring {
 	
-	public static String minimumWindow(String s, String t) {
-	   if(s == null || s.length() < t.length() || s.length() == 0) {
-		  return "";
-	   }
+    public static String minimumWindow(String s, String t) {
+       if(s == null || s.length() < t.length() || s.length() == 0) {
+	 return "";
+       }
 	
-	   int[] map = new int[256];
+       int[] map = new int[256];
        char[] String = s.toCharArray();
        char[] checkString = t.toCharArray();
        for(char c : checkString) {
@@ -34,23 +34,23 @@ import java.io.*;
          }
        }
   
-	   return minimumLength == Integer.MAX_VALUE ? "" : s.substring(minimumStart, minimumStart+minimumLength);     
+       return minimumLength == Integer.MAX_VALUE ? "" : s.substring(minimumStart, minimumStart+minimumLength);     
     }	 
 
-	public static void main(String[] args) { 	 
-	  String s = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  s = input.next();  
+    public static void main(String[] args) { 	 
+       String s = new String();	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the string: ");
+       s = input.next();  
 	  
-	  System.out.println();	
-	  String t = new String();	  
-	  System.out.print("Enter the string which you want to check is present in the first string: ");
-	  t = input.next();
+       System.out.println();	
+       String t = new String();	  
+       System.out.print("Enter the string which you want to check is present in the first string: ");
+       t = input.next();
 	  
-	  String result = minimumWindow(s, t);
-	  System.out.println();	
-	  System.out.println("The minimum window in "+ s + " which will contain all the characters in " + t + " is: " + result);
+       String result = minimumWindow(s, t);
+       System.out.println();	
+       System.out.println("The minimum window in "+ s + " which will contain all the characters in " + t + " is: " + result);
 	 
-	}
+    }
  }
