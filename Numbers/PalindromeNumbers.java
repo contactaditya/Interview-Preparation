279d2aacc51df51732390c7bf7af44cbd363b456
contactaditya <contactadityag@gmail.com>
2017-10-19 22:06:20 -0400

    This is a program to check if a number is palindrome or not.

--- Numbers/PalindromeNumbers.java
+++ Numbers/PalindromeNumbers.java
@@ -3,33 +3,29 @@ import java.util.*;
 
 public class PalindromeNumbers {
 	
-  public static boolean isPalindrome(int number) { 
-       int palindrome = number; // copied number into variable
+    public static boolean isPalindrome(int number) { 
+       if(number < 0 || (number % 10 == 0 && number != 0)) {
+    	 return false;
+       }
        int reverse = 0;
-
-       while (palindrome != 0) {
-          int remainder = palindrome % 10;
-          reverse = reverse * 10 + remainder;
-          palindrome = palindrome / 10;
-       }
-      
-       if (number == reverse) {
-         return true;
-       }
-
-    return false;
-  }	
+       while (number > reverse) {
+    	 reverse = reverse*10 + number%10;
+    	 number = number/10;
+       }  
+       return (number==reverse || number==reverse/10); 
+	}	
 	
-  public static void main(String[] args) { 	  
-     Scanner input = new Scanner(System.in);
-     System.out.print("Enter the number: ");
-     int number = input.nextInt();
+	public static void main(String[] args) { 	  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the number: ");
+	  int number = input.nextInt();
 		  
-     System.out.println();
-     if(isPalindrome(number)) {
-       System.out.println(number + " is a palindrome number.");
-     }  else {
-       System.out.println(number + " is not a palindrome number.");
-     }
-   }
+	  System.out.println();
+	  if(isPalindrome(number)) {
+	    System.out.println(number + " is a palindrome number.");
+	  }
+	  else {
+		System.out.println(number + " is not a palindrome number.");
+	  }
+	}
   }
