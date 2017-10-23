8f04500fedd4cb09cb02f7539fe34646df48d131
Aditya Gupta <contactadityag@gmail.com>
2017-10-23 00:25:21 -0400

    This is a program to print the Next Greater Number for every element in a circular array.

--- Stacks/NextGreaterElementII.java
+++ Stacks/NextGreaterElementII.java
@@ -3,36 +3,36 @@ import java.io.*;
 
   public class NextGreaterElementII {
 	  
-	public static int[] nextGreaterElements(int[] array) {
-	  int[] result = new int[array.length];
-      java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
-      for (int i = 2 * array.length - 1; i >= 0; --i) {
-        while (!stack.empty() && array[stack.peek()] <= array[i % array.length]) {
-           stack.pop();
+     public static int[] nextGreaterElements(int[] array) {
+        int[] result = new int[array.length];
+        java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
+        for (int i = 2 * array.length - 1; i >= 0; --i) {
+          while (!stack.empty() && array[stack.peek()] <= array[i % array.length]) {
+            stack.pop();
+          }
+          result[i % array.length] = stack.empty() ? -1 : array[stack.peek()];
+          stack.push(i % array.length);
         }
-        result[i % array.length] = stack.empty() ? -1 : array[stack.peek()];
-        stack.push(i % array.length);
-      }
-      return result;    
-	}	  
+        return result;    
+     }	  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+     public static void main(String[] args) {   
+        Scanner input = new Scanner(System.in);
+	System.out.print("Enter the elements in the array: ");
+	int number = input.nextInt();  
+	int array[] = new int[number];
 					
-	  System.out.println();	
-	  System.out.print("Please enter the elements in the array: ");
-	  for (int i = 0; i < array.length; i++) {
-		array[i] = input.nextInt();
-	  }
-
-	  int result[] = nextGreaterElements(array);
-	  System.out.println();	
-	  System.out.print("The next greater number for every element in the array is: ");
-	  for (int i=0; i < result.length; i++) {
-		System.out.print(result[i] + " ");
-	  }	  
+	System.out.println();	
+	System.out.print("Please enter the elements in the array: ");
+	for (int i = 0; i < array.length; i++) {
+	  array[i] = input.nextInt();
 	}
+
+	int result[] = nextGreaterElements(array);
+	System.out.println();	
+	System.out.print("The next greater number for every element in the array is: ");
+	for (int i=0; i < result.length; i++) {
+	  System.out.print(result[i] + " ");
+	}	  
+     }
   } 
