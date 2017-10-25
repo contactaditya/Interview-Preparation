bb0eda9c6c675933ad02a3a28aeff62ff20ebe1e
Aditya Gupta <contactadityag@gmail.com>
2017-10-24 21:09:57 -0400

    This is a program to find whether a number is a valid perfect square or not.

--- Numbers/ValidPerfectSquare.java
+++ Numbers/ValidPerfectSquare.java
@@ -3,23 +3,23 @@ import java.io.*;
 
  public class ValidPerfectSquare {
 	 
-	public static boolean isPerfectSquare(int number) {
-	  int low = 1, high = number;
-	  while (low <= high) {
-	    long mid = low + (high-low)/2;
-	    if (mid * mid == number) {
-	      return true;
-	    } else if (mid * mid < number) {
-	      low = (int) mid + 1;
-	    } else {
-	      high = (int) mid - 1;
-	    }
-	  }	
+    public static boolean isPerfectSquare(int number) {
+      int low = 1, high = number;
+      while (low <= high) {
+	long mid = low + (high-low)/2;
+	if (mid * mid == number) {
+	  return true;
+	} else if (mid * mid < number) {
+	  low = (int) mid + 1;
+	} else {
+	  high = (int) mid - 1;
+	}
+      }	
 	        
-	  return false;     
-	}	 
+      return false;     
+    }	 
 	
-	public boolean isPerfectSquare1(int number) {
+    public boolean isPerfectSquare1(int number) {
       long x = number;
       while (x * x > number) {
         x = (x + number / x)/2;
@@ -27,19 +27,19 @@ import java.io.*;
       return x * x == number;
     }
 
-	public static void main(String[] args) {
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();   
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number: ");
+       int number = input.nextInt();   
 		    	   
-	  boolean isPerfectSqaure = isPerfectSquare(number); 
+       boolean isPerfectSqaure = isPerfectSquare(number); 
 		   
-	  System.out.println();
-	  if(isPerfectSqaure) {
-		System.out.println(number + " is a perfect square.");  
-	  }
-	  else {
-		 System.out.println(number + " is not a perfect square.");    
-	  }
-	}
+       System.out.println();
+       if(isPerfectSqaure) {
+	 System.out.println(number + " is a perfect square.");  
+       }
+       else {
+	 System.out.println(number + " is not a perfect square.");    
+       }
+    }
  }
