bad39caaea02fea6e4185c489e0fc201f476666c
Aditya Gupta <contactadityag@gmail.com>
2017-10-24 20:57:18 -0400

    This is a program to decide whether there are two integers a and b such that a^2 + b^2 = c such that you are given a non-negative integer c.

--- Numbers/SumOfSquareNumbers.java
+++ Numbers/SumOfSquareNumbers.java
@@ -12,21 +12,21 @@ import java.io.*;
        } 	
        
        return false;      
-	}	 
+    }	 
 
-	public static void main(String[] args) {    
-	   Scanner input = new Scanner(System.in);
-	   System.out.print("Enter the number: ");
-	   int c = input.nextInt();   
+    public static void main(String[] args) {    
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number: ");
+       int c = input.nextInt();   
 	    	   
-	   boolean sumOfSquareNumbers = judgeSquareSum(c); 
+       boolean sumOfSquareNumbers = judgeSquareSum(c); 
 	   
-	   System.out.println();
-	   if(sumOfSquareNumbers) {
-		  System.out.println("The are two integers a and b such that: a^2 + b^2 = " + c);  
-	   }
-	   else {
-		  System.out.println("The are no two integers a and b such that: a^2 + b^2 = " + c);    
-	   }
-	}
+       System.out.println();
+       if(sumOfSquareNumbers) {
+	 System.out.println("The are two integers a and b such that: a^2 + b^2 = " + c);  
+       }
+       else {
+	 System.out.println("The are no two integers a and b such that: a^2 + b^2 = " + c);    
+       }
+    }
  }
