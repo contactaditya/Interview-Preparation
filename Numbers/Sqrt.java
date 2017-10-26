e7ddae472ae0632585e67092bc4f408af0f097e4
contactaditya <contactadityag@gmail.com>
2017-10-26 19:12:01 -0400

    This is a program to compute and return the square root of x.

---
+++ Numbers/Sqrt.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+ public class Sqrt {
+	 
+	public static int mySqrt(int number) { 
+	   if (number == 0) {
+		 return 0;
+	   }
+	   int start = 1, end = number;
+	   while (start < end) { 
+	     int mid = start + (end - start) / 2;
+		 if (mid <= number/mid && (mid + 1) > number/(mid + 1)) { 
+		   return mid; 
+		 }
+		 else if (mid > number/mid) {
+		   end = mid;
+		 }
+		 else {
+		   start = mid + 1;
+		 }
+	   }
+	   
+	   return start;	 
+	} 
+	
+	public static int mySqrt1(int number) {
+	   if (number == 0) {
+		 return 0;
+	   }
+	   long i = number;
+	   while(i * i > number) {  
+		 i = (i + number / i) / 2;
+	   }
+	   return (int)i;
+    } 
+
+	public static void main(String[] args) {   
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the number: ");
+	  int number = input.nextInt();  
+			
+	  int squareRoot = mySqrt(number);
+
+	  System.out.println();	
+	  System.out.print("The square root of the " + number + " is: " + squareRoot);
+	  
+	}
+ }
