1eed1bb2da723a5165b0e89004ece8283e49ca82
contactaditya <contactadityag@gmail.com>
2017-10-26 19:03:09 -0400

    This is a program in which you are provided with a column title as appear in an Excel sheet and return its corresponding column number.

---
+++ Strings/ExcelSheetColumnNumber.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+  public class ExcelSheetColumnNumber {
+	  
+	public static int titleToNumber(String string) {
+	   int number = 0;	
+	   for(int i=0; i<string.length(); i++) {
+	     number = 26 * number + (string.charAt(i)-'A') + 1; 
+	   }
+	   return number;      
+	}	  
+
+	public static void main(String[] args) {
+	   String string = new String();	  	
+	   Scanner input = new Scanner(System.in);
+	   System.out.print("Enter the string: ");
+	   string = input.next();  
+	   int result = titleToNumber(string);
+	   System.out.println();	
+	   System.out.println("The corresponding column title as it appears in an Excel sheet is: " + result);	
+	}
+  }
