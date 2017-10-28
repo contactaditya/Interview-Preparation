9f5169fa0bfaa6a9fb9c4e99d68cb65a9efdb701
Aditya Gupta <contactadityag@gmail.com>
2017-10-27 22:14:47 -0400

    This is a program in which you are given a positive integer and return its corresponding column title as it appears in an Excel sheet.

--- Strings/ExcelSheetColumnTitle.java
+++ Strings/ExcelSheetColumnTitle.java
@@ -3,23 +3,23 @@ import java.io.*;
 
   public class ExcelSheetColumnTitle {
 	
-	public static String convertToTitle(int number) {
-	   StringBuilder result = new StringBuilder();
-	   while(number>0){
-		 number--;
-	     result.insert(0, (char)('A' + number % 26));
-	     number /= 26;
-	   }
-
-	   return result.toString();     
-	}	
-
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();  
-	  String result = convertToTitle(number);
-	  System.out.println();	
-	  System.out.println("The corresponding column title as it appears in an Excel sheet is: " + result);	
+     public static String convertToTitle(int number) {
+	StringBuilder result = new StringBuilder();
+	while(number>0) {
+	   number--;
+	   result.insert(0, (char)('A' + number % 26));
+	   number /= 26;
 	}
+
+	return result.toString();     
+     }	
+
+     public static void main(String[] args) {   
+        Scanner input = new Scanner(System.in);
+	System.out.print("Enter the number: ");
+	int number = input.nextInt();  
+	String result = convertToTitle(number);
+	System.out.println();	
+	System.out.println("The corresponding column title as it appears in an Excel sheet is: " + result);	
+     }
   }
