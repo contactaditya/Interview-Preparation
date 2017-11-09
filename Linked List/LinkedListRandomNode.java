54f8943d59b783e1ec0fa93ca0a98a3a6c1e40d1
contactaditya <contactadityag@gmail.com>
2017-11-09 04:26:40 -0500

    This is a program to return a random node's value from the linked list in a singly linked list.
    Each node must have the same probability of being chosen.

---
+++ Linked List/LinkedListRandomNode.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+  public class LinkedListRandomNode {
+	  
+	private static LinkedListNode head;
+    Random random;
+    
+    public LinkedListRandomNode (LinkedListNode head) {
+       this.head = head;       
+       random = new Random();        
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+      LinkedListNode current = head;
+      int r = (int)current.data;
+      for(int i=1; current.next != null; i++) {       
+        current = current.next;
+        if(random.nextInt(i + 1) == i) {
+          r = (int)current.data;                        
+        }
+      }      
+      return r;	
+    }
+    
+	public static void main(String[] args) {   
+	  LinkedListRandomNode list = new LinkedListRandomNode(head);
+	  list.head = new LinkedListNode(1);
+	  list.head.next = new LinkedListNode(2);
+	  list.head.next.next = new LinkedListNode(3);
+
+	  int randomValue = list.getRandom();
+	  System.out.print("The random's node value from the linked list is: " + randomValue);	 
+
+	}
+  }
