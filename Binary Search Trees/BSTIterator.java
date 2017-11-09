86888262e209426c63b28145c891a6eec0a8c609
contactaditya <contactadityag@gmail.com>
2017-11-09 04:22:30 -0500

    This is a program to implement an iterator over a BST. Your iterator will be initialized with the root node of a BST.
    Calling next() will return the next smallest number in the BST.

---
+++ Binary Search Trees/BSTIterator.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+  public class BSTIterator { 
+	static Node root;
+	private Stack<Node> stack = new Stack<Node>();
+	  
+	public BSTIterator(Node root) {
+	  this.root = root;   
+	  while(root != null) {
+        stack.push(root);
+        root = root.left;
+      }   
+	}
+
+	/** @return whether we have a next smallest number */
+	public boolean hasNext() {
+	  return !stack.isEmpty() || root != null;       
+	}
+
+	/** @return the next smallest number */
+    public int next() { 
+      root = stack.pop();
+      int result = root.data;
+      root = root.right;	
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+     
+      return result;      
+	}	  
+
+	public static void main(String[] args) {
+	   BSTIterator tree = new BSTIterator(root);
+	   tree.root = new Node(4);
+	   tree.root.left = new Node(2);
+	   tree.root.right = new Node(6);
+	   tree.root.left.left = new Node(1);
+	   tree.root.left.right = new Node(3);
+	   tree.root.right.left = new Node(5);
+	   tree.root.right.right = new Node(7);
+	   
+	   BSTIterator i = new BSTIterator(root);
+ 
+	   while (i.hasNext()) {
+		 System.out.print(i.next() + " ");
+	   }
+	}
+  }
