f5e84b6362e365a31417c7a487ff6bff5e5a7cf7
Aditya Gupta <contactadityag@gmail.com>
2017-11-10 00:39:07 -0500

    This is a program to determine if a number is a happy number.
    A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.

--- Numbers/HappyNumber.java
+++ Numbers/HappyNumber.java
@@ -3,35 +3,35 @@ import java.io.*;
 
  public class HappyNumber {
 	 
-	public static boolean isHappy(int number) {
-	  if(number <= 0) {
-		return false;	
-	  }
-	  while(number >= 10) {
-	    int sum = 0;
-	    while (number != 0) {
-	      sum += (number%10)*(number%10);
-	      number /= 10;
-	    }
-	    number = sum;  
+    public static boolean isHappy(int number) {
+       if(number <= 0) {
+	 return false;	
+       }
+       while(number >= 10) {
+	 int sum = 0;
+	 while (number != 0) {
+	    sum += (number%10)*(number%10);
+	    number /= 10;
+	 }
+	 number = sum;  
 		  
-	  }
-	  return number == 1 || number == 7;      
-	}	 
+       }
+       return number == 1 || number == 7;      
+    }	 
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();  
+    public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number: ");
+       int number = input.nextInt();  
 		 		
-	  boolean result = isHappy(number);
+       boolean result = isHappy(number);
 
-	  System.out.println();	
-	  if(result) {
-		System.out.print(number + " is a happy number.");
-	  }
-	  else {
-		System.out.print(number + " is not a happy number.");  
-	  }
-	}
+       System.out.println();	
+       if(result) {
+	 System.out.print(number + " is a happy number.");
+       }
+       else {
+	 System.out.print(number + " is not a happy number.");  
+       }
+    }
  }
