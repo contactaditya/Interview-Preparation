de9d8812393873e6abc8ea14b137f2a805bf003c
Aditya Gupta <contactadityag@gmail.com>
2017-11-10 23:46:52 -0500

    This is a program to find the maximum length of an subarray that appears in both arrays A and B.

--- Dynamic Programming/MaximumLengthOfRepeatedSubarray.java
+++ Dynamic Programming/MaximumLengthOfRepeatedSubarray.java
@@ -17,7 +17,7 @@ import java.io.*;
              dp[i][j] = 0;
            } 
            else {
-        	 if(A[i - 1] == B[j - 1]) {
+             if(A[i - 1] == B[j - 1]) {
                 dp[i][j] = 1 + dp[i - 1][j - 1];
                 max = Math.max(max,dp[i][j]);
              }
@@ -25,34 +25,34 @@ import java.io.*;
          }
        }
        
-	   return max;     
+       return max;     
     }	 
 
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the first array: ");
-	  int number1 = input.nextInt();  
-	  int array1[] = new int[number1];
+    public static void main(String[] args) { 	 
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the first array: ");
+       int number1 = input.nextInt();  
+       int array1[] = new int[number1];
 		
-	  System.out.println();	
-	  System.out.print("Please enter the elements of the first array: ");
-	  for (int i = 0; i < array1.length; i++) {
-	    array1[i] = input.nextInt();
-	  }
+       System.out.println();	
+       System.out.print("Please enter the elements of the first array: ");
+       for (int i = 0; i < array1.length; i++) {
+	  array1[i] = input.nextInt();
+       }
 	  
-	  System.out.println();	
-	  System.out.print("Enter number of elements in the second array: ");
-	  int number2 = input.nextInt();  
-	  int array2[] = new int[number2];
+       System.out.println();	
+       System.out.print("Enter number of elements in the second array: ");
+       int number2 = input.nextInt();  
+       int array2[] = new int[number2];
 		
-	  System.out.println();	
-	  System.out.print("Please enter the elements of the second array: ");
-	  for (int i = 0; i < array2.length; i++) {
-	    array2[i] = input.nextInt();
-	  }
+       System.out.println();	
+       System.out.print("Please enter the elements of the second array: ");
+       for (int i = 0; i < array2.length; i++) {
+	 array2[i] = input.nextInt();
+       }
 			
-      int maxLength = findLength(array1, array2);
-      System.out.println();	   
-	  System.out.println("The maximum length of an subarray that appears in both arrays is: " + maxLength);
-	}
+       int maxLength = findLength(array1, array2);
+       System.out.println();	   
+       System.out.println("The maximum length of an subarray that appears in both arrays is: " + maxLength);
+    }
  }
