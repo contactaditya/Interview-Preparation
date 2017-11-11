c0c9d8d173ed495336b1c4c9720c59fc6f8ff394
Aditya Gupta <contactadityag@gmail.com>
2017-11-11 01:58:54 -0500

    This is a program to find the number of longest increasing subsequence in an unsorted array of integers.

--- Arrays/NumberOfLongestIncreasingSubsequence.java
+++ Arrays/NumberOfLongestIncreasingSubsequence.java
@@ -3,52 +3,52 @@ import java.io.*;
 
   public class NumberOfLongestIncreasingSubsequence {
 	 
-	public static int findNumberOfLIS(int[] array) {
-	   if (array == null || array.length == 0) {
-		  return 0;	  
-	   }
-	   int n = array.length, result = 0, maxLength = 0;
-       int length[] =  new int[n], count[] = new int[n];
-       for(int i = 0; i<n; i++) {
-         length[i] = count[i] = 1;
-         for(int j = 0; j<i; j++) {
-           if(array[i] > array[j]) {
-        	 if(length[i] == length[j] + 1) {
-        	   count[i] += count[j];   
-        	 }
-             if(length[i] < length[j] + 1) {
-        	   length[i] = length[j] + 1;
-        	   count[i] = count[j];
-             }   
-           }
-         }
-         if(maxLength == length[i]) {
-           result += count[i];
-         }
-         if(maxLength < length[i]) {
-           maxLength = length[i];
-           result = count[i];
-         } 
-       }
-	           
-	   return result;      
-	}	  
-
-	public static void main(String[] args) { 	   
-	   Scanner input = new Scanner(System.in);
-       System.out.print("Enter number of elements in the array: ");
-       int numbers = input.nextInt();   
-       int array[] = new int[numbers];
-
-       System.out.print("\n");
-       System.out.print("Please enter the actual values in the array: ");
-       for(int i = 0; i < numbers; i++) {
-         array[i] = input.nextInt(); 
-       }
-      
-       int numberOfLIS = findNumberOfLIS(array);
-      
-       System.out.println();	
-       System.out.println("The number of longest increasing subsequence are: " + numberOfLIS);	
+     public static int findNumberOfLIS(int[] array) {
+	if (array == null || array.length == 0) {
+           return 0;	  
 	}
+	int n = array.length, result = 0, maxLength = 0;
+        int length[] =  new int[n], count[] = new int[n];
+        for(int i = 0; i<n; i++) {
+          length[i] = count[i] = 1;
+          for(int j = 0; j<i; j++) {
+            if(array[i] > array[j]) {
+              if(length[i] == length[j] + 1) {
+        	 count[i] += count[j];   
+              }
+              if(length[i] < length[j] + 1) {
+        	 length[i] = length[j] + 1;
+        	 count[i] = count[j];
+              }   
+            }
+          }
+          if(maxLength == length[i]) {
+            result += count[i];
+          }
+          if(maxLength < length[i]) {
+            maxLength = length[i];
+            result = count[i];
+          } 
+        }
+	           
+	return result;      
+     }	  
+
+     public static void main(String[] args) { 	   
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of elements in the array: ");
+        int numbers = input.nextInt();   
+        int array[] = new int[numbers];
+
+        System.out.print("\n");
+        System.out.print("Please enter the actual values in the array: ");
+        for(int i = 0; i < numbers; i++) {
+          array[i] = input.nextInt(); 
+        }
+      
+        int numberOfLIS = findNumberOfLIS(array);
+       
+        System.out.println();	
+        System.out.println("The number of longest increasing subsequence are: " + numberOfLIS);	
+     }
   }
