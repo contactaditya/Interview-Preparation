e7f98a6ac00a6501339e8735aaa32203758c2976
Aditya Gupta <contactadityag@gmail.com>
2017-11-14 18:11:01 -0500

    This is a program to add two numbers represented by a linked list and returns the sum as a linked list.
    The most significant digit comes first and each of their nodes contain a single digit.

--- Linked List/AddTwoNumbersInALinkedList2.java
+++ Linked List/AddTwoNumbersInALinkedList2.java
@@ -2,23 +2,23 @@ import java.util.*;
 import java.io.*;
 
  public class AddTwoNumbersInALinkedList2 {
-	 private static LinkedListNode head;
-	 private static LinkedListNode head1;
-	 private static LinkedListNode head2;
-	 private static int size;  	 
+     private static LinkedListNode head; 
+     private static LinkedListNode head1; 
+     private static LinkedListNode head2;
+     private static int size;  	 
 
-	 public AddTwoNumbersInALinkedList2() {
-		// this is an empty list, so the reference to the head node is set to a new node with no data				  
-		head = new LinkedListNode(null);
-		size = 0;
-	 }
+     public AddTwoNumbersInALinkedList2() {
+	 // this is an empty list, so the reference to the head node is set to a new node with no data				  
+	 head = new LinkedListNode(null);
+	 size = 0;
+     }
 		
-	public static LinkedListNode addLists(LinkedListNode list1, LinkedListNode list2) {	 
+     public static LinkedListNode addLists(LinkedListNode list1, LinkedListNode list2) {	 
 			
-	  java.util.Stack<Integer> s1 = new java.util.Stack<Integer>();
-	  java.util.Stack<Integer> s2 = new java.util.Stack<Integer>();
+	 java.util.Stack<Integer> s1 = new java.util.Stack<Integer>();
+	 java.util.Stack<Integer> s2 = new java.util.Stack<Integer>();
 	        
-	  while(list1 != null) {
+	 while(list1 != null) {
 	    s1.push((int) list1.data);
 	    list1 = list1.next;
 	  }
@@ -44,30 +44,29 @@ import java.io.*;
 	  }
 	        
 	  return (int)list.data == 0 ? list.next : list; 
-    }
+     } 
 		
-    public static void PrintElementsOfALinkedList(LinkedListNode head) {	 
-	  LinkedListNode current = head; 		    
-	  while(current != null) {	   
-		System.out.print(current.getData() + " ");	 	
-		current = current.getNext();
-	  }
-	}  
+     public static void PrintElementsOfALinkedList(LinkedListNode head) {	 
+	 LinkedListNode current = head; 		    
+	 while(current != null) {	   
+            System.out.print(current.getData() + " ");	 	
+	    current = current.getNext();
+	 }
+      }  
 
-	public static void main(String[] args) {
-	  AddTwoNumbersInALinkedList2 list = new AddTwoNumbersInALinkedList2();
-	  list.head1 = new LinkedListNode(7);
-	  list.head1.next = new LinkedListNode(1);
-	  list.head1.next.next = new LinkedListNode(6);
+      public static void main(String[] args) {
+	 AddTwoNumbersInALinkedList2 list = new AddTwoNumbersInALinkedList2();
+	 list.head1 = new LinkedListNode(7);
+	 list.head1.next = new LinkedListNode(1);
+	 list.head1.next.next = new LinkedListNode(6);
 		  
-	  list.head2 = new LinkedListNode(5);
-	  list.head2.next = new LinkedListNode(9);
-	  list.head2.next.next = new LinkedListNode(2);
+	 list.head2 = new LinkedListNode(5);
+	 list.head2.next = new LinkedListNode(9);
+	 list.head2.next.next = new LinkedListNode(2);
 		   	  
-	  LinkedListNode current = addLists(head1, head2);
+	 LinkedListNode current = addLists(head1, head2);
 		  
-	  System.out.print("Contents of the linked list which contains the sum of the two linked lists are: ");
-	  PrintElementsOfALinkedList(current);	
-	}
-
+	 System.out.print("Contents of the linked list which contains the sum of the two linked lists are: ");
+	 PrintElementsOfALinkedList(current);	
+      }
   }
