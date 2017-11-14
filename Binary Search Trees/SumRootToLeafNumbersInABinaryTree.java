ece63a4bb5c191cce9792d24634b76d89f090729
Aditya Gupta <contactadityag@gmail.com>
2017-11-14 02:09:25 -0500

    This is a program to find the sum of each root-to-leaf path that could represent a number in a binary tree containing digits from 0-9 only.

--- Binary Search Trees/SumRootToLeafNumbersInABinaryTree.java
+++ Binary Search Trees/SumRootToLeafNumbersInABinaryTree.java
@@ -2,35 +2,35 @@ import java.util.*;
 import java.io.*;
 
   public class SumRootToLeafNumbersInABinaryTree { 	
-	private static Node root;	  
-	long pathSum = 0;
+    private static Node root;	  
+    long pathSum = 0;
     
-	public int sumNumbers(Node root) {
-	   sumNumbersHelper(root, 0);	        
-	   return (int)pathSum;	
-	} 
+    public int sumNumbers(Node root) {
+       sumNumbersHelper(root, 0);	        
+       return (int)pathSum;	
+    } 
 	
-	private void sumNumbersHelper(Node root, long currentSum) {
-	   if (root == null) {
-	      return;
-	   }	
+    private void sumNumbersHelper(Node root, long currentSum) {
+       if (root == null) {
+	  return;
+       }	
 		
-	   currentSum = currentSum * 10 + root.data;
-	   if (root.left == null && root.right == null) {
+       currentSum = currentSum * 10 + root.data;
+       if (root.left == null && root.right == null) {
           pathSum += currentSum;
        }
 	   
-	   sumNumbersHelper(root.left, currentSum);
-	   sumNumbersHelper(root.right, currentSum);	
-	}
+       sumNumbersHelper(root.left, currentSum);
+       sumNumbersHelper(root.right, currentSum);	
+    }
 
-	public static void main(String[] args) {    
-	  SumRootToLeafNumbersInABinaryTree tree = new SumRootToLeafNumbersInABinaryTree();
-	  tree.root = new Node(1);
-	  tree.root.left = new Node(2);
-	  tree.root.right = new Node(3);
+    public static void main(String[] args) {    
+       SumRootToLeafNumbersInABinaryTree tree = new SumRootToLeafNumbersInABinaryTree();
+       tree.root = new Node(1);
+       tree.root.left = new Node(2);
+       tree.root.right = new Node(3);
 
-	  int sum = tree.sumNumbers(tree.root);		
-	  System.out.print("The total sum of all root-to-leaf numbers is: " + sum);
-	}
+       int sum = tree.sumNumbers(tree.root);		
+       System.out.print("The total sum of all root-to-leaf numbers is: " + sum);
+    }
   }
