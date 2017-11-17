32b79ee0837880f4a94842ee8a0129afcac436ba
Aditya Gupta <contactadityag@gmail.com>
2017-11-17 00:42:44 -0500

    This is a program to solve the equation and find if the equation has no solution, infinite solution and unique solution.

--- Strings/SolveTheEquation.java
+++ Strings/SolveTheEquation.java
@@ -3,7 +3,7 @@ import java.io.*;
 
   public class SolveTheEquation {
 	  
-	public static String solveEquation(String equation) {
+    public static String solveEquation(String equation) {
        String[] lr = equation.split("=");	
        int lhs = 0, rhs = 0;
        for (String x: breakIt(lr[0])) {
@@ -31,11 +31,11 @@ import java.io.*;
          }
        }
        // In case of a unique solution, the ratio of the effective rhs and lhs gives the required result
-	   return "x=" + rhs/lhs;      
-	}	  
+       return "x=" + rhs/lhs;      
+    }	  
 
-	private static List<String> breakIt(String string) {
-	   List<String> result = new ArrayList<String>();
+    private static List<String> breakIt(String string) {
+       List<String> result = new ArrayList<String>();
        String r = "";
        for (int i = 0; i < string.length(); i++) {
     	 if (string.charAt(i) == '+' || string.charAt(i) == '-') {
@@ -50,24 +50,24 @@ import java.io.*;
        
        result.add(r);
        return result;
-	}
+    }
 
-	private static String coefficient(String x) {
-	   if (x.length() > 1 && x.charAt(x.length() - 2) >= '0' && x.charAt(x.length() - 2) <= '9') {
+    private static String coefficient(String x) {
+       if (x.length() > 1 && x.charAt(x.length() - 2) >= '0' && x.charAt(x.length() - 2) <= '9') {
           return x.replace("x", "");
-	   }
+       }
 		
-	   return x.replace("x", "1");
-	}
+       return x.replace("x", "1");
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  String equation = new String();	
-	  System.out.print("Enter the equation: ");
-	  equation = input.nextLine();  
-	  String solution = solveEquation(equation); 
-	  System.out.println();
-	  System.out.println("The solution of the equation " + equation + " is: " + solution);
+    public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       String equation = new String();	
+       System.out.print("Enter the equation: ");
+       equation = input.nextLine();  
+       String solution = solveEquation(equation); 
+       System.out.println();
+       System.out.println("The solution of the equation " + equation + " is: " + solution);
 	  
-	}
+    }
   }
