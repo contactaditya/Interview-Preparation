eb6959e39b54e4e14b6566bf0902fd1573dd7db9
Aditya Gupta <contactadityag@gmail.com>
2017-11-20 04:50:05 -0500

    This is a program to caclculate the lowest price you have to pay for exactly certain items as given.
    Make optimal use of the special offers.

--- Arrays/ShoppingOffers.java
+++ Arrays/ShoppingOffers.java
@@ -7,9 +7,9 @@ import java.io.*;
        return shopping(price, special, needs);
     }	  
 
-	private static int shopping(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
-	   int j = 0, result = buyWithoutSpecial(needs, price); 	
-	   for (List<Integer> s: special) {
+    private static int shopping(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+       int j = 0, result = buyWithoutSpecial(needs, price); 	
+       for (List<Integer> s: special) {
          List<Integer> temp = new ArrayList<Integer>(needs);
          for (j = 0; j < needs.size(); j++) {
            int difference = temp.get(j) - s.get(j);
@@ -23,54 +23,54 @@ import java.io.*;
          }
        }
 	
-	   return result;
-	}
+       return result;
+    }
 	
-	public static int buyWithoutSpecial(List<Integer> price, List<Integer> needs) {
-      int sum = 0;
-      for (int i = 0; i < needs.size(); i++) {
-        sum += needs.get(i) * price.get(i);
-      }
-      return sum;
+    public static int buyWithoutSpecial(List<Integer> price, List<Integer> needs) {
+       int sum = 0;
+       for (int i = 0; i < needs.size(); i++) {
+         sum += needs.get(i) * price.get(i);
+       }
+       return sum;
     }
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of items to sell: ");
-	  int numberofItems = input.nextInt();   
-	  List<Integer> price = new ArrayList<Integer>();
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of items to sell: "); 
+      int numberofItems = input.nextInt();   
+      List<Integer> price = new ArrayList<Integer>();
 	    
-	  System.out.println();
-	  System.out.print("Please enter the actual prices of the items: ");
-	  for (int i = 0; i < numberofItems; i++) {
-		price.add(input.nextInt());
-	  }
+      System.out.println();
+      System.out.print("Please enter the actual prices of the items: ");
+      for (int i = 0; i < numberofItems; i++) {
+	price.add(input.nextInt());
+      }
 	  
-	  System.out.println();
-	  System.out.print("Enter number of special offers: ");
-	  int numberofSpecialOffers = input.nextInt();   
+      System.out.println();
+      System.out.print("Enter number of special offers: ");
+      int numberofSpecialOffers = input.nextInt();   
 	   
-	  List<List<Integer>> special = new ArrayList<List<Integer>>();
-	  for (int i = 0; i < numberofSpecialOffers; i++) {
-		List<Integer> offers = new ArrayList<Integer>();  
-	    System.out.println(); 
-	    System.out.print("Please enter how many specific items you can buy using a special offer with the last number representing the price you need to pay for this offer: ");	  
-	    for (int j = 0; j < numberofItems+1; j++) {
-		  offers.add(input.nextInt());
-	    }
-	    special.add(offers);
-	  }
-	  
-	  List<Integer> needs = new ArrayList<Integer>();
-	  System.out.println();
-	  System.out.print("Please enter the number which we need to buy for each item: ");
-	  for (int i = 0; i < numberofItems; i++) {
-		needs.add(input.nextInt());
-	  }
-	  	  
-	  int result = shoppingOffers(price, special, needs); 
-	  System.out.println();
-	  System.out.println("The lowest price you have to pay for exactly " + numberofItems + " items where we take optimal use of special offers is: " + result);
-	
+      List<List<Integer>> special = new ArrayList<List<Integer>>();
+      for (int i = 0; i < numberofSpecialOffers; i++) {
+	List<Integer> offers = new ArrayList<Integer>();  
+	System.out.println(); 
+	System.out.print("Please enter how many specific items you can buy using a special offer with the last number representing the price you need to pay for this offer: ");	  
+	for (int j = 0; j < numberofItems+1; j++) {
+          offers.add(input.nextInt());
 	}
+	special.add(offers);
+      }
+	  
+      List<Integer> needs = new ArrayList<Integer>();
+      System.out.println();
+      System.out.print("Please enter the number which we need to buy for each item: ");
+      for (int i = 0; i < numberofItems; i++) {
+	needs.add(input.nextInt());
+      }
+	  	  
+      int result = shoppingOffers(price, special, needs); 
+      System.out.println();
+      System.out.println("The lowest price you have to pay for exactly " + numberofItems + " items where we take optimal use of special offers is: " + result);
+	
+    }
   }
