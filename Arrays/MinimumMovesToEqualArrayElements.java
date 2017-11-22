dca52c8f5b8d6425b3f8ccd83ceb1ca8c9d682b0
Aditya Gupta <contactadityag@gmail.com>
2017-11-21 23:41:19 -0500

    This is a program to find the minimum number of moves required to make all array elements equal.
    A move is incrementing n - 1 elements by 1.

--- Arrays/MinimumMovesToEqualArrayElements.java
+++ Arrays/MinimumMovesToEqualArrayElements.java
@@ -1,35 +1,35 @@
 import java.util.*;
 import java.io.*;
 
-  public class MinimumMovesToEqualArrayElements {
+ public class MinimumMovesToEqualArrayElements {
 	  
-	public static int minMoves(int[] array) {
-	   int min = array[0], sum = 0;
-       for (int n : array) {
-    	 sum += n;
-    	 if (n < min) {
-    	   min = n;
-    	 }
-       }
+   public static int minMoves(int[] array) {
+      int min = array[0], sum = 0;
+      for (int n : array) {
+    	sum += n;
+    	if (n < min) {
+    	  min = n;
+    	}
+      }
 		
-	   return sum - array.length * min;     
-	}  
+      return sum - array.length * min;     
+   }  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
+   public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int numbers = input.nextInt();   
       int array[] = new int[numbers];
   
       System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-	    array[i] = input.nextInt();
-	  }
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt();
+      }
 	  
-	  int minimumNumberOfMoves = minMoves(array);
+      int minimumNumberOfMoves = minMoves(array);
 	
-	  System.out.println();
-	  System.out.print("The minimum number of moves required to make all array elements equal are: " + minimumNumberOfMoves);
-	}
-  }
+      System.out.println();
+      System.out.print("The minimum number of moves required to make all array elements equal are: " + minimumNumberOfMoves);
+   }
+ }
