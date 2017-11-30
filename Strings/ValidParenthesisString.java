07d68efec31bcdaa2a4ef7dee6836df9aad3f8a7
contactaditya <contactadityag@gmail.com>
2017-11-30 04:33:35 -0500

    This is a program to check whether the given string is valid.
    We define the validity of a string by these rules:
    
    1) Any left parenthesis '(' must have a corresponding right parenthesis
    ')'.
    
    2) Any right parenthesis ')' must have a corresponding left parenthesis
    '('.
    
    3) Left parenthesis '(' must go before the corresponding right
    parenthesis ')'.
    
    4) '*' could be treated as a single right parenthesis ')' or a single
    left parenthesis '(' or an empty string.
    
    5) An empty string is also valid.

---
+++ Strings/ValidParenthesisString.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+ public class ValidParenthesisString {
+	 
+	public static boolean checkValidString(String string) {
+	   int low = 0, high = 0;	
+	   for (char c: string.toCharArray()) {
+         low += c == '(' ? 1 : -1;
+         high += c != ')' ? 1 : -1;
+         if (high < 0) {
+           break;
+         }
+         low = Math.max(low, 0);
+       }
+       return low == 0;   
+	}	 
+
+	public static void main(String[] args) {    
+	   String string = new String();	  	
+	   Scanner input = new Scanner(System.in);
+	   System.out.print("Enter the string: ");
+	   string = input.nextLine(); 
+		  
+	   System.out.println();
+	   if(checkValidString(string)) {
+		 System.out.println("The string " + string + " is a valid string.");
+	   }
+	   else {
+		 System.out.println("The string " + string + " is not a valid string.");  
+	   }
+	}
+ }
