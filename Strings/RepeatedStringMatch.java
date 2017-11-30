72b83b7e2f5121c00ab8071779a79e9cf1921c4e
Aditya Gupta <contactadityag@gmail.com>
2017-11-30 04:40:56 -0500

    This is a program to find the minimum number of times string1 has to be repeated such that string2 is a substring of it.

--- Strings/RepeatedStringMatch.java
+++ Strings/RepeatedStringMatch.java
@@ -3,34 +3,33 @@ import java.io.*;
 
  public class RepeatedStringMatch {
 	 
-	public static int repeatedStringMatch(String A, String B) {
-	  StringBuilder string = new StringBuilder();
-	  string.append(A);
-      int count = 1;
-      while(string.indexOf(B) < 0) {
+    public static int repeatedStringMatch(String A, String B) {
+       StringBuilder string = new StringBuilder();
        string.append(A);
-       if(string.length() > B.length() && string.indexOf(B) < 0) {
-         return -1;
+       int count = 1;
+       while(string.indexOf(B) < 0) {
+        string.append(A);
+        if(string.length() > B.length() && string.indexOf(B) < 0) {
+          return -1;
+        }
+        count++;   
        }
-       count++;   
-      }
-      return count;  
-	}	 
+       return count;  
+    }	 
 
-	public static void main(String[] args) {  
-	  String A = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the first string: ");
-	  A = input.next();  
+    public static void main(String[] args) {  
+       String A = new String();	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the first string: ");
+       A = input.next();  
 	   
-	  System.out.println();	
-	  String B = new String();	  
-	  System.out.print("Enter the second string: ");
-	  B = input.next();
+       System.out.println();	
+       String B = new String();	  
+       System.out.print("Enter the second string: ");
+       B = input.next();
 	  
-	  int minimumNumberOfTimes = repeatedStringMatch(A, B);
-	  System.out.println();	
-	  System.out.println("The minimum number of times " + A + " has to be repeated such that " + B + " is a substring of it is: " + minimumNumberOfTimes);	
-
-	}
+       int minimumNumberOfTimes = repeatedStringMatch(A, B);
+       System.out.println();	
+       System.out.println("The minimum number of times " + A + " has to be repeated such that " + B + " is a substring of it is: " + minimumNumberOfTimes);	
+    }
  }
