127f1f7c4b19539b2afe9900b4520bbf994503de
Aditya Gupta <contactadityag@gmail.com>
2017-12-06 08:09:38 -0500

    This is a program to find the pivot index of the array.
    The pivot index is the index where the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index.

--- Arrays/FindPivotIndex.java
+++ Arrays/FindPivotIndex.java
@@ -3,8 +3,8 @@ import java.io.*;
 
  public class FindPivotIndex {
 	 
-	public static int pivotIndex(int[] array) {
-	  int sum = 0, leftsum = 0;
+    public static int pivotIndex(int[] array) {
+      int sum = 0, leftsum = 0;
       for (int x: array) {
     	sum += x;
       }
@@ -17,20 +17,20 @@ import java.io.*;
       return -1;   
     }	 
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+    public static void main(String[] args) { 	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int number = input.nextInt();  
+       int array[] = new int[number];
 			
-	  System.out.println();	
-	  System.out.print("Please enter the elements in the array: ");
-	  for (int i = 0; i < array.length; i++) {
-		array[i] = input.nextInt();
-	  }
+       System.out.println();	
+       System.out.print("Please enter the elements in the array: ");
+       for (int i = 0; i < array.length; i++) {
+	 array[i] = input.nextInt();
+       }
 					
-	  int pivotIndex = pivotIndex(array);
-	  System.out.println(); 
-	  System.out.println("The pivot index of the array is: " + pivotIndex);
-	}
+       int pivotIndex = pivotIndex(array);
+       System.out.println(); 
+       System.out.println("The pivot index of the array is: " + pivotIndex);
+    }
  }
