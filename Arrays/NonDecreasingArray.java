4d7f6c5c3bdbb47a4f52ba1cff0cf2d257550754
Aditya Gupta <contactadityag@gmail.com>
2017-12-06 08:24:10 -0500

    This is a program to check if it could become non-decreasing by modifying at most 1 element in an array.
    We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).

--- Arrays/NonDecreasingArray.java
+++ Arrays/NonDecreasingArray.java
@@ -3,41 +3,41 @@ import java.io.*;
 
  public class NonDecreasingArray {
 	 
-	public static boolean checkPossibility(int[] array) {
-	   int count = 0;                                                                   
-	   for(int i = 0; i < array.length - 1; i++) {
-	     if(array[i] > array[i+1]) {
-	       count++;
-	       if(i > 0 && array[i+1] < array[i-1]) {
-	    	 array[i+1] = array[i];                    
-	       }
-	       else {
-	    	 array[i] = array[i+1];                                               
-	       }
-	     }
+    public static boolean checkPossibility(int[] array) {
+       int count = 0;                                                                   
+       for(int i = 0; i < array.length - 1; i++) {
+         if(array[i] > array[i+1]) {
+	   count++;
+	   if(i > 0 && array[i+1] < array[i-1]) {
+	     array[i+1] = array[i];                    
 	   }
-	   return count<=1;  	
-	}	 
+	   else {
+	     array[i] = array[i+1];                                               
+	   }
+	 }
+       }
+       return count<=1;  	
+    }	 
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int numbers = input.nextInt();   
+       int array[] = new int[numbers];
 
-	  System.out.println();	
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt();
-	  }
+       System.out.println();	
+       System.out.print("Please enter the actual values in the array: ");
+       for (int i = 0; i < numbers; i++) {
+	 array[i] = input.nextInt();
+       }
 	
-	  boolean result = checkPossibility(array); 
-	  System.out.println();
-	  if(result) {
-		System.out.println("The given array can become non-decreasing by modifying at most 1 element.");  
-	  }
-	  else {
-		System.out.println("The given array cannot become a non-decreasing by modifying at most 1 element.");  
-	  }
-	}
+       boolean result = checkPossibility(array); 
+       System.out.println();
+       if(result) {
+	 System.out.println("The given array can become non-decreasing by modifying at most 1 element.");  
+       }
+       else {
+	 System.out.println("The given array cannot become a non-decreasing by modifying at most 1 element.");  
+       }
+    }
  }
