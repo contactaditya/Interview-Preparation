a859de2a203680f1c8c84bb1791751def176287b
Aditya Gupta <contactadityag@gmail.com>
2017-12-07 14:07:38 -0500

    This is a program to find a list of every possible self dividing number including the upper and lower number bounds.
    A self-dividing number is a number that is divisible by every digit it contains.

--- Arrays/SelfDividingNumbers.java
+++ Arrays/SelfDividingNumbers.java
@@ -10,33 +10,33 @@ import java.io.*;
            answer.add(number);
          }
        }
- 	   return answer;     
-	}	 
+       return answer;     
+    }	 
 
-	private static boolean selfDividing(int number) {
-	  int x = number;
-	  while (x > 0) {
-	    int d = x % 10;
-	    x /= 10;
-	    if (d == 0 || (number % d) > 0) {
-	      return false;
-	    }
-	  }
-	  return true;
-	}
+    private static boolean selfDividing(int number) {
+       int x = number;
+       while (x > 0) {
+	 int d = x % 10;
+	 x /= 10;
+	 if (d == 0 || (number % d) > 0) {
+	   return false;
+	 }
+       }
+       return true;
+    }
 
-	public static void main(String[] args) { 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the lower bound for the numbers: ");
-	  int left = input.nextInt();  
+    public static void main(String[] args) { 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the lower bound for the numbers: ");
+      int left = input.nextInt();  
 		
-	  System.out.println();	
-	  System.out.print("Enter the upper bound for the numbers: ");
-	  int right = input.nextInt();  
+      System.out.println();	
+      System.out.print("Enter the upper bound for the numbers: ");
+      int right = input.nextInt();  
 		  
-	  List<Integer> result = new ArrayList<Integer>();	
-	  result = selfDividingNumbers(left, right); 
-	  System.out.println();
-	  System.out.println("The list of every possible self dividing number including the lower and upper number bound is: " + result);
-	}
+      List<Integer> result = new ArrayList<Integer>();	
+      result = selfDividingNumbers(left, right); 
+      System.out.println();
+      System.out.println("The list of every possible self dividing number including the lower and upper number bound is: " + result);
+    }
  }
