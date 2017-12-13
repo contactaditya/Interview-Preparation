61b7927c8327eff26bb062b508e6fcade16ca409
Aditya Gupta <contactadityag@gmail.com>
2017-12-13 09:42:27 -0500

    This is a program to count and return the number of maximum integers in the matrix after performing all the operations in the matrix.

--- Arrays/RangeAdditionII.java
+++ Arrays/RangeAdditionII.java
@@ -3,9 +3,9 @@ import java.io.*;
 
  public class RangeAdditionII {
 	 
-	public static int maxCount(int m, int n, int[][] operations) {
-	  for(int k = 0; k < operations.length; k++) {
-	    if(operations[k][0] < m){
+    public static int maxCount(int m, int n, int[][] operations) {
+      for(int k = 0; k < operations.length; k++) {
+	if(operations[k][0] < m){
           m = operations[k][0];
         }
         if(operations[k][1] < n){
@@ -13,33 +13,32 @@ import java.io.*;
         }
       }
       return m * n;    
-	}	
+    }	
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Please enter the number of rows in the matrix: ");
-	  int m = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Please enter the number of columns in the matrix: ");
-	  int n = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Please enter the lower range of operations: ");
-	  int a = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Please enter the higher range of operations: ");
-	  int b = input.nextInt();  
-	  int[][] array = new int[a][b];
-	  System.out.println();
-	  System.out.println("Please enter the actual values in the matrix: ");
-	  for(int i=0; i < a; i++) {
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Please enter the number of rows in the matrix: ");
+      int m = input.nextInt();  
+      System.out.println();
+      System.out.print("Please enter the number of columns in the matrix: ");
+      int n = input.nextInt();  
+      System.out.println();
+      System.out.print("Please enter the lower range of operations: ");
+      int a = input.nextInt();  
+      System.out.println();
+      System.out.print("Please enter the higher range of operations: ");
+      int b = input.nextInt();  
+      int[][] array = new int[a][b];
+      System.out.println();
+      System.out.println("Please enter the actual values in the matrix: ");
+      for(int i=0; i < a; i++) {
         for(int j=0; j < b; j++){
           array[i][j] = input.nextInt();
         }
       }
 	  
-	  int maxCount = maxCount(m, n, array);
-	  System.out.println();
-	  System.out.println("The number of maximum integers in the matrix after performing all the operations are: " + maxCount);	
-
-	}
+      int maxCount = maxCount(m, n, array);
+      System.out.println();
+      System.out.println("The number of maximum integers in the matrix after performing all the operations are: " + maxCount);	
+    }
  }
