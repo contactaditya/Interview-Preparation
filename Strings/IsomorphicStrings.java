bd07c15a07e2e103897a35c95d0dcbe53e42eb47
Aditya Gupta <contactadityag@gmail.com>
2017-12-16 08:49:23 -0500

    This is a program to determine if two strings s and t are isomorphic.

--- Strings/IsomorphicStrings.java
+++ Strings/IsomorphicStrings.java
@@ -3,10 +3,10 @@ import java.io.*;
 
   public class IsomorphicStrings {
 	
-	public static boolean isIsomorphic(String s, String t) { 
-	  if(s == null || t == null) {
+   public static boolean isIsomorphic(String s, String t) { 
+      if(s == null || t == null) {
         return false;
-	  }
+      }
  
       if(s.length() != t.length()) {
     	return false;	  
@@ -30,25 +30,25 @@ import java.io.*;
       }  
  
       return true;      
-	}	  
+   }	  
 
-	public static void main(String[] args) { 
-	  String s = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the first string: ");
-	  s = input.nextLine();  
-	  System.out.println();	
-	  String t = new String();	  
-	  System.out.print("Enter the second string: ");
-	  t = input.nextLine();
+   public static void main(String[] args) { 
+      String s = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the first string: "); 
+      s = input.nextLine();   
+      System.out.println();	
+      String t = new String();	  
+      System.out.print("Enter the second string: ");
+      t = input.nextLine();
 		  
-	  boolean isIsomorphic = isIsomorphic(s, t);
-	  System.out.println();
-	  if(isIsomorphic) {
-		System.out.println("Yes the strings " + s + " and " + t + " are isomorphic.");  
-	  }
+      boolean isIsomorphic = isIsomorphic(s, t);
+      System.out.println();
+      if(isIsomorphic) {
+	System.out.println("Yes the strings " + s + " and " + t + " are isomorphic.");  
+      }
       else {
     	System.out.println("No the strings " + s + " and " + t + " are not isomorphic.");   
-	  }
-	}
-  }
+      }
+   }
+ }
