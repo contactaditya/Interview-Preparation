8b56095c567bcd663932fc277c84dc6d6919c166
Aditya Gupta <contactadityag@gmail.com>
2017-12-16 08:57:41 -0500

    This is a program to find the longest uncommon subsequence of this group of two strings.
    The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this subsequence should not be any subsequence of the other strings.
    
    A subsequence is a sequence that can be derived from one sequence by deleting some characters without changing the order of the remaining elements. Trivially, any string is a subsequence of itself and an empty string is a subsequence of any string.

--- Strings/LongestUncommonSubsequenceI.java
+++ Strings/LongestUncommonSubsequenceI.java
@@ -3,16 +3,16 @@ import java.io.*;
 
  public class LongestUncommonSubsequenceI {
 	
-	public static int findLUSlength(String a, String b) {
-	   if (a.equals(b)) {
-	     return -1;
-	   }
-	   return Math.max(a.length(), b.length());  
-	}	 
+    public static int findLUSlength(String a, String b) {
+      if (a.equals(b)) {
+	 return -1;
+      }
+      return Math.max(a.length(), b.length());  
+    }	 
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);	
-	  String string1 = new String();	  
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);	
+      String string1 = new String();	  
       System.out.print("Enter the first string: ");
       string1 = input.next();  
 	  
@@ -26,5 +26,5 @@ import java.io.*;
       System.out.println();	
       System.out.println("The longest uncommon subsequence of this group of two strings is: " + longestUncommonSubsequence);	
 
-	}
+    }
  }
