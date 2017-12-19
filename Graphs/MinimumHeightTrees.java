694a91307c062b38975bab732ef13a25ef79283a
Aditya Gupta <contactadityag@gmail.com>
2017-12-19 11:29:38 -0500

    This is a program to find all the MHTs and return a list of their root labels.
    Among all possible rooted trees, those with minimum height are called minimum height trees (MHTs).

--- Graphs/MinimumHeightTrees.java
+++ Graphs/MinimumHeightTrees.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
  public class MinimumHeightTrees { 	
-	Node root;	    
+    Node root;	    
     Node previous; 
     
     public static List<Integer> findMinHeightTrees(int n, int[][] edges) {
@@ -53,28 +53,28 @@ import java.io.*;
       return leaves;        
     }
 
-	public static void main(String[] args) {   
+    public static void main(String[] args) {   
       Scanner input = new Scanner(System.in);
-	  System.out.print("Please enter the number of nodes in the graph: ");
-	  int n = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Please enter the number of rows in the list of undirected edges: ");
-	  int x = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Please enter the number of columns in the list of undirected edges: ");
-	  int y = input.nextInt();  
-	  int[][] edges = new int[x][y];
-	  System.out.println();
-	  System.out.println("Please enter the actual values in the matrix: ");
-	  for(int i=0; i < x; i++) {
+      System.out.print("Please enter the number of nodes in the graph: ");
+      int n = input.nextInt();  
+      System.out.println();
+      System.out.print("Please enter the number of rows in the list of undirected edges: ");
+      int x = input.nextInt();  
+      System.out.println();
+      System.out.print("Please enter the number of columns in the list of undirected edges: ");
+      int y = input.nextInt();  
+      int[][] edges = new int[x][y];
+      System.out.println();
+      System.out.println("Please enter the actual values in the matrix: ");
+      for(int i=0; i < x; i++) {
         for(int j=0; j < y; j++) {
           edges[i][j] = input.nextInt();
         }
       }
 	  
-	  List<Integer> result = new ArrayList<Integer>();
-	  System.out.println();
-	  result = findMinHeightTrees(n, edges); 
-	  System.out.println("A list of the root labels of all the minimum height trees is as follows: " + result);
-	}
+      List<Integer> result = new ArrayList<Integer>();
+      System.out.println();
+      result = findMinHeightTrees(n, edges); 
+      System.out.println("A list of the root labels of all the minimum height trees is as follows: " + result);
+    }
  }
