2da8e143c0af9c465a7a724ae792720d1f39d975
contactaditya <contactadityag@gmail.com>
2017-12-19 11:14:51 -0500

    This is a program to find the tilt of the whole binary tree.
    The tilt of a tree node is defined as the absolute difference between
    the sum of all left subtree node values and the sum of all right subtree
    node values. Null node has tilt 0.

---
+++ Binary Search Trees/BinaryTreeTilt.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+  public class BinaryTreeTilt {
+	Node root;	    
+    Node previous; 
+    static int tilt = 0;
+	  
+    public static int findTilt(Node root) {
+       traverse(root);
+       return tilt;     
+	}  
+ 
+	private static int traverse(Node root) {
+	   if(root==null) {
+          return 0;
+	   }
+       int left = traverse(root.left);
+       int right = traverse(root.right);
+       tilt += Math.abs(left-right);
+       return left + right + root.data;
+	}
+
+	public static void main(String[] args) {   
+	  BinaryTreeTilt tree = new BinaryTreeTilt();
+	  tree.root = new Node(1);
+	  tree.root.left = new Node(2);
+	  tree.root.right = new Node(3);
+	
+	  int tilt = findTilt(tree.root); 
+	  System.out.println("The tilt of the whole binary tree is: " + tilt);
+	  
+	}
+  }
