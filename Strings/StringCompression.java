70c55b3678441a04dc911a8557ed1d841f25c398
Aditya Gupta <contactadityag@gmail.com>
2017-12-21 18:54:21 -0500

    This is a program to compress an array of characters in-place and return the new length of the array.
    The length after compression must always be smaller than or equal to the original array.

--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -3,8 +3,8 @@ import java.io.*;
 
  public class StringCompression {
 	 
-	public static int compress(char[] chars) {  
-	  int start = 0;
+   public static int compress(char[] chars) {  
+      int start = 0;
       for (int end = 0, count = 0; end < chars.length; end++) {
     	count++;  
         if (end == chars.length - 1 || chars[end] != chars[end + 1]) {
@@ -12,7 +12,7 @@ import java.io.*;
           chars[start] = chars[end]; // Update the character at start pointer
           start++;
           if (count != 1) {
-        	// Copy over the character count to the array
+            // Copy over the character count to the array
             char[] array = String.valueOf(count).toCharArray();
             for(int i=0; i < array.length; i++,start++) {
               chars[start] = array[i];
@@ -23,23 +23,23 @@ import java.io.*;
         }
       }
       return start;      
-	}
+   }
 
-	public static void main(String[] args) {
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of characters in the array: ");
-	  int number = input.nextInt();  
-	  char array[] = new char[number];
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of characters in the array: ");
+      int number = input.nextInt();  
+      char array[] = new char[number];
 		  
-	  System.out.println();	
-	  System.out.print("Please enter the actual characters: ");
-	  for (int i = 0; i < array.length; i++) {
-		array[i] = input.next().charAt(0);
-	  }
+      System.out.println();	
+      System.out.print("Please enter the actual characters: ");
+      for (int i = 0; i < array.length; i++) {
+	array[i] = input.next().charAt(0);
+      }
 		    	   	
-	  int newLengthOfArray = compress(array);
-	  System.out.println();	
-	  System.out.print("The new length of the array after compression will be: " + newLengthOfArray);
+      int newLengthOfArray = compress(array);
+      System.out.println();	
+      System.out.print("The new length of the array after compression will be: " + newLengthOfArray);
 	  
-	}
+   }
  }
