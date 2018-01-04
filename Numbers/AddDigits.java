c2aba2ecfed6fdabb26ecdcf0c76c398f6cd2cd4
Aditya Gupta <contactadityag@gmail.com>
2018-01-04 04:48:54 -0500

    This is a program to repeatedly add all the digits of a number until the result has only one digit.

--- Numbers/AddDigits.java
+++ Numbers/AddDigits.java
@@ -3,31 +3,30 @@ import java.io.*;
 
   public class AddDigits { 	
 	  
-	public static int addDigits(int number) {
-	  return (number - 1) % 9 + 1;
-	}	
+    public static int addDigits(int number) {
+      return (number - 1) % 9 + 1;
+    }	
 	  
-	public static int addDigits1(int number) {
-	  if(number < 10) {
-		return number;
-	  }
-      else if(number%9 == 0) {
-		return 9;
+    public static int addDigits1(int number) {
+      if(number < 10) {
+	return number;
       }
-	  else {
-	    return number%9; 
-	  }     
-	}	  
+      else if(number%9 == 0) {
+	return 9;
+      }
+      else {
+	return number%9; 
+      }     
+    }	  
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();  
-	  System.out.println();	
+    public static void main(String[] args) {  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number: ");
+       int number = input.nextInt();  
+       System.out.println();	
 
-	  int result = addDigits(number);
-	  System.out.println();	
-	  System.out.print("The number after repeatedly adding all its digits until the result has only one digit is: " + result);
-	}
-
+       int result = addDigits(number);
+       System.out.println();	
+       System.out.print("The number after repeatedly adding all its digits until the result has only one digit is: " + result);
+    }
   }
