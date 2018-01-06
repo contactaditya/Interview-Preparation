0db9388a8c1eb6ba43313325f600644a03a544ad
Aditya Gupta <contactadityag@gmail.com>
2018-01-06 10:35:10 -0500

    This is a program to check whether the given array is a permutation.

--- Codility/PermCheck.java
+++ Codility/PermCheck.java
@@ -3,41 +3,41 @@ import java.io.*;
 
   public class PermCheck {
 	  
-	public static int isAPermutation(int array[], int N) {
-	   boolean seen[] = new boolean[array.length + 1];
-	   for (int i = 0; i < array.length; i++) {
-	     if(array[i] < 1 || array[i] > array.length) {
-	       return 0;
-	     }
-		 if(seen[array[i]] == true) {
-		   return 0;
-		 }
-		 else {
-		   seen[array[i]] = true;
-		 }
+      public static int isAPermutation(int array[], int N) {
+	 boolean seen[] = new boolean[array.length + 1];
+	 for (int i = 0; i < array.length; i++) {
+	   if(array[i] < 1 || array[i] > array.length) {
+	     return 0;
 	   }
+	   if(seen[array[i]] == true) {
+	     return 0;
+	   }
+           else {
+	     seen[array[i]] = true;
+	   }
+	 }
 	   
-	   return 1;  
-	}
+	 return 1;  
+      }
 
-	public static void main(String[] args) { 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+      public static void main(String[] args) { 
+	 Scanner input = new Scanner(System.in);
+	 System.out.print("Enter number of elements in the array: ");
+	 int numbers = input.nextInt();   
+	 int array[] = new int[numbers];
 					    
-	  System.out.println();	
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+	 System.out.println();	
+	 System.out.print("Please enter the actual values in the array: ");
+	 for (int i = 0; i < numbers; i++) {
+	   array[i] = input.nextInt(); 
+	 }
 	  
-	  int isAPermutation = isAPermutation(array, array.length);
-	  System.out.println();	
-	  if(isAPermutation == 1) {
-	    System.out.print("The array is a permutation.");
-	  } else {
-		System.out.print("The array is not a permutation.");  
-	  }
-	}
+	 int isAPermutation = isAPermutation(array, array.length);
+	 System.out.println();	
+	 if(isAPermutation == 1) {
+	   System.out.print("The array is a permutation.");
+	 } else {
+	   System.out.print("The array is not a permutation.");  
+	 }
+      }
   }
