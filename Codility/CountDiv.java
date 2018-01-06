a4e66663b29627273825833357d8bf5e1b095bee
contactaditya <contactadityag@gmail.com>
2018-01-06 10:24:56 -0500

    This is a program to compute the number of integers divisible by k in the range[a...b].

---
+++ Codility/CountDiv.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+  public class CountDiv { 
+	  
+	public static int countNumberOfDivisibleIntegers(int A, int B, int K) {
+	  int count = 0;
+	  for (int i = A; i <= B; i++) {
+         if(i % K == 0) {
+           count++;	 
+         }
+      }
+	  
+	  return count;  
+	}
+	
+	public static int countNumberOfDivisibleIntegers1(int A, int B, int K) {
+	  int count = 0;
+      count = B/K - A/K;
+      if(A % K == 0) {
+    	count++;
+      }
+      return count; 
+	}
+
+	public static void main(String[] args) {   
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the first integer: ");
+	  int A = input.nextInt();   
+		   
+	  System.out.println();
+	  System.out.print("Enter the second integer: ");
+	  int B = input.nextInt();   
+		   
+	  System.out.println();
+	  System.out.print("Enter the third integer: ");
+	  int K = input.nextInt(); 
+
+	  int countNumberOfDivisibleIntegers = countNumberOfDivisibleIntegers1(A, B, K);
+      System.out.println();
+      System.out.print("The number of integers within the range [" + A + ".." + B + "] that are divisible by " + K + " are: " + countNumberOfDivisibleIntegers);
+	}
+  }
