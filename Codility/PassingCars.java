2a21ae1871071fa576340217225a06bc53a03ed3
Aditya Gupta <contactadityag@gmail.com>
2018-01-08 23:41:41 -0500

    This is a program to return the number of pairs of passing cars.

--- Codility/PassingCars.java
+++ Codility/PassingCars.java
@@ -3,9 +3,9 @@ import java.io.*;
 
   public class PassingCars {
 	  
-	public static int countPassingCars(int array[]) {
-	  int countOfZeros = 0, count = 0;
-	  for (int i = 0; i < array.length; i++) {
+    public static int countPassingCars(int array[]) {  
+      int countOfZeros = 0, count = 0;
+      for (int i = 0; i < array.length; i++) {
         if (array[i] == 0) {
     	 countOfZeros++;                    
         }
@@ -17,22 +17,22 @@ import java.io.*;
         }
       }
       return count;
-	}
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();    
-	  int array[] = new int[numbers];
+    public static void main(String[] args) {   
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int numbers = input.nextInt();    
+       int array[] = new int[numbers];
 	    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-	    array[i] = input.nextInt();   
-	  }
+       System.out.println();
+       System.out.print("Please enter the actual values in the array: ");
+       for (int i = 0; i < numbers; i++) {
+	 array[i] = input.nextInt();   
+       }
 	  
-	  int numberOfPassingCars = countPassingCars(array);
-	  System.out.println();
-	  System.out.print("The number of pairs of passing cars are: " + numberOfPassingCars);
-	}
+       int numberOfPassingCars = countPassingCars(array);
+       System.out.println();
+       System.out.print("The number of pairs of passing cars are: " + numberOfPassingCars);
+    }
   }
