f5549f36204c996a923e16cbb752bfd97bfa02b8
Aditya Gupta <contactadityag@gmail.com>
2018-01-11 21:54:28 -0500

    This is a program to find the minimal average of any slice containing at least two elements.

--- Codility/MinAvgTwoSlice.java
+++ Codility/MinAvgTwoSlice.java
@@ -3,37 +3,37 @@ import java.io.*;
 
   public class MinAvgTwoSlice { 	
 	  
-	public static int getSliceWithMinimalAverage(int array[]) {
-	  int minIndex = 0;
-	  double minimumAverage = Double.MAX_VALUE;;
-	  for (int i = 0; i < array.length - 1; i++) {
-		double average = (array[i] + array[i + 1]) / 2.0;  
-		if(i < array.length - 2) {
-		  double threeSliceAverage = (array[i] + array[i + 1] + array[i + 2]) / 3.0;  
-		  average = Math.min(average, threeSliceAverage);
-		}
-		if (average < minimumAverage) {
-	      minimumAverage = average;
-	      minIndex = i;
-	    }
+    public static int getSliceWithMinimalAverage(int array[]) {
+      int minIndex = 0;
+      double minimumAverage = Double.MAX_VALUE;;
+      for (int i = 0; i < array.length - 1; i++) {
+	double average = (array[i] + array[i + 1]) / 2.0;  
+	if(i < array.length - 2) {
+	  double threeSliceAverage = (array[i] + array[i + 1] + array[i + 2]) / 3.0;  
+	  average = Math.min(average, threeSliceAverage);
+	}
+	if (average < minimumAverage) {
+	  minimumAverage = average;
+	  minIndex = i;
+	}
       }
       return minIndex;
-	}
+    }
 	   	  
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();    
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();    
+      int array[] = new int[numbers];
 	    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-	    array[i] = input.nextInt();   
-	  }
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt();   
+      }
 	  
-	  int sliceWithMinimalAverage = getSliceWithMinimalAverage(array);
-	  System.out.println();
-	  System.out.print("The starting position of a slice whose average is minimal is: " + sliceWithMinimalAverage);
-	}
+      int sliceWithMinimalAverage = getSliceWithMinimalAverage(array);
+      System.out.println();
+      System.out.print("The starting position of a slice whose average is minimal is: " + sliceWithMinimalAverage);
+    }
   }
