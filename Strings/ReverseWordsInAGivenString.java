7d6c038432e5e0e5b579dac4018a12fef38071f0
Aditya Gupta <contactadityag@gmail.com>
2018-01-14 21:49:59 -0500

    This program is to reverse the string word by word.

--- Strings/ReverseWordsInAGivenString.java
+++ Strings/ReverseWordsInAGivenString.java
@@ -8,7 +8,7 @@ public class ReverseWordsInAGivenString {
 	return "";
      }
 	  
-    // Split different words by space
+     // Split different words by space
      String[] array = string.split(" ");
      StringBuilder sb = new StringBuilder();
      for (int i = array.length - 1; i >= 0; --i) {
@@ -19,15 +19,14 @@ public class ReverseWordsInAGivenString {
     return sb.length() == 0 ? "" : sb.substring(0, sb.length() - 1);
   }
 
- public static void main(String[] args) {
-    String string = new String();	  	
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter the string: ");
-    string = input.nextLine(); 
+  public static void main(String[] args) {
+     String string = new String();	  	
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the string: ");
+     string = input.nextLine(); 
 	  
-    String reverse = reverseWords(string);
-    System.out.println();
-    System.out.println("The words in reverse order are: " + reverse);
-
+     String reverse = reverseWords(string);
+     System.out.println();
+     System.out.println("The words in reverse order are: " + reverse);
    }
 }
