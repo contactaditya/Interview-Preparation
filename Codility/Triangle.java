b25920f004842c40cecb8e47ff52899712a488af
Aditya Gupta <contactadityag@gmail.com>
2018-01-18 04:14:05 -0500

    This is a program to determine whether a triangle can be built from a given set of edges.

--- Codility/Triangle.java
+++ Codility/Triangle.java
@@ -3,22 +3,22 @@ import java.io.*;
 
   public class Triangle {     
 	  
-	public static int isTriangular(int[] array) {
-	  if (array.length < 3) {
-		return 0;	
-	  }
+    public static int isTriangular(int[] array) {
+      if (array.length < 3) {
+	return 0;	
+      }
       Arrays.sort(array);   
       for (int i = 0; i < array.length - 2; i++) {
     	if((long)array[i]+(long)array[i+1] > (long)array[i+2]) {
-           return 1;
-  	    }
-  	  }
+          return 1;
+  	}
+      }
   
       return 0;
     }
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number of elements in the array: ");
       int lengthOfArray = input.nextInt();
       int[] array = new int[lengthOfArray];    
@@ -36,5 +36,5 @@ import java.io.*;
       else {
         System.out.println("There exists no triangular triplet for this array");  
       }
-	}
+    }
   }
