5146aa8a9df9db6fc994c33e070cde17e6cc9595
Aditya Gupta <contactadityag@gmail.com>
2018-01-25 05:04:46 -0500

    This is a program to divide array A into K blocks and minimize the largest sum of any block.

--- Codility/MinMaxDivision.java
+++ Codility/MinMaxDivision.java
@@ -3,17 +3,17 @@ import java.io.*;
 
   public class MinMaxDivision {
 	  
-	public static int getMinimalLargeSum(int K, int M, int array[]) {
-	  int max = 0;
-	  int sum = 0;
+    public static int getMinimalLargeSum(int K, int M, int array[]) {
+      int max = 0;
+      int sum = 0;
 	   
-	  for (int i = 0; i < array.length; i++) {
+      for (int i = 0; i < array.length; i++) {
         sum += array[i];
         max = Math.max(max, array[i]);
       }
-	  int low = max, high = sum;
-	  while(low <= high) {
-		int mid = low + (high-low)/2;
+      int low = max, high = sum;
+      while(low <= high) {
+	int mid = low + (high-low)/2;
         int intervals = countIntervals(array, mid);
         if (intervals > K) {
           low = mid + 1;
@@ -23,43 +23,43 @@ import java.io.*;
       }
 	  
       return low;		
-	}
+    }
 
-	private static int countIntervals(int[] array, int target) {
-	  int sum = 0, count = 0;
-	  for (int i = 0; i < array.length; i++) {
-	    sum += array[i];
-	    if (sum > target) {
+    private static int countIntervals(int[] array, int target) {
+      int sum = 0, count = 0;
+      for (int i = 0; i < array.length; i++) {
+        sum += array[i];
+	if (sum > target) {
           count++;
           sum = array[i];
         }
-	  }
+      }
 	 
-	  return count + (sum > 0 ? 1 : 0);
-	}
+      return count + (sum > 0 ? 1 : 0);
+    }
 
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the maximum value of the array: ");
-	  int M = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Enter number of elements in the array: ");
-	  int N = input.nextInt();   
-	  int array[] = new int[N];
+    public static void main(String[] args) { 	 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the maximum value of the array: "); 
+      int M = input.nextInt();  
+      System.out.println();
+      System.out.print("Enter number of elements in the array: ");
+      int N = input.nextInt();   
+      int array[] = new int[N];
 		    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < N; i++) {
-		array[i] = input.nextInt(); 
-	  }
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < N; i++) {
+	array[i] = input.nextInt(); 
+      }
 	  
-	  System.out.println();
-	  System.out.print("Enter the number of blocks that you want to divide the array: ");
-	  int K = input.nextInt();  
+      System.out.println();
+      System.out.print("Enter the number of blocks that you want to divide the array: ");
+      int K = input.nextInt();  
 	  
-	  int result = getMinimalLargeSum(K, M, array);
-	  System.out.println();
-	  System.out.print("The minimal large sum is: " + result);
+      int result = getMinimalLargeSum(K, M, array);
+      System.out.println();
+      System.out.print("The minimal large sum is: " + result);
 
-	}
+    }
   }
