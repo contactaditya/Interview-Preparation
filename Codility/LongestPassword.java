d83d85294b81044cc4163946bc0ec2ebdb13f60b
Aditya Gupta <contactadityag@gmail.com>
2018-01-26 09:06:28 -0500

    This is a program to find the longest word that satisfies specific conditions in a string containing words.

--- Codility/LongestPassword.java
+++ Codility/LongestPassword.java
@@ -3,27 +3,27 @@ import java.io.*;
 
   public class LongestPassword {
 	  
-	public static int getLengthLongestPassword(String password) {
-	  String[] words = password.replaceAll("\\s+", " ").split(" ");
-	  int maxLength = -1;
-	  for (String word : words) {
-	    if (word.matches("^[0-9a-zA-Z]*$") && word.replaceAll("[0-9]+", "").length() % 2 == 0 && word.replaceAll("[a-zA-Z]+", "").length() % 2 == 1) {
-	      maxLength = Math.max(maxLength, word.length());
-	    }
+     public static int getLengthLongestPassword(String password) {
+	String[] words = password.replaceAll("\\s+", " ").split(" ");
+	int maxLength = -1;
+	for (String word : words) {
+	  if (word.matches("^[0-9a-zA-Z]*$") && word.replaceAll("[0-9]+", "").length() % 2 == 0 && word.replaceAll("[a-zA-Z]+", "").length() % 2 == 1) {
+	    maxLength = Math.max(maxLength, word.length());
 	  }
-	  return maxLength;        
-	}	  
-
-	public static void main(String[] args) {   
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.next(); 
-	  
-	  int longestPassword = getLengthLongestPassword(string);
-	
-	  System.out.println();	
-	  System.out.println("The length of the longest word from the string that is a valid password is: " + longestPassword);	
-
 	}
+	return maxLength;        
+     }	  
+
+     public static void main(String[] args) {   
+       String string = new String();	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the string: ");
+       string = input.next(); 
+	  
+       int longestPassword = getLengthLongestPassword(string);
+	
+       System.out.println();	
+       System.out.println("The length of the longest word from the string that is a valid password is: " + longestPassword);	
+
+     }
   }
