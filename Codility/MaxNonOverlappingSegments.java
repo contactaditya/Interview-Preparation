b6f90f76918f4b8b397fb7444ca5db4b040af1a8
Aditya Gupta <contactadityag@gmail.com>
2018-01-26 08:55:09 -0500

    This is a program to find a maximal set of non-overlapping segments in two arrays.

--- Codility/MaxNonOverlappingSegments.java
+++ Codility/MaxNonOverlappingSegments.java
@@ -3,22 +3,22 @@ import java.io.*;
 
   public class MaxNonOverlappingSegments {
 	  
-	public static int getMaxNonOverlappingSegments(int A[], int B[], int N) {      
-	  if (A.length == 0) {
-	    return 0;
-	  }
-	  int last = 0, count = 1;  
-	  for (int i = 1; i < A.length; i++) {
-	    if (A[i] > B[last]) {
-	      count++;     
-	      last = i;
-	    }
-	  }
-	  return count; 
-	}  
+    public static int getMaxNonOverlappingSegments(int A[], int B[], int N) {      
+      if (A.length == 0) {
+	return 0;
+      }
+      int last = 0, count = 1;  
+      for (int i = 1; i < A.length; i++) {
+	if (A[i] > B[last]) {
+	  count++;     
+	  last = i;
+	}
+      }
+      return count; 
+    }  
   
-	public static void main(String[] args) {      
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {      
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int N = input.nextInt();  
       int A[] = new int[N];
@@ -26,19 +26,19 @@ import java.io.*;
       System.out.println();	
       System.out.print("Please enter the elements in the first array: ");
       for(int i = 0; i < A.length; i++) {
-	    A[i] = input.nextInt();
+	A[i] = input.nextInt();
       }
 	 
       int B[] = new int[N]; 
       System.out.println();	
       System.out.print("Please enter the elements in the second array: ");
       for(int i = 0; i < B.length; i++) {
-	    B[i] = input.nextInt();
+	B[i] = input.nextInt();
       }
 	  	  
       int sizeOfMaximalNumberSegments = getMaxNonOverlappingSegments(A, B, N);
 	  
       System.out.println();		
       System.out.print("The size of a non-overlapping set containing the maximal number of segments is: " + sizeOfMaximalNumberSegments);   
-	}
+    }
   }
