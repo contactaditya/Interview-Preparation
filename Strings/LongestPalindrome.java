777235beb3b176586c12a19475deae859df25e3e
Aditya Gupta <contactadityag@gmail.com>
2018-01-27 21:27:58 -0500

    This is a program to find the length of the longest palindromes that can be built with the letters in the given string.

--- Strings/LongestPalindrome.java
+++ Strings/LongestPalindrome.java
@@ -3,34 +3,33 @@ import java.io.*;
  
   public class LongestPalindrome {
 	  
-	public static int longestPalindrome(String string) {
-	  int[] dictionary = new int[255];
-	  char[] ch = string.toCharArray();
-	  for (char c: ch) {
-		dictionary[c]++;
-	  }
-	  int count = 0;
-	  boolean hasOdd = false;
-	  for (int i : dictionary) {
-	    if (i%2 == 1) {
-	      hasOdd=true;
-	    } 
-	    count += (i/2)*2;
-	  }
-	  
-	  return count + (hasOdd ? 1 : 0);  
+    public static int longestPalindrome(String string) {
+      int[] dictionary = new int[255];
+      char[] ch = string.toCharArray();
+      for (char c: ch) {
+	dictionary[c]++;
+      } 
+      int count = 0;
+      boolean hasOdd = false;
+      for (int i : dictionary) {
+        if (i%2 == 1) {
+	  hasOdd=true;
 	} 
-
-	public static void main(String[] args) {  
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.next(); 
+	count += (i/2)*2;
+      }
 	  
-	  int lengthOfLongestPalindrome = longestPalindrome(string);
-	
-	  System.out.println();	
-	  System.out.println("The length of the longest palindromes that can be built with the letters of the string is: " + lengthOfLongestPalindrome);	
+      return count + (hasOdd ? 1 : 0);  
+    } 
 
-	}
+    public static void main(String[] args) {  
+      String string = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the string: ");
+      string = input.next(); 
+	  
+      int lengthOfLongestPalindrome = longestPalindrome(string);
+	
+      System.out.println();	
+      System.out.println("The length of the longest palindromes that can be built with the letters of the string is: " + lengthOfLongestPalindrome);	
+    }
   }
