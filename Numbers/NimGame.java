f7a6354ca48d653d96008cd6607cc1bcf171b64f
Aditya Gupta <contactadityag@gmail.com>
2018-01-27 21:30:20 -0500

    This is a program to determine whether you can win the game given the number of stones in the heap.

--- Numbers/NimGame.java
+++ Numbers/NimGame.java
@@ -3,25 +3,25 @@ import java.io.*;
 
   public class NimGame {
 	  
-	public static boolean canWinNim(int number) {
-	  if(number % 4 == 0) {
-		return false;   
-	  }	
+    public static boolean canWinNim(int number) {
+      if(number % 4 == 0) {
+	 return false;   
+      }	
 	  
-	  return true;      
-	}
+      return true;      
+    }
 
-	public static void main(String[] args) {    
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {    
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number of the stones in the heap: ");
       int number = input.nextInt();   
       boolean canWinNim = canWinNim(number);
       System.out.println();
       if(canWinNim) {
-	    System.out.println("Yes you can win the game.");  
+	System.out.println("Yes you can win the game.");  
       }
       else {
-	    System.out.println("No you can not win the game.");  
+	System.out.println("No you can not win the game.");  
       }
-	}
-  }
+    }
+  } 
