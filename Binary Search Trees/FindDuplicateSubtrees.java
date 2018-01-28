2fe4a74e963168fb00fb4208afcb5b5bf854597b
Aditya Gupta <contactadityag@gmail.com>
2018-01-27 21:35:18 -0500

    This is a program to return the root node of all the duplicate subtrees in the binary tree.

--- Binary Search Trees/FindDuplicateSubtrees.java
+++ Binary Search Trees/FindDuplicateSubtrees.java
@@ -2,37 +2,37 @@ import java.util.*;
 import java.io.*;
 
   public class FindDuplicateSubtrees { 
-	private static Node root;	  
-	private static int t;
-	private static Map<String, Integer> trees;
-	private static Map<Integer, Integer> count;
-	private static List<Node> answer;
+    private static Node root;	  
+    private static int t;
+    private static Map<String, Integer> trees;
+    private static Map<Integer, Integer> count;
+    private static List<Node> answer;
 	  
-	public static List<Node> findDuplicateSubtrees(Node root) {
-	  t = 1;
-	  trees = new HashMap();
-	  count = new HashMap();
-	  answer = new ArrayList();
-	  lookup(root);
-	  return answer;  
-	}	 
+    public static List<Node> findDuplicateSubtrees(Node root) {
+      t = 1;
+      trees = new HashMap();
+      count = new HashMap();
+      answer = new ArrayList();
+      lookup(root);
+      return answer;  
+    }	 
 	
-	public static int lookup(Node node) {
-	  if (node == null) {
-		return 0;
-	  }
-	  String serial = node.data + "," + lookup(node.left) + "," + lookup(node.right);
-	  int uid = trees.computeIfAbsent(serial, x-> t++);
-	  count.put(uid, count.getOrDefault(uid, 0) + 1);
-	  if (count.get(uid) == 2) {
-		answer.add(node);
-	  }
+    public static int lookup(Node node) {
+      if (node == null) {
+	return 0;
+      }
+      String serial = node.data + "," + lookup(node.left) + "," + lookup(node.right);
+      int uid = trees.computeIfAbsent(serial, x-> t++);
+      count.put(uid, count.getOrDefault(uid, 0) + 1);
+      if (count.get(uid) == 2) {
+	answer.add(node);
+      }
 	  
-	  return uid;
-	}	
+      return uid;
+    }	
 
-	public static void main(String[] args) {  
-	  FindDuplicateSubtrees tree = new FindDuplicateSubtrees();
+    public static void main(String[] args) {  
+      FindDuplicateSubtrees tree = new FindDuplicateSubtrees();
       tree.root = new Node(1);
       tree.root.left = new Node(2);
       tree.root.right = new Node(3);
