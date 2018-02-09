e61550b349755b679e1accb65799e620b35425a1
Aditya Gupta <contactadityag@gmail.com>
2018-02-08 19:55:17 -0500

    This is a program to count the number of segments in the string.
    A segment is defined to be a contiguous sequence of non space characters.

--- Strings/NumberOfSegmentsInAString.java
+++ Strings/NumberOfSegmentsInAString.java
@@ -3,23 +3,23 @@ import java.io.*;
 
   public class NumberOfSegmentsInAString {
 	  
-	public static int countSegments(String string) {
-	  int countSegments = 0;
-	  for(int i=0; i < string.length(); i++) {
-		if(string.charAt(i)!=' ' && (i==0 || string.charAt(i-1)==' ')) {
-		   countSegments++;      
-		}
-	  }
-	  return countSegments;
+    public static int countSegments(String string) {
+       int countSegments = 0;
+       for(int i=0; i < string.length(); i++) {
+	 if(string.charAt(i)!=' ' && (i==0 || string.charAt(i-1)==' ')) {
+	    countSegments++;      
+	 }
+       }
+       return countSegments;
     }	  
 
-	public static void main(String[] args) { 	 
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine(); 
-	  int countSegments = countSegments(string);
-	  System.out.println();
-	  System.out.println("The number of segments in the string are: " + countSegments);
-	}
+    public static void main(String[] args) { 	 
+       String string = new String();	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the string: ");
+       string = input.nextLine(); 
+       int countSegments = countSegments(string);
+       System.out.println();
+       System.out.println("The number of segments in the string are: " + countSegments);
+    }
   }
