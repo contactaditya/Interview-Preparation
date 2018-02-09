d1a4bb357766d03d562d919f17141f223869280b
Aditya Gupta <contactadityag@gmail.com>
2018-02-08 20:12:07 -0500

    This is a program to find the maximum number of consecutive 1s in the given binary array.

--- Bit Manipulation/MaxConsecutiveOnes.java
+++ Bit Manipulation/MaxConsecutiveOnes.java
@@ -3,37 +3,37 @@ import java.io.*;
 
   public class MaxConsecutiveOnes {
 	  
-	public static int findMaxConsecutiveOnes(int[] array) {
-	  int result = 0;
-	  int count = 0;
-	  for (int i = 0; i < array.length; i++) {
-	    if (array[i] == 1) {
-	      count++;
-	      result = Math.max(count, result);
-	    }
-	    else {
-	      count = 0;
-	    }
+    public static int findMaxConsecutiveOnes(int[] array) {
+      int result = 0;
+      int count = 0;
+      for (int i = 0; i < array.length; i++) {
+	if (array[i] == 1) {
+	  count++;
+	  result = Math.max(count, result);
+	}
+	else {
+	  count = 0;
+	}
       }	
 		
-	  return result;     
-	}	  
+      return result;     
+    }	  
 
-	public static void main(String[] args) {
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the binary array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the binary array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 		    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the binary array: ");
+      System.out.println();
+      System.out.print("Please enter the actual values in the binary array: ");
       for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+	 array[i] = input.nextInt(); 
+      }
 		   
-	  System.out.println();
-	  int maxConsecutiveOnes = findMaxConsecutiveOnes(array);
+      System.out.println();
+      int maxConsecutiveOnes = findMaxConsecutiveOnes(array);
 			  
-	  System.out.println("The maximum number of consecutive 1s in the array are: " + maxConsecutiveOnes);
-	}
+      System.out.println("The maximum number of consecutive 1s in the array are: " + maxConsecutiveOnes);
+    }
   }
