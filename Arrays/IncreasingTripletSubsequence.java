67de228df4d8046dab9e372d5beb2c4806be6820
Aditya Gupta <contactadityag@gmail.com>
2018-02-08 19:50:15 -0500

    This is a program to return whether an increasing subsequence of length 3 exists or not in an unsorted array.

--- Arrays/IncreasingTripletSubsequence.java
+++ Arrays/IncreasingTripletSubsequence.java
@@ -3,24 +3,24 @@ import java.io.*;
 
   public class IncreasingTripletSubsequence {
 	
-	public static boolean increasingTriplet(int[] array) {	
-	  int min = Integer.MAX_VALUE;
-	  int secondMin = Integer.MAX_VALUE;
+    public static boolean increasingTriplet(int[] array) {	
+      int min = Integer.MAX_VALUE;
+      int secondMin = Integer.MAX_VALUE;
 
-	  for (int n : array) {
-	    if (n <= min) {
-	      min = n;
-	    } else if (n <= secondMin) {
-	      secondMin = n;
-		} else {
-		  return true;
-		}
-	  }
-	  return false;	
+      for (int n : array) {
+        if (n <= min) {
+	  min = n;
+	} else if (n <= secondMin) {
+	  secondMin = n;
+	} else {
+	  return true;
 	}
+      }
+      return false;	
+    }
 	  
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int number = input.nextInt();  
       int array[] = new int[number];
@@ -34,9 +34,9 @@ import java.io.*;
       boolean isIncreasingTriplet = increasingTriplet(array);  
       System.out.println();	
       if(isIncreasingTriplet) {
- 		System.out.print("Yes there is atleast one increasing subsequence of length 3 that exists in the unsorted array.");  
- 	  } else {
- 		System.out.print("No there is not even one increasing subsequence of length 3 that exists in the unsorted array.");    
- 	  }
-	}
+ 	System.out.print("Yes there is atleast one increasing subsequence of length 3 that exists in the unsorted array.");  
+      } else {
+ 	System.out.print("No there is not even one increasing subsequence of length 3 that exists in the unsorted array.");    
+      }
+    }
   }
