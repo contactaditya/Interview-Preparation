505e821f8fd9d345ebabdad2887e53b9a3e695a0
Aditya Gupta <contactadityag@gmail.com>
2018-02-08 20:14:44 -0500

    This is a program to find minimum cost to reach the top of the floor and you can either start from the step with index 0 or index 1.

--- Arrays/MinCostClimbingStairs.java
+++ Arrays/MinCostClimbingStairs.java
@@ -3,29 +3,29 @@ import java.io.*;
 
   public class MinCostClimbingStairs {
 	  
-	public static int minCostClimbingStairs(int[] cost) {
-	  for (int i = 2; i < cost.length; i++) {
-		cost[i] += Math.min(cost[i-1],cost[i-2]);    
-	  }		
+    public static int minCostClimbingStairs(int[] cost) {
+      for (int i = 2; i < cost.length; i++) {
+	cost[i] += Math.min(cost[i-1],cost[i-2]);    
+      }		
 		
-	  return Math.min(cost[cost.length-1],cost[cost.length-2]);      
-	}	  
+      return Math.min(cost[cost.length-1],cost[cost.length-2]);      
+    }	  
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 		    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
       for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+	array[i] = input.nextInt(); 
+      }
 		   
-	  System.out.println();
-	  int minimumCost = minCostClimbingStairs(array);
+      System.out.println();
+      int minimumCost = minCostClimbingStairs(array);
 			  
-	  System.out.println("The minimum cost to reach the top of the floor is: " + minimumCost);
-	}
+      System.out.println("The minimum cost to reach the top of the floor is: " + minimumCost);
+    }
   }
