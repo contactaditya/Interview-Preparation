2f25b5ac1f9a712cfdd3ebfb002ac4832494e2d1
Aditya Gupta <contactadityag@gmail.com>
2018-02-12 00:18:24 -0500

    This is a program to split the array into several subsequences where each subsequences consist of at least 3 consecutive integers.
    You are given an integer array sorted in ascending order (may contain duplicates).

--- Arrays/SplitArrayIntoConsecutiveSubsequences.java
+++ Arrays/SplitArrayIntoConsecutiveSubsequences.java
@@ -4,13 +4,13 @@ import java.io.*;
 
   public class SplitArrayIntoConsecutiveSubsequences {
 	  
-	public static boolean isPossible(int[] array) {
-	  if (array.length < 3) {
-		return false;
-	  }
-	  int previousNumber = Integer.MIN_VALUE, previous1 = 0, previous2 = 0, previous3 = 0;
-	  int currentNumber = array[0], current1 = 0, current2 = 0, current3 = 0;
-	  for (int i = 0; i < array.length; i++) {
+    public static boolean isPossible(int[] array) {
+      if (array.length < 3) {
+	return false;
+      }
+      int previousNumber = Integer.MIN_VALUE, previous1 = 0, previous2 = 0, previous3 = 0;
+      int currentNumber = array[0], current1 = 0, current2 = 0, current3 = 0;
+      for (int i = 0; i < array.length; i++) {
         int count = 1;
         while (i < array.length - 1 && array[i] == array[i + 1]) {
            i++;
@@ -36,31 +36,30 @@ import java.io.*;
           current1 = count;
           current3 = 0;
         }
-	  }
+      }
 		
-	  return current1 == 0 && current2 == 0;
-	}
+      return current1 == 0 && current2 == 0;
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 					    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt(); 
+      }
 
-	  boolean isPossible = isPossible(array);
-	  System.out.println();	
-	  if(isPossible) {
-		System.out.println("Yes the array can be split into several subsequences, where each subsequences consist of at least 3 consecutive integers.");  
-	  }
-	  else {
-		System.out.println("No the array cannot be split into several subsequences, where each subsequences consist of at least 3 consecutive integers.");  
-	  }
-
-	}
+      boolean isPossible = isPossible(array);
+      System.out.println();	
+      if(isPossible) {
+	System.out.println("Yes the array can be split into several subsequences, where each subsequences consist of at least 3 consecutive integers.");  
+      }
+      else {
+	System.out.println("No the array cannot be split into several subsequences, where each subsequences consist of at least 3 consecutive integers.");  
+      }
+    }
   }
