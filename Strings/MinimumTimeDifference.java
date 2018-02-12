6a266d5eb7e0c1b218859c7621824993f77bc17e
Aditya Gupta <contactadityag@gmail.com>
2018-02-11 23:39:00 -0500

    This is a program to find the minimum minutes difference between any two time points in a list of 24-hour clock time.

--- Strings/MinimumTimeDifference.java
+++ Strings/MinimumTimeDifference.java
@@ -3,11 +3,11 @@ import java.io.*;
 
   public class MinimumTimeDifference {
 	  
-	public static int findMinDifference(List<String> timePoints) {
-	  boolean array[] = new boolean[24*60];
-	  int min = 24*60, max = 0;
-	  for(String time : timePoints) {
-	    int hour1 = (time.charAt(0) - '0') * 10 + time.charAt(1) - '0';
+    public static int findMinDifference(List<String> timePoints) {
+      boolean array[] = new boolean[24*60];
+      int min = 24*60, max = 0;
+      for(String time : timePoints) {
+	int hour1 = (time.charAt(0) - '0') * 10 + time.charAt(1) - '0';
         int minute1 = (time.charAt(3) - '0') * 10 + time.charAt(4) - '0';
                     
         if(array[hour1 * 60 + minute1]) {
@@ -16,9 +16,9 @@ import java.io.*;
         array[hour1 * 60 + minute1] = true;
       }
 	  
-	  int previous = 0, first = Integer.MAX_VALUE;
-	  int last = Integer.MIN_VALUE;
-	  int result = Integer.MAX_VALUE;
+      int previous = 0, first = Integer.MAX_VALUE;
+      int last = Integer.MIN_VALUE;
+      int result = Integer.MAX_VALUE;
 
       for (int i = 0; i < 24*60; i++) {
         if (array[i]) {
@@ -32,22 +32,22 @@ import java.io.*;
       }
 
       return Math.min(result, first + 1440 - previous);
-	}	  
+    }	  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Please enter the number of elements in the list: ");
-	  int numberOfElements = input.nextInt();
-	  System.out.println();
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Please enter the number of elements in the list: ");
+      int numberOfElements = input.nextInt();
+      System.out.println();
 
-	  System.out.println("Please enter the time in Hour:Minutes format: ");
-	  ArrayList<String> timePoints = new ArrayList<String>();
-	  for (int i = 0; i < numberOfElements; i++) {
-		timePoints.add(input.nextLine());
-	  }		 
-	  int minDifference = findMinDifference(timePoints); 
-	  System.out.println();
-	  System.out.println("The minimum minutes difference between any two time points in the list is: " + minDifference);
+      System.out.println("Please enter the time in Hour:Minutes format: ");
+      ArrayList<String> timePoints = new ArrayList<String>();
+      for (int i = 0; i < numberOfElements; i++) {
+	timePoints.add(input.nextLine());
+      }		 
+      int minDifference = findMinDifference(timePoints); 
+      System.out.println();
+      System.out.println("The minimum minutes difference between any two time points in the list is: " + minDifference);
 
-	}
+    }
   }
