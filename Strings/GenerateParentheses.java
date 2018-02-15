e41d5b458c756e31a3a83c54949ca7b777966b13
Aditya Gupta <contactadityag@gmail.com>
2018-02-15 05:51:48 -0500

    This is a program to generate all combinations of well-formed parentheses given n pairs of parentheses.

--- Strings/GenerateParentheses.java
+++ Strings/GenerateParentheses.java
@@ -3,31 +3,31 @@ import java.io.*;
 
   public class GenerateParentheses {
 	
-	public static List<String> generateParenthesis(int number) {
+    public static List<String> generateParenthesis(int number) {
       List<String> answer = new ArrayList<String>();
-	  if (number == 0) {
-	    answer.add("");
-	  }	else {
-		for (int c = 0; c < number; c++) {
+      if (number == 0) {
+	answer.add("");
+      } else {
+	for (int c = 0; c < number; c++) {
           for (String left: generateParenthesis(c)) {
             for (String right: generateParenthesis(number-1-c)) {
               answer.add("(" + left + ")" + right);  
             }
           }
-	    }
-	  }
-		
-	  return answer;      
 	}
+      }
+	    
+      return answer;      
+    }
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number of pairs of parentheses: ");
-	  int numberOfParentheses = input.nextInt();
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number of pairs of parentheses: ");
+      int numberOfParentheses = input.nextInt();
 	 
-	  List<String> result = new ArrayList<String>();
-	  result = generateParenthesis(numberOfParentheses); 
-	  System.out.println();
-	  System.out.println("The solution set is: " + result);
-	}
+      List<String> result = new ArrayList<String>();
+      result = generateParenthesis(numberOfParentheses); 
+      System.out.println();
+      System.out.println("The solution set is: " + result);
+    }
   }
