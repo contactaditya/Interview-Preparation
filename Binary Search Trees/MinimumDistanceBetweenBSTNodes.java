227ff3ae9fe540f50b287822cf1f402d3dc27962
Aditya Gupta <contactadityag@gmail.com>
2018-02-15 05:10:07 -0500

    This is a program to find the minimum difference between the values of any two different nodes in the BST.

--- Binary Search Trees/MinimumDistanceBetweenBSTNodes.java
+++ Binary Search Trees/MinimumDistanceBetweenBSTNodes.java
@@ -2,36 +2,36 @@ import java.util.*;
 import java.io.*;
 
   public class MinimumDistanceBetweenBSTNodes {
-	Node root;	    
-	Integer previous, answer;
+    Node root;	    
+    Integer previous, answer;
 	 
-	public int minDiffInBST(Node root) {
-	  previous = null;
-	  answer = Integer.MAX_VALUE;
-	  dfs(root);
-	  return answer;    
-	}  
+    public int minDiffInBST(Node root) {
+      previous = null;
+      answer = Integer.MAX_VALUE;
+      dfs(root);
+      return answer;    
+    }  
 
-	private void dfs(Node node) {
-	  if (node == null) {
-		return;
-	  }
-	  dfs(node.left);
-	  if (previous != null) {
-	    answer = Math.min(answer, node.data - previous);
-	  }
-	  previous = node.data;
+    private void dfs(Node node) {
+      if (node == null) {
+	return;
+      }
+      dfs(node.left);
+      if (previous != null) {
+	answer = Math.min(answer, node.data - previous);
+      }
+      previous = node.data;
       dfs(node.right);
-	}
+    }
 
-	public static void main(String[] args) { 	 
-	  MinimumDistanceBetweenBSTNodes tree = new MinimumDistanceBetweenBSTNodes();
-	  tree.root = new Node(4);
-	  tree.root.right = new Node(6);
-	  tree.root.left = new Node(2);
-	  tree.root.left.left = new Node(1);
-	  tree.root.left.right = new Node(3);
+    public static void main(String[] args) { 	 
+      MinimumDistanceBetweenBSTNodes tree = new MinimumDistanceBetweenBSTNodes();
+      tree.root = new Node(4);
+      tree.root.right = new Node(6);
+      tree.root.left = new Node(2);
+      tree.root.left.left = new Node(1);
+      tree.root.left.right = new Node(3);
 	  
-	  System.out.println("The minimum difference between the values of any two different nodes in the tree is: " + tree.minDiffInBST(tree.root));		
-	}
+      System.out.println("The minimum difference between the values of any two different nodes in the tree is: " + tree.minDiffInBST(tree.root));		
+    }
   }
