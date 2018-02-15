467940ae36434afa3cf0891a8f18ef247897a50d
Aditya Gupta <contactadityag@gmail.com>
2018-02-15 05:43:47 -0500

    This is a program to return the summary of the ranges in a sorted array without duplicates.

--- Arrays/SummaryRanges.java
+++ Arrays/SummaryRanges.java
@@ -6,9 +6,9 @@ import java.io.*;
     public static List<String> summaryRanges(int[] array) {
       List<String> list = new ArrayList<String>();
       if(array.length == 1) {
-  		list.add(array[0] + "");
-  		return list;
-  	  }
+  	list.add(array[0] + "");
+  	return list;
+      }
       for (int i = 0; i < array.length; i++) {
         int number = array[i];
         while (i < array.length - 1 && array[i] + 1 == array[i + 1]) {
@@ -24,21 +24,21 @@ import java.io.*;
       return list;
     }
 	   
-	public static void main(String[] args) { 	   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 	    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt(); 
+      }
 
-	  List<String> result = new ArrayList<String>();
-	  result = summaryRanges(array); 
-	  System.out.println();
-	  System.out.println("The summary of the range of the sorted integer array is: " + result);
-	}
+      List<String> result = new ArrayList<String>();
+      result = summaryRanges(array); 
+      System.out.println();
+      System.out.println("The summary of the range of the sorted integer array is: " + result);
+    }
   }
