fd3adf5bda0b04c38b2f6a01789426d42d2b8833
Aditya Gupta <contactadityag@gmail.com>
2018-02-15 04:26:23 -0500

    This is a program to predict whether player 1 is the winner given an array of scores that are non-negative integers.

--- Arrays/PredictTheWinner.java
+++ Arrays/PredictTheWinner.java
@@ -3,36 +3,36 @@ import java.io.*;
 
   public class PredictTheWinner {
 	  
-	public static boolean PredictTheWin(int[] array) {
-	  int dp[] = new int[array.length];	
-	  for(int i = array.length; i >= 0; i--) {
-	    for (int j = i + 1; j < array.length; j++) {
-	      dp[j] = Math.max(array[i] - dp[j], array[j] - dp[j - 1]);	
-	    }
-	  }
-		
-	  return dp[array.length - 1] >= 0;      
-	}	  
-
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
-		    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
-
-	  boolean PredictTheWinner = PredictTheWin(array);
-	  System.out.println();
-	  if(PredictTheWinner) {
-		System.out.println("Yes player 1 can win the game.");  
-	  }
-	  else {
-	    System.out.println("No player 1 will never be the winner.");    
+     public static boolean PredictTheWin(int[] array) {
+        int dp[] = new int[array.length];	
+	for(int i = array.length; i >= 0; i--) {
+	  for (int j = i + 1; j < array.length; j++) {
+	     dp[j] = Math.max(array[i] - dp[j], array[j] - dp[j - 1]);	
 	  }
 	}
+		
+	return dp[array.length - 1] >= 0;      
+     }	  
+
+     public static void main(String[] args) {   
+        Scanner input = new Scanner(System.in);
+	System.out.print("Enter number of elements in the array: ");
+	int numbers = input.nextInt();   
+	int array[] = new int[numbers];
+		    
+	System.out.println();
+	System.out.print("Please enter the actual values in the array: ");
+	for (int i = 0; i < numbers; i++) {
+	  array[i] = input.nextInt(); 
+	}
+
+	boolean PredictTheWinner = PredictTheWin(array);
+	System.out.println();
+	if(PredictTheWinner) {
+	  System.out.println("Yes player 1 can win the game.");  
+	}
+	else {
+	  System.out.println("No player 1 will never be the winner.");    
+        }
+     }
   }
