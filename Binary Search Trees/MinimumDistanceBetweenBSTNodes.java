0d7e6fa6f9b9610b1f0e017be6bbfb7416cfe71d
contactaditya <contactadityag@gmail.com>
2018-02-15 04:03:17 -0500

    This is a program to find the minimum difference between the values of any two different nodes in the BST.

---
+++ Binary Search Trees/MinimumDistanceBetweenBSTNodes.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+  public class MinimumDistanceBetweenBSTNodes {
+	Node root;	    
+	Integer previous, answer;
+	 
+	public int minDiffInBST(Node root) {
+	  previous = null;
+	  answer = Integer.MAX_VALUE;
+	  dfs(root);
+	  return answer;    
+	}  
+
+	private void dfs(Node node) {
+	  if (node == null) {
+		return;
+	  }
+	  dfs(node.left);
+	  if (previous != null) {
+	    answer = Math.min(answer, node.data - previous);
+	  }
+	  previous = node.data;
+      dfs(node.right);
+	}
+
+	public static void main(String[] args) { 	 
+	  MinimumDistanceBetweenBSTNodes tree = new MinimumDistanceBetweenBSTNodes();
+	  tree.root = new Node(4);
+	  tree.root.right = new Node(6);
+	  tree.root.left = new Node(2);
+	  tree.root.left.left = new Node(1);
+	  tree.root.left.right = new Node(3);
+	  
+	  System.out.println("The minimum difference between the values of any two different nodes in the tree is: " + tree.minDiffInBST(tree.root));		
+	}
+  }
