15a0750826c115f494fd31b9902ca2cd9960aa7d
Aditya Gupta <contactadityag@gmail.com>
2018-02-15 05:54:57 -0500

    This is a program to return the minimum number of steps required to reach the destination.

--- Numbers/ReachANumber.java
+++ Numbers/ReachANumber.java
@@ -3,19 +3,19 @@ import java.io.*;
 
   public class ReachANumber {
 	  
-	public static int reachNumber(int target) {
-	  target = Math.abs(target);
-	  int k = 0;	
+    public static int reachNumber(int target) {
+      target = Math.abs(target);
+      int k = 0;	
 	  
-	  while (target > 0) {
+      while (target > 0) {
         target -= ++k;
-	  }
+      }
 		
       return target % 2 == 0 ? k : k + 1 + k%2;      
-	}	  
+    }	  
 
-	public static void main(String[] args) {    
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {    
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int number = input.nextInt();
 		  
@@ -23,5 +23,5 @@ import java.io.*;
       System.out.println();
       System.out.println("The minimum number of steps required to reach the destination are: " + minimumNumberOfSteps);
 
-	}
+    }
   }
