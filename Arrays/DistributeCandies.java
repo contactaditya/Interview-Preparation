d100850dc14b919d009fdce20fff03b105f98ce5
Aditya Gupta <contactadityag@gmail.com>
2018-02-15 04:28:22 -0500

    This is a program to return the maximum number of kinds of candies the sister could gain.
    Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister.

--- Arrays/DistributeCandies.java
+++ Arrays/DistributeCandies.java
@@ -3,16 +3,16 @@ import java.io.*;
 
   public class DistributeCandies {
 	  
-	public static int distributeCandies(int[] candies) {
-	  HashSet<Integer> set = new HashSet<Integer>();
-	  for (int candy : candies) {
-	    set.add(candy);
-	  }
-	  return Math.min(set.size(), candies.length/2);	      
-	}	  
+    public static int distributeCandies(int[] candies) {
+      HashSet<Integer> set = new HashSet<Integer>();
+      for (int candy : candies) {
+	set.add(candy);
+      }
+      return Math.min(set.size(), candies.length/2);	      
+    }	  
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number of elements in the array: ");
       int lengthOfArray = input.nextInt();
       int[] candies = new int[lengthOfArray];    
@@ -25,5 +25,5 @@ import java.io.*;
       int distributeCandies = distributeCandies(candies);
       System.out.println();	
       System.out.println("The maximum number of kinds of candies the sister could gain are: " + distributeCandies);	
-	}
+    }
   }
