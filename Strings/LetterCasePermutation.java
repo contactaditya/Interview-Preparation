87c727f257c6eb7753588c42d10345a7e028b037
Aditya Gupta <contactadityag@gmail.com>
2018-02-20 02:01:49 -0500

    This is a program to return a list of all possible strings we could create by transforming every letter to be lowercase or uppercase.

--- Strings/LetterCasePermutation.java
+++ Strings/LetterCasePermutation.java
@@ -3,14 +3,14 @@ import java.io.*;
 
   public class LetterCasePermutation {
 	  
-	public static List<String> letterCasePermutation(String string) {
-	  List<String> result = new ArrayList<String>();
+    public static List<String> letterCasePermutation(String string) {
+      List<String> result = new ArrayList<String>();
       dfs(string.toCharArray(), 0, result);
       return result;    
     }  
 
-	private static void dfs(char[] s, int start, List<String> result) {  
-	  result.add(new String(s));
+    private static void dfs(char[] s, int start, List<String> result) {  
+      result.add(new String(s));
       for (int i = start; i < s.length; i++) {
         char c = s[i];
         if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
@@ -19,17 +19,17 @@ import java.io.*;
           s[i] -= c > 'Z' ? 'A' - 'a' : -('A' - 'a');
         }
       }
-	}
+    }
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);	
-	  String string = new String();	
-	  System.out.print("Enter the string: ");
-	  string = input.next();  
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);	
+      String string = new String();	
+      System.out.print("Enter the string: ");
+      string = input.next();  
 	  
-	  List<String> result = new ArrayList<String>();
-	  result = letterCasePermutation(string); 
-	  System.out.println();
-	  System.out.println("A list of all possible strings we could create are: " + result);
-	}
+      List<String> result = new ArrayList<String>();
+      result = letterCasePermutation(string); 
+      System.out.println();
+      System.out.println("A list of all possible strings we could create are: " + result);
+    }
   }
