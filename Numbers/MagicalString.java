22c08c5af8b99de2f04d23c66a4ec01fe006a771
Aditya Gupta <contactadityag@gmail.com>
2018-02-20 03:51:19 -0500

    This is a program to return the number of '1's in the first N number in the magical string S.

--- Numbers/MagicalString.java
+++ Numbers/MagicalString.java
@@ -3,14 +3,14 @@ import java.io.*;
 
   public class MagicalString {
 	  
-	public static int magicalString(int number) {
-	  if (number <= 0) {
-		return 0;
-	  }
-	  if (number <= 3) {
-		return 1;	
-	  }
-	  int array[] = new int[number + 1];
+    public static int magicalString(int number) {
+      if (number <= 0) {
+	 return 0;
+      }
+      if (number <= 3) {
+	 return 1;	
+      }
+      int array[] = new int[number + 1];
       array[0] = 1; array[1] = 2; array[2] = 2;
       int head = 2, tail = 3, value = 1, count = 1;
       while (tail < number) {
@@ -26,16 +26,16 @@ import java.io.*;
         head++;
       }
       
-	  return count;      
-	}	  
+      return count;      
+    }	  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();   
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int number = input.nextInt();   
 	 
-	  int numberOfOnes = magicalString(number);
-	  System.out.println();
-	  System.out.print("The number of '1's in the first " + number + " in the magical string S: " + numberOfOnes);
-	}
+      int numberOfOnes = magicalString(number);
+      System.out.println();
+      System.out.print("The number of '1's in the first " + number + " in the magical string S: " + numberOfOnes);
+    }
   }
