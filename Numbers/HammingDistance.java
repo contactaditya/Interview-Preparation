cdee51230862321a8a1135b00ff6aaef1099c303
Aditya Gupta <contactadityag@gmail.com>
2018-02-20 02:36:51 -0500

    This is a program to calculate the Hamming distance.
    The Hamming distance between two integers is the number of positions at which the corresponding bits are different.

--- Numbers/HammingDistance.java
+++ Numbers/HammingDistance.java
@@ -3,8 +3,8 @@ import java.io.*;
 
   public class HammingDistance {
 	  
-	public static int hammingDistance(int x, int y) {
-	  int result = x^y;	
+    public static int hammingDistance(int x, int y) {
+      int result = x^y;	
       int count = 0;
       do {
         count += result & 1;
@@ -12,18 +12,18 @@ import java.io.*;
       } while(result != 0);
               
       return count;
-	}	  
+    }	  
 
-	public static void main(String[] args) {        
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the first integer: ");
-	  int x = input.nextInt();  
-	  System.out.println();	
-	  System.out.print("Enter the second integer: ");
-	  int y = input.nextInt();  
+    public static void main(String[] args) {        
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the first integer: ");
+      int x = input.nextInt();  
+      System.out.println();	
+      System.out.print("Enter the second integer: ");
+      int y = input.nextInt();  
 	  
-	  int hammingDistance = hammingDistance(x, y);
-	  System.out.println();	
-	  System.out.print("The hamming distance is: " + hammingDistance);
-	}
+      int hammingDistance = hammingDistance(x, y);
+      System.out.println();	
+      System.out.print("The hamming distance is: " + hammingDistance);
+    }
   }
