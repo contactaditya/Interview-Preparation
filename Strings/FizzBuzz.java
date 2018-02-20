357afd0e39eb66111468ae39014a0095a93bdb1b
Aditya Gupta <contactadityag@gmail.com>
2018-02-20 02:05:44 -0500

    This a program that outputs the string representation of numbers from 1 to n.
    But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are
    multiples of both three and five output “FizzBuzz”.

--- Strings/FizzBuzz.java
+++ Strings/FizzBuzz.java
@@ -3,32 +3,32 @@ import java.io.*;
 
   public class FizzBuzz {
 	  
-	public static List<String> fizzBuzz(int number) {
-	   List<String> result = new ArrayList<String>();
-	   for(int i = 1; i <= number; i++) {  
-		 if (i % 3 == 0 && i % 5 == 0) {
-		   result.add("FizzBuzz");   
-		 } else if(i % 3 == 0) {
-		   result.add("Fizz"); 
-		 } else if(i % 5 == 0) {
-		   result.add("Buzz");		 
-		 }
-	     else {
-		   result.add(String.valueOf(i));
-	     }
-	   }
+    public static List<String> fizzBuzz(int number) {
+       List<String> result = new ArrayList<String>();
+       for(int i = 1; i <= number; i++) {  
+	 if (i % 3 == 0 && i % 5 == 0) {
+	   result.add("FizzBuzz");   
+	 } else if(i % 3 == 0) {
+	   result.add("Fizz"); 
+	 } else if(i % 5 == 0) {
+	   result.add("Buzz");		 
+	 }
+	 else {
+           result.add(String.valueOf(i));
+	 }
+       }
 		
-	   return result;     
+       return result;     
     }	  
 
-	public static void main(String[] args) { 	  
-	   Scanner input = new Scanner(System.in);
-	   System.out.print("Enter number of elements in the array: ");
-	   int number = input.nextInt();   
+    public static void main(String[] args) { 	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int number = input.nextInt();   
    
-	   List<String> result = new ArrayList<String>();
-	   result = fizzBuzz(number);
-	   System.out.println();
-	   System.out.print("The solution set is: " + result);
-	}
+       List<String> result = new ArrayList<String>();
+       result = fizzBuzz(number);
+       System.out.println();
+       System.out.print("The solution set is: " + result);
+    }
   }
