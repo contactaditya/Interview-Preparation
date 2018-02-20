b5e4c5a35f62e72d871246c849a681c17a05c799
Aditya Gupta <contactadityag@gmail.com>
2018-02-20 02:16:41 -0500

    This is a program to return all possible subsets (the power set) given a set of distinct integers array.

--- Arrays/Subsets.java
+++ Arrays/Subsets.java
@@ -3,67 +3,67 @@ import java.io.*;
 
   public class Subsets {
 	 
-	static List<List<Integer>> answer = new ArrayList<List<Integer>>();	  
+    static List<List<Integer>> answer = new ArrayList<List<Integer>>();	  
 	  
     public static List<List<Integer>> subsets(int[] array) {
       helper(array, 0, new ArrayList<Integer>());
       return answer;	 
     }	  
 
-	private static void helper(int[] array, int index, List<Integer> current) {
-      if(index >= array.length){
-	    answer.add(current);
-	    return;
-	  }
+    private static void helper(int[] array, int index, List<Integer> current) {
+      if(index >= array.length) {
+	answer.add(current);
+	return;
+      }
 	        
-	  List<Integer> next = new ArrayList<Integer>(current);
-	  next.add(array[index]);
-	  helper(array, index+1, current);
-	  helper(array, index+1, next);	
-	}
+      List<Integer> next = new ArrayList<Integer>(current);
+      next.add(array[index]);
+      helper(array, index+1, current);
+      helper(array, index+1, next);	
+    }
 
-	public static List<List<Integer>> subsets1(int[] array) {
-	  List<List<Integer>> result = new ArrayList<List<Integer>>();	
-	  if(array.length == 0){
-	    return result;
-	  }	
+    public static List<List<Integer>> subsets1(int[] array) {
+      List<List<Integer>> result = new ArrayList<List<Integer>>();	
+      if(array.length == 0) {
+	return result;
+      }	
 	  
-	  Arrays.sort(array);
-	  dfs(array, 0, new ArrayList<Integer>(), result);
-	  return result;      
-	}
+      Arrays.sort(array);
+      dfs(array, 0, new ArrayList<Integer>(), result);
+      return result;      
+    }
 	  
-	private static void dfs(int[] array, int start, List<Integer> arrayList, List<List<Integer>> result) {
-	  result.add(new ArrayList<Integer>(arrayList));
-	  for(int i = start; i < array.length; i++) {
-		arrayList.add(array[i]);
-	    dfs(array, i + 1, arrayList, result);
-	    arrayList.remove(arrayList.size() - 1);
-	  }	
-	}
+    private static void dfs(int[] array, int start, List<Integer> arrayList, List<List<Integer>> result) {
+      result.add(new ArrayList<Integer>(arrayList));
+      for(int i = start; i < array.length; i++) {
+	arrayList.add(array[i]);
+	dfs(array, i + 1, arrayList, result);
+	arrayList.remove(arrayList.size() - 1);
+      }	
+    }
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 	    
-	  System.out.println();	
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		 array[i] = input.nextInt(); 
-	  }
+      System.out.println();	
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt(); 
+      }
 		
-	  List<List<Integer>> result = new ArrayList<List<Integer>>();
+      List<List<Integer>> result = new ArrayList<List<Integer>>();
 	   
-	  result = subsets(array); 
+      result = subsets(array); 
 	   
-	  System.out.println();
-	  System.out.println("A solution set is: ");
-	  System.out.println('[');
-	  for(List<Integer> innerList : result) {
-		 System.out.println(" " + innerList);
-	  }
-	  System.out.println(']');
-	}
+      System.out.println();
+      System.out.println("A solution set is: ");
+      System.out.println('[');
+      for(List<Integer> innerList : result) {
+	System.out.println(" " + innerList);
+      }
+      System.out.println(']');
+    }
   }
