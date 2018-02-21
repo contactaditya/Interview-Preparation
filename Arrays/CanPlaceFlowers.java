14639eeadf833ef98ca01e9fcc24cb11f2a9bee2
contactaditya <contactadityag@gmail.com>
2018-02-21 06:09:18 -0500

    This is a program to find the complement of a positive integer.

---
+++ Arrays/CanPlaceFlowers.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+  public class CanPlaceFlowers {
+	  
+	public static boolean canPlaceFlowers(int[] flowerbed, int n) {   
+	  int count = 0;	
+	  for (int i = 0; i < flowerbed.length; i++) {
+        if(flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+          flowerbed[i] = 1;	
+          count++;
+        }
+      }
+
+	  return count >= n;      
+	}	  
+
+	public static void main(String[] args) { 	  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter number of elements in the array: ");
+	  int numbers = input.nextInt();   
+	  int flowerbed[] = new int[numbers];
+	    
+	  System.out.println();
+	  System.out.print("Please enter the actual values in the array: ");
+	  for (int i = 0; i < numbers; i++) {
+		flowerbed[i] = input.nextInt(); 
+	  }
+	  
+	  System.out.println();
+	  System.out.print("Enter the number: ");
+	  int n = input.nextInt();  
+	   
+	  boolean canBePlanted = canPlaceFlowers(flowerbed, n);
+	  System.out.println();
+	  if(canBePlanted) {
+		System.out.println("Yes " + n + " new flowers can be planted in it without violating the no-adjacent-flowers rule."); 
+	  }
+	  else {
+		System.out.println("No " + n + " new flowers cannot be planted in it without violating the no-adjacent-flowers rule."); 
+	  }
+	}
+  }
---
+++ Numbers/NumberComplement.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+  public class NumberComplement {
+	  
+	public static int findComplement(int number) {
+	  return ~number & (Integer.highestOneBit(number) - 1); 
+	} 
+
+	public static void main(String[] args) {     
+	  Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int number = input.nextInt();  
+		 		
+      int complementNumber = findComplement(number);
+
+      System.out.println();	
+      System.out.print("The complement number of the given integer is: " + complementNumber);
+	}
+  }
