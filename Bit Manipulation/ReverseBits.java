b36d7d04519523c370f0b2b4c4fc5430cf47d34d
Aditya Gupta <contactadityag@gmail.com>
2018-02-22 01:38:32 -0500

    This is a program to reverse bits of a given 32 bits unsigned integer.

--- Bit Manipulation/ReverseBits.java
+++ Bit Manipulation/ReverseBits.java
@@ -3,10 +3,10 @@ import java.io.*;
 
   public class ReverseBits {
 	  
-	public static int reverseBits(int number) { 
-	  if (number == 0) {
-		return 0;
-	  }
+    public static int reverseBits(int number) { 
+      if (number == 0) {
+	return 0;
+      }
     
       int result = 0;
       for (int i = 0; i < 32; i++) {
@@ -14,16 +14,16 @@ import java.io.*;
         number = number >> 1;  
       }
 	
-	  return result;     
-	}  
+      return result;     
+    }  
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int number = input.nextInt();
 		  
       int result = reverseBits(number);
       System.out.println();
-	  System.out.println("The reverse of " + number + " in bits is: " + result);
-	}
+      System.out.println("The reverse of " + number + " in bits is: " + result);
+    }
   }
