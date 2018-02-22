05e915ef0ff993f0ea40c5cd20b186bf541060ce
Aditya Gupta <contactadityag@gmail.com>
2018-02-22 00:52:05 -0500

    This is a program to check if the letters can be rearranged so that two characters that are adjacent to each other are not the same.

--- Strings/ReorganizeString.java
+++ Strings/ReorganizeString.java
@@ -3,21 +3,21 @@ import java.io.*;
  
   public class ReorganizeString {
 	  
-	public static String reorganizeString(String string) {
-	  int[] counts = new int[26];
+    public static String reorganizeString(String string) {
+      int[] counts = new int[26];
 	  
-	  for (char c: string.toCharArray()) {
-		counts[c-'a'] += 100;
-	  }
+      for (char c: string.toCharArray()) {
+	counts[c-'a'] += 100;
+      }
 	  
-	  for (int i = 0; i < 26; ++i) {
-		counts[i] += i;
-	  }
-	  Arrays.sort(counts);
+      for (int i = 0; i < 26; ++i) {
+	counts[i] += i;
+      }
+      Arrays.sort(counts);
 	  
-	  char[] answer = new char[string.length()];
-	  int t = 1;
-	  for (int code : counts) {
+      char[] answer = new char[string.length()];
+      int t = 1;
+      for (int code : counts) {
         int ct = code / 100;
         char ch = (char) ('a' + (code % 100));
         if (ct > (string.length() + 1) / 2) {
@@ -32,19 +32,19 @@ import java.io.*;
         }
       }
 	 	
-	  return String.valueOf(answer);      
-	}	  
+      return String.valueOf(answer);      
+    }	  
 
-	public static void main(String[] args) {     
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {     
+      String string = new String();	  
+      Scanner input = new Scanner(System.in);
 
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine();
+      System.out.print("Enter the string: ");
+      string = input.nextLine();
 		  
-	  String result = reorganizeString(string);
-	  System.out.println();
-	  System.out.println("The string after the letters can be rearranged so that two characters that are adjacent to each other are not the same is: " + result);  
+      String result = reorganizeString(string);
+      System.out.println();
+      System.out.println("The string after the letters can be rearranged so that two characters that are adjacent to each other are not the same is: " + result);  
 	 
-	}
+    }
   }
