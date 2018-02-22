e458432eeca4296e81f35e7dc8e5e0fe25730532
contactaditya <contactadityag@gmail.com>
2018-02-22 00:35:13 -0500

    This is a program to judge whether the usage of capitals in it is right or not.
    We define the usage of capitals in a word to be right when one of the
    following cases holds:
    
    1) All letters in this word are capitals, like "USA".
    
    2) All letters in this word are not capitals, like "leetcode".
    
    3) Only the first letter in this word is capital if it has more than one
    letter, like "Google".

---
+++ Strings/DetectCapital.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+  public class DetectCapital {
+	  
+	public static boolean detectCapitalUse(String word) {
+	  int count = 0;
+	  for(char c: word.toCharArray()) {
+		if('Z' - c >= 0) {
+		  count++;
+		}
+	  }
+	  return ((count == 0 || count == word.length()) || (count == 1 && 'Z' - word.charAt(0) >= 0));     
+	}  
+
+	public static void main(String[] args) {    
+	  String word = new String();	  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the word: ");
+	  word = input.nextLine();  
+	    
+	  boolean detectCapitalUse = detectCapitalUse(word);
+	  System.out.println();
+	  if(detectCapitalUse) {
+		System.out.println("The usage of capitals in the word is right.");  
+	  }
+      else {
+		System.out.println("The usage of capitals in the word is not right.");  
+	  }
+	}
+  }
