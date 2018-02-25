8fd45262c4346e8340d48d78e47a9fe3863df8a6
Aditya Gupta <contactadityag@gmail.com>
2018-02-24 23:51:06 -0500

    This is a program to design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
    push(x) -- Push element x onto stack.
    
    pop() -- Removes the element on top of the stack.
    
    top() -- Get the top element.
    
    getMin() -- Retrieve the minimum element in the stack.

--- Stacks/MinStack.java
+++ Stacks/MinStack.java
@@ -3,54 +3,53 @@ import java.io.*;
 
   public class MinStack {  
 	
-	private int min;
-	private java.util.Stack<Integer> stack;
-	
-	/** initialize your data structure here. */
-	public MinStack() {
-	  stack = new java.util.Stack<Integer>();     
-	  min = Integer.MAX_VALUE;
-	}
+    private int min;
+    private java.util.Stack<Integer> stack;
+	 
+     /** initialize your data structure here. */
+    public MinStack() {
+       stack = new java.util.Stack<Integer>();     
+       min = Integer.MAX_VALUE;
+    }
 	    
-	public void push(int x) {     
-	 /**
-	  * Add an item to back of stack.
-	  */
+    public void push(int x) {     
+      /**
+       * Add an item to back of stack.
+       */
 			   
-	  if(x <= min) {          
-	    stack.push(min);
-	    min = x;
-	  }
+       if(x <= min) {          
+	 stack.push(min);
+	 min = x;
+       }
 	        
-	  stack.push(x);  
-	}
+       stack.push(x);  
+    }
 	    
-	public void pop() {
-	 /**
-	  * Removes the next item from the front of the stack.	 
-	  */	 	
+    public void pop() {
+      /**
+       * Removes the next item from the front of the stack.	 
+       */	 	
       if(stack.pop() == min) {
     	min = stack.pop();
       }
-	}
+    }
 	    
-	public int top() {
-	  return stack.peek();
-	}
+    public int top() {
+      return stack.peek();
+    }
 	    
-	public int getMin() { 
-	  /**
-	   * It finds the minimum value of the stack.
-	   */	 
-						  
-	   return min;
-	}
+    public int getMin() { 
+     /**
+      * It finds the minimum value of the stack.
+      */	 					  
+       return min;
+    }
 
-	public static void main(String[] args) throws IOException {   
-	  MinStack minStack = new MinStack();	
-	  int choice = 0;
+    public static void main(String[] args) throws IOException {   
+      MinStack minStack = new MinStack();	
+      int choice = 0;
     
-      do{	
+       do{	
     	  System.out.println("1. For inserting an element in the stack.");
     	  System.out.println("2. For deleting an element from the stack.");
     	  System.out.println("3. For displaying the top element of the stack.");
@@ -63,23 +62,23 @@ import java.io.*;
     	  choice = Integer.parseInt(br.readLine());
     	  switch (choice) {
     	  	case 1:  System.out.println();
-    	  	   		 System.out.print("Enter a element to insert in the stack: ");
-    	  	   		 BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));  	
-    	  	   		 int element = Integer.parseInt(br1.readLine());    	  	   	     
-    	  	   		 minStack.push(element);
-    	  	   		 break;
+    	  	   	 System.out.print("Enter a element to insert in the stack: ");
+    	  	   	 BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));  	
+    	  	   	 int element = Integer.parseInt(br1.readLine());    	  	   	     
+    	  	   	 minStack.push(element);
+    	  	   	 break;
     	  	case 2:  minStack.pop();
                		 break;
     	  	case 3:  int top = minStack.top();
-    	  	   		 System.out.println();
-    	  	   		 System.out.println("The top element of the stack is: " + top);
-    	  	   		 break;
+    	  	   	 System.out.println();
+    	  	   	 System.out.println("The top element of the stack is: " + top);
+    	  	   	 break;
     	  	case 4:  int getMin = minStack.getMin();
-    	  	   		 System.out.println();
-    	  	   		 System.out.println("The minimum element in the stack is: " + getMin);
-    	  	   		 break;     
+    	  	         System.out.println();
+    	  	         System.out.println("The minimum element in the stack is: " + getMin);
+    	  	   	 break;     
           }
     	  System.out.println();
-		} while(choice!=5);
-	}
+       } while(choice!=5);
+    }
   }
