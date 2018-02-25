f999b9cfc0fc9b14ce9c8745582a917833c1ff33
Aditya Gupta <contactadityag@gmail.com>
2018-02-25 00:27:05 -0500

    This is a program to convert an integer into an hexadecimal.

--- Numbers/ConvertANumberToHexadecimal.java
+++ Numbers/ConvertANumberToHexadecimal.java
@@ -3,22 +3,22 @@ import java.io.*;
 
   public class ConvertANumberToHexadecimal {
 	  
-	static char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};	  
+    static char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};	  
 	  
-	public static String toHex(int number) { 
-	  if(number == 0) {
-		return "0";
-	  }
-	  String result = "";
+    public static String toHex(int number) { 
+      if(number == 0) {
+	return "0";
+      }
+      String result = "";
       while(number != 0){
         result = map[(number & 15)] + result; 
         number = (number >>> 4);
       }
       return result;    
-	}	 
+    }	 
 	
-	public static void main(String[] args) {       
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {       
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int number = input.nextInt();  
 
@@ -26,5 +26,5 @@ import java.io.*;
       System.out.println();	
       System.out.print("The number " + number + " in hexadecimal form is: " + result);
 
-	}
+    }
   }
