b11919f428d9fa3061b6187689913af7acc6d0de
Aditya Gupta <contactadityag@gmail.com>
2018-02-24 23:54:19 -0500

    This is a program to find the minimum no of moves required to make all array elements equal, where a move is +1 or -1 a selected element.

--- Arrays/MinimumMovesToEqualArrayElementsII.java
+++ Arrays/MinimumMovesToEqualArrayElementsII.java
@@ -3,12 +3,12 @@ import java.io.*;
 
   public class MinimumMovesToEqualArrayElementsII {
 	  
-	public static int minMoves2(int[] array) { 
- 	  if (array == null || array.length <= 1) {
-		return 0;
-	  }
-	  Arrays.sort(array);
-	  int i = 0, j = array.length-1;
+    public static int minMoves2(int[] array) { 
+      if (array == null || array.length <= 1) {
+	return 0;
+      }
+      Arrays.sort(array);
+      int i = 0, j = array.length-1;
       int count = 0;
       while(i < j) {
         count += array[j] - array[i];
@@ -16,23 +16,23 @@ import java.io.*;
         j--;
       }
       return count;
-	} 
+    } 
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int numbers = input.nextInt();   
       int array[] = new int[numbers];
 
       System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-	   array[i] = input.nextInt();
-	  }
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt();
+      }
 	  
-	  int minimumNumberOfMoves = minMoves2(array);
+      int minimumNumberOfMoves = minMoves2(array);
 	
-	  System.out.println();
-	  System.out.print("The minimum number of moves required to make all array elements equal are: " + minimumNumberOfMoves);
-	}
+      System.out.println();
+      System.out.print("The minimum number of moves required to make all array elements equal are: " + minimumNumberOfMoves);
+    }
   }
