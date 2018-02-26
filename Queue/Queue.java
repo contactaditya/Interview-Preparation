37ecfc33934e2efc603a8aa0711d01f224011cbe
Aditya Gupta <contactadityag@gmail.com>
2018-02-26 03:32:08 -0500

    This is the program to implement a queue in java using Linked List.

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,76 +1,74 @@
 import java.io.*;
 import java.util.*;
 
-public class Queue {
+  public class Queue {
 	
-  private static LinkedList<Object> queue = new LinkedList<Object>();
+    private static LinkedList<Object> queue = new LinkedList<Object>();
 	
-  public static void enqueue(Object object) {
-   /**
-    * Add an item to back of queue.
-    */
-    queue.addLast(object);
-  }
-
- public static Object dequeue() {
-   /**
-    * Removes the next item from the front of the queue.	 
-    */	 
-    if(queue.isEmpty()) {
-      System.out.println("The queue is already empty. No element can be removed from the queue.");  
-      return -1;
+    public static void enqueue(Object object) {
+      /**
+       * Add an item to back of queue.
+       */
+       queue.addLast(object);
     }
-   return queue.removeFirst();
- }
 
- public static boolean isEmpty() {
-  /**
-   * Test if the queue is empty.
-   */	
-   if(queue.isEmpty()) {	   
-   System.out.println("The queue is currently empty and has no elements.");	 	  	   
-   }
-   else {
-   System.out.println("The queue is currently not empty.");	 		   
-   }
-   return queue.isEmpty();
- }
- 
- public static int size() {
-   /**
-    * Returns the number of items currently in the queue.
-    */
-   System.out.println("The size of the queue is: " + queue.size());	 
-   return queue.size();
- }
- 
- public static void display() {
-   /**
-    * Displays all the contents of the queue.
-    */
-   System.out.println("Contents of the queue are: " + queue);
- }
- 
- public static Object peek() {
-   /**
-    * Returns the front item from the queue without popping it.
-    */	 
-    if(queue.isEmpty()) {  
-      System.out.println("The queue is empty so we can't see the front item of the queue.");  
-      return -1;
-    }	 
-    else {
-      System.out.println("The following element is the top element of the queue:" + queue.getFirst());  	  
+    public static Object dequeue() {
+      /**
+       * Removes the next item from the front of the queue.	 
+       */	 
+       if(queue.isEmpty()) {
+         System.out.println("The queue is already empty. No element can be removed from the queue.");  
+         return -1;
+       }
+       return queue.removeFirst();
     }
-   return queue.getFirst();
- }
+
+    public static boolean isEmpty() {
+      /**
+       *Test if the queue is empty.
+       */	
+       if(queue.isEmpty()) {	   
+         System.out.println("The queue is currently empty and has no elements.");	 	  	   
+       }
+       else {
+         System.out.println("The queue is currently not empty.");	 		   
+       }
+       return queue.isEmpty();
+    }
+ 
+    public static int size() {
+      /**
+       * Returns the number of items currently in the queue.
+       */
+       System.out.println("The size of the queue is: " + queue.size());	 
+       return queue.size();
+    }
+ 
+    public static void display() {
+      /**
+       * Displays all the contents of the queue.
+       */
+       System.out.println("Contents of the queue are: " + queue);
+    }
+ 
+    public static Object peek() {
+      /**
+       * Returns the front item from the queue without popping it.
+       */	 
+       if(queue.isEmpty()) {  
+         System.out.println("The queue is empty so we can't see the front item of the queue.");  
+         return -1;
+       }	 
+       else {
+         System.out.println("The following element is the top element of the queue:" + queue.getFirst());  	  
+       }
+       return queue.getFirst();
+    }
  	
-  public static void main(String[] args) throws NumberFormatException, IOException {
-    // TODO Auto-generated method stub
-	  
-    int choice = 0;
+    public static void main(String[] args) throws NumberFormatException, IOException {  
+      int choice = 0;
     
-    do{	
+      do{	
         System.out.println("1. For inserting an element in the queue.");
     	System.out.println("2. For deleting an element from the queue.");
     	System.out.println("3. For displaying the contents of the queue.");
@@ -98,8 +96,8 @@ public class Queue {
       	           break;  
           case 6:  peek();
 	           break;     
-       }
-      System.out.print("\n");
-    } while(choice!=7);
-   }
+        }
+        System.out.print("\n");
+      } while(choice!=7);
+    }
   }
