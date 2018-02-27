b1403e3d0c1f36c41cd36bff82000fd7d4563508
Aditya Gupta <contactadityag@gmail.com>
2018-02-27 04:53:45 -0500

    This is the program for finding the continuos subarray which has the largest sum. It implements the Kadane's Algorithm in java.

--- Arrays/MaximumSubArray.java
+++ Arrays/MaximumSubArray.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class MaximumSubArray {
 
   public static int maxSubArraySum(int[] array) {
-
     int max = array[0];
     int sum[] = new int[array.length];
     sum[0] = array[0];
@@ -13,25 +12,24 @@ public class MaximumSubArray {
        sum[i] = Math.max(array[i], sum[i-1] + array[i]);
        max = Math.max(max, sum[i]);
     }
-   return max; 	
+    return max; 	
   }	
 	
   public static void main(String[] args) throws NumberFormatException, IOException {
-	  
-       Scanner input = new Scanner(System.in);
-       System.out.print("Enter number of elements in the first array: ");
-       int number = input.nextInt();  
-       int array[] = new int[number];
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter number of elements in the first array: ");
+    int number = input.nextInt();  
+    int array[] = new int[number];
 	
-       System.out.println();	
-       System.out.print("Please enter number: ");
-       for (int i = 0; i < array.length; i++) {
-	  array[i] = input.nextInt();
-       }
+    System.out.println();	
+    System.out.print("Please enter number: ");
+    for (int i = 0; i < array.length; i++) {
+      array[i] = input.nextInt();
+    }
 		
-       int max = maxSubArraySum(array);
-       System.out.print("\n");
+    int max = maxSubArraySum(array);
+    System.out.print("\n");
 	    
-       System.out.println("The value of the sum of the maximum contiguous subarray within an array is: " + max);
-   }     
- }
+    System.out.println("The value of the sum of the maximum contiguous subarray within an array is: " + max);
+  }     
+}
