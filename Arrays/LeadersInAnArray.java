30fd643ea9b944e9548c2c567769b674abb193b9
Aditya Gupta <contactadityag@gmail.com>
2018-02-28 12:04:47 -0500

    This is a program to print all the leaders in the array.
    An element is leader if it is greater than all the elements to its right side. And the rightmost element is always a leader.

--- Arrays/LeadersInAnArray.java
+++ Arrays/LeadersInAnArray.java
@@ -3,11 +3,11 @@ import java.io.*;
  
   public class LeadersInAnArray { 	
 	
-	public static void printLeaders(int array[]) { 
-	  int maxFromRight = array[array.length - 1];	
-	  /* Rightmost element is always leader */
-	  System.out.println();
-	  System.out.print("All the leaders in the array are: ");
+    public static void printLeaders(int array[]) { 
+      int maxFromRight = array[array.length - 1];	
+      /* Rightmost element is always leader */
+      System.out.println();
+      System.out.print("All the leaders in the array are: ");
       System.out.print(maxFromRight + " ");
       for (int i = array.length - 2; i >= 0; i--) {
         if(maxFromRight < array[i]) {           
@@ -15,21 +15,21 @@ import java.io.*;
           System.out.print(maxFromRight + " ");
         }
       }    
-	}
+    }
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 	    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt(); 
+      }
 	   
-	  printLeaders(array);
+      printLeaders(array);
 	  
-	}
+    }
   }
