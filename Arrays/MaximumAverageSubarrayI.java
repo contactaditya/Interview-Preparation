dffc58bc12125f14205c154d9d635830c9ba6037
Aditya Gupta <contactadityag@gmail.com>
2018-02-28 12:17:21 -0500

    This is a program to find the contiguous subarray of given length k that has the maximum average value.

--- Arrays/MaximumAverageSubarrayI.java
+++ Arrays/MaximumAverageSubarrayI.java
@@ -3,9 +3,9 @@ import java.io.*;
 
   public class MaximumAverageSubarrayI {
 	
-	public static double findMaxAverage(int[] array, int k) {
-	  double sum = 0;
-	  for(int i = 0; i < k; i++) {
+    public static double findMaxAverage(int[] array, int k) {
+      double sum = 0;
+      for(int i = 0; i < k; i++) {
         sum += array[i];
       }
       double maxSum = sum;
@@ -14,28 +14,27 @@ import java.io.*;
         maxSum = Math.max(maxSum,sum);
       }
       
-	  return maxSum/k; 
+      return maxSum/k; 
     }
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();  
+      int array[] = new int[number];
 		
-	  System.out.println();	
-	  System.out.print("Please enter the elements of the array: ");
-	  for (int i = 0; i < array.length; i++) {
-	    array[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the elements of the array: ");
+      for (int i = 0; i < array.length; i++) {
+	 array[i] = input.nextInt();
+      }
 	  
-	  System.out.println();	
-	  System.out.print("Enter the length of the contiguous subarray: ");
-	  int k = input.nextInt();  
+      System.out.println();	
+      System.out.print("Enter the length of the contiguous subarray: ");
+      int k = input.nextInt();  
 			
       double maxAverage = findMaxAverage(array, k);
       System.out.println();	
-	  System.out.println("The maximum average value in the the contiguous subarray of given length " + k + " is: " + maxAverage);
-
-	}
+      System.out.println("The maximum average value in the the contiguous subarray of given length " + k + " is: " + maxAverage);
+    }
   }
