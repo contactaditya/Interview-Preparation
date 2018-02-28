a164fd440b2ef1b4406e747d001401dbc13978c2
Aditya Gupta <contactadityag@gmail.com>
2018-02-28 12:35:50 -0500

    This is a program to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.

--- Strings/RepeatedDNASequences.java
+++ Strings/RepeatedDNASequences.java
@@ -3,9 +3,9 @@ import java.io.*;
 
   public class RepeatedDNASequences {
 	  
-	public static List<String> findRepeatedDnaSequences(String string) {
-	  Set<String> seen = new HashSet<String>();
-	  Set<String> repeated = new HashSet<String>();
+    public static List<String> findRepeatedDnaSequences(String string) {
+      Set<String> seen = new HashSet<String>();
+      Set<String> repeated = new HashSet<String>();
 
       for(int i = 0; i <= string.length() - 10; i++) {
         String substring = string.substring(i, i + 10);
@@ -14,19 +14,18 @@ import java.io.*;
         }
       }
       
-	  return new LinkedList<String>(repeated);   
-	}	  
+      return new LinkedList<String>(repeated);   
+    }	  
 
-	public static void main(String[] args) {   
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine(); 
+    public static void main(String[] args) {   
+      String string = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the string: ");
+      string = input.nextLine(); 
 	  
-	  List<String> result = new ArrayList<String>();
-	  result = findRepeatedDnaSequences(string);
-	  System.out.println();
-	  System.out.println("All the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule are: " + result);
-	
-	}
+      List<String> result = new ArrayList<String>();
+      result = findRepeatedDnaSequences(string);
+      System.out.println();
+      System.out.println("All the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule are: " + result);
+    }
   }
