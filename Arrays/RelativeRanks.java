914e19f935c47b0c52099c55007bc757fe033dca
Aditya Gupta <contactadityag@gmail.com>
2018-02-28 12:39:19 -0500

    This is a program to find the relative ranks and the people with the top three highest scores and who will be awarded medals.

--- Arrays/RelativeRanks.java
+++ Arrays/RelativeRanks.java
@@ -3,17 +3,17 @@ import java.io.*;
 
   public class RelativeRanks {
 	  
-	public static String[] findRelativeRanks(int[] array) {
-	  String[] result = new String[array.length];	
-	  if(array == null || array.length == 0) {
-		return new String[0];
-	  }
-	  Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-	  for(int i = 0; i < array.length; i++){
+    public static String[] findRelativeRanks(int[] array) {
+      String[] result = new String[array.length];	
+      if(array == null || array.length == 0) {
+	return new String[0];
+      }
+      Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+      for(int i = 0; i < array.length; i++){
         map.put(array[i], i);
       }
-	  Arrays.sort(array);
-	  for (int i = 0; i < array.length; i++) {
+      Arrays.sort(array);
+      for (int i = 0; i < array.length; i++) {
         if (i == array.length-1) {
           result[map.get(array[i])] = "Gold Medal";
         }
@@ -28,32 +28,32 @@ import java.io.*;
         }
       }
 	
-	  return result;      
-	}	  
+      return result;      
+    }	  
 
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt();
-	  }
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt();
+      }
 	  	  
-	  String result[] = findRelativeRanks(array);
-	  System.out.println();	
-	  System.out.print("The relative ranks and the people with the top three highest scores, who will be awarded medals: Gold Medal, Silver Medal and Bronze Medal are: ");
-	  System.out.print("[");	
-	  for (int i=0; i < result.length; i++) {
-		if(i != result.length - 1) {  
-		  System.out.print(result[i] + ", ");
-		}
-		else {
-		  System.out.print(result[i] + "]");	
-		}
-	  }
+      String result[] = findRelativeRanks(array);
+      System.out.println();	
+      System.out.print("The relative ranks and the people with the top three highest scores, who will be awarded medals: Gold Medal, Silver Medal and Bronze Medal are: ");
+      System.out.print("[");	
+      for (int i=0; i < result.length; i++) {
+	if(i != result.length - 1) {  
+	  System.out.print(result[i] + ", ");
 	}
+	else {
+	  System.out.print(result[i] + "]");	
+	}
+      }
+    }
   }
