028288b9389e036bd0fee9fb27e999e967823ba2
Aditya Gupta <contactadityag@gmail.com>
2018-03-03 03:43:43 -0500

    This is the solution to the String Rotation problem.

--- Cracking The Coding Interview Book/Chapter 1/StringRotation.java
+++ Cracking The Coding Interview Book/Chapter 1/StringRotation.java
@@ -1,29 +1,29 @@
 import java.util.*;
 
-public class StringRotation {
+ public class StringRotation {
 
-  public static boolean isRotation(String string1, String string2) {
+   public static boolean isRotation(String string1, String string2) {
      int length = string1.length();
 	
      // Check that string 1 and string 2 are equal length and not empty
 	  
      if(length == string2.length() && length > 0) {
-     // Concatenate string 1 and string 1 within new buffer
+       // Concatenate string 1 and string 1 within new buffer
        String s1s1 = string1 + string1;
        return isSubstring(s1s1, string2); 
      }	  
-   return false;
-  }	
+     return false;
+   }	
   
-  public static boolean isSubstring(String big, String small) {
+   public static boolean isSubstring(String big, String small) {
      if (big.indexOf(small) >= 0) {
        return true;
      } else {
        return false;
      }   
-  }
+   }
   
-  public static void main(String[] args) { 
+   public static void main(String[] args) { 
      String string1 = new String();	  
      Scanner input1 = new Scanner(System.in); 
      System.out.print("Enter the first string: ");
@@ -36,14 +36,12 @@ public class StringRotation {
      string2 = input2.next();
 	  
      boolean subsequence = isRotation(string1, string2);
-	 
-     if(subsequence) {
-	System.out.println();
-	System.out.println("The string can be rotated.");  
+     System.out.println(); 
+     if(subsequence) { 
+       System.out.println("The string can be rotated.");  
      }
      else {
-	System.out.println();
-	System.out.println("The string cannot be rotated.");  
+       System.out.println("The string cannot be rotated.");  
      }
-  }
-}
+   }
+ }
