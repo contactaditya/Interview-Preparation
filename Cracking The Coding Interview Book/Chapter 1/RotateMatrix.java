2d2d14ab7a7b64533cde09847c9eb9d0de95d058
Aditya Gupta <contactadityag@gmail.com>
2018-03-03 03:41:36 -0500

    This is the solution to the Rotate Matrix problem.

--- Cracking The Coding Interview Book/Chapter 1/RotateMatrix.java
+++ Cracking The Coding Interview Book/Chapter 1/RotateMatrix.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
- public class RotateMatrix {
+  public class RotateMatrix {
+	  
     public static boolean rotate(int[][] array) {
        if(array.length == 0 || array.length != array[0].length) {
 	  return false;
@@ -20,48 +21,47 @@ import java.util.*;
 	    // right -> bottom
 	    array[last][last - offset] = array[i][last];
 	    // top -> right
-	    array[i][last] = top;
-			 
+	    array[i][last] = top;	 
 	  }
        }
-    return true;
-   }
+       return true;
+    }
 	
- private static void printMatrix(int[][] array) {
-    System.out.println(); 
-    System.out.println("The matrix after rotation is: "); 
-    System.out.println();
-    for (int i = 0; i < array.length; i++) {
-      for (int j = 0; j < array[i].length; j++) {
-	 System.out.print(array[i][j] + " ");
-      } 
-    System.out.println();
-  }	
-}
+    private static void printMatrix(int[][] array) {
+       System.out.println(); 
+       System.out.println("The matrix after rotation is: "); 
+       System.out.println();
+       for (int i = 0; i < array.length; i++) {
+         for (int j = 0; j < array[i].length; j++) {
+	   System.out.print(array[i][j] + " ");
+         } 
+         System.out.println();
+       }	
+    }
 	
-public static void main(String[] args) { 	  
-   Scanner input = new Scanner(System.in);
-   System.out.print("Please enter the number of rows in the matrix: ");
-   int x = input.nextInt();  
-   System.out.println();
-   System.out.print("Please enter the number of columns in the matrix: ");
-   int y = input.nextInt();  
-   int[][] array = new int[x][y];
-   System.out.println();
-   System.out.println("Please enter the actual values in the matrix: ");
-   for(int i=0; i < x; i++) {
-     for(int j=0; j < y; j++){
-        array[i][j] = input.nextInt();
-     }
-   }
+    public static void main(String[] args) { 	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Please enter the number of rows in the matrix: ");
+       int x = input.nextInt();  
+       System.out.println();
+       System.out.print("Please enter the number of columns in the matrix: ");
+       int y = input.nextInt();  
+       int[][] array = new int[x][y];
+       System.out.println();
+       System.out.println("Please enter the actual values in the matrix: ");
+       for(int i=0; i < x; i++) {
+         for(int j=0; j < y; j++){
+           array[i][j] = input.nextInt();
+         }
+       }
 	  
-   boolean rotate = rotate(array); 
-   if(rotate) {
-      printMatrix(array);
-   }
-   else {
-      System.out.println();
-      System.out.println("The matrix cannot be rotated.");  
-   }
+       boolean rotate = rotate(array); 
+       if(rotate) {
+         printMatrix(array);
+       }
+       else {
+         System.out.println();
+         System.out.println("The matrix cannot be rotated.");  
+       }
+    }
   }
- }
