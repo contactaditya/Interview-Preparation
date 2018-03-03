1944447bbd835772f4e010384f6043528827c260
Aditya Gupta <contactadityag@gmail.com>
2018-03-03 04:05:26 -0500

    This is the solution to the Zero Matrix problem.

--- Cracking The Coding Interview Book/Chapter 1/ZeroMatrix.java
+++ Cracking The Coding Interview Book/Chapter 1/ZeroMatrix.java
@@ -40,35 +40,35 @@ import java.util.*;
       }
    }
 	
- private static void printMatrix(int[][] array) {
-    System.out.println();
-    System.out.println("The matrix after entire row and column are set to 0 is: "); 
-    System.out.println();
-    for (int i = 0; i < array.length; i++) {
-      for (int j = 0; j < array[i].length; j++) {
-	 System.out.print(array[i][j] + " ");
-      }
-    System.out.println();
-   }	
- }
+   private static void printMatrix(int[][] array) {
+      System.out.println();
+      System.out.println("The matrix after entire row and column are set to 0 is: "); 
+      System.out.println();
+      for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array[i].length; j++) {
+	  System.out.print(array[i][j] + " ");
+        }
+        System.out.println();
+      }	
+   }
 
- public static void main(String[] args) {   
-    Scanner input = new Scanner(System.in);
-    System.out.print("Please enter the number of rows in the matrix: ");
-    int x = input.nextInt();  
-    System.out.println();
-    System.out.print("Please enter the number of columns in the matrix: ");
-    int y = input.nextInt();  
-    int[][] array = new int[x][y];
-    System.out.println();
-    System.out.println("Please enter the actual values in the matrix: ");
-    for(int i=0; i < x; i++) {
-       for(int j=0; j < y; j++){
-         array[i][j] = input.nextInt();
-       }
-    }
+   public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Please enter the number of rows in the matrix: ");
+      int x = input.nextInt();  
+      System.out.println();
+      System.out.print("Please enter the number of columns in the matrix: ");
+      int y = input.nextInt();  
+      int[][] array = new int[x][y];
+      System.out.println();
+      System.out.println("Please enter the actual values in the matrix: ");
+      for(int i=0; i < x; i++) {
+        for(int j=0; j < y; j++){
+          array[i][j] = input.nextInt();
+        }
+      }
 	  
-    setZeroes(array); 
-    printMatrix(array);
-  }
-}
+      setZeroes(array); 
+      printMatrix(array);
+   }
+ } 
