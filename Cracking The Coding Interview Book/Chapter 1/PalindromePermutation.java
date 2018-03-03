1206ace76e6d2098f64e5ad46fae47bca0932be0
Aditya Gupta <contactadityag@gmail.com>
2018-03-03 03:09:54 -0500

    This is the solution to the Palindrome Permutation problem.

--- Cracking The Coding Interview Book/Chapter 1/PalindromePermutation.java
+++ Cracking The Coding Interview Book/Chapter 1/PalindromePermutation.java
@@ -21,40 +21,40 @@ import java.io.*;
       return true;
     }
     
-  public static int getCharNumber(Character c) {
-     int a = Character.getNumericValue('a');
-     int z = Character.getNumericValue('z');
-     int value = Character.getNumericValue(c);
+    public static int getCharNumber(Character c) {
+      int a = Character.getNumericValue('a');
+      int z = Character.getNumericValue('z');
+      int value = Character.getNumericValue(c);
 	  
-     if (a <= value && value <= z) {
+      if (a <= value && value <= z) {
 	return value - a;
-     }
-    return -1;
-  }
-    
- public static int[] buildCharFrequencyTable(String phrase) {
-    int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
-    for (char c : phrase.toCharArray()) {
-       int x = getCharNumber(c);
-       if (x != -1) {
-	 table[x]++;
-       }
+      }
+      return -1;
     }
-  return table;
- }
+    
+    public static int[] buildCharFrequencyTable(String phrase) {
+      int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+      for (char c : phrase.toCharArray()) {
+        int x = getCharNumber(c);
+        if (x != -1) {
+	  table[x]++;
+        }
+      }
+      return table;
+    }
     	 
- public static void main(String[] args) {
-     String string = new String();	  
-     Scanner input = new Scanner(System.in);
-     System.out.print("Enter the first string: ");
-     string = input.nextLine(); 
+    public static void main(String[] args) {
+      String string = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the first string: ");
+      string = input.nextLine(); 
 		  
-     System.out.println();
-     if(isPermutationOfPalindrome(string)) {
+      System.out.println();
+      if(isPermutationOfPalindrome(string)) {
 	System.out.println("The string is a permutation of the palindrome.");
-     }
-     else {
+      }
+      else {
         System.out.println("The string is not a permutation of the palindrome.");  
-     }
-   }
+      }
+    }
  } 
