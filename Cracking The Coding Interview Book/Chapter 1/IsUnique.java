65cd7d7bd42bd60e72670693a4d8d02cd7dea9a7
Aditya Gupta <contactadityag@gmail.com>
2018-03-03 03:03:13 -0500

    This is the solution to the Is Unique Problem.

--- Cracking The Coding Interview Book/Chapter 1/IsUnique.java
+++ Cracking The Coding Interview Book/Chapter 1/IsUnique.java
@@ -1,43 +1,40 @@
 import java.util.*;
 
-public class IsUnique {
+ public class IsUnique {
 	 
-  public static boolean isUniqueChars(String string) {
-	if(string.length() > 128) {
-	   return false;	
-	}
+    public static boolean isUniqueChars(String string) {
+       if(string.length() > 128) {
+	 return false;	
+       }
 	
-	boolean[] char_set = new boolean[128];
+       boolean[] char_set = new boolean[128];
 	
-	for(int i = 0;i < string.length(); i++) {
-	   int value = string.charAt(i);
-	   if(char_set[value]) {
-		  return false;
-	   }
-	   
-	   char_set[value] = true;
-	}
+       for(int i = 0;i < string.length(); i++) {
+	 int value = string.charAt(i);
+	 if(char_set[value]) {
+           return false;
+	 }
+	 char_set[value] = true;
+       }
 	  
-    return true;	  
-  }
+       return true;	  
+    }
 	
-  public static void main(String[] args) {  
-	String string = new String();	  
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter the string: ");
-    string = input.next(); 
+    public static void main(String[] args) {  
+       String string = new String();	  
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the string: ");
+       string = input.next(); 
   
-    boolean isUnique = isUniqueChars(string);
+       boolean isUnique = isUniqueChars(string);
     
-    System.out.println();
+       System.out.println();
     
-    if(isUnique) {
-      System.out.print("The string has all unique characters.");	
+       if(isUnique) {
+         System.out.print("The string has all unique characters.");	
+       }
+       else {
+         System.out.print("The string does not have all unique characters.");	
+       }
     }
-    
-    else {
-      System.out.print("The string does not have all unique characters.");	
-    }
-
-  }
-}
+ }
