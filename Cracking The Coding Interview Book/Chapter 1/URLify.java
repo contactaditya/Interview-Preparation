c4cda7b834d87003d15ccec6af1db1ef1010bcc8
Aditya Gupta <contactadityag@gmail.com>
2018-03-03 04:01:24 -0500

    This is the solution to the URLify problem.

--- Cracking The Coding Interview Book/Chapter 1/URLify.java
+++ Cracking The Coding Interview Book/Chapter 1/URLify.java
@@ -3,38 +3,38 @@ import java.util.*;
  public class URLify {
 	 
    public static void replaceSpaces(char[] string, int trueLength) {
-      int spaceCount = 0, index = 0;
-      for (int i = 0; i < trueLength; i++) {
-	if(string[i] == ' ') {
+     int spaceCount = 0, index = 0;
+     for (int i = 0; i < trueLength; i++) {
+       if(string[i] == ' ') {
 	  spaceCount++;	
-	}		  
-      }
+       }		  
+     }
 		
-      index = trueLength + spaceCount * 2;
-      if(trueLength < string.length) {
-	 string[trueLength] = '\0';
-      }
+     index = trueLength + spaceCount * 2;
+     if(trueLength < string.length) {
+	string[trueLength] = '\0';
+     }
 	   
-      for (int i = trueLength - 1; i >= 0; i--) {
-	if(string[i] == ' ') {
-	  string[index - 1] = '0';	 
-	  string[index - 2] = '2';	 
-	  string[index - 3] = '%';	 	 
-	  index = index - 3;	 
-        } 
-        else {
-	  string[index - 1] = string[i];	 
-	  index--;	 
-	}		 
-      }
-      System.out.println();
-      System.out.print("The string with %20 in place of spaces is: ");    
-      for(int i = 0; i < string.length; i++) {
-	 System.out.print(string[i]);  
-      }
+     for (int i = trueLength - 1; i >= 0; i--) {
+       if(string[i] == ' ') {
+	 string[index - 1] = '0';	 
+	 string[index - 2] = '2';	 
+	 string[index - 3] = '%';	 	 
+	 index = index - 3;	 
+       } 
+       else {
+	 string[index - 1] = string[i];	 
+	 index--;	 
+       }		 
+     }
+     System.out.println();
+     System.out.print("The string with %20 in place of spaces is: ");    
+     for(int i = 0; i < string.length; i++) {
+       System.out.print(string[i]);  
+     }
    }
 	 
- public static void main(String[] args) { 	
+   public static void main(String[] args) { 	
      String string = new String();	  
      Scanner input = new Scanner(System.in);
      System.out.print("Enter the string with spaces: ");
