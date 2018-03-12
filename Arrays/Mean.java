be25919250524724dde118a8fc389104fb0658b9
Aditya Gupta <contactadityag@gmail.com>
2018-03-12 18:28:40 -0400

    This is the program for calculating the mean of an array.

--- Arrays/Mean.java
+++ Arrays/Mean.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Mean {
 	
   public static long calculateMean(long[] array) {
-
     long n = array.length;  
     long sum = 0;
     long mean = 0;
@@ -21,32 +20,29 @@ public class Mean {
   }
 
   public static void main(String[] args) throws NumberFormatException, IOException {
-		
-     Scanner input = new Scanner(System.in);
-     System.out.print("Enter number of elements in the array: ");
-     int numbers = input.nextInt();   
-     long array[] = new long[numbers];
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter number of elements in the array: ");
+    int numbers = input.nextInt();   
+    long array[] = new long[numbers];
     
-     System.out.print("\n");
-     System.out.print("Please enter the actual values in the array: ");
-     for (int i = 0; i < numbers; i++) {
-       array[i] = input.nextInt();
-     }
+    System.out.print("\n");
+    System.out.print("Please enter the actual values in the array: ");
+    for (int i = 0; i < numbers; i++) {
+      array[i] = input.nextInt();
+    }
 	
-     System.out.print("\n");
+    System.out.print("\n");
 	
-     System.out.print("The actual values in the array are: ");
-     for (int i = 0; i < numbers; i++) {
-       System.out.print(array[i] + " ");
-     }  
+    System.out.print("The actual values in the array are: ");
+    for (int i = 0; i < numbers; i++) {
+      System.out.print(array[i] + " ");
+    }  
 	
-     System.out.print("\n");
-	  	
-     long mean = calculateMean(array);
+    System.out.print("\n");
+	  
+    long mean = calculateMean(array);
     
-     System.out.print("\n");
-    
-     System.out.println("The value of the mean of the array is: " + mean);
-
+    System.out.print("\n");
+    System.out.println("The value of the mean of the array is: " + mean);
   }
 }
