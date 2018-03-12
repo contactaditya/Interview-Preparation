ce0c2b13ace924c4fb40e76b32b18c05de28df83
Aditya Gupta <contactadityag@gmail.com>
2018-03-12 18:25:16 -0400

    This is a program to find the second largest element in the array.

--- Arrays/SecondLargestElementInAnArray.java
+++ Arrays/SecondLargestElementInAnArray.java
@@ -3,50 +3,50 @@ import java.io.*;
 
  public class SecondLargestElementInAnArray { 	
 	 
-	public static void printSecondLargest(int[] array) {
-	  int n = array.length;  
-	  int first = 0;
-	  int second = 0;
+    public static void printSecondLargest(int[] array) {
+       int n = array.length;  
+       int first = 0;
+       int second = 0;
 	   
-	  if(n < 2) {
-		System.out.print("Invalid size of the array.");   
-	  }
+       if(n < 2) {
+	 System.out.print("Invalid size of the array.");   
+       }
 	   
-	  first = second = Integer.MIN_VALUE;
+       first = second = Integer.MIN_VALUE;
 	    
-	  for(int i = 0; i < n; i++) {
-		if(array[i] > first) {
-		   second = first;
-		   first = array[i]; 
-		}
+       for(int i = 0; i < n; i++) {
+	 if(array[i] > first) {
+	   second = first;
+	   first = array[i]; 
+	 }
 		
-		else if (array[i] > second && array[i] != first) {
-		  second = array[i];	
-		}
-	  }
+	 else if (array[i] > second && array[i] != first) {
+	   second = array[i];	
+	 }
+       }
 	  
-	  if (second == Integer.MIN_VALUE) {
-		System.out.print("There is no second largest element in the array");     
-	  }
-	  else {
-		System.out.print("The second largest element in the array is: " + second);       
-	  }
+       if (second == Integer.MIN_VALUE) {
+	 System.out.print("There is no second largest element in the array");     
+       }
+       else {
+	 System.out.print("The second largest element in the array is: " + second);       
+       }
     }
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
   
-	  System.out.print("\n");
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt();
-	  }
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt();
+      }
 	  
-	  System.out.println();
-	  printSecondLargest(array);
+      System.out.println();
+      printSecondLargest(array);
 
-	}
+    }
  }
