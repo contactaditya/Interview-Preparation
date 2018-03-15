9f60107cc9f93b19c878e2829d465e43b05c732c
Aditya Gupta <contactadityag@gmail.com>
2018-03-14 22:43:32 -0400

    This program counts the number of leaf nodes in a binary tree.

--- Binary Search Trees/CountLeafNodesInABinaryTree.java
+++ Binary Search Trees/CountLeafNodesInABinaryTree.java
@@ -5,29 +5,27 @@ import java.util.*;
     Node root;	    
     Node previous;
     
- public int getLeafCount(Node node) {
-		 
-    if (node == null) {
-       return 0;
-    }
+    public int getLeafCount(Node node) {	 
+      if (node == null) {
+        return 0;
+      }
    	
-    if (node.left == null && node.right == null) {    
-       return 1;
-    }	 
-    else {
-       return getLeafCount(node.left) + getLeafCount(node.right);
-    }   	
- }
+      if (node.left == null && node.right == null) {    
+        return 1;
+      }	 
+      else {
+        return getLeafCount(node.left) + getLeafCount(node.right);
+      }    	
+    }
 
- public static void main(String[] args) {
-    CountLeafNodesInABinaryTree tree = new CountLeafNodesInABinaryTree();
-    tree.root = new Node(1);
-    tree.root.left = new Node(2);
-    tree.root.right = new Node(3);
-    tree.root.left.left = new Node(4);
-    tree.root.left.right = new Node(5);
+    public static void main(String[] args) {
+      CountLeafNodesInABinaryTree tree = new CountLeafNodesInABinaryTree();
+      tree.root = new Node(1);
+      tree.root.left = new Node(2);
+      tree.root.right = new Node(3);
+      tree.root.left.left = new Node(4);
+      tree.root.left.right = new Node(5);
 	         
-    System.out.println("The leaf count of binary tree is : " + tree.getLeafCount(tree.root));
-
-  }
-}
+      System.out.println("The leaf count of binary tree is : " + tree.getLeafCount(tree.root));
+    }
+ }
