874589d2b80346d556a7dd44eab52aacfaf953be
Aditya Gupta <contactadityag@gmail.com>
2018-03-16 13:32:20 -0400

    This is a program to check whether a string can become another string after some number of shifts.

--- Strings/RotateString.java
+++ Strings/RotateString.java
@@ -3,27 +3,27 @@ import java.io.*;
 
   public class RotateString {
 	  
-	public static boolean rotateString(String A, String B) {
-	   return A.length() == B.length() && (A + A).contains(B);      
-	}	  
+    public static boolean rotateString(String A, String B) {
+      return A.length() == B.length() && (A + A).contains(B);      
+    }	  
 
-	public static void main(String[] args) {      
-	  String A = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  A = input.nextLine();  
-	  System.out.println();	
-	  String B = new String();	  
-	  System.out.print("Enter the string: ");
-	  B = input.nextLine();
+    public static void main(String[] args) {      
+      String A = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the string: ");
+      A = input.nextLine();  
+      System.out.println();	
+      String B = new String();	  
+      System.out.print("Enter the string: ");
+      B = input.nextLine();
 		  
-	  boolean rotateString = rotateString(A, B);
-	  System.out.println();
-	  if(rotateString) {
-		System.out.println("The string " + A + " can become " + B + " after some number of shifts on " + A);  
-	  }
+      boolean rotateString = rotateString(A, B);
+      System.out.println();
+      if(rotateString) {
+	System.out.println("The string " + A + " can become " + B + " after some number of shifts on " + A);  
+      }
       else {
-		System.out.println("The string " + A + " cannot become " + B + " after some number of shifts on " + A);  
-	  }
-	}
+	System.out.println("The string " + A + " cannot become " + B + " after some number of shifts on " + A);  
+      }
+    }
   }
