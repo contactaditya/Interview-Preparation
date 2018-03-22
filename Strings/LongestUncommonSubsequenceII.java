2ba3b8977c998c341936bf88cbad62094198f162
Aditya Gupta <contactadityag@gmail.com>
2018-03-21 22:38:14 -0400

    This is a program to find the longest uncommon subsequence from a given a list of strings.
    The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this subsequence should not be any subsequence of the other strings.

--- Strings/LongestUncommonSubsequenceII.java
+++ Strings/LongestUncommonSubsequenceII.java
@@ -3,9 +3,9 @@ import java.io.*;
 
   public class LongestUncommonSubsequenceII {
 	  
-	public static int findLUSlength(String[] string) { 
-	  int max = -1;
-	  for(int i = 0; i < string.length; i++) {
+    public static int findLUSlength(String[] string) { 
+      int max = -1;
+      for(int i = 0; i < string.length; i++) {
         int j;
         for(j = 0; j < string.length; j++) {
           if(i != j && isSubsequence(string[i], string[j])) {
@@ -15,38 +15,38 @@ import java.io.*;
         if (j == string.length) {
           max = Math.max(max, string[i].length());
         } 
-	  }
+      }
 
       return max;      
-	}	
+    }	
 	
-	public static boolean isSubsequence(String a, String b) {
-	  int i = 0;
-	  for (int j = 0; j < b.length() && i < a.length(); j++) {
-	    if(a.charAt(i) == b.charAt(j)) {
-	       i++;
-	    }
-	  }
-	  return i == a.length();
+    public static boolean isSubsequence(String a, String b) {
+      int i = 0;
+      for (int j = 0; j < b.length() && i < a.length(); j++) {
+	if(a.charAt(i) == b.charAt(j)) {
+	  i++;
+	}
+      }
+      return i == a.length();
     }
 	
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the dictionary: ");
-	  int numbers = input.nextInt();   
-	  String string[] = new String[numbers];
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the dictionary: ");
+      int numbers = input.nextInt();   
+      String string[] = new String[numbers];
 		    
-	  System.out.println();
-	  input.nextLine();
-	  System.out.print("Please enter the actual values in the dictionary: ");
-	  System.out.println();
-	  for (int i = 0; i < numbers; i++) {
-		string[i] = input.nextLine(); 
-	  }
+      System.out.println();
+      input.nextLine();
+      System.out.print("Please enter the actual values in the dictionary: ");
+      System.out.println();
+      for (int i = 0; i < numbers; i++) {
+	string[i] = input.nextLine(); 
+      }
 	  
-	  int longestUncommonSubsequence = findLUSlength(string);
+      int longestUncommonSubsequence = findLUSlength(string);
 	  
       System.out.println();	
       System.out.println("The longest uncommon subsequence of this group of strings is: " + longestUncommonSubsequence);	
-	}
+    }
   }
