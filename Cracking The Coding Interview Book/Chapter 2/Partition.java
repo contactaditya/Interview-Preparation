f277dcee3676a8c9875c3c3aad7c2e61b3d5e40f
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 03:23:08 -0400

    This is the solution to the Partition Problem.

--- Cracking The Coding Interview Book/Chapter 2/Partition.java
+++ Cracking The Coding Interview Book/Chapter 2/Partition.java
@@ -4,36 +4,36 @@ import java.util.*;
     private static LinkedListNode head;
     private static int size;  
 
-   public Partition() {
-   // this is an empty list, so the reference to the head node is set to a new node with no data				  
+    public Partition() {
+      // this is an empty list, so the reference to the head node is set to a new node with no data				  
       head = new LinkedListNode(null);
       size = 0;
-   }
+    }
 	
-  public static LinkedListNode partition(LinkedListNode node, int x) {
+    public static LinkedListNode partition(LinkedListNode node, int x) {
       LinkedListNode head = node;
       LinkedListNode tail = node;
 			     
       while(node != null) {    
 	LinkedListNode next = node.next;
         if((int)node.data < x) {
-        // Insert node at head
+          // Insert node at head
           node.next = head;
           head = node;
         } else {
-        // Insert node at tail
+          // Insert node at tail
           tail.next = node;
           tail = node;   	 
         }
         node = next;
       }
 	   
-    tail.next = null;
+      tail.next = null;
 	   
-    return head;
-  }
+      return head;
+    }
 
-  public static void main(String[] args) {   
+    public static void main(String[] args) {   
       Partition list = new Partition();
       list.head = new LinkedListNode(3);
       list.head.next = new LinkedListNode(5);
@@ -50,15 +50,15 @@ import java.util.*;
       LinkedListNode current = list.partition(head, value);
 	  
       if(current == null) {
-	 System.out.println("The element is not present in the linkedlist. There is no partition in the linkedlist.");  
+	System.out.println("The element is not present in the linkedlist. There is no partition in the linkedlist.");  
       }
       else {
-	 System.out.println();  
-	 System.out.print("Contents of the linked list are: ");	 
-	 while(current != null) {	   
-	    System.out.print(current.getData() + " ");	 	
-	    current = current.getNext();
-	 }
+	System.out.println();  
+	System.out.print("Contents of the linked list are: ");	 
+	while(current != null) {	   
+	  System.out.print(current.getData() + " ");	 	
+	  current = current.getNext();
+	}
       }
-   }
+    }
 }
