3db26914d3da3ede1806295940a65d558e3768e4
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 21:15:28 -0400

    This is a program to read a text file in Java.

--- ReadingATextFileInJava.java
+++ ReadingATextFileInJava.java
@@ -4,24 +4,24 @@ import java.io.*;
  public class ReadingATextFileInJava {
 
    public static void main(String[] args) throws FileNotFoundException, IOException {
-	Scanner input = new Scanner(System.in);
-	String fileName = new String();	  
-	String contentLine = null;
-	System.out.print("Please enter the path of the file that you want to read: ");	
-	fileName = input.next();  
+     Scanner input = new Scanner(System.in);
+     String fileName = new String();	  
+     String contentLine = null;
+     System.out.print("Please enter the path of the file that you want to read: ");	
+     fileName = input.next();  
+	   
+     // FileReader reads text files in the default encoding of the system.
+     FileReader fileReader = new FileReader(fileName);
 
-        // FileReader reads text files in the default encoding of the system.
-        FileReader fileReader = new FileReader(fileName);
-
-        // Always wrap FileReader in BufferedReader.
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+     // Always wrap FileReader in BufferedReader.
+     BufferedReader bufferedReader = new BufferedReader(fileReader);
        
-        System.out.println();
-        while((contentLine = bufferedReader.readLine()) != null) {
-          System.out.println(contentLine);
-        }   
+     System.out.println();
+     while((contentLine = bufferedReader.readLine()) != null) {
+       System.out.println(contentLine);
+     }   
 
-        bufferedReader.close();  
-        input.close();
+     bufferedReader.close();  
+     input.close();
    }
  }
