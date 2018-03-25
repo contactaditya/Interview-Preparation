8c50bac3006b1112b5da3f98aba46f554f5beab8
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 21:02:45 -0400

    This is the solution to the Magic Index Problem.

--- Cracking The Coding Interview Book/Chapter 8/MagicIndex.java
+++ Cracking The Coding Interview Book/Chapter 8/MagicIndex.java
@@ -2,11 +2,11 @@ import java.util.*;
 
  public class MagicIndex {
 	 
-  public static int magicFast(int array[]) {
+   public static int magicFast(int array[]) {
      return magicFast(array, 0, array.length - 1);
-  }	 
+   }	 
 	 
-  public static int magicFast(int array[], int start, int end) {
+   public static int magicFast(int array[], int start, int end) {
      if(end < start) {
        return -1;
      }
@@ -15,36 +15,36 @@ import java.util.*;
      int midValue = array[midIndex];
 	 
      if(midValue == midIndex) {
-	return midIndex; 
+       return midIndex; 
      }
 	   
      int leftIndex = Math.min(midIndex - 1, midValue);  
      int left = magicFast(array, start, leftIndex);
      if(left >= 0) {
-	return left; 
+       return left; 
      }
 	
-    int rightIndex = Math.max(midIndex + 1, midValue);
-    int right = magicFast(array, rightIndex, end);
+     int rightIndex = Math.max(midIndex + 1, midValue);
+     int right = magicFast(array, rightIndex, end);
 	 
-    return right;
-  }	 
+     return right;
+   }	 
 
- public static void main(String[] args) { 	
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter number of elements in the array: ");
-    int numbers = input.nextInt();   
-    int array[] = new int[numbers];
+   public static void main(String[] args) { 	
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter number of elements in the array: ");
+     int numbers = input.nextInt();   
+     int array[] = new int[numbers];
       
-    System.out.print("\n");
-    System.out.print("Please enter the actual values in the array: ");
-    for (int i = 0; i < numbers; i++) {
+     System.out.print("\n");
+     System.out.print("Please enter the actual values in the array: ");
+     for (int i = 0; i < numbers; i++) {
        array[i] = input.nextInt();
-    }
+     }
 	 
-    int magicIndex = magicFast(array);
+     int magicIndex = magicFast(array);
 	  
-    System.out.println();
-    System.out.println("The magic index in the array is at: " + magicIndex);
+     System.out.println();
+     System.out.println("The magic index in the array is at: " + magicIndex);
    }
  }
