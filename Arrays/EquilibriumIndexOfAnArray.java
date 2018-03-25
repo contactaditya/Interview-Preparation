35e316b95e34f8274d262eb7b8278bea9ca53095
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 22:56:39 -0400

    This is the program for finding the equilibrium index in an array.

--- Arrays/EquilibriumIndexOfAnArray.java
+++ Arrays/EquilibriumIndexOfAnArray.java
@@ -3,28 +3,26 @@ import java.util.Scanner;
 
 public class EquilibriumIndexOfAnArray {
  
- public static void equilibrium(int array[]) {
-   int sum = 0;	 
-   int leftsum = 0;
+  public static void equilibrium(int array[]) {
+    int sum = 0;	 
+    int leftsum = 0;
 
-   for(int i = 0; i < array.length; i++) {
+    for(int i = 0; i < array.length; i++) {
       sum = sum + array[i];  
-   }
+    }
   
-   System.out.print("The numbers that satisfy the equilibrium index are: ");
+    System.out.print("The numbers that satisfy the equilibrium index are: ");
   
-   for(int i = 0; i < array.length; i++) {
-      sum = sum - array[i]; 
-	  
+    for(int i = 0; i < array.length; i++) {
+      sum = sum - array[i];   
       if(leftsum == sum) {
         System.out.print(array[i] + " ");
       }
-	
       leftsum = leftsum + array[i];		 
-   }
- }
+    }
+  }
  
- public static void main(String[] args) {
+  public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.print("Enter number of elements in the array: ");
     int number = input.nextInt();  
@@ -37,5 +35,5 @@ public class EquilibriumIndexOfAnArray {
     }
     System.out.println();	
     equilibrium(array);  
- }
+  }
 }
