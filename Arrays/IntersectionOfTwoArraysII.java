81a40ef819a04becbb580c80567d6bb061e95872
Aditya Gupta <contactadityag@gmail.com>
2018-03-24 22:33:00 -0400

    This is a program to find intersection of the two given arrays.

--- Arrays/IntersectionOfTwoArraysII.java
+++ Arrays/IntersectionOfTwoArraysII.java
@@ -3,47 +3,42 @@ import java.io.*;
 
  public class IntersectionOfTwoArraysII { 
 	 
-     public static int[] getIntersection(int[] array, int[] array1) {
-	        
-	if (array == null || array1 == null || array.length == 0 || array1.length == 0) {
-	   return new int[0];
-	}  
+    public static int[] getIntersection(int[] array, int[] array1) {        
+       if (array == null || array1 == null || array.length == 0 || array1.length == 0) {
+	 return new int[0];
+       }  
 		        
-	Arrays.sort(array);
-	Arrays.sort(array1);  
+       Arrays.sort(array);
+       Arrays.sort(array1);  
 		         
-	int i = 0, j = 0;
-	int x = array.length;
-	int y = array1.length;
+       int i = 0, j = 0;
+       int x = array.length;
+       int y = array1.length;
 		  
-	ArrayList<Integer> list = new ArrayList();
+       ArrayList<Integer> list = new ArrayList();
 		  
-	while(i < x && j < y) {
-          if(array[i] < array1[j]) {
-	     i++;  
-	  }
-		   
-	  else if(array[i] > array1[j]) {
-	     j++;  
-	  }
-		   
-	  else {
-	    list.add(array[i]);
-	    i++;
-	    j++;
-	  }
-	}
+       while(i < x && j < y) {
+         if(array[i] < array1[j]) {
+	   i++;  
+	 }	   
+	 else if(array[i] > array1[j]) {
+	   j++;  
+	 }	   
+	 else {
+	   list.add(array[i]);
+	   i++;
+	   j++;
+	 }
+       }
 
-	return listToArray(list);       
+       return listToArray(list);       
     }
 		     
     private static int[] listToArray(List<Integer> list) {
-       int[] result = new int[list.size()];
-		        
+       int[] result = new int[list.size()];	        
        for (int i = 0; i < list.size(); i++) {
 	  result[i] = list.get(i);
        }
-		        
        return result;
     } 	 
 
@@ -59,16 +54,15 @@ import java.io.*;
 	  array[i] = input.nextInt();
        }
 		    
-       Scanner input1 = new Scanner(System.in);
        System.out.println();	
        System.out.print("Enter number of elements in the second array: ");
-       int number1 = input1.nextInt();  
+       int number1 = input.nextInt();  
        int array1[] = new int[number1];
-		  
+		 
        System.out.println();	
        System.out.print("Please enter number: ");
        for (int i = 0; i < array1.length; i++) {
-	  array1[i] = input1.nextInt();
+	  array1[i] = input.nextInt();
        }
 		  
        int result[] = new int[array.length + array1.length];
