3fa42804df16c0f6a15d21a8eac9a9c26affceaa
Aditya Gupta <contactadityag@gmail.com>
2018-03-27 07:04:35 -0400

    This is the solution to the Queue via Stacks Problem.

--- Cracking The Coding Interview Book/Chapter 3/QueueViaStacks.java
+++ Cracking The Coding Interview Book/Chapter 3/QueueViaStacks.java
@@ -8,18 +8,18 @@ import java.util.Stack;
     Stack<T> stackNewest, stackOldest;
     
     public QueueViaStacks() {
-       stackNewest = new Stack<T>();	
-       stackOldest = new Stack<T>();	
+      stackNewest = new Stack<T>();	
+      stackOldest = new Stack<T>();	
     }
     
     public int size() {
-       System.out.println();
-       System.out.println("The size of the queue is: " + stackNewest.size() + stackOldest.size()); 
-       return stackNewest.size() + stackOldest.size();	
+      System.out.println();
+      System.out.println("The size of the queue is: " + stackNewest.size() + stackOldest.size()); 
+      return stackNewest.size() + stackOldest.size();	
     }
     
     public void add(T value) {
-    // Push onto stackNewest, which always the newest elements on top	
+      // Push onto stackNewest, which always the newest elements on top	
       stackNewest.push(value);	 	
     }
     
@@ -43,7 +43,7 @@ import java.util.Stack;
         System.out.print(stackOldest.get(i) + " ");
       }
 	  
-       System.out.println();
+      System.out.println();
     }
     
     public boolean isEmpty() {
@@ -77,47 +77,47 @@ import java.util.Stack;
     }
     
     public T remove() {
-       shiftStacks(); // Ensure stackOldest has the current element
-       return stackOldest.pop();   
+      shiftStacks(); // Ensure stackOldest has the current element
+      return stackOldest.pop();   
     }
 
     public static void main(String[] args) throws NumberFormatException, IOException { 
-       int choice = 0;
-       QueueViaStacks<Integer> queue = new QueueViaStacks<Integer>();	
+      int choice = 0;
+      QueueViaStacks<Integer> queue = new QueueViaStacks<Integer>();	
     
-       do{	
-           System.out.println("1. For inserting an element in the queue.");
-           System.out.println("2. For deleting an element from the queue.");
-    	   System.out.println("3. For displaying the contents of the queue.");
-           System.out.println("4. For checking the contents of the queue are empty or not");
-           System.out.println("5. For checking the size of the queue");
-           System.out.println("6. For returning the front item from the queue without popping it.");
-           System.out.println("7. For exiting from the program.");
+      do{	
+          System.out.println("1. For inserting an element in the queue.");
+          System.out.println("2. For deleting an element from the queue.");
+    	  System.out.println("3. For displaying the contents of the queue.");
+          System.out.println("4. For checking the contents of the queue are empty or not");
+          System.out.println("5. For checking the size of the queue");
+          System.out.println("6. For returning the front item from the queue without popping it.");
+          System.out.println("7. For exiting from the program.");
     	
-           System.out.println();
-           System.out.print("Enter your choice: ");
-           BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
-           choice = Integer.parseInt(br.readLine());
-           switch (choice) {
-      	       case 1:  System.out.println();  
-    	                System.out.print("Enter a element to insert in the queue: ");
-    	       		Scanner input = new Scanner(System.in);
-      	       		int element = input.nextInt();
-    	       		queue.add(element);
-    	       		break;
-      	       case 2:  queue.remove();
+          System.out.println();
+          System.out.print("Enter your choice: ");
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+          choice = Integer.parseInt(br.readLine());
+          switch (choice) {
+      	    case 1:  System.out.println();  
+    	             System.out.print("Enter a element to insert in the queue: ");
+    	       	     Scanner input = new Scanner(System.in);
+      	       	     int element = input.nextInt();
+    	             queue.add(element);
+    	       	     break;
+      	    case 2:  queue.remove();
                		break;
-      	       case 3:  queue.display();
+      	    case 3:  queue.display();
                		break;
-      	       case 4:  queue.isEmpty();
+      	    case 4:  queue.isEmpty();
                		break;
-      	       case 5:  queue.size();
+      	    case 5:  queue.size();
       	       		break;  
-      	       case 6:  queue.peek();
+      	    case 6:  queue.peek();
 	       		break;     
-    	   }
-       System.out.print("\n");
-     } while(choice!=7);
-  }
-}
+    	  }
+          System.out.print("\n");
+        } while(choice!=7);
+    }
+ }
 
