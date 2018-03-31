1e263e1c714a069a7d7a3e76fd0cf706d50601ad
Aditya Gupta <contactadityag@gmail.com>
2018-03-31 13:11:10 -0400

    This is a program to calculate union and intersection of two sorted arrays in java.

--- Arrays/UnionAndIntersection.java
+++ Arrays/UnionAndIntersection.java
@@ -77,54 +77,53 @@ import java.util.*;
       System.out.println();	
    }
  
-   public static void main(String[] args) throws NumberFormatException, IOException {	  
-     Scanner input = new Scanner(System.in);
-     System.out.print("Enter number of elements in the first array: ");
-     int number = input.nextInt();  
-     int array[] = new int[number];
-  
-     System.out.println();	
-     System.out.print("Please enter number: ");
-     for (int i = 0; i < array.length; i++) {
-       array[i] = input.nextInt();
-     }
-    
-     Arrays.sort(array);
-  
-     Scanner input1 = new Scanner(System.in);
-     System.out.println();	
-     System.out.print("Enter number of elements in the second array: ");
-     int number1 = input1.nextInt();  
-     int array1[] = new int[number1];
-  
-     System.out.println();	
-     System.out.print("Please enter number: ");
-     for(int i = 0; i < array1.length; i++) {
-       array1[i] = input1.nextInt();
-     } 
-  
-     Arrays.sort(array1);
-  
-     int choice = 0;
-     System.out.println();	
+   public static void main(String[] args) throws NumberFormatException, IOException {	   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the first array: ");
+      int number = input.nextInt();  
+      int array[] = new int[number];
+	  
+      System.out.println();	
+      System.out.print("Please enter number: ");
+      for (int i = 0; i < array.length; i++) {
+	array[i] = input.nextInt();
+      }
 	    
-     do{	
-	 System.out.println("1. To calculate the interection of the sorted arrays.");
-	 System.out.println("2. To calculate the union of the sorted arrays.");
-	 System.out.println("3. For exiting from the program.");
-	   
-	 System.out.println();	
-	    	
-	 System.out.print("Enter your choice: ");
-	 BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));  
-	 choice = Integer.parseInt(br1.readLine());
-	 switch (choice) {
-	   case 1:  getIntersection(array, array1);
-	    	    break;
-	   case 2:  getUnion(array, array1);
-	            break;    
-	 }
-	 System.out.print("\n");
-       } while(choice!=3);
+      Arrays.sort(array);
+	  
+      System.out.println();	
+      System.out.print("Enter number of elements in the second array: ");
+      int number1 = input.nextInt();  
+      int array1[] = new int[number1];
+	  
+      System.out.println();	
+      System.out.print("Please enter number: ");
+      for(int i = 0; i < array1.length; i++) {
+	array1[i] = input.nextInt();
+      } 
+	  
+      Arrays.sort(array1);
+	  
+      int choice = 0;
+      System.out.println();	
+		    
+      do{	
+          System.out.println("1. To calculate the interection of the sorted arrays.");
+          System.out.println("2. To calculate the union of the sorted arrays.");
+          System.out.println("3. For exiting from the program.");
+		   
+          System.out.println();	
+		    	
+	  System.out.print("Enter your choice: ");
+	  BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));  
+	  choice = Integer.parseInt(br1.readLine());
+	  switch (choice) {
+	     case 1:  getIntersection(array, array1);
+		      break;
+	     case 2:  getUnion(array, array1);
+		      break;    
+	  }
+	  System.out.print("\n");
+        } while(choice!=3);
    }     
  }
