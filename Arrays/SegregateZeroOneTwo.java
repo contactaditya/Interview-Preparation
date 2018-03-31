f6ca24b8a9330fcacf81cf7744ee49830bd56af9
Aditya Gupta <contactadityag@gmail.com>
2018-03-31 13:29:24 -0400

    This program segregate 0s, 1s and 2s of the array.

--- Arrays/SegregateZeroOneTwo.java
+++ Arrays/SegregateZeroOneTwo.java
@@ -1,13 +1,13 @@
 import java.io.*;
 import java.util.*;
 
- public class SegregateZeroOneTwo {    
+public class SegregateZeroOneTwo {    
 	 
   public static void segregateZeroOneTwo(int[] array) {
-     int low = 0, high = array.length - 1;
-     int mid = 0, temp=0;
-     while(mid <= high) {
-       switch (array[mid]) {
+    int low = 0, high = array.length - 1;
+    int mid = 0, temp=0;
+    while(mid <= high) {
+      switch (array[mid]) {
          case 0: temp =  array[low];
                  array[low]  = array[mid];
                  array[mid] = temp;
@@ -21,28 +21,28 @@ import java.util.*;
                  array[high] = temp;
                  high--;
                  break;         
-       }
-     } 
+      }
+    } 
   }
    
   public static void main(String[] args) {    
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter number of elements in the array: ");
-      int number = input.nextInt();  
-      int array[] = new int[number];
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter number of elements in the array: ");
+    int number = input.nextInt();  
+    int array[] = new int[number];
 			
-      System.out.println();	
-      System.out.print("Please enter the actual values in the array which are either 0 or 1 or 2: ");
-      for (int i = 0; i < array.length; i++) {
-	 array[i] = input.nextInt();
-      }
+    System.out.println();	
+    System.out.print("Please enter the actual values in the array which are either 0 or 1 or 2: ");
+    for (int i = 0; i < array.length; i++) {
+      array[i] = input.nextInt();
+    }
 				 
-      segregateZeroOneTwo(array);
-      System.out.print("\n");
+    segregateZeroOneTwo(array);
+    System.out.print("\n");
 			    
-      System.out.print("The array after segregation is: ");
-      for (int i = 0; i < array.length; i++) {
-	 System.out.print(array[i] + " ");
-      }
-   }
- }
+    System.out.print("The array after segregation is: ");
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
+  }
+}
