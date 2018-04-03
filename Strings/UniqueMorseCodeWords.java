d5f41dca5ba374718eed4a7c464294286ad86a3c
Aditya Gupta <contactadityag@gmail.com>
2018-04-03 08:36:38 -0400

    This is a program to return the number of different transformations of the unique morse code words among all words we have.

--- Strings/UniqueMorseCodeWords.java
+++ Strings/UniqueMorseCodeWords.java
@@ -3,14 +3,14 @@ import java.io.*;
 
   public class UniqueMorseCodeWords {
 	  
-	public static int uniqueMorseRepresentations(String[] words) {
-	  String[] MORSE = new String[]{".-","-...","-.-.","-..",".","..-.","--.",
+    public static int uniqueMorseRepresentations(String[] words) {
+      String[] MORSE = new String[]{".-","-...","-.-.","-..",".","..-.","--.",
                                     "....","..",".---","-.-",".-..","--","-.",
                                     "---",".--.","--.-",".-.","...","-","..-",
                                     "...-",".--","-..-","-.--","--.."};     
 	  
-	  Set<String> seen = new HashSet<String>();
-	  for (String word : words) {
+      Set<String> seen = new HashSet<String>();
+      for (String word : words) {
         StringBuilder code = new StringBuilder();
         for (char c : word.toCharArray()) {
           code.append(MORSE[c - 'a']);
@@ -18,25 +18,25 @@ import java.io.*;
         seen.add(code.toString());
       }
 	  
-	  return seen.size();
-	}  
+      return seen.size();
+    }  
 
-	public static void main(String[] args) { 	   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  String words[] = new String[numbers];
+    public static void main(String[] args) { 	   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      String words[] = new String[numbers];
 		    
-	  System.out.println();
-	  input.nextLine();
-	  System.out.print("Please enter the actual values in the array: ");
-	  System.out.println();
-	  for (int i = 0; i < numbers; i++) {
-		words[i] = input.nextLine(); 
-	  }
+      System.out.println();
+      input.nextLine();
+      System.out.print("Please enter the actual values in the array: ");
+      System.out.println();
+      for(int i = 0; i < numbers; i++) {
+	words[i] = input.nextLine(); 
+      }
 	   
-	  int result = uniqueMorseRepresentations(words);
-	  System.out.println();
-	  System.out.print("The number of different transformations among all words we have are: " + result);
-	}
+      int result = uniqueMorseRepresentations(words);
+      System.out.println();
+      System.out.print("The number of different transformations among all words we have are: " + result);
+    }
   }
