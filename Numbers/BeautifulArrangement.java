5b1483064d7bde1fb46a2e6865f8971388e440c7
Aditya Gupta <contactadityag@gmail.com>
2018-04-03 08:10:16 -0400

    This is a program to return the number of beautiful arrangements as an array.
    A beautiful arrangement is if one of the following is true for the ith position (1 <= i <= N) in the given array:
    
    1) The number at the ith position is divisible by i.
    
    2) i is divisible by the number at the ith position.

--- Numbers/BeautifulArrangement.java
+++ Numbers/BeautifulArrangement.java
@@ -4,31 +4,31 @@ import java.io.*;
   public class BeautifulArrangement {
     static int count = 0; 
 	
-	public static int countArrangement(int number) {
+    public static int countArrangement(int number) {
       boolean visited[] = new boolean[number + 1];
-	  calculate(number, 1, visited);
-	  return count;      
-	}	  
+      calculate(number, 1, visited);
+      return count;      
+    }	  
 
-	private static void calculate(int number, int position, boolean[] visited) {
-	  if(position > number) {
-		count++;  
-	  }
-	  for(int i = 1; i <= number; i++) {
-		if(!visited[i] && (position % i == 0 || i % position == 0)) {
-	      visited[i] = true;
-	      calculate(number, position + 1, visited);
-	      visited[i] = false;
-		}
-	  }
+    private static void calculate(int number, int position, boolean[] visited) {
+      if(position > number) {
+	count++;  
+      }
+      for(int i = 1; i <= number; i++) {
+	if(!visited[i] && (position % i == 0 || i % position == 0)) {
+	  visited[i] = true;
+	  calculate(number, position + 1, visited);
+	  visited[i] = false;
 	}
+      }
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();   
-	  int numberOfArrangements = countArrangement(number);
-	  System.out.println();
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int number = input.nextInt();   
+      int numberOfArrangements = countArrangement(number);
+      System.out.println();
       System.out.println("The number of beautiful arrangement that you can construct are: " + numberOfArrangements);  
-	}
+    }
   }
