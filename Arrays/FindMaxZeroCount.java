0f4766c7f8c98559b9045ee68771448870bf8a5d
Aditya Gupta <contactadityag@gmail.com>
2018-04-03 08:49:13 -0400

    This is a program to find the maximum number of zeros in an array with one flip of a subarray allowed.

--- Arrays/FindMaxZeroCount.java
+++ Arrays/FindMaxZeroCount.java
@@ -4,44 +4,43 @@ import java.io.*;
  public class FindMaxZeroCount {
 	
    public static int findMaxZeroCount(int[] array) {
-      int n = array.length;  
-      int originalZeroCount = 0;
-      int maxDifference = 0; 
-      int currentMax = 0;
+     int n = array.length;  
+     int originalZeroCount = 0;
+     int maxDifference = 0; 
+     int currentMax = 0;
 	   
-      for(int i=0; i<n; i++) {
-		   
-         // Count of zeros in original array 
-	 if (array[i] == 0) {
-	   originalZeroCount++;  
-	 }
+     for(int i=0; i<n; i++) {	   
+       // Count of zeros in original array 
+       if (array[i] == 0) {
+	 originalZeroCount++;  
+       }
 		 
-	 // Value to be considered for finding maximum sum
-	 int value = (array[i] == 1) ? 1 : -1;
+       // Value to be considered for finding maximum sum
+       int value = (array[i] == 1) ? 1 : -1;
 	     
-	 // Update current max and max_diff
-	 currentMax = Math.max(value, currentMax + value);
-	 maxDifference = Math.max(maxDifference, currentMax);	   
-      }
-      maxDifference = Math.max(0, maxDifference);
+       // Update current max and max_diff
+       currentMax = Math.max(value, currentMax + value);
+       maxDifference = Math.max(maxDifference, currentMax);	   
+     }
+     maxDifference = Math.max(0, maxDifference);
 		   
      return originalZeroCount + maxDifference;
    }	 
  
    public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
-       System.out.print("Enter number of elements in the array: ");
-       int numbers = input.nextInt();   
-       int array[] = new int[numbers];
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter number of elements in the array: ");
+     int numbers = input.nextInt();   
+     int array[] = new int[numbers];
 		    
-       System.out.print("\n");
-       System.out.print("Please enter the actual values in the array: ");
-       for (int i = 0; i < numbers; i++) {
-	  array[i] = input.nextInt(); 
-       }
+     System.out.print("\n");
+     System.out.print("Please enter the actual values in the array: ");
+     for (int i = 0; i < numbers; i++) {
+       array[i] = input.nextInt(); 
+     }
 	 	   
-       int maxZeroes = findMaxZeroCount(array);
-       System.out.print("\n");
-       System.out.print("Maximum number zeros in an array with one flip of a subarray allowed is: " + maxZeroes);
+     int maxZeroes = findMaxZeroCount(array);
+     System.out.print("\n");
+     System.out.print("Maximum number zeros in an array with one flip of a subarray allowed is: " + maxZeroes);
    }
  }
