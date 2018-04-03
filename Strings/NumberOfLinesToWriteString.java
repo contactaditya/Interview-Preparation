f378c43dbdb5a9b770f4e5bc3ce4e828298875db
Aditya Gupta <contactadityag@gmail.com>
2018-04-03 08:12:21 -0400

    This is a program to return the number of lines that have at least one character from the given string and the width used by the last line.

--- Strings/NumberOfLinesToWriteString.java
+++ Strings/NumberOfLinesToWriteString.java
@@ -3,9 +3,9 @@ import java.io.*;
 
   public class NumberOfLinesToWriteString {
 	  
-	public static int[] numberOfLines(int[] widths, String string) {   
-	  int lines = 1, width = 0;
-	  for (char c: string.toCharArray()) {
+    public static int[] numberOfLines(int[] widths, String string) {   
+      int lines = 1, width = 0;
+      for (char c: string.toCharArray()) {
         int w = widths[c - 'a'];
         width += w;
         if (width > 100) {
@@ -14,26 +14,26 @@ import java.io.*;
         }
       }
 		
-	  return new int[]{lines, width};      
-	}	  
+      return new int[]{lines, width};      
+    }	  
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  int widths[] = new int[26];
-	  System.out.print("Please enter the elements in the widths array: ");
-	  for (int i = 0; i < 26; i++) {
-		widths[i] = input.nextInt();
-	  }
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      int widths[] = new int[26];
+      System.out.print("Please enter the elements in the widths array: ");
+      for (int i = 0; i < 26; i++) {
+	widths[i] = input.nextInt();
+      }
 	
-	  String string = new String();	
-	  System.out.println();	
-	  System.out.print("Enter the letters of the given string: ");
-	  string = input.next();  
+      String string = new String();	
+      System.out.println();	
+      System.out.print("Enter the letters of the given string: ");
+      string = input.next();  
 	  
-	  System.out.println();
-	  int[] result = numberOfLines(widths, string);
+      System.out.println();
+      int[] result = numberOfLines(widths, string);
       System.out.println("The number of lines having at least from one character from string " + string + " is: " + result[0]);	 
       System.out.println();
       System.out.println("The width used by the last such line is: " + result[1]);	 
-	}
+    }
   }
