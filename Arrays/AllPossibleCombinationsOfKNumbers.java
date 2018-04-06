8b9bea77f6ad66a2998d5eba61c51c9ef14a2204
Aditya Gupta <contactadityag@gmail.com>
2018-04-06 00:20:44 -0400

    This is a program to return all possible combinations of k numbers out of the given n numbers.

--- Arrays/AllPossibleCombinationsOfKNumbers.java
+++ Arrays/AllPossibleCombinationsOfKNumbers.java
@@ -4,39 +4,39 @@ import java.io.*;
   public class AllPossibleCombinationsOfKNumbers {
 	  
     public static List<List<Integer>> combine(int n, int k) {
-       List<List<Integer>> result = new ArrayList<List<Integer>>();		
-       if (k > n || k < 0) {
-         return result;
-       }	
-       if (k == 0) {
-         result.add(new ArrayList<Integer>());
-         return result;
-       }
-       result = combine(n-1, k-1);
-       for (List<Integer> list : result) {
-         list.add(n);
-       }
+      List<List<Integer>> result = new ArrayList<List<Integer>>();		
+      if (k > n || k < 0) {
+        return result;
+      }	
+      if (k == 0) {
+        result.add(new ArrayList<Integer>());
+        return result;
+      }
+      result = combine(n-1, k-1);
+      for (List<Integer> list : result) {
+        list.add(n);
+      } 
 		
-       result.addAll(combine(n - 1, k));
-       return result;    
+      result.addAll(combine(n - 1, k));
+      return result;    
     }
     
     public static List<List<Integer>> combine1(int n, int k) {
-       List<List<Integer>> combinations = new ArrayList<List<Integer>>();
-       combine(combinations, new ArrayList<Integer>(), 1, n, k);
-       return combinations;
+      List<List<Integer>> combinations = new ArrayList<List<Integer>>();
+      combine(combinations, new ArrayList<Integer>(), 1, n, k);
+      return combinations;
     }	
 
     private static void combine(List<List<Integer>> combinations, ArrayList<Integer> list, int start, int n, int k) {
-       if(k==0) {
-	  combinations.add(new ArrayList<Integer>(list));
-          return;
-       }
-       for(int i=start;i<=n;i++) {
-	  list.add(i);
-	  combine(combinations, list, i+1, n, k-1);
-	  list.remove(list.size()-1);		
-       }
+      if(k==0) {
+	combinations.add(new ArrayList<Integer>(list));
+        return;
+      }
+      for(int i=start;i<=n;i++) {
+	list.add(i);
+	combine(combinations, list, i+1, n, k-1);
+	list.remove(list.size()-1);		
+      }
     }
 
     public static void main(String[] args) {
