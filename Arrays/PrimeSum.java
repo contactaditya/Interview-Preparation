e2edc1d7161f528a8e84f7c63944815c948cdce2
Aditya Gupta <contactadityag@gmail.com>
2018-04-07 09:58:39 -0400

    This is a program to print two prime numbers whose sum will be equal to given number given an even number greater than 2.

--- Arrays/PrimeSum.java
+++ Arrays/PrimeSum.java
@@ -3,50 +3,47 @@ import java.io.*;
 
  public class PrimeSum {
 	 
-    public static ArrayList<Integer> primeSum(int number) {
-       ArrayList<Integer> result = new ArrayList<Integer>();
-       if(number < 2) {
-    	 return result;  
-       }
+   public static ArrayList<Integer> primeSum(int number) {
+     ArrayList<Integer> result = new ArrayList<Integer>();
+     if(number < 2) {
+       return result;  
+     }
        
-       int first = 2;
-       int second = number - first;
-       while(first <= second){
-         second = number - first;
+     int first = 2;
+     int second = number - first;
+     while(first <= second){
+       second = number - first;
 
-         if(isPrime(second) == true && isPrime(first)){
-            result.add(first);
-            result.add(second);
-            return result;
-         }
-         first++;
+       if(isPrime(second) == true && isPrime(first)){
+         result.add(first);
+         result.add(second);
+         return result;
        }
-       return result;
-    } 
+       first++;
+     }
+     return result;
+   } 
     
-    public static boolean isPrime(int number) {
-    	
-       if(number < 2) {
-         return false;  
+   public static boolean isPrime(int number) { 	
+     if(number < 2) {
+       return false;  
+     }
+     for(int i=2; i<=Math.sqrt(number); i++) {
+       if(number % i==0) {
+         return false;
        }
-       
-       for(int i=2; i<=Math.sqrt(number); i++) {
-         if(number % i==0) {
-           return false;
-         }
-       }
-      return true;
-    }
+     }
+     return true;
+   }
 
-    public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
-       System.out.print("Enter an even number greater than 2: ");
-       int number = input.nextInt();  
-       ArrayList<Integer> result = new ArrayList<Integer>();
+   public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter an even number greater than 2: ");
+     int number = input.nextInt();  
+     ArrayList<Integer> result = new ArrayList<Integer>();
 			
-       result = primeSum(number);
-
-       System.out.println();	
-       System.out.print("The two prime numbers whose sum will be equal to given number: " + result);
-    }
+     result = primeSum(number);
+     System.out.println();	
+     System.out.print("The two prime numbers whose sum will be equal to given number: " + result);
+   }
  }
