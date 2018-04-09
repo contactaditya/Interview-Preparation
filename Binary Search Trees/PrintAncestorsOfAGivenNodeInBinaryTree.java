97f3c30f832c3330c02bc3283afbc468bd463e33
Aditya Gupta <contactadityag@gmail.com>
2018-04-09 00:50:35 -0400

    This is a program that prints all the ancestors of the key in the given binary tree.

--- Binary Search Trees/PrintAncestorsOfAGivenNodeInBinaryTree.java
+++ Binary Search Trees/PrintAncestorsOfAGivenNodeInBinaryTree.java
@@ -1,29 +1,28 @@
 import java.io.*;
 import java.util.*;
 
- public class PrintAncestorsOfAGivenNodeInBinaryTree {   
+  public class PrintAncestorsOfAGivenNodeInBinaryTree {   
     Node root;	    
     Node previous;
 
- public boolean printAncestors(Node node, int target) {
-		 
-     if (node == null) {
+    public boolean printAncestors(Node node, int target) {	 
+      if (node == null) {
 	return false;
-     }
+      }
 	 
-     if (node.data == target) {
+      if (node.data == target) {
         return true;	 
-     }
+      }
 	 
-     if (printAncestors(node.left, target) || printAncestors(node.right, target)) {
+      if (printAncestors(node.left, target) || printAncestors(node.right, target)) {
         System.out.print(node.data + " ");
         return true;
-     }
+      }
 
-   return false;
- } 
+      return false;
+    } 
 
-  public static void main(String[] args) {   	
+    public static void main(String[] args) {   	
       PrintAncestorsOfAGivenNodeInBinaryTree tree = new PrintAncestorsOfAGivenNodeInBinaryTree();
       tree.root = new Node(1);
       tree.root.left = new Node(2);
@@ -37,7 +36,6 @@ import java.util.*;
       int key = input.nextInt();   
       System.out.println();   
       System.out.print("The Ancestors of the key " + key + " of the binary tree are: ");
-      tree.printAncestors(tree.root, key);			
-		
-     }
+      tree.printAncestors(tree.root, key);				
+    }
   }
