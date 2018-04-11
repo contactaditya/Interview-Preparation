79ea4c98f113696215e9953bb95bcd5469c02516
Aditya Gupta <contactadityag@gmail.com>
2018-04-11 00:15:37 -0400

    This is a program to find the sum of two Binary Strings and return the result which is also a Binary String.

--- Strings/AddTwoBinaryStrings.java
+++ Strings/AddTwoBinaryStrings.java
@@ -4,43 +4,42 @@ import java.io.*;
  public class AddTwoBinaryStrings { 	
 	 
    public static String addBinary(String number1, String number2) {
-      StringBuilder sb = new StringBuilder();
-      int carry = 0;	
-      int i = number1.length() - 1, j = number2.length() -1;
-      while(i >= 0 || j >= 0) {
-    	int sum = carry;  
-    	if (j >= 0) {
-    	  sum += number2.charAt(j--) - '0';	
-    	}
-    	if (i >= 0) {
-      	  sum += number1.charAt(i--) - '0';	
-      	}
-        sb.append(sum % 2);
-        carry = sum/2;
-      }
+     StringBuilder sb = new StringBuilder();
+     int carry = 0;	
+     int i = number1.length() - 1, j = number2.length() -1;
+     while(i >= 0 || j >= 0) {
+       int sum = carry;  
+       if (j >= 0) {
+    	 sum += number2.charAt(j--) - '0';	
+       }
+       if (i >= 0) {
+      	 sum += number1.charAt(i--) - '0';	
+       }
+       sb.append(sum % 2);
+       carry = sum/2;
+     }
       
-      if(carry != 0) {
-        sb.append(carry);
-      }
+     if(carry != 0) {
+       sb.append(carry);
+     }
 	
      return sb.reverse().toString();     
    }	 
 
    public static void main(String[] args) {
-      String number1 = new String();	  
-      Scanner input1 = new Scanner(System.in);
-      System.out.print("Enter the first string: ");
-      number1 = input1.next();  
+     String number1 = new String();	  
+     Scanner input1 = new Scanner(System.in);
+     System.out.print("Enter the first string: ");
+     number1 = input1.next();  
+	  
+     System.out.println();	
+     String number2 = new String();	  
+     Scanner input2 = new Scanner(System.in);
+     System.out.print("Enter the second string: ");
+     number2 = input2.next();
 		  
-      System.out.println();	
-      String number2 = new String();	  
-      Scanner input2 = new Scanner(System.in);
-      System.out.print("Enter the second string: ");
-      number2 = input2.next();
-		  
-      String result = addBinary(number1, number2);
-      System.out.println();
-      System.out.println("The sum of the two numbers is: " + result);
-
+     String result = addBinary(number1, number2);
+     System.o.println();
+     System.out.println("The sum of the two numbers is: " + result);
    }
  }
