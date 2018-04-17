072f8986b685b89dc6ac0298e6394ae94b658da6
Aditya Gupta <contactadityag@gmail.com>
2018-04-17 10:03:59 -0400

    This is the program for finding the Depth First Traversal for a graph.

--- Graphs/GraphDFS.java
+++ Graphs/GraphDFS.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class GraphDFS {  
+ public class GraphDFS {  
    private int vertices; // Number of vertices
    private LinkedList<Integer> adj[]; // Adjacency List for each vertex
 
@@ -15,7 +15,7 @@ public class GraphDFS {
    }
 
    void addEdge(int source, int destination) {
-      adj[source].add(destination); 
+     adj[source].add(destination); 
    }
  
    public void DFS(int source) { 
@@ -23,7 +23,7 @@ public class GraphDFS {
      boolean visited[] = new boolean[vertices];
   
      for(int i = 0; i < vertices; i++) {  
-        visited[i] = false;
+       visited[i] = false;
      }
   
      System.out.print("The DFS traversal of the graph is: ");
@@ -37,60 +37,59 @@ public class GraphDFS {
   
    public void DFSUtil(int source, boolean visited[]) { 
      // Create a stack for DFS.  
-    java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
-    visited[source] = true;	   
-    stack.push(source);
+     java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
+     visited[source] = true;	   
+     stack.push(source);
 	    
-    while(!stack.isEmpty()) {
-      // Peek a vertex from stack and print it.  
-      int v = stack.peek();
-      System.out.print(v + " ");     
-      stack.pop();
+     while(!stack.isEmpty()) {
+       // Peek a vertex from stack and print it.  
+       int v = stack.peek();
+       System.out.print(v + " ");     
+       stack.pop();
 	 	
-      for(Integer neighbour : adj[v]) {
-        if(!visited[neighbour]) {
-          visited[neighbour] = true;
-          stack.push(neighbour);
-        }  
-      }
-    }
+       for(Integer neighbour : adj[v]) {
+         if(!visited[neighbour]) {
+           visited[neighbour] = true;
+           stack.push(neighbour);
+         }  
+       }
+     }
    }
 
-   public void DFS1(int source) {
-		
-      java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
-      List<Integer> list = new ArrayList<Integer>();
-      stack.push(source);
-      list.add(source);
+   public void DFS1(int source) {		
+     java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
+     List<Integer> list = new ArrayList<Integer>();
+     stack.push(source);
+     list.add(source);
   
-      System.out.println();
-      System.out.println();
-      System.out.print("The DFS traversal of the graph is: ");
+     System.out.println();
+     System.out.println();
+     System.out.print("The DFS traversal of the graph is: ");
    
-      while(!stack.empty()) {
-        int temp = stack.pop();
-        System.out.print(temp + " "); //Printing the root
-        for(Integer i : adj[temp]) {
-          if(!list.contains(i)) {
-            list.add(i);
-            stack.push(i);						
-          }
-        }
-      }
+     while(!stack.empty()) {
+       int temp = stack.pop();
+       System.out.print(temp + " "); //Printing the root
+       for(Integer i : adj[temp]) {
+         if(!list.contains(i)) {
+           list.add(i);
+           stack.push(i);						
+	 }
+       }
+     }
    }
 		
    public static void main(String[] args) {
-      GraphDFS g = new GraphDFS(5);
+     GraphDFS g = new GraphDFS(5);
 	 
-      g.addEdge(1, 0);
-      g.addEdge(0, 2);
-      g.addEdge(2, 1);
-      g.addEdge(0, 3);
-      g.addEdge(3, 4);
-      g.addEdge(4, 0);
+     g.addEdge(1, 0);
+     g.addEdge(0, 2);
+     g.addEdge(2, 1);
+     g.addEdge(0, 3);
+     g.addEdge(3, 4);
+     g.addEdge(4, 0);
   
-      g.DFS(0);
+     g.DFS(0);
   
-      g.DFS1(0);
+     g.DFS1(0);
    }
-}
+ }
