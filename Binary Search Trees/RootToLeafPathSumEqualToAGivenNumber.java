be59f44bbbb2144ad8b54db23f1aedcb55061068
Aditya Gupta <contactadityag@gmail.com>
2018-04-19 10:33:14 -0400

    This is a program in a Binary Tree to find if the tree has a root to leaf path such that adding up all the values along the path equals the given sum.

--- Binary Search Trees/RootToLeafPathSumEqualToAGivenNumber.java
+++ Binary Search Trees/RootToLeafPathSumEqualToAGivenNumber.java
@@ -1,32 +1,32 @@
 import java.util.*;
 import java.io.*;
 
- public class RootToLeafPathSumEqualToAGivenNumber { 
+   public class RootToLeafPathSumEqualToAGivenNumber { 
      static Node root;	    
      static Node previous;
 	
      public static boolean hasPathSum(Node node, int sum) {
-	if(node == null) {
-	  return (sum == 0);
-	}
-	else {
-          boolean answer = false;   
-          /* otherwise check both subtrees */
-          int subsum = sum - node.data;
-          if (subsum == 0 && node.left == null && node.right == null) {
-            return true;
-          }
-          if (node.left != null) {
-            answer = answer || hasPathSum(node.left, subsum);
-          }  
-          if (node.right != null) {
-            answer = answer || hasPathSum(node.right, subsum);
-          }    
+       if(node == null) {
+	 return (sum == 0);
+       }
+       else {
+         boolean answer = false;   
+         /* otherwise check both subtrees */
+         int subsum = sum - node.data;
+         if (subsum == 0 && node.left == null && node.right == null) {
+           return true;
+         }
+         if (node.left != null) {
+           answer = answer || hasPathSum(node.left, subsum);
+         }  
+         if (node.right != null) {
+           answer = answer || hasPathSum(node.right, subsum);
+         }    
          return answer;
        }
      }
 
-    public static void main(String[] args) {   
+     public static void main(String[] args) {   
        RootToLeafPathSumEqualToAGivenNumber tree = new RootToLeafPathSumEqualToAGivenNumber();
        tree.root = new Node(10);
        tree.root.left = new Node(8);
@@ -46,7 +46,6 @@ import java.io.*;
 	 System.out.println("There is a root to leaf path with sum " + targetSum);
        } else {
          System.out.println("There is no root to leaf path with sum " + targetSum);
-       }	  
-		
-    }
- }
+       }	  	
+     }
+   }
