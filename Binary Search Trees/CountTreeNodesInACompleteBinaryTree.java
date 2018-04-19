dd1d892123da43be1f29260650c3324e49bf782f
Aditya Gupta <contactadityag@gmail.com>
2018-04-19 10:45:36 -0400

    This is a program to count the number of nodes in a complete binary tree.
    In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as
    possible. It can have between 1 and 2h nodes inclusive at the last level h.

--- Binary Search Trees/CountTreeNodesInACompleteBinaryTree.java
+++ Binary Search Trees/CountTreeNodesInACompleteBinaryTree.java
@@ -1,65 +1,63 @@
 import java.util.*;
 import java.io.*;
 
- public class CountTreeNodesInACompleteBinaryTree {
-	Node root;	    
-	Node previous;
+   public class CountTreeNodesInACompleteBinaryTree {
+      Node root;	    
+      Node previous;
 	    
-	public int countNodes(Node root) {	 
-	   if (root == null) {
-	   	 return 0;
-	   }
+      public int countNodes(Node root) {	 
+	if (root == null) {
+	  return 0;
+	}
 	   
-	   int leftHeight = findLeftHeight(root);
-	   int rightHeight = findRightHeight(root);
+	int leftHeight = findLeftHeight(root);
+	int rightHeight = findRightHeight(root);
 	   
-	   if (leftHeight == rightHeight) {
+	if (leftHeight == rightHeight) {
           return (2 << (leftHeight-1)) - 1;
-       }
+        }
 	
-	   return countNodes(root.left) + countNodes(root.right) + 1;
-	}
+	return countNodes(root.left) + countNodes(root.right) + 1;
+      }
  
-	private int findLeftHeight(Node root) {
-	   if (root == null) {
-	      return 0;
-	   }
+      private int findLeftHeight(Node root) {
+	 if (root == null) {
+	   return 0;
+	 }
 	        
-	   int height = 1;
+	 int height = 1;
 	        
-	   while (root.left != null) {
-	     height++;
-	     root = root.left;
-	   }
+	 while (root.left != null) {
+	    height++;
+	    root = root.left;
+	 }
 	        
-	   return height;
-	}
+	 return height;
+      }
 
-	private int findRightHeight(Node root) { 	   
-	   if (root == null) {
-	      return 0;
-	   }
+      private int findRightHeight(Node root) { 	   
+	 if (root == null) {
+	   return 0;
+	 }
 	        
-	   int height = 1;
+	 int height = 1;
 	        
-	   while (root.right != null) {
-	     height++;
-	     root = root.right;
-	   }
+	 while (root.right != null) {
+	   height++;
+	   root = root.right;
+	 }
 	        
-	   return height;
-	}
+	 return height;
+      } 
 
-	public static void main(String[] args) {   
-	  CountTreeNodesInACompleteBinaryTree tree = new CountTreeNodesInACompleteBinaryTree();
-	  tree.root = new Node(1);
-	  tree.root.left = new Node(2);
-	  tree.root.right = new Node(3);
-	  tree.root.left.left = new Node(4);
-	  tree.root.left.right = new Node(5);
+      public static void main(String[] args) {   
+	CountTreeNodesInACompleteBinaryTree tree = new CountTreeNodesInACompleteBinaryTree();
+	tree.root = new Node(1);
+	tree.root.left = new Node(2);
+	tree.root.right = new Node(3);
+	tree.root.left.left = new Node(4);
+	tree.root.left.right = new Node(5);
 	         
-	  System.out.println("The number of nodes in the complete binary tree are: " + tree.countNodes(tree.root));
-
-
-	}
- }
+        System.out.println("The number of nodes in the complete binary tree are: " + tree.countNodes(tree.root));
+      }
+   }
