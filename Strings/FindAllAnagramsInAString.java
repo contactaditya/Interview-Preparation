287394634d668e11de6445b2cafc0e061e2d0398
Aditya Gupta <contactadityag@gmail.com>
2018-04-20 22:08:28 -0400

    This is a program to find all the anagrams in the string.

--- Strings/FindAllAnagramsInAString.java
+++ Strings/FindAllAnagramsInAString.java
@@ -4,19 +4,19 @@ import java.io.*;
  public class FindAllAnagramsInAString { 	
 	
     public static List<Integer> findAnagrams(String string1, String string2) {
-       List<Integer> result = new ArrayList<Integer>();	
-       if (string1 == null ||  string1 == null || string1.length() < string2.length()) {
-	  return result;		
-       }
-       int m = string1.length(), n = string2.length();
-       for (int i = 0; i < m-n+1; i++) {
-         String current = string1.substring(i, i+n);
-         if (isAnagram(current, string2)) {
-           result.add(i);
-         }
-       }
-	   
-       return result;
+      List<Integer> result = new ArrayList<Integer>();	
+      if (string1 == null ||  string1 == null || string1.length() < string2.length()) {
+	return result;		
+      }
+      int m = string1.length(), n = string2.length();
+      for (int i = 0; i < m-n+1; i++) {
+        String current = string1.substring(i, i+n);
+        if (isAnagram(current, string2)) {
+          result.add(i);
+        }
+      }
+	    
+      return result;
     }	  
 	
     public static boolean isAnagram(String string1, String string2) {  
@@ -40,49 +40,49 @@ import java.io.*;
     }
 	
     public static List<Integer> findAnagrams1(String string1, String string2) {
-       List<Integer> result = new ArrayList<Integer>();	
-       if (string1 == null ||  string1 == null || string1.length() < string2.length()) {
-	 return result;		
-       }
-       int[] letters = new int['z'-'a'+1];
-       for (int i=0;i<string2.length();i++) {
-	  letters[string2.charAt(i)-'a']--;
-	  letters[string1.charAt(i)-'a']++;
-       }
-       for (int i=0;i<=string1.length()-string2.length();i++) {
-	 boolean anagram = true;
-	 for (int j=0;j<letters.length;j++) {
-	   if (letters[j] != 0) {
-	     anagram = false;
-	     break;
-	   }
-	 }
-	 if (anagram) {
-	   result.add(i);
-	 }
-	 if (i < string1.length()) {
-	   letters[string1.charAt(i)-'a']--;
-	 }
-	 if (i < string1.length()-string2.length()) {
-	   letters[string1.charAt(i+string2.length())-'a']++;
-	 }
-       }
-       return result;
+      List<Integer> result = new ArrayList<Integer>();	
+      if (string1 == null ||  string1 == null || string1.length() < string2.length()) {
+	return result;		
+      }
+      int[] letters = new int['z'-'a'+1];
+      for (int i=0; i<string2.length(); i++) {
+	letters[string2.charAt(i)-'a']--;
+	letters[string1.charAt(i)-'a']++;
+      }
+      for (int i=0; i<=string1.length()-string2.length(); i++) {
+	boolean anagram = true;
+	for (int j=0; j<letters.length; j++) {
+	  if (letters[j] != 0) {
+	    anagram = false;
+	    break;
+	  }
+	}
+	if (anagram) {
+	  result.add(i);
+	}
+	if (i < string1.length()) {
+	  letters[string1.charAt(i)-'a']--;
+	}
+	if (i < string1.length()-string2.length()) {
+	  letters[string1.charAt(i+string2.length())-'a']++;
+	}
+      }
+      return result;
     }	  	
 
     public static void main(String[] args) {   
-       String string1 = new String();	  
-       Scanner input1 = new Scanner(System.in);
-       System.out.print("Enter the first string: ");
-       string1 = input1.next();  
-       System.out.println();	
-       String string2 = new String();	  
-       Scanner input2 = new Scanner(System.in);
-       System.out.print("Enter the second string: ");
-       string2 = input2.next();
-       List<Integer> result = new ArrayList<Integer>();
-       System.out.println();
-       result = findAnagrams(string1, string2);
-       System.out.println("All the anagrams with the starting indices are: " + result); 
+      String string1 = new String();	  
+      Scanner input1 = new Scanner(System.in);
+      System.out.print("Enter the first string: ");
+      string1 = input1.next();  
+      System.out.println();	
+      String string2 = new String();	  
+      Scanner input2 = new Scanner(System.in);
+      System.out.print("Enter the second string: ");
+      string2 = input2.next();
+      List<Integer> result = new ArrayList<Integer>();
+      System.out.println();
+      result = findAnagrams(string1, string2);
+      System.out.println("All the anagrams with the starting indices are: " + result); 
     }
   }
