8143761f493ece0b6a5b43e7a5defbcd74d2d2f0
Aditya Gupta <contactadityag@gmail.com>
2018-04-23 20:20:29 -0400

    This is a program to calculate the sum of the average of each K adjacent (non-empty) groups.

--- Arrays/LargestSumOfAverages.java
+++ Arrays/LargestSumOfAverages.java
@@ -3,7 +3,7 @@ import java.io.*;
 
   public class LargestSumOfAverages {
 	  
-	public static double largestSumOfAverages(int[] array, int K) {
+    public static double largestSumOfAverages(int[] array, int K) {
       int N = array.length;
       double[] P = new double[N+1];	
       for (int i = 0; i < N; i++) {
@@ -17,32 +17,32 @@ import java.io.*;
       for (int k = 0; k < K-1; k++) {
         for (int i = 0; i < N; i++) {
           for (int j = i+1; j < N; j++) {
-        	dp[i] = Math.max(dp[i], (P[j]-P[i]) / (j-i) + dp[j]);  
+            dp[i] = Math.max(dp[i], (P[j]-P[i]) / (j-i) + dp[j]);  
           }
         }
       }
 		
-	  return dp[0];      
-	}  
+      return dp[0];      
+    }  
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 
-	  System.out.println();	
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt();
+      }
 	  
-	  System.out.println();
-	  System.out.print("Enter the number of counters: ");
-	  int K = input.nextInt();   
+      System.out.println();
+      System.out.print("Enter the number of counters: ");
+      int K = input.nextInt();   
 		
-	  double largestSumOfAverages = largestSumOfAverages(array, K); 
-	  System.out.println();	
-	  System.out.print("The largest score that can be achieved in " + K + " adjacent (non-empty) groups is: " + largestSumOfAverages);
-	}
+      double largestSumOfAverages = largestSumOfAverages(array, K); 
+      System.out.println();	
+      System.out.print("The largest score that can be achieved in " + K + " adjacent (non-empty) groups is: " + largestSumOfAverages);
+    }
   }
