473db6f3662f0a5516eb6dfcb5201d4dde61e999
Aditya Gupta <contactadityag@gmail.com>
2018-04-23 20:25:37 -0400

    This is a program to return the most frequent word that is not in the list of banned words.

--- Strings/MostCommonWord.java
+++ Strings/MostCommonWord.java
@@ -3,9 +3,9 @@ import java.io.*;
 
   public class MostCommonWord {
 	  
-	public static String mostCommonWord(String paragraph, String[] banned) {
-	  paragraph += ".";	
-	  String answer = "";
+    public static String mostCommonWord(String paragraph, String[] banned) {
+      paragraph += ".";	
+      String answer = "";
       int ansfrequency = 0;
       
       Set<String> banset = new HashSet<String>();
@@ -20,38 +20,38 @@ import java.io.*;
         } else if (word.length() > 0) {
             String finalword = word.toString();
             if(!banset.contains(finalword)) {
-        	  count.put(finalword, count.getOrDefault(finalword, 0) + 1);  
-        	  if(count.get(finalword) > ansfrequency) {
-        	    answer = finalword;
-        	    ansfrequency = count.get(finalword);  
-        	  }
+              count.put(finalword, count.getOrDefault(finalword, 0) + 1);  
+              if(count.get(finalword) > ansfrequency) {
+        	answer = finalword;
+        	ansfrequency = count.get(finalword);  
+              }
             }
             word = new StringBuilder();
         }
       }
 			
       return answer;      
-	}	  
+    }	  
 
-	public static void main(String[] args) {    
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of banned words: ");
-	  int numbers = input.nextInt();   
-	  String banned[] = new String[numbers];    
-	  System.out.println();
-	  input.nextLine();
-	  System.out.print("Please enter the banned words: ");
-	  System.out.println();
-	  for (int i = 0; i < numbers; i++) {
-		banned[i] = input.nextLine(); 
-	  }
-	  System.out.println();	
-	  String paragraph = new String();	  
-	  System.out.print("Enter the paragraph: ");
-	  paragraph = input.nextLine();
+    public static void main(String[] args) {    
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of banned words: ");
+      int numbers = input.nextInt();   
+      String banned[] = new String[numbers];    
+      System.out.println();
+      input.nextLine();
+      System.out.print("Please enter the banned words: ");
+      System.out.println();
+      for (int i = 0; i < numbers; i++) {
+	banned[i] = input.nextLine(); 
+      }
+      System.out.println();	
+      String paragraph = new String();	  
+      System.out.print("Enter the paragraph: ");
+      paragraph = input.nextLine();
 		  
-	  String mostCommonWord = mostCommonWord(paragraph, banned);
-	  System.out.println();	
+      String mostCommonWord = mostCommonWord(paragraph, banned);
+      System.out.println();	
       System.out.print("The most frequent word that is not in the list of banned words is: " + mostCommonWord);
-	}
+    }
   }
