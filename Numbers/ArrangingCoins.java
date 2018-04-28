b41ce7b32156895b247b9ea349248bcfe0f0d9be
Aditya Gupta <contactadityag@gmail.com>
2018-04-28 00:55:36 -0400

    This is a program to find the total number of full staircase rows that can be formed from the total n coins.

--- Numbers/ArrangingCoins.java
+++ Numbers/ArrangingCoins.java
@@ -3,17 +3,17 @@ import java.io.*;
 
   public class ArrangingCoins {
 	  
-	public static int arrangeCoins(int number) {
-	  return (int)((-1 + Math.sqrt(1 + 8 * (long)number)) / 2);
-	}	  
+    public static int arrangeCoins(int number) {
+      return (int)((-1 + Math.sqrt(1 + 8 * (long)number)) / 2); // n = (1 + x) * x / 2
+    }	  
 
-	public static void main(String[] args) {    
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of coins: ");
-	  int number = input.nextInt();  
+    public static void main(String[] args) {    
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of coins: ");
+      int number = input.nextInt();  
 		    
-	  int numberOfRows = arrangeCoins(number);
-	  System.out.println();
-	  System.out.println("The total number of full staircase rows that can be formed are: " + numberOfRows);
-	}
+      int numberOfRows = arrangeCoins(number);
+      System.out.println();
+      System.out.println("The total number of full staircase rows that can be formed are: " + numberOfRows);
+    }
   }
