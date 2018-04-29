89faad09ada56a1cb5bccbd2940dce43ad37e0a6
Aditya Gupta <contactadityag@gmail.com>
2018-04-29 19:32:35 -0400

    This is a program to return 1 - number in lexicographical order.

--- Numbers/LexicographicalNumbers.java
+++ Numbers/LexicographicalNumbers.java
@@ -3,12 +3,12 @@ import java.io.*;
 
   public class LexicographicalNumbers {
 	  
-	public static List<Integer> lexicalOrder(int number) {
+    public static List<Integer> lexicalOrder(int number) {
       List<Integer> list = new ArrayList<Integer>();	
-	  int current = 1;
-	  for (int i = 1; i <= number; i++) {
-	    list.add(current);
-	    if (current * 10 <= number) {
+      int current = 1;
+      for (int i = 1; i <= number; i++) {
+	list.add(current);
+	if (current * 10 <= number) {
           current *= 10; // Going to the left most leaf as far as possible
         } else if (current % 10 != 9 && current + 1 <= number) {
           current++; // Found the left most, doing level traversal (all children from left to right)
@@ -18,19 +18,19 @@ import java.io.*;
           }
           current = current/10 + 1; // Preparing to do the level traversal again on upper level 
         }
-	  }
+      }
 		  
-	  return list;           
-	}  
+      return list;           
+    }  
 
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();   
+    public static void main(String[] args) { 	 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int number = input.nextInt();   
 	
-	  List<Integer> result = new ArrayList<Integer>();
-	  result = lexicalOrder(number);
-	  System.out.println();	
+      List<Integer> result = new ArrayList<Integer>();
+      result = lexicalOrder(number);
+      System.out.println();	
       System.out.print("The integer " + number + " in lexicographical order is: " + result);
-	}
+    }
   }
