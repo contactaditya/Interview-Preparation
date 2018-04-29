244ca249dcf2828a65b0a0900dce22204ceff61a
Aditya Gupta <contactadityag@gmail.com>
2018-04-29 19:23:19 -0400

    This is the program to get the most number of chunks that we could have made in the array.

--- Arrays/MaxChunksToMakeSorted.java
+++ Arrays/MaxChunksToMakeSorted.java
@@ -3,7 +3,7 @@ import java.io.*;
 
   public class MaxChunksToMakeSorted {
 	  
-	public static int maxChunksToSorted(int[] array) {
+    public static int maxChunksToSorted(int[] array) {
       int result = 0;
       
       for (int i = 0, max = 0; i < array.length; i++) {
@@ -13,23 +13,23 @@ import java.io.*;
         }
       }
 		
-	  return result;      
-	}	  
+      return result;      
+    }	  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();  
+      int array[] = new int[number];
 	
-	  System.out.println();	
-	  System.out.print("Please enter the elements in the array: ");
-	  for (int i = 0; i < array.length; i++) {
-		array[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the elements in the array: ");
+      for (int i = 0; i < array.length; i++) {
+	array[i] = input.nextInt();
+      }
 	
-	  System.out.println();	
-	  int maxNumberOfChunks = maxChunksToSorted(array);
-	  System.out.print("The most number of chunks that we could have made are: " + maxNumberOfChunks);  
-	}
+      System.out.println();	
+      int maxNumberOfChunks = maxChunksToSorted(array);
+      System.out.print("The most number of chunks that we could have made are: " + maxNumberOfChunks);  
+    }
   }
