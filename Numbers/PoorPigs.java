7160cd8b1bf565da9b090a1326b9acd94fd791c8
Aditya Gupta <contactadityag@gmail.com>
2018-04-29 22:12:16 -0400

    This is a program to calculate the minimum amount of pigs you need to figure out which bucket contains the poison within one hour.

--- Numbers/PoorPigs.java
+++ Numbers/PoorPigs.java
@@ -3,32 +3,32 @@ import java.io.*;
 
   public class PoorPigs {
 	  
-	public static int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-	  if (buckets < 2) {
-		return 0;
-	  }
+    public static int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+      if (buckets < 2) {
+	return 0;
+      }
 		    
-	  if (minutesToDie > minutesToTest) {
-		return Integer.MAX_VALUE;
-	  }
+      if (minutesToDie > minutesToTest) {
+	return Integer.MAX_VALUE;
+      }
 	  
-	  int k = minutesToTest/minutesToDie;
-	  return (int)Math.ceil((Math.log(buckets) / Math.log(k + 1)));
-	}	  
+      int k = minutesToTest/minutesToDie;
+      return (int)Math.ceil((Math.log(buckets) / Math.log(k + 1)));
+    }	  
 
-	public static void main(String[] args) {    
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of buckets: ");
-	  int buckets = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Enter number of minutes to die: ");
-	  int minutesToDie = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Enter number of minutes to test: ");
-	  int minutesToTest = input.nextInt();  
+    public static void main(String[] args) {    
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of buckets: ");
+      int buckets = input.nextInt();  
+      System.out.println();
+      System.out.print("Enter number of minutes to die: ");
+      int minutesToDie = input.nextInt();  
+      System.out.println();
+      System.out.print("Enter number of minutes to test: ");
+      int minutesToTest = input.nextInt();  
 		    
-	  int result = poorPigs(buckets, minutesToDie, minutesToTest);
-	  System.out.println();
-	  System.out.print("The minimum amount of pigs you need to figure out which bucket contains the poison within one hour is: " + result);
-	}
+      int result = poorPigs(buckets, minutesToDie, minutesToTest);
+      System.out.println();
+      System.out.print("The minimum amount of pigs you need to figure out which bucket contains the poison within one hour is: " + result);
+    }
   }
