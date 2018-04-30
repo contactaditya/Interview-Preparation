0c56dfc039972a80e3ca9753d1817c2f2f562ad1
Aditya Gupta <contactadityag@gmail.com>
2018-04-29 23:25:45 -0400

    This is a program to find the maximum path sum in a binary tree. The path may start and end at any node in the tree.

--- Binary Search Trees/MaximumPathSumInABinaryTree.java
+++ Binary Search Trees/MaximumPathSumInABinaryTree.java
@@ -1,40 +1,39 @@
 import java.util.*;
 import java.io.*;
 
- public class MaximumPathSumInABinaryTree {
-     private static Node root;	    
-     private static Node previous; 
-     int maxValue;
+  public class MaximumPathSumInABinaryTree {
+    private static Node root;	    
+    private static Node previous; 
+    int maxValue;
 	
-     public int findMaximumSum(Node node) {
-        maxValue = Integer.MIN_VALUE;
-	maxPathDown(root);
-	return maxValue;
-     } 
+    public int findMaximumSum(Node node) {
+      maxValue = Integer.MIN_VALUE;
+      maxPathDown(root);
+      return maxValue;
+    } 
 	
-     public int maxPathDown(Node node) {
-	if (node == null) {
-	  return 0;
-	}
-	int left = Math.max(0, maxPathDown(node.left));
-	int right = Math.max(0, maxPathDown(node.right));
-	maxValue = Math.max(maxValue, left + right + node.data);
-	return Math.max(left, right) + node.data;
-     } 
+    public int maxPathDown(Node node) {
+      if (node == null) {
+	return 0;
+      } 
+      int left = Math.max(0, maxPathDown(node.left));
+      int right = Math.max(0, maxPathDown(node.right));
+      maxValue = Math.max(maxValue, left + right + node.data);
+      return Math.max(left, right) + node.data;
+    } 
 
-     public static void main(String[] args) {
-       MaximumPathSumInABinaryTree tree = new MaximumPathSumInABinaryTree();
-       tree.root = new Node(10);
-       tree.root.left = new Node(2);
-       tree.root.right = new Node(10);
-       tree.root.left.left = new Node(20);
-       tree.root.left.right = new Node(1);
-       tree.root.right.right = new Node(-25);
-       tree.root.right.right.left = new Node(3);
-       tree.root.right.right.right = new Node(4);
-       
-       int maximumSum = tree.findMaximumSum(tree.root);		
-       System.out.print("Maximum path sum in a binary tree is: " + maximumSum);
-
-     }
- }
+    public static void main(String[] args) {
+      MaximumPathSumInABinaryTree tree = new MaximumPathSumInABinaryTree();
+      tree.root = new Node(10);
+      tree.root.left = new Node(2);
+      tree.root.right = new Node(10);
+      tree.root.left.left = new Node(20);
+      tree.root.left.right = new Node(1);
+      tree.root.right.right = new Node(-25);
+      tree.root.right.right.left = new Node(3);
+      tree.root.right.right.right = new Node(4);
+      
+      int maximumSum = tree.findMaximumSum(tree.root);		
+      System.out.print("Maximum path sum in a binary tree is: " + maximumSum);
+    }
+  }
