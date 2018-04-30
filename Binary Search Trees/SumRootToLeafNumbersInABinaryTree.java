aebb6ee56ed32227e8f44d9cc4251127b395c5f7
Aditya Gupta <contactadityag@gmail.com>
2018-04-29 22:39:22 -0400

    This is a program to find the sum of each root-to-leaf path that could represent a number in a binary tree containing digits from 0-9 only.

--- Binary Search Trees/SumRootToLeafNumbersInABinaryTree.java
+++ Binary Search Trees/SumRootToLeafNumbersInABinaryTree.java
@@ -6,31 +6,31 @@ import java.io.*;
     long pathSum = 0;
     
     public int sumNumbers(Node root) {
-       sumNumbersHelper(root, 0);	        
-       return (int)pathSum;	
+      sumNumbersHelper(root, 0);	        
+      return (int)pathSum;	
     } 
 	
     private void sumNumbersHelper(Node root, long currentSum) {
-       if (root == null) {
-	  return;
-       }	
+      if (root == null) {
+	return;
+      }	
 		
-       currentSum = currentSum * 10 + root.data;
-       if (root.left == null && root.right == null) {
-          pathSum += currentSum;
-       }
+      currentSum = currentSum * 10 + root.data;
+      if (root.left == null && root.right == null) {
+        pathSum += currentSum;
+      }
 	   
-       sumNumbersHelper(root.left, currentSum);
-       sumNumbersHelper(root.right, currentSum);	
+      sumNumbersHelper(root.left, currentSum);
+      sumNumbersHelper(root.right, currentSum);	
     }
 
     public static void main(String[] args) {    
-       SumRootToLeafNumbersInABinaryTree tree = new SumRootToLeafNumbersInABinaryTree();
-       tree.root = new Node(1);
-       tree.root.left = new Node(2);
-       tree.root.right = new Node(3);
+      SumRootToLeafNumbersInABinaryTree tree = new SumRootToLeafNumbersInABinaryTree();
+      tree.root = new Node(1);
+      tree.root.left = new Node(2);
+      tree.root.right = new Node(3);
 
-       int sum = tree.sumNumbers(tree.root);		
-       System.out.print("The total sum of all root-to-leaf numbers is: " + sum);
+      int sum = tree.sumNumbers(tree.root);		
+      System.out.print("The total sum of all root-to-leaf numbers is: " + sum);
     }
   }
