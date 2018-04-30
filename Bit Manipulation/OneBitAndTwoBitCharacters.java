16801cd1238bd3b8f18a0f47bab40331e85342e3
Aditya Gupta <contactadityag@gmail.com>
2018-04-29 21:27:44 -0400

    This is a program to return whether the last character must be a one-bit character or not. The given string will always end with a zero.
    We have two special characters. The first character can be represented by one bit 0. The second character can be represented by two bits (10 or 11).

--- Bit Manipulation/OneBitAndTwoBitCharacters.java
+++ Bit Manipulation/OneBitAndTwoBitCharacters.java
@@ -3,19 +3,19 @@ import java.io.*;
 
   public class OneBitAndTwoBitCharacters {
 	  
-	public static boolean isOneBitCharacter(int[] bits) {   
-	  int ones = 0; 	
-	  // Starting from one but last, as last one is always 0.
-	  for(int i = bits.length - 2; i >= 0 && bits[i] != 0 ; i--) {
+    public static boolean isOneBitCharacter(int[] bits) {   
+      int ones = 0; 	
+      // Starting from one but last, as last one is always 0.
+      for(int i = bits.length - 2; i >= 0 && bits[i] != 0 ; i--) {
         ones++;
       }
-	  if(ones % 2 > 0) {
-		return false;  
-	  }
+      if(ones % 2 > 0) {
+	return false;  
+      }
       return true;      
-	}	  
+    }	  
 
-	public static void main(String[] args) { 	  
+    public static void main(String[] args) { 	  
       Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int number = input.nextInt();  
@@ -27,13 +27,13 @@ import java.io.*;
         bits[i] = input.nextInt();
       }
 			
-	  boolean result = isOneBitCharacter(bits);
+      boolean result = isOneBitCharacter(bits);
       System.out.println();
-	  if(result) {
-		System.out.println("The last character must be a one-bit character.");  
-	  }
-	  else {
-		System.out.println("The last character will not be a one-bit character.");  
-	  }
-	}
+      if(result) {
+	System.out.println("The last character must be a one-bit character.");  
+      }
+      else {
+	System.out.println("The last character will not be a one-bit character.");  
+      }
+    }
   }
