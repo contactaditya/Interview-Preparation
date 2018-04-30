89bcf01c2c93d6e63a44c5415c30c9e98e320d8f
Aditya Gupta <contactadityag@gmail.com>
2018-04-29 21:25:21 -0400

    This is a program to print the sequence of gray code.
    The gray code is a binary numeral system where two successive values differ in only one bit. A gray code sequence must begin with 0.

--- Numbers/GrayCode.java
+++ Numbers/GrayCode.java
@@ -3,23 +3,23 @@ import java.io.*;
 
   public class GrayCode {
 	  
-	public static List<Integer> grayCode(int number) {
-	  List<Integer> result = new LinkedList<Integer>();	
-	  for (int i = 0; i < 1 << number; i++) {
-		result.add(i ^ i >> 1);
-	  }
+    public static List<Integer> grayCode(int number) {
+      List<Integer> result = new LinkedList<Integer>();	
+      for (int i = 0; i < 1 << number; i++) {
+	result.add(i ^ i >> 1);
+      }
 	
-	  return result;      
-	}	  
+      return result;      
+    }	  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();   
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int number = input.nextInt();   
 	
-	  List<Integer> result = new ArrayList<Integer>();
-	  result = grayCode(number);
-	  System.out.println();	
+      List<Integer> result = new ArrayList<Integer>();
+      result = grayCode(number);
+      System.out.println();	
       System.out.print("The sequence of the gray code is: " + result);
-	}
+    }
   } 
