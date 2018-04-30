6e69de64f04da7393d1700447cdeaa221fc8c694
Aditya Gupta <contactadityag@gmail.com>
2018-04-29 22:14:26 -0400

    This is a program to calculate the shortest distance from the character C in the string.

--- Strings/ShortestDistanceToACharacter.java
+++ Strings/ShortestDistanceToACharacter.java
@@ -3,19 +3,19 @@ import java.io.*;
 
   public class ShortestDistanceToACharacter {
 	
-	public static int[] shortestToChar(String S, char C) {
-	  int length = S.length();
-	  int answer[] = new int[length];
-	  int previous = Integer.MIN_VALUE/2;
-	  // Find shortest distant to character on left.
-	  for (int i = 0; i < length; i++) {
+    public static int[] shortestToChar(String S, char C) {
+      int length = S.length();
+      int answer[] = new int[length];
+      int previous = Integer.MIN_VALUE/2;
+      // Find shortest distant to character on left.
+      for (int i = 0; i < length; i++) {
         if (S.charAt(i) == C) {
           previous = i;
         }
         answer[i] = i - previous;
       }
-	  // Find shortest distant to character on right.
-	  previous = Integer.MAX_VALUE/2;
+      // Find shortest distant to character on right.
+      previous = Integer.MAX_VALUE/2;
       for (int i = length-1; i >= 0; i--) {
         if (S.charAt(i) == C) {
           previous = i;
@@ -23,23 +23,23 @@ import java.io.*;
         answer[i] = Math.min(answer[i], previous - i);
       }
       
-	  return answer;  
+      return answer;  
     }
 
-	public static void main(String[] args) {   
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine(); 
-	  System.out.println();
-	  System.out.print("Enter the character: ");
-	  char C = input.next().charAt(0); 
+    public static void main(String[] args) {   
+      String string = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the string: ");
+      string = input.nextLine(); 
+      System.out.println();
+      System.out.print("Enter the character: ");
+      char C = input.next().charAt(0); 
 	  
-	  int result[] = shortestToChar(string, C);
-	  System.out.println();
-	  System.out.println("The array of integers representing the shortest distance from the character C in the string is: ");  
-	  for (int i = 0; i < result.length; i++) {
-		System.out.print(result[i] + " ");
-	  }
-	}
+      int result[] = shortestToChar(string, C);
+      System.out.println();
+      System.out.println("The array of integers representing the shortest distance from the character C in the string is: ");  
+      for (int i = 0; i < result.length; i++) {
+	System.out.print(result[i] + " ");
+      }
+    }
   }
