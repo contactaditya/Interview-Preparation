a1b2a5046e672a8031b28232f39d3aec114e72bc
Aditya Gupta <contactadityag@gmail.com>
2018-05-02 17:01:18 -0400

    This is a program to return an edge that can be removed so that the resulting graph is a tree of N nodes.
    The answer edge [u, v] should be in the same format, with u < v.

--- Graphs/RedundantConnection.java
+++ Graphs/RedundantConnection.java
@@ -38,9 +38,9 @@ class DSU {
 
   public class RedundantConnection {
     static int MAX_EDGE_VAL = 1000;
-  
-	public static int[] findRedundantConnection(int[][] edges) {  
-	  DSU dsu = new DSU(MAX_EDGE_VAL + 1);
+   
+    public static int[] findRedundantConnection(int[][] edges) {  
+      DSU dsu = new DSU(MAX_EDGE_VAL + 1);
       for (int[] edge: edges) {
         if (!dsu.union(edge[0], edge[1])) {
           return edge;
@@ -48,26 +48,26 @@ class DSU {
       }
 		
       throw new AssertionError();
-	}	  
+    }	  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Please enter the number of rows of the graph: ");
-	  int x = input.nextInt();  
-	  System.out.println();
-	  int[][] edges = new int[x][2];
-	  System.out.println("Please enter the actual values of the edges in the graph: ");
-	  for(int i = 0; i < x; i++) {
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Please enter the number of rows of the graph: ");
+      int x = input.nextInt();  
+      System.out.println();
+      int[][] edges = new int[x][2];
+      System.out.println("Please enter the actual values of the edges in the graph: ");
+      for(int i = 0; i < x; i++) {
         for(int j = 0; j < 2; j++) {
           edges[i][j] = input.nextInt();
         }
       }
 	  
-	  int result[] = findRedundantConnection(edges); 
-	  System.out.println();
-	  System.out.print("An edge that can be removed so that the resulting graph is a tree of N nodes: ");
-	  for(int i = 0; i < result.length; i++) {
-		System.out.print(result[i] + " ");
+      int result[] = findRedundantConnection(edges); 
+      System.out.println();
+      System.out.print("An edge that can be removed so that the resulting graph is a tree of N nodes: ");
+      for(int i = 0; i < result.length; i++) {
+	System.out.print(result[i] + " ");
       }
-	}
+    }
   }
