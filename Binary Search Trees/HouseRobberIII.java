b8e1ba6697ffda61c8f61ab31fef7bb750fff18c
Aditya Gupta <contactadityag@gmail.com>
2018-05-02 16:58:18 -0400

    This is a program to determine the maximum amount of money the thief can rob tonight without alerting the police.

--- Binary Search Trees/HouseRobberIII.java
+++ Binary Search Trees/HouseRobberIII.java
@@ -3,19 +3,19 @@ import java.io.*;
 
   public class HouseRobberIII { 	
     private static Node root;	    
-	private static Node previous; 
+    private static Node previous; 
 	  
-	public static int rob(Node root) {
-	  if (root == null) {
-		return 0;	
+    public static int rob(Node root) {
+      if (root == null) {
+	return 0;	
       }
 	  
-	  int[] value = robSum(root);
+      int[] value = robSum(root);
       return Math.max(value[0], value[1]); 
-	}	  
+    }	  
 
-	private static int[] robSum(Node root) { 
-	  int[] result = new int[2];
+    private static int[] robSum(Node root) { 
+      int[] result = new int[2];
       if (root == null) {
     	return result;
       }
@@ -26,17 +26,17 @@ import java.io.*;
       result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
       result[1] = root.data + left[0] + right[0];
 	
-	  return result;
-	}
+      return result;
+    }
 
-	public static void main(String[] args) { 	 	  
-	  HouseRobberIII tree = new HouseRobberIII();
-	  tree.root = new Node(3);
-	  tree.root.left = new Node(2);
-	  tree.root.right = new Node(3);
-	  tree.root.left.right = new Node(3);
-	  tree.root.right.right = new Node(1);
+    public static void main(String[] args) { 	 	  
+      HouseRobberIII tree = new HouseRobberIII();
+      tree.root = new Node(3);
+      tree.root.left = new Node(2); 
+      tree.root.right = new Node(3);
+      tree.root.left.right = new Node(3);
+      tree.root.right.right = new Node(1);
 		  
-	  System.out.println("The maximum amount of money the thief can rob tonight without alerting the police is: " + tree.rob(tree.root));
-	}
+      System.out.println("The maximum amount of money the thief can rob tonight without alerting the police is: " + tree.rob(tree.root));
+    }
   }
