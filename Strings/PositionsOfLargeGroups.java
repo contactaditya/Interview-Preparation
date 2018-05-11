2805a97a1ca7a30d9df96525017333e337aa5a04
Aditya Gupta <contactadityag@gmail.com>
2018-05-10 20:20:01 -0400

    This is a program to find the postions of all the large groups in the given string.
    A group is considered a large group if it has consecutive groups of the same character and the group contains atleast 3 or more characters.

--- Strings/PositionsOfLargeGroups.java
+++ Strings/PositionsOfLargeGroups.java
@@ -3,36 +3,36 @@ import java.io.*;
 
   public class PositionsOfLargeGroups {
 
-	public static List<List<Integer>> largeGroupPositions(String string) {
-	  List<List<Integer>> result = new ArrayList<List<Integer>>();
-	  int i = 0, N = string.length(); // i is the start of each group
-	  for(int j = 0; j < N; j++) {
-	    if (j == N-1 || string.charAt(j) != string.charAt(j+1)) {  
-	      // Here, [i, j] represents a group.	
-	      if(j - i + 1 >= 3) {
-	        result.add(Arrays.asList(new Integer[]{i, j}));  
-	      }
-	      i = j + 1;
-	    }
+    public static List<List<Integer>> largeGroupPositions(String string) {
+      List<List<Integer>> result = new ArrayList<List<Integer>>();
+      int i = 0, N = string.length(); // i is the start of each group
+      for(int j = 0; j < N; j++) {
+        if (j == N-1 || string.charAt(j) != string.charAt(j+1)) {  
+	  // Here, [i, j] represents a group.	
+	  if(j - i + 1 >= 3) {
+	    result.add(Arrays.asList(new Integer[]{i, j}));  
 	  }
+	  i = j + 1;
+        }
+      }
 		
-      return result;      
-	}
+      return result;       
+    }
 	
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
-	  String string = new String();	  
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine(); 	  
-	  List<List<Integer>> result = new ArrayList<List<Integer>>();
+    public static void main(String[] args) { 	 
+      Scanner input = new Scanner(System.in);
+      String string = new String();	  
+      System.out.print("Enter the string: ");
+      string = input.nextLine(); 	  
+      List<List<Integer>> result = new ArrayList<List<Integer>>();
 	   
-	  result = largeGroupPositions(string); 
-	  System.out.println();
-	  System.out.println("All the positions of the large group are: ");
-	  System.out.println('[');
-	  for(List<Integer> innerList : result) {
-		System.out.println(innerList + " ");
-	  }
-	  System.out.println(']');
-	}
+      result = largeGroupPositions(string); 
+      System.out.println();
+      System.out.println("All the positions of the large group are: ");
+      System.out.println('[');
+      for(List<Integer> innerList : result) {
+	System.out.println(innerList + " ");
+      }
+      System.out.println(']');
+    }
   }
