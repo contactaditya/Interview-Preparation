d675f82ae942ed8b92b8f1659cb22686ebc97b6b
Aditya Gupta <contactadityag@gmail.com>
2018-05-10 20:30:41 -0400

    This is a program to print all the elements of the matrix in diagonal order.

--- Arrays/DiagonalTraverse.java
+++ Arrays/DiagonalTraverse.java
@@ -3,7 +3,7 @@ import java.io.*;
 
   public class DiagonalTraverse {
 	  
-	public static int[] findDiagonalOrder(int[][] matrix) {
+    public static int[] findDiagonalOrder(int[][] matrix) {
       if (matrix.length == 0) {
     	return new int[0];
       }
@@ -13,13 +13,13 @@ import java.io.*;
     	if((row + column) % 2 == 0) {
     	  // moving up
           if(column == n - 1) { 
-        	row++; 
+            row++; 
           } else if(row == 0) {
-        	column++; 
+            column++; 
           }
           else {
-        	row--;
-        	column++;
+            row--;
+            column++;
           }
     	} else {
           if(row == m - 1) { 
@@ -33,34 +33,34 @@ import java.io.*;
     	}
       }
 		
-	  return array;      
+      return array;      
     }	  
 	
-	private static void printMatrix(int[] array) {
-	  System.out.println();
-	  System.out.print("All elements of the matrix in diagonal order are: "); 
+    private static void printMatrix(int[] array) {
+      System.out.println();
+      System.out.print("All elements of the matrix in diagonal order are: "); 
       for (int i = 0; i < array.length; i++) {
-		System.out.print(array[i] + " ");	
-	  }	
-	}	
+	System.out.print(array[i] + " ");	
+      }	
+    }	
 
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Please enter the number of rows in the matrix: ");
-	  int x = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Please enter the number of columns in the matrix: ");
-	  int y = input.nextInt();  
-	  int[][] matrix = new int[x][y];
-	  System.out.println();
-	  System.out.println("Please enter the actual values in the matrix: ");
-	  for(int i=0; i < x; i++) {
+    public static void main(String[] args) { 	 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Please enter the number of rows in the matrix: ");
+      int x = input.nextInt();  
+      System.out.println();
+      System.out.print("Please enter the number of columns in the matrix: ");
+      int y = input.nextInt();  
+      int[][] matrix = new int[x][y];
+      System.out.println();
+      System.out.println("Please enter the actual values in the matrix: ");
+      for(int i=0; i < x; i++) {
         for(int j=0; j < y; j++) {
           matrix[i][j] = input.nextInt();
         }
       }
 	  
-	  int result[] = findDiagonalOrder(matrix); 
-	  printMatrix(result);
-	}
+      int result[] = findDiagonalOrder(matrix); 
+      printMatrix(result);
+    }
   }
