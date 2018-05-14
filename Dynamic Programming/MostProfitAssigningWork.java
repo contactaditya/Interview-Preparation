e4b095653d74c157aebc8c4ff424e0a89410cfce
Aditya Gupta <contactadityag@gmail.com>
2018-05-13 23:10:43 -0400

    This is a program to find the most profit that we can make given the difficulty and the profit of the job.

--- Dynamic Programming/MostProfitAssigningWork.java
+++ Dynamic Programming/MostProfitAssigningWork.java
@@ -3,29 +3,29 @@ import java.io.*;
 
   public class MostProfitAssigningWork {
 	  
-	public static int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-	  Map<Integer, Integer> map = new HashMap<>();
-	  for (int i = 0; i < profit.length; i++) {
-	    map.put(difficulty[i], Math.max(map.getOrDefault(difficulty[i], 0), profit[i]));
-	  }
-	  int max = Integer.MIN_VALUE;
-	  for (int n : worker) {
-	    max = Math.max(max, n);
-	  }
-	  int[] dp = new int[max + 1];
-	  for (int i = 1; i < dp.length; i++) {
-	    dp[i] = Math.max(dp[i - 1], map.getOrDefault(i, 0));
-	  }
-	  int result = 0;
-	  for (int n : worker) {
-	    result += dp[n];
-	  }
-	  return result;       
-	}	  
+    public static int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+      Map<Integer, Integer> map = new HashMap<>(); 
+      for (int i = 0; i < profit.length; i++) {
+	map.put(difficulty[i], Math.max(map.getOrDefault(difficulty[i], 0), profit[i]));
+      } 
+      int max = Integer.MIN_VALUE;
+      for (int n : worker) {
+	max = Math.max(max, n);
+      }
+      int[] dp = new int[max + 1];
+      for (int i = 1; i < dp.length; i++) {
+	dp[i] = Math.max(dp[i - 1], map.getOrDefault(i, 0));
+      }
+      int result = 0;
+      for (int n : worker) {
+	result += dp[n];
+      } 
+      return result;       
+    }	  
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the difficulty array: ");
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the difficulty array: ");
       int number = input.nextInt();  
       int difficulty[] = new int[number];
 	 
@@ -43,7 +43,7 @@ import java.io.*;
     	profit[i] = input.nextInt();
       }
       
-	  System.out.print("Enter number of elements in the worker array: ");
+      System.out.print("Enter number of elements in the worker array: ");
       int number1 = input.nextInt();  
       int worker[] = new int[number1];
 	 
@@ -53,9 +53,8 @@ import java.io.*;
     	worker[i] = input.nextInt();
       }
       
-	  int mostProfit = maxProfitAssignment(difficulty, profit, worker);   
-	  System.out.println();	
+      int mostProfit = maxProfitAssignment(difficulty, profit, worker);   
+      System.out.println();	
       System.out.print("The most profit that can be made is: " + mostProfit);
-
-	}
+    }
   }
