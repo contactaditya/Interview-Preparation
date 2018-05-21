8ba5c0029f08c1a77f068cae5ff3c26dc7b60fd9
Aditya Gupta <contactadityag@gmail.com>
2018-05-21 00:44:54 -0400

    This is a program to find out how many binary tree that can be made using an array of unique integers.
    We make a binary tree using these integers and each number may be used for any number of times.

--- Arrays/BinaryTreesWithFactors.java
+++ Arrays/BinaryTreesWithFactors.java
@@ -3,9 +3,9 @@ import java.io.*;
 
   public class BinaryTreesWithFactors {
 	  
-	public static int numFactoredBinaryTrees(int[] array) { 
-	  long MOD = (long) Math.pow(10, 9) + 7;
-	  long result = 0L;
+    public static int numFactoredBinaryTrees(int[] array) { 
+      long MOD = (long) Math.pow(10, 9) + 7;
+      long result = 0L;
       Arrays.sort(array);
    
       Map<Integer, Long> dp = new HashMap<Integer, Long>();
@@ -22,21 +22,21 @@ import java.io.*;
     	result = (result + x) % MOD;
       }
       return (int) result;   
-	}	  
+    }	  
 
-	public static void main(String[] args) {    
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) {    
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 						    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
       for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
-	  int numberOfBinaryTree = numFactoredBinaryTrees(array);
-	  System.out.println();
-	  System.out.print("The number of binary trees we can make are: " + numberOfBinaryTree);
-	}
+	array[i] = input.nextInt(); 
+      }
+      int numberOfBinaryTree = numFactoredBinaryTrees(array);
+      System.out.println();
+      System.out.print("The number of binary trees we can make are: " + numberOfBinaryTree);
+    }
   } 
