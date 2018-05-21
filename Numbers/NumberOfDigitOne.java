cf2d98f5a37f42d56b9c577e03cc78e8f732cd02
Aditya Gupta <contactadityag@gmail.com>
2018-05-21 00:04:24 -0400

    This is a program to count the total number of digit 1 appearing in all non-negative integers less than or equal to n.

--- Numbers/NumberOfDigitOne.java
+++ Numbers/NumberOfDigitOne.java
@@ -3,22 +3,22 @@ import java.io.*;
 
   public class NumberOfDigitOne {
 	  
-	public static int countDigitOne(int number) {   
-	  int counter = 0;
-	  for (long i = 1; i <= number; i *= 10) {
-	    long divider = i * 10;
-	    counter += (number/divider) * i + Math.min(Math.max(number % divider - i + 1, 0L), i);
-	  }
-	  return counter;
-	}  
+    public static int countDigitOne(int number) {    
+      int counter = 0;
+      for (long i = 1; i <= number; i *= 10) {
+	long divider = i * 10;
+	counter += (number/divider) * i + Math.min(Math.max(number % divider - i + 1, 0L), i);
+      }
+      return counter;
+    }  
 	
-	public static int countDigitOne1(int number) {   
-   	  if(number <= 0) {
-		return 0;
-	  }
-	  if(number < 10) {
-		return 1;
-	  }
+    public static int countDigitOne1(int number) {   
+      if(number <= 0) {
+	return 0;
+      }
+      if(number < 10) {
+	return 1; 
+      }
       int count = 1;
       while(number/count > 9) {
     	count *= 10;
@@ -30,13 +30,13 @@ import java.io.*;
       }
     } 
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int number = input.nextInt();  
 
       int result = countDigitOne(number);
       System.out.println();	
       System.out.print("The total number of digit 1 appearing in all non-negative integers less than or equal to " + number + " is: " + result);
-	}
+    }
   }
