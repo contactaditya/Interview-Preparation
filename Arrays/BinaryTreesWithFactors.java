f635184aa10923513029df64a090735155b63b3a
contactaditya <contactadityag@gmail.com>
2018-05-20 23:51:45 -0400

    This is a program to find out how many binary tree that can be made using an array of unique integers.
    We make a binary tree using these integers and each number may be used for any number of times.

---
+++ Arrays/BinaryTreesWithFactors.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+  public class BinaryTreesWithFactors {
+	  
+	public static int numFactoredBinaryTrees(int[] array) { 
+	  long MOD = (long) Math.pow(10, 9) + 7;
+	  long result = 0L;
+      Arrays.sort(array);
+   
+      Map<Integer, Long> dp = new HashMap<Integer, Long>();
+      for (int i = 0; i < array.length; i++) {
+        dp.put(array[i], 1L);
+        for (int j = 0; j < i; j++) {
+          if (array[i] % array[j] == 0 && dp.containsKey(array[i] / array[j])) { 
+        	dp.put(array[i], (dp.get(array[i]) + dp.get(array[j]) * dp.get(array[i] / array[j])) % MOD);
+          }
+        }	
+      }
+      long answer = 0;
+      for (long x : dp.values()) {
+    	result = (result + x) % MOD;
+      }
+      return (int) result;   
+	}	  
+
+	public static void main(String[] args) {    
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter number of elements in the array: ");
+	  int numbers = input.nextInt();   
+	  int array[] = new int[numbers];
+						    
+	  System.out.println();
+	  System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+		array[i] = input.nextInt(); 
+	  }
+	  int numberOfBinaryTree = numFactoredBinaryTrees(array);
+	  System.out.println();
+	  System.out.print("The number of binary trees we can make are: " + numberOfBinaryTree);
+	}
+  } 
