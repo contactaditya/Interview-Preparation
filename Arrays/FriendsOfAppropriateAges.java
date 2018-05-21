0a7434b47b3dd9600eef00c7132cc4755ecbd3b5
Aditya Gupta <contactadityag@gmail.com>
2018-05-21 00:15:32 -0400

    This is a program to find how many total friend requests that have been made.
    Person A will NOT friend request person B (B != A) if any of the following conditions are true:
    
    age[B] <= 0.5 * age[A] + 7
    age[B] > age[A]
    age[B] > 100 && age[A] < 100
    
    Also, people will not friend request themselves.

--- Arrays/FriendsOfAppropriateAges.java
+++ Arrays/FriendsOfAppropriateAges.java
@@ -3,48 +3,48 @@ import java.io.*;
 
   public class FriendsOfAppropriateAges {
 	  
-	public static int numFriendRequests(int[] ages) {
-	  int totalFriendRequests = 0;	
-	  int[] count = new int[121];
-	  for(int x : ages) {
-		count[x]++;
-	  }
-	  for (int ageA = 0; ageA <= 120; ageA++) {
-	    int countA = count[ageA];
-	    for (int ageB = 0; ageB <= 120; ageB++) {
+    public static int numFriendRequests(int[] ages) {
+      int totalFriendRequests = 0;	
+      int[] count = new int[121];
+      for(int x : ages) {
+	count[x]++;
+      }
+      for (int ageA = 0; ageA <= 120; ageA++) {
+        int countA = count[ageA];
+	for (int ageB = 0; ageB <= 120; ageB++) {
           int countB = count[ageB];
           if (ageA * 0.5 + 7 >= ageB) {
-        	continue;
+            continue;
           }
           if (ageA < ageB) {
-        	continue;
+            continue;
           }
           if (ageA < 100 && ageB > 100) {
-        	continue;
+            continue;
           }
           totalFriendRequests += countA * countB;
           if (ageA == ageB) {
-        	totalFriendRequests -= countA;
+            totalFriendRequests -= countA;
           }
         }
-	  }
+      }
 		
-	  return totalFriendRequests;      
-	}	  
+      return totalFriendRequests;      
+    }	  
 
-	public static void main(String[] args) { 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of people: ");
-	  int numbers = input.nextInt();   
-	  int ages[] = new int[numbers];
+    public static void main(String[] args) { 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of people: ");
+      int numbers = input.nextInt();   
+      int ages[] = new int[numbers];
 						    
-	  System.out.println();
-	  System.out.print("Please enter the actual age of the people: ");
+      System.out.println();
+      System.out.print("Please enter the actual age of the people: ");
       for (int i = 0; i < numbers; i++) {
-		ages[i] = input.nextInt(); 
-	  }
-	  int totalFriendRequests = numFriendRequests(ages);
-	  System.out.println();
-	  System.out.print("The total number of friend requests that have been made are: " + totalFriendRequests);
-	}
+	ages[i] = input.nextInt(); 
+      }
+      int totalFriendRequests = numFriendRequests(ages);
+      System.out.println();
+      System.out.print("The total number of friend requests that have been made are: " + totalFriendRequests);
+    }
   }
