2d5b41a2c2eb56c88f2fd763745b5cd3e06622fb
Aditya Gupta <contactadityag@gmail.com>
2018-05-21 00:06:37 -0400

    This is a program to find out how many bulbs are on after n rounds.

--- Numbers/BulbSwitcher.java
+++ Numbers/BulbSwitcher.java
@@ -3,17 +3,17 @@ import java.io.*;
 
   public class BulbSwitcher {
 	  
-	public static int bulbSwitch(int numberOfBulbs) {
-	  return (int) Math.sqrt(numberOfBulbs);   
+    public static int bulbSwitch(int numberOfBulbs) {
+      return (int) Math.sqrt(numberOfBulbs);   
     }	  
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number of bulbs: ");
       int numberOfBulbs = input.nextInt();  
 		    
       int result = bulbSwitch(numberOfBulbs);
       System.out.println();
       System.out.println("The number of bulbs that are on after " + numberOfBulbs + " rounds are: " + result); 
-	}
+    }
   }
