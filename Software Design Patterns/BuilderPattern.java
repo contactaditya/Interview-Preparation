d8ce7a35041d2dd9c686e015e587ad53ed4888f6
Aditya Gupta <contactadityag@gmail.com>
2018-05-21 01:12:58 -0400

    This is the program which is an example of a builder pattern implementation.

--- Software Design Patterns/BuilderPattern.java
+++ Software Design Patterns/BuilderPattern.java
@@ -4,62 +4,62 @@ import java.io.*;
  public class BuilderPattern {
 	 
    static class Car {
-      private int wheels;
-      private String color;
+     private int wheels;
+     private String color;
 
-      public Car() {
-      }
+     public Car() {
+     }
 
-      public String getColor() {
-	 return color;
-      }
+     public String getColor() {
+       return color;
+     }
 
-      public void setColor(final String color) {
-	 this.color = color;
-      }
+     public void setColor(final String color) {
+       this.color = color;
+     }
 
-      public int getWheels() {
-	 return wheels;
-      }
+     public int getWheels() {
+       return wheels;
+     }
 
-      public void setWheels(final int wheels) {
-	 this.wheels = wheels;
-      }
+     public void setWheels(final int wheels) {
+       this.wheels = wheels;
+     }
 
-      @Override
-      public String toString() {
-	 return "Car [wheels = " + wheels + ", color = " + color + "]";
-      }
+     @Override
+     public String toString() {
+       return "Car [wheels = " + wheels + ", color = " + color + "]";
+     }
    }
    
    static class CarBuilder {
-       private Car car;
+     private Car car;
 
-       public CarBuilder() {
-	  car = new Car();
-       }
+     public CarBuilder() {
+       car = new Car();
+     }
 
-       public Car build() {
-	  return car;
-       }
+     public Car build() {
+       return car;
+     }
 
-       public CarBuilder setColor(final String color) {
-	   car.setColor(color);
-	   return this;
-       }
+     public CarBuilder setColor(final String color) {
+       car.setColor(color);
+       return this;
+     }
 
-       public CarBuilder setWheels(final int wheels) {
-	   car.setWheels(wheels);
-	   return this;
-       }
+     public CarBuilder setWheels(final int wheels) {
+       car.setWheels(wheels);
+       return this;
+     }
    }
    
    public static void main(String[] args) {
-      Car builder = new CarBuilder()
+     Car builder = new CarBuilder()
 	  .setWheels(4)
           .setColor("Red")
           .build();
 		
-      System.out.println(builder);
+     System.out.println(builder);
    }
  } 
