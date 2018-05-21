3e01686692153f5e60402b7bd194f548a3b0d7db
Aditya Gupta <contactadityag@gmail.com>
2018-05-21 01:05:39 -0400

    This is the program which is an example of a Decorator Pattern.

--- Software Design Patterns/DecoratorPattern.java
+++ Software Design Patterns/DecoratorPattern.java
@@ -1,86 +1,85 @@
 import java.io.*;
 import java.util.*;
 
-public class DecoratorPattern {
+ public class DecoratorPattern {
 	
    public interface Coffee {
-      public double getCost(); 
-      public String getIngredients(); 
+     public double getCost(); 
+     public String getIngredients(); 
    }
 	
    public static class SimpleCoffee implements Coffee {
-       @Override
-       public double getCost() {
-	  return 1;
-       }
+     @Override
+     public double getCost() {
+       return 1;
+     }
 
-       @Override
-       public String getIngredients() {
-	  return "Coffee";
-       }
+     @Override
+     public String getIngredients() {
+       return "Coffee";
+     }
    }
 	
    public static abstract class CoffeeDecorator implements Coffee {
-       protected final Coffee decoratedCoffee;
+     protected final Coffee decoratedCoffee;
 
-       public CoffeeDecorator(Coffee c) {
-	  this.decoratedCoffee = c;
-       }
+     public CoffeeDecorator(Coffee c) {
+       this.decoratedCoffee = c;
+     }
 
-       public double getCost() { // Implementing methods of the interface
-	  return decoratedCoffee.getCost();
-       }
+     public double getCost() { // Implementing methods of the interface
+       return decoratedCoffee.getCost();
+     }
 
-       public String getIngredients() {
-	  return decoratedCoffee.getIngredients();
-       }
+     public String getIngredients() {
+       return decoratedCoffee.getIngredients();
+     }
    }
 	
    // Decorator WithMilk mixes milk into coffee.
 
    static class WithMilk extends CoffeeDecorator {
-       public WithMilk(Coffee c) {
-	  super(c);
-       }
+     public WithMilk(Coffee c) {
+       super(c);
+     }
 
-       public double getCost() { // Overriding methods defined in the abstract superclass
-	  return super.getCost() + 0.5;
-       }
+     public double getCost() { // Overriding methods defined in the abstract superclass
+       return super.getCost() + 0.5;
+     }
 
-       public String getIngredients() {
-	  return super.getIngredients() + ", Milk";
-       }
-    }
+     public String getIngredients() {
+       return super.getIngredients() + ", Milk";
+     }
+   }
 	 
    // Decorator WithSprinkles mixes sprinkles onto coffee.
 
    static class WithSprinkles extends CoffeeDecorator {
-       public WithSprinkles(Coffee c) {
-	  super(c);
-       }
+     public WithSprinkles(Coffee c) {
+       super(c);
+     }
 
-       public double getCost() {
-	  return super.getCost() + 0.2;
-       }
+     public double getCost() {
+       return super.getCost() + 0.2;
+     }
 
-       public String getIngredients() {
-	  return super.getIngredients() + ", Sprinkles";
-       } 
+     public String getIngredients() {
+       return super.getIngredients() + ", Sprinkles";
+     } 
    }
 	
    public static void printInfo(Coffee c) {
-      System.out.println("Cost: " + c.getCost() + "; Ingredients: " + c.getIngredients());
+     System.out.println("Cost: " + c.getCost() + "; Ingredients: " + c.getIngredients());
    }
 
    public static void main(String[] args) {  
-	Coffee c = new SimpleCoffee();
-        printInfo(c);
+     Coffee c = new SimpleCoffee();
+     printInfo(c);
 
-        c = new WithMilk(c);
-        printInfo(c);
+     c = new WithMilk(c);
+     printInfo(c);
 
-      	c = new WithSprinkles(c);
-      	printInfo(c);
-	  
+     c = new WithSprinkles(c);
+     printInfo(c);  
    }
-}
+ }
