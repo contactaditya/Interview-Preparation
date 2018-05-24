bc0e7ece889384d9e49d3d079463f77e95e4f8b8
Aditya Gupta <contactadityag@gmail.com>
2018-05-24 01:07:51 -0400

    This is a program to return the base 7 string representation of the given integer.

--- Numbers/Base7.java
+++ Numbers/Base7.java
@@ -3,8 +3,8 @@ import java.io.*;
 
   public class Base7 {
 	
-	public static String convertToBase7(int number) {
-	  String i = "";
+    public static String convertToBase7(int number) {
+      String i = "";
       if (number < 0) {
         i = "-";
         number = Math.abs(number);
@@ -17,20 +17,20 @@ import java.io.*;
         number /= 7;
       }
 	  
-	  return i + Integer.toString(result);  
+      return i + Integer.toString(result);  
     }
 	
-	public static String convertToBase7One(int number) {
- 	  return Integer.toString(number, 7);   
-	}
+    public static String convertToBase7One(int number) {
+      return Integer.toString(number, 7);   
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int number = input.nextInt();  
 
       String result = convertToBase7(number);
       System.out.println();	
       System.out.print("The base 7 string representation of the integer " + number + " is: " + result);
-	}
+    }
   }
