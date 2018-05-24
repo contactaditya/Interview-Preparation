0deb7472c8e87ab0c0e334c6bc38ec42452165d3
contactaditya <contactadityag@gmail.com>
2018-05-24 01:10:39 -0400

    This is a program to check if the number of global inversions is equal to the number of local inversions.
    The number of (global) inversions is the number of i < j with 0 <= i < j < N and A[i] > A[j].
    
    The number of local inversions is the number of i with 0 <= i < N and A[i] > A[i+1].

---
+++ Arrays/GlobalAndLocalInversions.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+  public class GlobalAndLocalInversions {
+	  
+	public static boolean isIdealPermutation(int[] array) {
+	  for (int i = 0; i < array.length; i++) {
+		if (array[i] < i - 1 || array[i] > i + 1) {  
+		  return false;
+		}
+	  }
+		
+		
+	  return true;      
+	}	  
+
+	public static void main(String[] args) {  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter number of elements in the array: ");
+	  int numbers = input.nextInt();   
+	  int array[] = new int[numbers];
+					    
+	  System.out.println();
+	  System.out.print("Please enter the actual values in the array: ");
+	  for (int i = 0; i < numbers; i++) {
+		array[i] = input.nextInt(); 
+	  }
+
+	  boolean result = isIdealPermutation(array);
+
+	  System.out.println();	
+	  if(result) {
+	    System.out.print("The number of global inversions is equal to the number of local inversions.");
+	  }
+	  else {
+		System.out.print("The number of global inversions is not equal to the number of local inversions.");  
+	  }
+	}
+  }
