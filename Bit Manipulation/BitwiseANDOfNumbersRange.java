9de00232334d6a3f5c8de523e00bcda1f5bc9506
Aditya Gupta <contactadityag@gmail.com>
2018-05-27 23:57:20 -0400

    This is a program to return the bitwise AND of all numbers in this range, inclusive.

--- Bit Manipulation/BitwiseANDOfNumbersRange.java
+++ Bit Manipulation/BitwiseANDOfNumbersRange.java
@@ -3,27 +3,27 @@ import java.io.*;
 
   public class BitwiseANDOfNumbersRange {
 	  
-	public static int rangeBitwiseAnd(int m, int n) {  
-	  int offset = 0;	
+    public static int rangeBitwiseAnd(int m, int n) {  
+      int offset = 0;	
       while(m != n) {
     	m >>= 1;
         n >>= 1;
         offset++;
       }
 			        
-	  return m <<= offset;     
+      return m <<= offset;     
     }	  
 	  
-	public static int rangeBitwiseAnd1(int m, int n) {  
-	  while(m < n) {
-		n = n & (n-1);
-	  }
+    public static int rangeBitwiseAnd1(int m, int n) {  
+      while(m < n) {
+	n = n & (n-1);
+      }
 	        
-	  return n;     
+      return n;     
     }	  
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int m = input.nextInt();  
       
@@ -34,5 +34,5 @@ import java.io.*;
       int result = rangeBitwiseAnd(m, n);
       System.out.println();	
       System.out.print("The bitwise AND of all numbers in this range is: " + result);
-	}
+    }
   }
