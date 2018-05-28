21315bc7bdeac24895cd1d65ca7fbae4a34f3d72
Aditya Gupta <contactadityag@gmail.com>
2018-05-28 00:04:45 -0400

    This is a program to calculate the starting gas station's index if you can travel around the circuit once in the clockwise direction.

--- Arrays/GasStation.java
+++ Arrays/GasStation.java
@@ -3,9 +3,9 @@ import java.io.*;
 
   public class GasStation {
 	  
-	public static int canCompleteCircuit(int[] gas, int[] cost) {  
-	  int gasSum = 0;
-	  int costSum = 0;
+    public static int canCompleteCircuit(int[] gas, int[] cost) {  
+      int gasSum = 0;
+      int costSum = 0;
       int start = 0;
       int fuel = 0;
       for(int i = 0; i < gas.length; i++) {
@@ -21,35 +21,35 @@ import java.io.*;
         return -1;
       }
 
-	  return start;      
-	}  
+      return start;      
+    }  
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number of gas stations: ");
-	  int numbers = input.nextInt();   
-	  int gas[] = new int[numbers];
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number of gas stations: ");
+      int numbers = input.nextInt();   
+      int gas[] = new int[numbers];
 	    
-	  System.out.println();
-	  System.out.print("Please enter the amount of gas at respective stations: ");
-	  for (int i = 0; i < numbers; i++) {
-		gas[i] = input.nextInt(); 
-	  }
+      System.out.println();
+      System.out.print("Please enter the amount of gas at respective stations: ");
+      for (int i = 0; i < numbers; i++) {
+	gas[i] = input.nextInt(); 
+      }
 
-	  int cost[] = new int[numbers];
+      int cost[] = new int[numbers];
 	    
-	  System.out.println();
-	  System.out.print("Please enter the cost of gas to travel from station i to its next station (i+1): ");
-	  for (int i = 0; i < numbers; i++) {
-		cost[i] = input.nextInt(); 
-	  }
+      System.out.println();
+      System.out.print("Please enter the cost of gas to travel from station i to its next station (i+1): ");
+      for (int i = 0; i < numbers; i++) {
+	cost[i] = input.nextInt(); 
+      }
 	   
-	  int result = canCompleteCircuit(gas, cost);
-	  System.out.println();
-	  if(result != -1) {
-	    System.out.print("The starting gas station's index if you travel around the circuit once is: " + result);
-	  } else {
-		System.out.print("You cannot the travel around the circuit even once in the clockwise direction with the given constraints.");  
-	  }
-	}
+      int result = canCompleteCircuit(gas, cost);
+      System.out.println();
+      if(result != -1) {
+	System.out.print("The starting gas station's index if you travel around the circuit once is: " + result);
+      } else {
+	System.out.print("You cannot the travel around the circuit even once in the clockwise direction with the given constraints.");  
+      }
+    }
   }
