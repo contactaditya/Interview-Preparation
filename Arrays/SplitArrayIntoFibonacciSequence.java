578606e913fb06e837920101db8ba6e66ee0cbe9
Aditya Gupta <contactadityag@gmail.com>
2018-06-04 19:07:09 -0400

    This is a program to return any Fibonacci-like sequence split from a string of digits, or return [] if it cannot be done.
    Formally, a Fibonacci-like sequence is a list F of non-negative integers such that:
    
    0 <= F[i] <= 2^31 - 1, (that is, each integer fits a 32-bit signed integer type);
    
    F.length >= 3;
    
    and F[i] + F[i+1] = F[i+2] for all 0 <= i < F.length - 2.

--- Arrays/SplitArrayIntoFibonacciSequence.java
+++ Arrays/SplitArrayIntoFibonacciSequence.java
@@ -3,47 +3,47 @@ import java.io.*;
 
   public class SplitArrayIntoFibonacciSequence {
 	  
-	public static List<Integer> splitIntoFibonacci(String string) {  
-	  if(string == null || string.length() < 3) {
-		return null;
-	  }
+    public static List<Integer> splitIntoFibonacci(String string) {  
+      if(string == null || string.length() < 3) {
+	return null;
+      }
       int length = string.length();
       for (int i = 0; i < length/2; i++) {
         for (int j = i+1; j < length-i-1; j++) {  
           List<Integer> result = new ArrayList<Integer>();	
           try {
-        	int f0 = Integer.parseInt(string.substring(0, i+1)), f1 = Integer.parseInt(string.substring(i+1,j+1));	
-        	int k = j; 
-        	result.add(f0);
-        	while (k < length-i-1 && string.substring(k+1).startsWith(String.valueOf(f0+f1))) {
+            int f0 = Integer.parseInt(string.substring(0, i+1)), f1 = Integer.parseInt(string.substring(i+1,j+1));	
+            int k = j; 
+            result.add(f0);
+            while (k < length-i-1 && string.substring(k+1).startsWith(String.valueOf(f0+f1))) {
               result.add(f1);
               int f2 = f0 + f1;
               f0 = f1; 
               f1 = f2; 
               k += String.valueOf(f2).length();
             }
-        	if (k >= length-i-1) {
+            if (k >= length-i-1) {
               result.add(f1);
               return result;
-        	}  
+            }  
           } catch(Exception e) {
-        	j = length; 
+            j = length; 
           }
         }
       }
 	  
-	  return new ArrayList<Integer>();
-	}	  
+      return new ArrayList<Integer>();
+    }	  
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
       String string = new String();	  
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine();	
+      System.out.print("Enter the string: ");
+      string = input.nextLine();	
 		  
-	  List<Integer> result = new ArrayList<Integer>();
+      List<Integer> result = new ArrayList<Integer>();
       result = splitIntoFibonacci(string); 
-	  System.out.println();
-	  System.out.println("The Fibonacci-like sequence split from " + string  + " is: " + result);
-	}
+      System.out.println();
+      System.out.println("The Fibonacci-like sequence split from " + string  + " is: " + result);
+    }
   }
