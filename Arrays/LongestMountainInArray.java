5abfac14ebd02001e2df5c95b3bb71a8ba5b7314
Aditya Gupta <contactadityag@gmail.com>
2018-06-04 18:37:42 -0400

    This is a program to calculate the length of the longest mountain in the array.
    Let's call any (contiguous) subarray B (of A) a mountain if the following properties hold:
    
    1) B.length >= 3
    
    2) There exists some 0 < i < B.length - 1 such that B[0] < B[1] < ... B[i-1] < B[i] > B[i+1] > ... > B[B.length - 1]

--- Arrays/LongestMountainInArray.java
+++ Arrays/LongestMountainInArray.java
@@ -1,44 +1,44 @@
 import java.util.*;
 import java.io.*;
 
-  public class LongestMountainInArray {
+   public class LongestMountainInArray {
 	
-	public static int longestMountain(int[] array) {
-	  int lengthOfLongestMountain = 0;
-	  int up = 0;
-	  int down = 0;
-	  for (int i = 1; i < array.length; i++) {
-	    if (down > 0 && array[i - 1] < array[i] || array[i - 1] == array[i]) {
-	      up = down = 0;
-	    }
-	    if (array[i - 1] < array[i]) {
-		  up++;
-	    }
-	    if (array[i - 1] > array[i]) {
-		  down++;
-	    }
-	    if (up > 0 && down > 0 && up + down + 1 > lengthOfLongestMountain) {
-	      lengthOfLongestMountain = up + down + 1;
-	    }
-	  }
+     public static int longestMountain(int[] array) {
+       int lengthOfLongestMountain = 0;
+       int up = 0;
+       int down = 0;
+       for (int i = 1; i < array.length; i++) {
+         if (down > 0 && array[i - 1] < array[i] || array[i - 1] == array[i]) {
+	   up = down = 0;
+	 }
+	 if (array[i - 1] < array[i]) {
+	   up++;
+	 }
+	 if (array[i - 1] > array[i]) {
+	   down++;
+	 }
+	 if (up > 0 && down > 0 && up + down + 1 > lengthOfLongestMountain) {
+	   lengthOfLongestMountain = up + down + 1;
+	 }
+       }
 		
-	  return lengthOfLongestMountain;      
-	}
+       return lengthOfLongestMountain;      
+     }
 
-	public static void main(String[] args) { 	
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+     public static void main(String[] args) { 	
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter number of elements in the array: ");
+       int numbers = input.nextInt();   
+       int array[] = new int[numbers];
 					    
-	  System.out.print("\n");
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+       System.out.println();
+       System.out.print("Please enter the actual values in the array: ");
+       for (int i = 0; i < numbers; i++) {
+	 array[i] = input.nextInt(); 
+       }
 
-	  int result = longestMountain(array);
-	  System.out.println();
-	  System.out.print("The length of the longest mountain in the array is: " + result);
-	}
-  }
+       int result = longestMountain(array);
+       System.out.println();
+       System.out.print("The length of the longest mountain in the array is: " + result);
+     }
+   }
