ef725f6af4ba23dd2a05030996eb99d4324d80c0
Aditya Gupta <contactadityag@gmail.com>
2018-06-04 18:41:14 -0400

    This is a program to find out if two strings String 1 and String 2 are equal when both are typed into empty text editors.  # means a backspace character.

--- Strings/BackspaceStringCompare.java
+++ Strings/BackspaceStringCompare.java
@@ -3,17 +3,17 @@ import java.io.*;
 
   public class BackspaceStringCompare {
 	  
-	public static boolean backspaceCompare(String string1, String string2) {
-	  int i = string1.length() - 1, j = string2.length() - 1;
-	  int skipString1 = 0, skipString2 = 0;
+    public static boolean backspaceCompare(String string1, String string2) {
+      int i = string1.length() - 1, j = string2.length() - 1;
+      int skipString1 = 0, skipString2 = 0;
 	  
-	  // While there may be chars in build(string1) or build (string2)
-	  while (i >= 0 || j >= 0) { 
-	    // Find position of next possible char in build(string1)
+      // While there may be chars in build(string1) or build (string2)
+      while (i >= 0 || j >= 0) { 
+	// Find position of next possible char in build(string1)
         while (i >= 0) { 
           if (string1.charAt(i) == '#') {
-        	skipString1++;
-        	i--;
+            skipString1++;
+            i--;
           }
           else if (skipString1 > 0) {
             skipString1--; 
@@ -26,8 +26,8 @@ import java.io.*;
         // Find position of next possible char in build(string2)
         while (j >= 0) { 
           if (string2.charAt(j) == '#') {
-        	skipString2++;
-        	j--;
+            skipString2++;
+            j--;
           }
           else if (skipString2 > 0) {
             skipString2--; 
@@ -47,13 +47,13 @@ import java.io.*;
         }
         i--;
         j--;
-	  }
-	  
+      }
+		
       return true;     
-	}  
+    }  
 
-	public static void main(String[] args) {   
-	  String string1 = new String();	  
+    public static void main(String[] args) {   
+      String string1 = new String();	  
       Scanner input = new Scanner(System.in);
       System.out.print("Enter the first string: ");
       string1 = input.nextLine();  
@@ -70,5 +70,5 @@ import java.io.*;
       else {
         System.out.println("The string " + string1 + " and the string " + string2 + " are not equal when both are typed into empty text editors.");  
       }
-	}
+    }
   }
