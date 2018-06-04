ca965333618eea6dea8090718dec8828aca740ca
Aditya Gupta <contactadityag@gmail.com>
2018-06-04 18:49:39 -0400

    This is a program to simply an absolute path for a file (Unix-style).

--- Stacks/SimplifyPath.java
+++ Stacks/SimplifyPath.java
@@ -3,8 +3,8 @@ import java.io.*;
 
   public class SimplifyPath {
 	  
-	public static String simplifyPath(String path) {  
-	  Deque<String> stack = new LinkedList<String>();
+    public static String simplifyPath(String path) {  
+      Deque<String> stack = new LinkedList<String>();
       Set<String> skip = new HashSet<String>(Arrays.asList("..",".",""));
       for (String token : path.split("/")) {
         if (token.equals("..") && !stack.isEmpty()) {
@@ -17,17 +17,17 @@ import java.io.*;
       for (String dir : stack) {
     	result = "/" + dir + result;	
       }
-	  return result.isEmpty() ? "/" : result;      
-	}	  
+      return result.isEmpty() ? "/" : result;      
+    }	  
 
-	public static void main(String[] args) {    
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {    
+      Scanner input = new Scanner(System.in);
       String path = new String();	  
-	  System.out.print("Enter the string: ");
-	  path = input.nextLine();	
+      System.out.print("Enter the string: ");
+      path = input.nextLine();	
 		  
-	  String result = simplifyPath(path);
-	  System.out.println();
-	  System.out.println("The simplified path for the file (Unix-style) is: " + result);
-	}
+      String result = simplifyPath(path);
+      System.out.println();
+      System.out.println("The simplified path for the file (Unix-style) is: " + result);
+    }
   }
