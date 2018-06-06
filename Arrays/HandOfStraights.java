702dc63fdedc49199eba043d07818b6fe21b0a4e
Aditya Gupta <contactadityag@gmail.com>
2018-06-06 19:30:45 -0400

    This is a program to find out if it is possible to rearrange the cards into groups so that each group is size W, and consists of W consecutive cards.

--- Arrays/HandOfStraights.java
+++ Arrays/HandOfStraights.java
@@ -3,8 +3,8 @@ import java.io.*;
 
   public class HandOfStraights {
 	  
-	public static boolean isNStraightHand(int[] hand, int W) {
-	  Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
+    public static boolean isNStraightHand(int[] hand, int W) {
+      Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
       for(int i : hand) {
     	map.put(i, map.getOrDefault(i, 0) + 1);
       }
@@ -16,38 +16,38 @@ import java.io.*;
         for(int j=i; j<i+W; j++) {
           Integer c = map.get(j);
           if(c == null || c < count) {
-        	return false;
+            return false;
           }
           map.put(j, c-count);
         }
       }
-	  return true;      
-	}	  
+      return true;      
+    }	  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int hand[] = new int[number];
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();  
+      int hand[] = new int[number];
 	
-	  System.out.println();	
-	  System.out.print("Please enter the elements in the array: ");
-	  for (int i = 0; i < hand.length; i++) {
-		hand[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the elements in the array: ");
+      for (int i = 0; i < hand.length; i++) {
+	hand[i] = input.nextInt();
+      }
 	  
-	  System.out.println();	
-	  System.out.print("Enter the number: ");
-	  int W = input.nextInt();  
+      System.out.println();	
+      System.out.print("Enter the number: ");
+      int W = input.nextInt();  
 	 
 	
-	  boolean result = isNStraightHand(hand, W);
-	  System.out.println();	
-	  if(result) {
-	    System.out.print("Yes it is possible to rearrange the cards into groups so that each group is size " + W + ", and consists of " + W + " consecutive cards.");
-	  }
-	  else {
-		System.out.print("No it is not possible to rearrange the cards into groups so that each group is size " + W + ", and consists of " + W + " consecutive cards.");  
-	  }
-	}
+      boolean result = isNStraightHand(hand, W);
+      System.out.println();	
+      if(result) {
+	System.out.print("Yes it is possible to rearrange the cards into groups so that each group is size " + W + ", and consists of " + W + " consecutive cards.");
+      }
+      else {
+	System.out.print("No it is not possible to rearrange the cards into groups so that each group is size " + W + ", and consists of " + W + " consecutive cards.");  
+      }
+    }
   }
