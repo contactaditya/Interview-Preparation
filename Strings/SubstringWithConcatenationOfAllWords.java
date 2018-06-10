4416f2245d5da1f2a35b3f82c6942192857f7a86
Aditya Gupta <contactadityag@gmail.com>
2018-06-10 16:12:13 -0400

    This is a program to find all starting indices of substring(s) in string such that is a concatenation of each word in words exactly once and without any intervening characters.

--- Strings/SubstringWithConcatenationOfAllWords.java
+++ Strings/SubstringWithConcatenationOfAllWords.java
@@ -3,8 +3,8 @@ import java.io.*;
 
   public class SubstringWithConcatenationOfAllWords {
 	  
-	public static List<Integer> findSubstring(String string, String[] words) {
-	  List<Integer> result = new ArrayList<Integer>();   
+    public static List<Integer> findSubstring(String string, String[] words) {
+      List<Integer> result = new ArrayList<Integer>();   
       if(words == null || words.length == 0 || string.length() == 0) {
     	return result;
       }
@@ -22,7 +22,7 @@ import java.io.*;
           // count: number of exceeded occurences in current window
           // start: start index of current window of size windowLength  
           if(start + windowLength > stringLength) {
-        	break;
+            break;
           }
           String word = string.substring(j, j + wordLength);
           if(!map.containsKey(word)) {
@@ -58,29 +58,29 @@ import java.io.*;
         }
       }
     
-	  return result;
-	}	  
+      return result;
+    }	  
 
-	public static void main(String[] args) {   
+    public static void main(String[] args) {   
       String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine();  
-	  System.out.println();	
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the string: ");
+      string = input.nextLine();  
+      System.out.println();	
 		  
       System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  String words[] = new String[numbers];
+      int numbers = input.nextInt();   
+      String words[] = new String[numbers];
 		    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-	    words[i] = input.next(); 
-	  }
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	words[i] = input.next(); 
+      }
 	      
-	  List<Integer> result = new ArrayList<Integer>();
-	  result = findSubstring(string, words); 
-	  System.out.println();
-	  System.out.println("The starting indices of substring(s) in " + string + " that is a concatenation of each word in words exactly once and without any intervening characters is: " + result);		   
-	}
+      List<Integer> result = new ArrayList<Integer>();
+      result = findSubstring(string, words); 
+      System.out.println();
+      System.out.println("The starting indices of substring(s) in " + string + " that is a concatenation of each word in words exactly once and without any intervening characters is: " + result);		   
+    }
   }
