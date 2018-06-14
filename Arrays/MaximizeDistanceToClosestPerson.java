e2cad45793ac40358e6da39029e7080dce7413f2
Aditya Gupta <contactadityag@gmail.com>
2018-06-13 22:29:36 -0400

    This is a program to find the maximum distance to the closest person in a row of seats. 1 represents a person sitting in that seat, and 0 represents that the seat is empty.

--- Arrays/MaximizeDistanceToClosestPerson.java
+++ Arrays/MaximizeDistanceToClosestPerson.java
@@ -3,16 +3,16 @@ import java.io.*;
 
   public class MaximizeDistanceToClosestPerson {
 	  
-	public static int maxDistanceToClosest(int[] seats) {
-	  int maxDistanceToClosest = 0;	
-	  int N = seats.length;
+     public static int maxDistanceToClosest(int[] seats) {
+      int maxDistanceToClosest = 0;	
+      int N = seats.length;
       int previous = -1, future = 0;
 
-	  for (int i = 0; i < seats.length; i++) {   
-	    if (seats[i] == 1) {
+      for (int i = 0; i < seats.length; i++) {   
+	if (seats[i] == 1) {
           previous = i;
         }
-	    else {
+	else {
           while (future < N && seats[future] == 0 || future < i) {
             future++;
           }
@@ -20,25 +20,25 @@ import java.io.*;
           int right = future == N ? N : future - i;
           maxDistanceToClosest = Math.max(maxDistanceToClosest, Math.min(left, right));
         }
-	  }
+      }
 		
-	  return maxDistanceToClosest;      
+      return maxDistanceToClosest;      
     }	  
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of seats in the row: ");
-	  int numbers = input.nextInt();    
-	  int seats[] = new int[numbers];
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of seats in the row: ");
+      int numbers = input.nextInt();    
+      int seats[] = new int[numbers];
 	    
-	  System.out.println();
-	  System.out.print("Please enter whether a person is sitting in that seat or if the seat is empty using 1 and 0: ");
-	  for (int i = 0; i < numbers; i++) {
-		seats[i] = input.nextInt();   
-	  }
+      System.out.println();
+      System.out.print("Please enter whether a person is sitting in that seat or if the seat is empty using 1 and 0: ");
+      for (int i = 0; i < numbers; i++) {
+	seats[i] = input.nextInt();   
+      }
 	  
-	  int maximumDistanceToTheClosestPerson = maxDistanceToClosest(seats);
-	  System.out.println();
-	  System.out.print("The maximum distance to closest person is: " + maximumDistanceToTheClosestPerson);
-	}
+      int maximumDistanceToTheClosestPerson = maxDistanceToClosest(seats);
+      System.out.println();
+      System.out.print("The maximum distance to closest person is: " + maximumDistanceToTheClosestPerson);
+    }
   }
