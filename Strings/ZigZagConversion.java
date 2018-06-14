c8ca18da94737cd9a88eb9a6cb240a5aea011eb6
Aditya Gupta <contactadityag@gmail.com>
2018-06-13 22:21:08 -0400

    This is a program to take a string and make the zig zag conversion given the number of rows.

--- Strings/ZigZagConversion.java
+++ Strings/ZigZagConversion.java
@@ -3,16 +3,16 @@ import java.io.*;
 
   public class ZigZagConversion {
 	  
-	public static String convert(String string, int numberOfRows) {
-	  if (numberOfRows == 1) {
-		return string;
-	  }
-	  List<StringBuilder> rows = new ArrayList<StringBuilder>();
-	  // First Row
-	  for (int i = 0; i < Math.min(numberOfRows, string.length()); i++) {
+    public static String convert(String string, int numberOfRows) {
+      if (numberOfRows == 1) {
+	return string;
+      }
+      List<StringBuilder> rows = new ArrayList<StringBuilder>();
+      // First Row
+      for (int i = 0; i < Math.min(numberOfRows, string.length()); i++) {
         rows.add(new StringBuilder());
-	  }
-	  int currentRow = 0;
+      }
+      int currentRow = 0;
       boolean goingDown = false;
       // Rows between
       for (char c : string.toCharArray()) {
@@ -29,18 +29,18 @@ import java.io.*;
     	result.append(row);
       }
       return result.toString();
-	}	  
+    }	  
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  String string = new String();	
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine();  
-	  System.out.println();
-	  System.out.print("Enter the number of rows: ");
-	  int numberOfRows = input.nextInt();  
-	  String result = convert(string, numberOfRows);
-	  System.out.println();
-	  System.out.print("The string after Zig Zag Conversion is: " + result);
-	}
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      String string = new String();	
+      System.out.print("Enter the string: ");
+      string = input.nextLine();  
+      System.out.println();
+      System.out.print("Enter the number of rows: ");
+      int numberOfRows = input.nextInt();  
+      String result = convert(string, numberOfRows);
+      System.out.println();
+      System.out.print("The string after Zig Zag Conversion is: " + result);
+    }
   } 
