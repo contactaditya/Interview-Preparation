1f4ef84046a4b7c0064eee60a1db4062f6978cf3
Aditya Gupta <contactadityag@gmail.com>
2018-06-15 00:39:38 -0400

    This is a program to find the final string after all shifts are applied to the string.

--- Strings/ShiftingLetters.java
+++ Strings/ShiftingLetters.java
@@ -3,24 +3,24 @@ import java.io.*;
 
   public class ShiftingLetters {
 	  
-	public static String shiftingLetters(String string, int[] shifts) {
-	  StringBuilder result = new StringBuilder();
-	  int X = 0;
-	  // Let X be the number of times the current ith character is shifted
-	  for (int shift : shifts) {
+    public static String shiftingLetters(String string, int[] shifts) {
+      StringBuilder result = new StringBuilder();
+      int X = 0;
+      // Let X be the number of times the current ith character is shifted
+      for (int shift : shifts) {
         X = (X + shift) % 26;
-	  }
-	  for (int i = 0; i < string.length(); i++) {
+      }
+      for (int i = 0; i < string.length(); i++) {
         int index = string.charAt(i) - 'a';
         result.append((char) ((index + X) % 26 + 97));
         X = Math.floorMod(X - shifts[i], 26);
       }
 	  
-	  return result.toString();
-	} 
+      return result.toString();
+    } 
 
-	public static void main(String[] args) {    
-	  String string = new String();	  
+    public static void main(String[] args) {    
+      String string = new String();	  
       Scanner input = new Scanner(System.in);
       System.out.print("Enter the string: ");
       string = input.next();  
@@ -31,13 +31,13 @@ import java.io.*;
       int shifts[] = new int[numbers];
       
       System.out.println();	
-	  System.out.print("Please enter the elements of the array: ");
-	  for (int i = 0; i < shifts.length; i++) {
-		shifts[i] = input.nextInt();
+      System.out.print("Please enter the elements of the array: ");
+      for (int i = 0; i < shifts.length; i++) {
+	shifts[i] = input.nextInt();
       }
       
       String result = shiftingLetters(string, shifts);
       System.out.println();
       System.out.print("The final string after all such shifts to " + string  + " are applied : " + result);
-	}
+    }
   }
