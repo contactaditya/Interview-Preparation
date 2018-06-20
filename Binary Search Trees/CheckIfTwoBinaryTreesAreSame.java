0ba6b3a6674b4a3fc2808d7f0006c07259aff038
Aditya Gupta <contactadityag@gmail.com>
2018-06-19 22:55:22 -0400

    This is a program to check if two binary trees are equal or not.

--- Binary Search Trees/CheckIfTwoBinaryTreesAreSame.java
+++ Binary Search Trees/CheckIfTwoBinaryTreesAreSame.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
  public class CheckIfTwoBinaryTreesAreSame {     
-	private static Node root1;	
+    private static Node root1;	
     private static Node root2;		    
     private static Node previous = null;	
 
@@ -11,31 +11,31 @@ import java.io.*;
     	 return true;
        }
        if (t1 == null || t2 == null) {
-	     return false;
+	 return false;
        }
        
        return (t1.data == t2.data) && isSameTree(t1.left, t2.left) && isSameTree(t1.right, t2.right);
     }
 
-	public static void main(String[] args) {
-	  CheckIfTwoBinaryTreesAreSame tree = new CheckIfTwoBinaryTreesAreSame();
-	  tree.root1 = new Node(1);
-	  tree.root1.left = new Node(2);
+    public static void main(String[] args) {
+      CheckIfTwoBinaryTreesAreSame tree = new CheckIfTwoBinaryTreesAreSame();
+      tree.root1 = new Node(1);
+      tree.root1.left = new Node(2);
       tree.root1.right = new Node(3);
-	  tree.root1.left.left = new Node(4);
-	  tree.root1.left.right = new Node(5);
+      tree.root1.left.left = new Node(4);
+      tree.root1.left.right = new Node(5);
 		   
-	  tree.root2 = new Node(1);
-	  tree.root2.left = new Node(2);
-	  tree.root2.right = new Node(3);
-	  tree.root2.left.left = new Node(4);
-	  tree.root2.left.right = new Node(5);
+      tree.root2 = new Node(1);
+      tree.root2.left = new Node(2);
+      tree.root2.right = new Node(3); 
+      tree.root2.left.left = new Node(4);
+      tree.root2.left.right = new Node(5);
 		 
-	  if (tree.isSameTree(tree.root1, tree.root2) == true) {
-		System.out.println("The two binary trees are equal.");
-	  }
-	  else {
-		System.out.println("The two binary trees are not equal.");
-	  }
-	}
- }
+      if (tree.isSameTree(tree.root1, tree.root2) == true) {
+	System.out.println("The two binary trees are equal.");
+      }
+      else {
+	System.out.println("The two binary trees are not equal.");
+      }
+    }
+  }
