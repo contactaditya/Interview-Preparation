f71ea72e1ce2104939a0716deba6601601a9ce55
Aditya Gupta <contactadityag@gmail.com>
2018-06-20 23:19:07 -0400

    This is a program to find the element that appears only once in an array where every element appears twice.

--- Arrays/SingleNonDuplicate1.java
+++ Arrays/SingleNonDuplicate1.java
@@ -3,15 +3,15 @@ import java.io.*;
 
   public class SingleNonDuplicate1 { 
 	
-	public static int singleNumber(final List<Integer> list) {
-	  int number = list.get(0); 	
-	  for (int i = 1; i < list.size(); i++) {
-	    number = number ^ list.get(i);
-	  }	
-	  return number; 
-	}
+    public static int singleNumber(final List<Integer> list) {
+      int number = list.get(0); 	
+      for (int i = 1; i < list.size(); i++) {
+	number = number ^ list.get(i);
+      }	 
+      return number; 
+    }
 
-	public static void main(String[] args) {  
+    public static void main(String[] args) {  
       Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the list: ");
       int number = input.nextInt();  
@@ -20,11 +20,11 @@ import java.io.*;
       System.out.println();	
       System.out.print("Please enter the elements of the list: ");
       for (int i = 0; i < number; i++) {
-	    list.add(input.nextInt());
+	list.add(input.nextInt());
       }
       
       int singleNumber = singleNumber(list); 
       System.out.println();	
       System.out.print("The only element that appears only once in the list is: " + singleNumber);
-	}
+    }
   }
