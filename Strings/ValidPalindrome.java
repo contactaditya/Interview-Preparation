f89b54e65b03438ecbc348c294192fc7989b0638
Aditya Gupta <contactadityag@gmail.com>
2018-06-21 00:22:30 -0400

    This is a program to determine if a given string is a palindrome or not.

--- Strings/ValidPalindrome.java
+++ Strings/ValidPalindrome.java
@@ -4,27 +4,27 @@ import java.io.*;
  public class ValidPalindrome {
 	
    public static boolean isPalindrome(String string) {
-      if (string.isEmpty()) {
-	return true;
-      }
+     if (string.isEmpty()) {
+       return true;
+     }
 	   
-      String actual = string.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-      String reverse = new StringBuffer(actual).reverse().toString();
-      return actual.equals(reverse);     
+     String actual = string.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+     String reverse = new StringBuffer(actual).reverse().toString();
+     return actual.equals(reverse);     
    } 
 
    public static void main(String[] args) { 	  
-      String string = new String();	  	
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter the string: ");
-      string = input.nextLine(); 
+     String string = new String();	  	
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the string: ");
+     string = input.nextLine(); 
 	  
-      System.out.println();
-      if(isPalindrome(string)) {
-	System.out.println("The string is a valid palindrome.");
-      }
-      else {
-	System.out.println("The string is not a valid palindrome.");  
-      }
+     System.out.println();
+     if(isPalindrome(string)) {
+       System.out.println("The string is a valid palindrome.");
+     }
+     else {
+       System.out.println("The string is not a valid palindrome.");  
+     }
    }
  }
