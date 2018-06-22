df65f5ca32c8667e405a63076806e0250b1981cc
Aditya Gupta <contactadityag@gmail.com>
2018-06-22 18:45:38 -0400

    This is a program to find the sum of contiguous subarray which has the largest sum.

--- Arrays/FindLargestSumContiguousSubarray.java
+++ Arrays/FindLargestSumContiguousSubarray.java
@@ -4,35 +4,34 @@ import java.io.*;
  public class FindLargestSumContiguousSubarray {
 	 
    public static int maxSubArraySum(int[] array) {
-
-      int maxSoFar = Integer.MIN_VALUE;
-      int maxEndingHere = 0;
+     int maxSoFar = Integer.MIN_VALUE;
+     int maxEndingHere = 0;
 		    
-      for(int i = 0; i < array.length; i++) {
-	maxEndingHere = maxEndingHere + array[i];
-	if (maxEndingHere < 0) {
-	  maxEndingHere = 0;
-	}
-	else if (maxSoFar < maxEndingHere) {
-	  maxSoFar = maxEndingHere;
-	}
-      }
+     for(int i = 0; i < array.length; i++) {
+       maxEndingHere = maxEndingHere + array[i];
+       if (maxEndingHere < 0) {
+	 maxEndingHere = 0;
+       }
+       else if (maxSoFar < maxEndingHere) {
+	 maxSoFar = maxEndingHere;
+       }
+     }
      return maxSoFar; 	
    }	 
 
    public static void main(String[] args) {   
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter number of elements in the array: ");
-      int number = input.nextInt();  
-      int array[] = new int[number];
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter number of elements in the array: ");
+     int number = input.nextInt();  
+     int array[] = new int[number];
 			
-      System.out.println();	
-      System.out.print("Please enter the elements of the array: ");
-      for (int i = 0; i < number; i++) {
-	array[i] = input.nextInt();
-      }
-      int maximumSum = maxSubArraySum(array);
-      System.out.println();	
-      System.out.print("The sum of the contiguous subarray within an array which has the largest sum is: " + maximumSum);
+     System.out.println();	
+     System.out.print("Please enter the elements of the array: ");
+     for (int i = 0; i < number; i++) {
+       array[i] = input.nextInt();
+     }
+     int maximumSum = maxSubArraySum(array);
+     System.out.println();	
+     System.out.print("The sum of the contiguous subarray within an array which has the largest sum is: " + maximumSum);
    }
  }
