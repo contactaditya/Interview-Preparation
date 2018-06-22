a5032fec9fc2d583dba854b356bfb74c181d660a
Aditya Gupta <contactadityag@gmail.com>
2018-06-22 19:10:02 -0400

    This is a program to determine if a given string is palindrome or not if we can delete at most one character from the string.

--- Strings/ValidPalindromeII.java
+++ Strings/ValidPalindromeII.java
@@ -1,16 +1,16 @@
 import java.util.*;
 import java.io.*;
 
- public class ValidPalindromeII {	
+  public class ValidPalindromeII {	
 	 
     public static boolean validPalindrome(String string) {
-       for (int i=0; i < string.length()/2; i++) {
-	 if (string.charAt(i) != string.charAt(string.length()-1-i)) {
-	   int j = string.length()-1-i;
-	   return (isPalindromeRange(string, i+1, j) || isPalindromeRange(string, i, j-1));
-	 }
-       } 
-       return true; 
+      for (int i=0; i < string.length()/2; i++) {
+	if (string.charAt(i) != string.charAt(string.length()-1-i)) {
+	  int j = string.length()-1-i;
+	  return (isPalindromeRange(string, i+1, j) || isPalindromeRange(string, i, j-1));
+	}
+      } 
+      return true; 
     } 
 
     private static boolean isPalindromeRange(String string, int i, int j) {
@@ -23,17 +23,17 @@ import java.io.*;
     }
 
     public static void main(String[] args) { 	  
-       String string = new String();	  	
-       Scanner input = new Scanner(System.in);
-       System.out.print("Enter the string: ");
-       string = input.nextLine(); 
+      String string = new String();	  	
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the string: ");
+      string = input.nextLine(); 
 		  
-       System.out.println();
-       if(validPalindrome(string)) {
-	 System.out.println("The string " + string + " can be made a valid palindrome if can delete at most one character from the string.");
-       }
-       else {
-	 System.out.println("The string " + string + " cannot be made a valid palindrome if can delete at most one character from the string.");  
-       }
+      System.out.println();
+      if(validPalindrome(string)) {
+	System.out.println("The string " + string + " can be made a valid palindrome if can delete at most one character from the string.");
+      }
+      else {
+	System.out.println("The string " + string + " cannot be made a valid palindrome if can delete at most one character from the string.");  
+      }
     }
   }
