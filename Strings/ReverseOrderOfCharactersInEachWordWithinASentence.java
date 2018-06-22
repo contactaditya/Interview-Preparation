f7a3ba54979464d23ec6be2904ff3842a2161dab
Aditya Gupta <contactadityag@gmail.com>
2018-06-22 19:15:48 -0400

    This is a program to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.

--- Strings/ReverseOrderOfCharactersInEachWordWithinASentence.java
+++ Strings/ReverseOrderOfCharactersInEachWordWithinASentence.java
@@ -3,24 +3,24 @@ import java.util.*;
 
  public class ReverseOrderOfCharactersInEachWordWithinASentence {  
 	 
-  public static String reverseWords(String string) {  
+   public static String reverseWords(String string) {  
      if (string == null || string.length() == 0) {
-        return "";
+       return "";
      }
 	 
      // Split different words by space
      String[] array = string.split(" ");
      StringBuffer sb = new StringBuffer();
      for (String s : array) { 
-	StringBuffer temp = new StringBuffer(s);  
-	sb.append(temp.reverse());
-        sb.append(" ");
+       StringBuffer temp = new StringBuffer(s);  
+       sb.append(temp.reverse());
+       sb.append(" ");
      }
      sb.setLength(sb.length()-1);
      return sb.toString().trim();
-  }
+   }
 
-  public static void main(String[] args) { 
+   public static void main(String[] args) { 
      String string = new String();	  	
      Scanner input = new Scanner(System.in);
      System.out.print("Enter the string: ");
@@ -29,5 +29,5 @@ import java.util.*;
      String reverse = reverseWords(string);
      System.out.println();
      System.out.println("The string in reverse order of characters in each word within a sentence is: " + reverse);
-  } 
-}
+   }  
+ }
