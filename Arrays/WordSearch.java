c82e6c950b9234897b56a4a6a6ea7fe9ef425b26
Aditya Gupta <contactadityag@gmail.com>
2018-06-29 23:39:09 -0400

    This is a program in an array to remove all instances of that value in place and return the new length.

--- Arrays/WordSearch.java
+++ Arrays/WordSearch.java
@@ -1,24 +1,24 @@
 import java.util.*;
 import java.io.*;
 
-public class WordSearch {
+ public class WordSearch {
 	 
-  public static boolean exist(char[][] board, String word) {
+   public static boolean exist(char[][] board, String word) {
      int m = board.length;
      int n = board[0].length;
 	   
      boolean result = false;
      for(int i=0; i<m; i++) {
        for(int j=0; j<n; j++) {
-	 if(dfs(board,word,i,j,0)){
-	    result = true;
+	 if(dfs(board,word,i,j,0)) {
+	   result = true;
 	 }
        }
      }  
-    return result;   
-  }
+     return result;   
+   }
 	       
-  public static boolean dfs(char[][] board, String word, int i, int j, int k) {
+   public static boolean dfs(char[][] board, String word, int i, int j, int k) {
      int m = board.length;
      int n = board[0].length;
 	   
@@ -36,39 +36,39 @@ public class WordSearch {
 	}
        board[i][j]=temp;
      } 
-    return false;
-  }   	  
+     return false;
+   }   	  
 
-  public static void main(String[] args) {  
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter number of rows in the board: ");
-      int rows = input.nextInt();  
-      System.out.println();	
-      System.out.print("Enter number of columns in the board: ");
-      int columns = input.nextInt();  
+   public static void main(String[] args) {  
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter number of rows in the board: ");
+     int rows = input.nextInt();  
+     System.out.println();	
+     System.out.print("Enter number of columns in the board: ");
+     int columns = input.nextInt();  
 	 
-      char boggle[][] = new char[rows][columns];
+     char boggle[][] = new char[rows][columns];
 				
-      System.out.println();	
-      System.out.println("Please enter the elements in the boggle array: ");
-      for (int i = 0; i < rows; i++) {
-	for (int j = 0; j < columns; j++) {
-	   boggle[i][j] = input.next().charAt(0);
-	}
-      }
+     System.out.println();	
+     System.out.println("Please enter the elements in the boggle array: ");
+     for (int i = 0; i < rows; i++) {
+       for (int j = 0; j < columns; j++) {
+	 boggle[i][j] = input.next().charAt(0);
+       }
+     }
 	  
-      String word = new String();	
-      System.out.println();	
-      System.out.print("Enter a word which you want to check whether it exists in the grid: ");
-      word = input.next();  
+     String word = new String();	
+     System.out.println();	
+     System.out.print("Enter a word which you want to check whether it exists in the grid: ");
+     word = input.next();  
 	  
-      boolean result = exist(boggle, word);
-      System.out.println();
-      if(result) {
-	 System.out.println("The word " + word + " exists in the grid.");  
-      }
-      else {
-	 System.out.println("The word " + word + " does not exist in the grid.");  
-      }
-  }
-}
+     boolean result = exist(boggle, word);
+     System.out.println();
+     if(result) {
+       System.out.println("The word " + word + " exists in the grid.");  
+     }
+     else {
+       System.out.println("The word " + word + " does not exist in the grid.");  
+     }
+   }
+ }
