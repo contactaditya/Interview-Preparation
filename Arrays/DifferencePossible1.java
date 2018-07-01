1f498df16f7b01ed8abb4f59fbfe3cdeed5ee7f6
Aditya Gupta <contactadityag@gmail.com>
2018-07-01 01:28:48 -0400

    This is a program to find if there exists 2 indices i and j such that A[i] - A[j] = k, i != j in an unsorted array.

--- Arrays/DifferencePossible1.java
+++ Arrays/DifferencePossible1.java
@@ -1,36 +1,36 @@
 import java.util.*;
 
   public class DifferencePossible1 {
-	  
-	private static int diffPossible1(ArrayList<Integer> list, int value) {      
+	   
+    private static int diffPossible1(ArrayList<Integer> list, int value) {      
       for (int i = 0; i < list.size(); i++) {
         for (int j = 0; j < list.size(); j++) {
           if(i == j) {
-	        continue;
-	      }
-	      if(list.get(i) - list.get(j) == value) {
-	        return 1;
-	      }	    
+	    continue;
+	  }
+	  if(list.get(i) - list.get(j) == value) {
+	    return 1;
+	  }	    
         }  
       }
 				
-	  return 0;
+      return 0;
     }	  
 	
-	private static int diffPossible(ArrayList<Integer> list, int value) {      
-	  HashSet<Integer> visited = new HashSet<Integer>();
-	  for (Integer number : list) {
-	    if (visited.contains(number + value) || visited.contains(number - value)) {
-	      return 1;
-	    }
-	    visited.add(number);
-	  }
+    private static int diffPossible(ArrayList<Integer> list, int value) {      
+      HashSet<Integer> visited = new HashSet<Integer>();
+      for (Integer number : list) {
+        if (visited.contains(number + value) || visited.contains(number - value)) {
+	  return 1;
+        }
+	visited.add(number);
+      }
 					
-	  return 0;
-	}	  
+      return 0;
+    }	  
 	  
-	public static void main(String[] args) {
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the list: ");
       int number = input.nextInt();  
       ArrayList<Integer> list = new ArrayList<Integer>();	
@@ -38,12 +38,12 @@ import java.util.*;
       System.out.println();	
       System.out.print("Please enter the elements of the list: ");
       for (int i = 0; i < number; i++) {
-	    list.add(input.nextInt());
+	list.add(input.nextInt());
       }
     
-	  System.out.println();	
-	  System.out.print("Enter a value which remove all the instances of that place in the array: ");
-	  int value = input.nextInt();  
+      System.out.println();	
+      System.out.print("Enter a value which remove all the instances of that place in the array: ");
+      int value = input.nextInt();  
 	  
       input.close();
     
@@ -53,7 +53,7 @@ import java.util.*;
         System.out.print("Yes there are 2 indices i and j such that A[i] - A[j] = " + value + ", i != j");
       }
       else {
-  	    System.out.print("No there are no 2 indices i and j such that A[i] - A[j] = " + value + ", i != j");  
+  	System.out.print("No there are no 2 indices i and j such that A[i] - A[j] = " + value + ", i != j");  
       }
-	}
+    }
   }
