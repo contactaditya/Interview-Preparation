245657669af67e0a35c09bb2b170d98ecd1995e0
Aditya Gupta <contactadityag@gmail.com>
2018-07-08 01:16:36 -0400

    This is a program to return all possible permutations from a given collection of numbers.
    No two entries in the permutation sequence should be the same.
    
    For the purpose of this problem, assume that all the numbers in the collection are unique.

--- Arrays/AllPossiblePermutationsInAnArrayList.java
+++ Arrays/AllPossiblePermutationsInAnArrayList.java
@@ -5,36 +5,36 @@ import java.util.Scanner;
 
   public class AllPossiblePermutationsInAnArrayList { 	
 	 
-	public static ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> list) { 
+    public static ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> list) { 
       ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();	
-	  if (list == null || list.size() == 0) {
-	    return result;
-	  }
-	  Collections.sort(list);
-	  backtrack(result, new ArrayList<Integer>(), list, new boolean[list.size()]);	
-	  return result;    
-	}	 
+      if (list == null || list.size() == 0) {
+	return result;
+      } 
+      Collections.sort(list);
+      backtrack(result, new ArrayList<Integer>(), list, new boolean[list.size()]);	
+      return result;    
+    }	 
 
-	private static void backtrack(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> list, ArrayList<Integer> array, boolean[] used) {
-	  if(list.size() == array.size()) {
-	    result.add(new ArrayList<Integer>(list));
-	  } 
-	  else {
-	    for(int i = 0; i < array.size(); i++) { 
-	      if(used[i] || i > 0 && array.get(i) == array.get(i - 1) && !used[i - 1]) {
-		    continue; // This element already exists, skips
-		  }
-		  used[i] = true; 
-		  list.add(array.get(i));
-		  backtrack(result, list, array, used);
-		  used[i] = false; 
-	      list.remove(list.size() - 1);
-	    }
+    private static void backtrack(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> list, ArrayList<Integer> array, boolean[] used) {
+      if(list.size() == array.size()) {
+	result.add(new ArrayList<Integer>(list));
+      } 
+      else {
+        for(int i = 0; i < array.size(); i++) { 
+	  if(used[i] || i > 0 && array.get(i) == array.get(i - 1) && !used[i - 1]) {
+	    continue; // This element already exists, skips
 	  }
-	} 
+	  used[i] = true; 
+	  list.add(array.get(i));
+	  backtrack(result, list, array, used);
+	  used[i] = false; 
+	  list.remove(list.size() - 1);
+	}
+      }
+    } 
 	  
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the list: ");
       int number = input.nextInt();  
       ArrayList<Integer> list = new ArrayList<Integer>();	
@@ -42,7 +42,7 @@ import java.util.Scanner;
       System.out.println();	
       System.out.print("Please enter the elements of the list: ");
       for (int i = 0; i < number; i++) {
-	    list.add(input.nextInt());
+	list.add(input.nextInt());
       }
 		  
       ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
@@ -54,7 +54,7 @@ import java.util.Scanner;
         System.out.println(innerList + " ");
       }
       System.out.println(']');
-	}
+    }
   }
 
 
