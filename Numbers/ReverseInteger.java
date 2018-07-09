7f3964a2dc81e6e669a61934a1f6593fd066c1d4
Aditya Gupta <contactadityag@gmail.com>
2018-07-09 01:51:59 -0400

    This is a program to reverse digits of a given integer.

--- Numbers/ReverseInteger.java
+++ Numbers/ReverseInteger.java
@@ -1,30 +1,29 @@
 import java.util.*;
 import java.io.*;
 
-  public class ReverseInteger {
+ public class ReverseInteger {
 	  
    public static int reverse(int number) {
      long reverse = 0;
      while (number != 0) {
-	reverse = reverse*10 + number%10;
-	number = number/10; 
+       reverse = reverse*10 + number%10;
+       number = number/10; 
      }  	
 	        
      if(reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
-	return 0;    
+       return 0;    
      }     
 		         
      return (int) reverse; 
    }	  
 
    public static void main(String[] args) {  
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter the number: ");
-      int number = input.nextInt();
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the number: ");
+     int number = input.nextInt();
 		  
-      int result = reverse(number);
-      System.out.println();
-      System.out.println("The reverse of " + number + " is: " + result);
-    
+     int result = reverse(number);
+     System.out.println();
+     System.out.println("The reverse of " + number + " is: " + result);
    }
  }
