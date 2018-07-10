2c369483da3bcac3b52ddaaf1ed19cddd25c2495
Aditya Gupta <contactadityag@gmail.com>
2018-07-10 00:48:02 -0400

    This is a program to find out whether there are two distinct indices in the array such that the absolute difference between array[i] and array[j] is at most t.
    The absolute difference between i and j is at most k.

--- Arrays/ContainsNearByDuplicateInAnArray.java
+++ Arrays/ContainsNearByDuplicateInAnArray.java
@@ -4,63 +4,63 @@ import java.io.*;
  public class ContainsNearByDuplicateInAnArray {
 	 
    private static boolean containsNearbyDuplicate(int[] array, int k) {
-      if (array.length <= 1) {
-         return false;
-      }
+     if (array.length <= 1) {
+       return false;
+     }
 	   
-      HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+     HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
 	   
-      for (int i = 0; i < array.length; i++) {
-        if(hashMap.containsKey(array[i]) && i - hashMap.get(array[i]) <= k) {
-           return true;
-        }
-        hashMap.put(array[i], i);
-      }
+     for (int i = 0; i < array.length; i++) {
+       if(hashMap.containsKey(array[i]) && i - hashMap.get(array[i]) <= k) {
+          return true;
+       }
+       hashMap.put(array[i], i);
+     }
 	   
-      return false;
+     return false;
    } 
 	 
    private static boolean containsNearbyDuplicate1(int[] array, int k) {
-      if (array.length <= 1 || k==0) {
-	 return false;
-      }
+     if (array.length <= 1 || k==0) {
+       return false;
+     }
 	     
-      HashSet<Integer> hashSet = new HashSet<Integer>();
+     HashSet<Integer> hashSet = new HashSet<Integer>();
 		   
-      for(int i = 0; i < array.length; i++) {
-	if(i > k) {
-	  hashSet.remove(array[i-k-1]);
-	}
-	if(!hashSet.add(array[i])) {
-	  return true;
-        }
-      }
+     for(int i = 0; i < array.length; i++) {
+       if(i > k) {
+	 hashSet.remove(array[i-k-1]);
+       }
+       if(!hashSet.add(array[i])) {
+	 return true;
+       }
+     }
 	   
-      return false;
+     return false;
    } 
 
    public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter number of elements in the array: ");
-      int number = input.nextInt();  
-      int array[] = new int[number];
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter number of elements in the array: ");
+     int number = input.nextInt();  
+     int array[] = new int[number];
 				  
-      System.out.println();	
-      System.out.print("Please enter the actual numbers in the array: ");
-      for (int i = 0; i < array.length; i++) {
-	 array[i] = input.nextInt();
-      } 
-      System.out.println();	
-      System.out.print("Enter an integer t which represents the absolute difference between two indexes: ");
-      int k = input.nextInt();  
+     System.out.println();	
+     System.out.print("Please enter the actual numbers in the array: ");
+     for (int i = 0; i < array.length; i++) {
+       array[i] = input.nextInt();
+     } 
+     System.out.println();	
+     System.out.print("Enter an integer t which represents the absolute difference between two indexes: ");
+     int k = input.nextInt();  
 	  
-      boolean duplicates = containsNearbyDuplicate(array, k);  
-      System.out.println();	
-      if(duplicates) {
-	 System.out.println("The array contains duplicates.");  
-      } 
-      else {
-	 System.out.println("The array does not contain any duplicates.");
-      }
+     boolean duplicates = containsNearbyDuplicate(array, k);  
+     System.out.println();	
+     if(duplicates) {
+       System.out.println("The array contains duplicates.");  
+     } 
+     else {
+       System.out.println("The array does not contain any duplicates.");
+     }
    }
  }
