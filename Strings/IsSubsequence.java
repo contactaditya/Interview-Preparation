08ac0597a5eec38261d863babc74e325425f409e
Aditya Gupta <contactadityag@gmail.com>
2018-07-12 16:11:10 -0400

    This is a program to check if a given string S is subsequence of another string T.

--- Strings/IsSubsequence.java
+++ Strings/IsSubsequence.java
@@ -4,41 +4,41 @@ import java.io.*;
  public class IsSubsequence {
 	 
    public static boolean isSubsequence(String s, String t) {
-      if (s.length() == 0) {
-	 return true; 
-      }
-      int indexS = 0, indexT = 0;
-      while (indexT < t.length()) {
-        if (t.charAt(indexT) == s.charAt(indexS)) {
-          indexS++;
-          if (indexS == s.length()) {
-            return true;
-          }
-        }
-        indexT++;
-      }
+     if (s.length() == 0) {
+       return true; 
+     }
+     int indexS = 0, indexT = 0;
+     while (indexT < t.length()) {
+       if (t.charAt(indexT) == s.charAt(indexS)) {
+         indexS++;
+         if (indexS == s.length()) {
+           return true;
+         }
+       }
+       indexT++;
+     }
 
-      return false;     
+     return false;     
    }  
 
    public static void main(String[] args) { 
-      String s = new String();	  
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter the first string: ");
-      s = input.next();  
-      System.out.println();	
-      String t = new String();	  
-      System.out.print("Enter the second string: ");
-      t = input.next();
+     String s = new String();	  
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the first string: ");
+     s = input.next();  
+     System.out.println();	
+     String t = new String();	  
+     System.out.print("Enter the second string: ");
+     t = input.next();
 	  
-      boolean result = isSubsequence(s, t);
+     boolean result = isSubsequence(s, t);
 
-      System.out.println();
-      if(result) {
-	System.out.print("The string " + s + " is subsequence of " + t);	
-      }
-      else {
-	System.out.print("The string " + s + " is not a subsequence of " + t);	
-      } 
+     System.out.println();
+     if(result) {
+       System.out.print("The string " + s + " is subsequence of " + t);	
+     }
+     else {
+       System.out.print("The string " + s + " is not a subsequence of " + t);	
+     } 
    }
  }
