648e1f6f16fcaed21468209aa7ba20c8564306b3
Aditya Gupta <contactadityag@gmail.com>
2018-07-12 15:59:55 -0400

    This is a program to find the first non-repeating character in a string and return it's index.

--- Strings/FirstUniqueCharacterInAString.java
+++ Strings/FirstUniqueCharacterInAString.java
@@ -4,25 +4,25 @@ import java.io.*;
  public class FirstUniqueCharacterInAString {
 	 
    public static int firstUniqueCharacter(String string) {
-      int frequency[] = new int[26];
-      for(int i = 0; i < string.length(); i++) {
-    	frequency[string.charAt(i) - 'a']++;
-      }
-      for(int i = 0; i < string.length(); i++) {
-        if(frequency[string.charAt(i) - 'a'] == 1) {
-          return i;
-        }
-      }
-      return -1;
+     int frequency[] = new int[26];
+     for(int i = 0; i < string.length(); i++) {
+       frequency[string.charAt(i) - 'a']++;
+     }
+     for(int i = 0; i < string.length(); i++) {
+       if(frequency[string.charAt(i) - 'a'] == 1) {
+         return i;
+       }
+     }
+     return -1;
    }	 
 
    public static void main(String[] args) {	  
-      String string = new String();	  
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter the string: ");
-      string = input.nextLine(); 
-      int firstUniqueCharacter = firstUniqueCharacter(string);
-      System.out.println();
-      System.out.println("The index of the first non-repeating character in the " + string + " is: " + firstUniqueCharacter);  	 
+     String string = new String();	  
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the string: ");
+     string = input.nextLine(); 
+     int firstUniqueCharacter = firstUniqueCharacter(string);
+     System.out.println();
+     System.out.println("The index of the first non-repeating character in the " + string + " is: " + firstUniqueCharacter);  	 
    }
  }
