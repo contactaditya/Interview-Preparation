27fcad3f3e1fea95a0c1455d6a9ed66ad8e2a001
Aditya Gupta <contactadityag@gmail.com>
2018-07-12 16:07:31 -0400

    This is a program to determine if two strings s and t are isomorphic.

--- Strings/IsomorphicStrings.java
+++ Strings/IsomorphicStrings.java
@@ -1,54 +1,54 @@
 import java.util.*;
 import java.io.*;
 
-  public class IsomorphicStrings {
+ public class IsomorphicStrings {
 	
    public static boolean isIsomorphic(String s, String t) { 
-      if(s == null || t == null) {
-        return false;
-      }
+     if(s == null || t == null) {
+       return false;
+     }
  
-      if(s.length() != t.length()) {
-    	return false;	  
-      }
+     if(s.length() != t.length()) {
+       return false;	  
+     }
       
-      Map<Character, Character> map = new HashMap<Character, Character>();
-      for(int i=0; i<s.length(); i++) {
-        char c1 = s.charAt(i);
-        char c2 = t.charAt(i);
+     Map<Character, Character> map = new HashMap<Character, Character>();
+     for(int i=0; i<s.length(); i++) {
+       char c1 = s.charAt(i);
+       char c2 = t.charAt(i);
    
-        if(map.containsKey(c1)) {
-          if(map.get(c1) != c2) { // if not consistant with previous ones
+       if(map.containsKey(c1)) {
+         if(map.get(c1) != c2) { // if not consistant with previous ones
             return false;
-          } 
-        } else {
-          if(map.containsValue(c2)) { //if c2 is already being mapped
-            return false;
-          }
-          map.put(c1, c2);
-        }
-      }  
+         } 
+       } else {
+         if(map.containsValue(c2)) { //if c2 is already being mapped
+           return false;
+         }
+         map.put(c1, c2);
+       }
+     }  
  
-      return true;      
+     return true;      
    }	  
 
    public static void main(String[] args) { 
-      String s = new String();	  
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter the first string: "); 
-      s = input.nextLine();   
-      System.out.println();	
-      String t = new String();	  
-      System.out.print("Enter the second string: ");
-      t = input.nextLine();
+     String s = new String();	  
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the first string: "); 
+     s = input.nextLine();   
+     System.out.println();	
+     String t = new String();	  
+     System.out.print("Enter the second string: ");
+     t = input.nextLine();
 		  
-      boolean isIsomorphic = isIsomorphic(s, t);
-      System.out.println();
-      if(isIsomorphic) {
-	System.out.println("Yes the strings " + s + " and " + t + " are isomorphic.");  
-      }
-      else {
-    	System.out.println("No the strings " + s + " and " + t + " are not isomorphic.");   
-      }
+     boolean isIsomorphic = isIsomorphic(s, t);
+     System.out.println();
+     if(isIsomorphic) {
+       System.out.println("Yes the strings " + s + " and " + t + " are isomorphic.");  
+     }
+     else {
+       System.out.println("No the strings " + s + " and " + t + " are not isomorphic.");   
+     }
    }
  }
