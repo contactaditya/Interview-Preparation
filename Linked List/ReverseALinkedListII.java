3b12161283a2fe78fc3e0474dadd2dd7572d6aa1
Aditya Gupta <contactadityag@gmail.com>
2018-07-11 21:09:19 -0400

    This is a program to reverse a linked list from a starting position to ending position.

--- Linked List/ReverseALinkedListII.java
+++ Linked List/ReverseALinkedListII.java
@@ -6,67 +6,67 @@ import java.io.*;
     private static int size;  
 
     public ReverseALinkedListII() {
-       // this is an empty list, so the reference to the head node is set to a new node with no data				  
-       head = new LinkedListNode(null);
-       size = 0;
+      // this is an empty list, so the reference to the head node is set to a new node with no data				  
+      head = new LinkedListNode(null);
+      size = 0;
     } 
     
     public static LinkedListNode reverseBetween(LinkedListNode head, int m, int n) { 
-       if (head == null || head.next == null) {
-	  return head;	 
-       }
-       LinkedListNode dummy = new LinkedListNode(0); // create a dummy node to mark the head of this list
-       dummy.next = head;
-       LinkedListNode previous = dummy; // Make a pointer previous as a marker for the node before reversing
-       for(int i = 0; i<m-1; i++) {
-	  previous = previous.next; 
-       }
+      if (head == null || head.next == null) {
+	return head;	 
+      }
+      LinkedListNode dummy = new LinkedListNode(0); // create a dummy node to mark the head of this list
+      dummy.next = head;
+      LinkedListNode previous = dummy; // Make a pointer previous as a marker for the node before reversing
+      for(int i = 0; i < m - 1; i++) {
+	previous = previous.next; 
+      }
+	  
+      LinkedListNode start = previous.next; // a pointer to the beginning of a sub-list that will be reversed
+      LinkedListNode end = start.next; // a pointer to a node that will be reversed 
 	   
-       LinkedListNode start = previous.next; // a pointer to the beginning of a sub-list that will be reversed
-       LinkedListNode end = start.next; // a pointer to a node that will be reversed 
-	   
-       for(int i = 0; i<n-m; i++) {
-	  start.next = end.next;
-	  end.next = previous.next;
-	  previous.next = end;
-	  end = start.next;
-       }
+      for(int i = 0; i < n - m; i++) {
+	start.next = end.next;
+	end.next = previous.next;
+	previous.next = end;
+	end = start.next;
+      }
 		    
-       return dummy.next;
+      return dummy.next;
     }       
 
     public static LinkedListNode reverseBetween1(LinkedListNode head, int m, int n) { 
-       if (head == null || head.next == null) {
-	  return head;	 
-       }
-       LinkedListNode dummy = new LinkedListNode(0);
-       dummy.next = head;
-       LinkedListNode previous = dummy, current = head;
-       int k = 1;
-       while(current.next != null && k != m) {
-          current = current.next;
-          previous = previous.next;
-          k++;
-       }
+      if (head == null || head.next == null) {
+	return head;	 
+      }
+      LinkedListNode dummy = new LinkedListNode(0);
+      dummy.next = head;
+      LinkedListNode previous = dummy, current = head;
+      int k = 1;
+      while(current.next != null && k != m) {
+        current = current.next;
+        previous = previous.next;
+        k++;
+      }
        
-       while(current.next != null && k != n) {
-   	 LinkedListNode preNext = previous.next;
-         previous.next = current.next;
-         LinkedListNode next = current.next;
-         current.next = next.next;
-         next.next = preNext;
-         k++;
-       }
+      while(current.next != null && k != n) {
+   	LinkedListNode preNext = previous.next;
+        previous.next = current.next;
+        LinkedListNode next = current.next;
+        current.next = next.next;
+        next.next = preNext;
+        k++;
+      }
 	    
-       return dummy.next;
+      return dummy.next;
     }   
 	 	 
     public static void PrintElementsOfALinkedList(LinkedListNode head) {	 
-       LinkedListNode current = head; 		    
-       while(current != null) {	   
-	  System.out.print(current.getData() + " ");	 	
-          current = current.getNext();
-       }
+      LinkedListNode current = head; 		    
+      while(current != null) {	   
+	System.out.print(current.getData() + " ");	 	
+        current = current.getNext();
+      }
     } 	 
 
     public static void main(String[] args) {
