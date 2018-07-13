f3801b82ea237aa205d74c5345c4486199001aa8
Aditya Gupta <contactadityag@gmail.com>
2018-07-12 21:35:57 -0400

    This is a program to get the counts array where counts[i] is the number of smaller elements to the right of array.

--- Binary Search Trees/CountOfSmallerNumbersAfterSelfInAnArray.java
+++ Binary Search Trees/CountOfSmallerNumbersAfterSelfInAnArray.java
@@ -10,71 +10,69 @@ import java.io.*;
     //Node constructor
     
     public BSTNode1(int value) {
-       data = value;
+      data = value;
     }
   }
 
   public class CountOfSmallerNumbersAfterSelfInAnArray {
 	  
-     public static List<Integer> countSmaller(int[] array) {
-	List<Integer> result = new ArrayList<>();
-	if(array == null || array.length == 0) {
-	   return result;	
-	}
+    public static List<Integer> countSmaller(int[] array) {
+      List<Integer> result = new ArrayList<>();
+      if(array == null || array.length == 0) {
+	return result;	
+      }
 	  
-	BSTNode1 root = new BSTNode1(array[array.length - 1]);
-	result.add(0);
-	for(int i = array.length - 2; i >= 0; i--) {
-           int count = insertNode(root, array[i]);
-           result.add(count);
-	}
+      BSTNode1 root = new BSTNode1(array[array.length - 1]);
+      result.add(0);
+      for(int i = array.length - 2; i >= 0; i--) {
+        int count = insertNode(root, array[i]);
+        result.add(count);
+      }
 	  
-	Collections.reverse(result);
-	return result;     
-     }	  
+      Collections.reverse(result);
+      return result;     
+    }	  
 	  
-     private static int insertNode(BSTNode1 root, int value) {
-        int thisCount = 0;
-	while(true) {
-	  if(value <= root.data) {
-	    root.count++;
-	    if(root.left == null) {
-	      root.left = new BSTNode1(value); 
-	      break;
-	    } else {
-	      root = root.left;
-	    }
+    private static int insertNode(BSTNode1 root, int value) {
+      int thisCount = 0;
+      while(true) {
+        if(value <= root.data) {
+	  root.count++;
+	  if(root.left == null) {
+	    root.left = new BSTNode1(value); 
+	    break;
 	  } else {
-	    thisCount += root.count;
-	    if(root.right == null) {
-	      root.right = new BSTNode1(value); 
-	      break;
-	    } else {
-	      root = root.right;
-	    }
+	    root = root.left;
+	  }
+	} else {
+	  thisCount += root.count;
+	  if(root.right == null) {
+	    root.right = new BSTNode1(value); 
+	    break;
+	  } else {
+	    root = root.right;
 	  }
 	}
-	return thisCount;
-     }
+      }
+      return thisCount;
+    }
 
-     public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	System.out.print("Enter number of elements in the array: ");
-	int numbers = input.nextInt();   
-	int array[] = new int[numbers];
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 		    
-	System.out.print("\n");
-	System.out.print("Please enter the actual values in the array: ");
-	for (int i = 0; i < numbers; i++) {
-	  array[i] = input.nextInt(); 
-	}
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt(); 
+      }
 		   		   
-	List<Integer> result = new ArrayList<Integer>();
+      List<Integer> result = new ArrayList<Integer>();	   
+      result = countSmaller(array); 
 		   
-	result = countSmaller(array); 
-		   
-	System.out.println();
-	System.out.println("The array containing number of smaller elements to the right of array[i] is: " + result);	
-
-     }
+      System.out.println();
+      System.out.println("The array containing number of smaller elements to the right of array[i] is: " + result);
+    }
   }
