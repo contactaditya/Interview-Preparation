777826ddde2fb952f456a4c5973df5a608632a8c
Aditya Gupta <contactadityag@gmail.com>
2018-07-12 21:28:45 -0400

    This is a program to compute the length of the diameter of the binary tree.  The diameter of a binary tree is the length of the longest path between any two nodes in a tree.

--- Binary Search Trees/FindDiameterOfBinaryTree.java
+++ Binary Search Trees/FindDiameterOfBinaryTree.java
@@ -7,35 +7,35 @@ import java.io.*;
     int max = 0;
 	  
     public int diameterOfBinaryTree(Node root) {
-       if (root == null) {
-	 return 0;
-       }	
-       maxDepth(root);
-       return max;
+      if (root == null) {
+	return 0;
+      }	
+      maxDepth(root);
+      return max;
     }	  
 
     private int maxDepth(Node root) {
-       if (root == null) {
-	 return 0;
-       }
+      if (root == null) {
+	return 0;
+      }
 	        
-       int left = maxDepth(root.left);
-       int right = maxDepth(root.right);
+      int left = maxDepth(root.left);
+      int right = maxDepth(root.right);
 	        
-       max = Math.max(max, left + right);
+      max = Math.max(max, left + right);
 	        
-       return Math.max(left, right) + 1;
+      return Math.max(left, right) + 1;
     }
 
     public static void main(String[] args) { 	   
-       FindDiameterOfBinaryTree tree = new FindDiameterOfBinaryTree();
-       tree.root = new Node(1);
-       tree.root.left = new Node(2);
-       tree.root.right = new Node(3);
-       tree.root.left.left = new Node(4);
-       tree.root.left.right = new Node(5);
-       int lengthOfDiameter = tree.diameterOfBinaryTree(tree.root);		
+      FindDiameterOfBinaryTree tree = new FindDiameterOfBinaryTree();
+      tree.root = new Node(1);
+      tree.root.left = new Node(2);
+      tree.root.right = new Node(3);
+      tree.root.left.left = new Node(4);
+      tree.root.left.right = new Node(5);
+      int lengthOfDiameter = tree.diameterOfBinaryTree(tree.root);		
 		     
-       System.out.print("The length of the diameter of the binary tree is: " + lengthOfDiameter);
+      System.out.print("The length of the diameter of the binary tree is: " + lengthOfDiameter);
     }
   }
