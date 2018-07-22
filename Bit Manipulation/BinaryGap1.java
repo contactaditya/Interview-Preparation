194c5a7b2f00c9bb2a710c473776fd9391b2bf88
Aditya Gupta <contactadityag@gmail.com>
2018-07-21 21:29:36 -0400

    This is a program to return the longest distance between two consecutive 1's in the binary representation of the number.

--- Bit Manipulation/BinaryGap1.java
+++ Bit Manipulation/BinaryGap1.java
@@ -3,10 +3,10 @@ import java.io.*;
 
   public class BinaryGap1 {
 	  
-	public static int binaryGap(int number) { 
-	  if (number <= 0) {
-		return 0;
-	  }
+    public static int binaryGap(int number) { 
+      if (number <= 0) {
+	return 0;
+      }
       int max = 0, currentDistance = 0;
       boolean firstOne = true;
       while (number > 0) {
@@ -15,7 +15,7 @@ import java.io.*;
           if (firstOne) {
             firstOne = false;
           } else {
-        	max = Math.max(max, currentDistance);
+            max = Math.max(max, currentDistance);
           }
           currentDistance = 1;
         } else {
@@ -26,27 +26,27 @@ import java.io.*;
       return max;
     }
 	
-	public static int binaryGap1(int number) { 
-	  int last = -1, answer = 0;
-	  for (int i = 0; i < 32; i++) {
-	    if (((number >> i) & 1) > 0) {
-	      if (last >= 0) {
-	        answer = Math.max(answer, i - last);
-	      }
-	      last = i;
-	    }
+    public static int binaryGap1(int number) { 
+      int last = -1, answer = 0;
+      for (int i = 0; i < 32; i++) {
+        if (((number >> i) & 1) > 0) {
+	  if (last >= 0) {
+	    answer = Math.max(answer, i - last);
 	  }
+	  last = i;
+	}
+      }
 	            
-	  return answer;
-	}
+      return answer;
+    }
       
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();  
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int number = input.nextInt();  
 
-	  int longestBinaryGap = binaryGap(number);
-	  System.out.println();	
-	  System.out.print("The length of the longest distance between two consecutive 1's is: " + longestBinaryGap);
-	}
+      int longestBinaryGap = binaryGap(number);
+      System.out.println();	
+      System.out.print("The length of the longest distance between two consecutive 1's is: " + longestBinaryGap);
+    }
   }
