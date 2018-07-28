a3dfcd42a564a90a626d7e14c1d66962a5cdc79c
Aditya Gupta <contactadityag@gmail.com>
2018-07-27 23:04:42 -0400

    This is a program to return the smallest K for which A and B are K-similar.
    Strings A and B are K-similar (for some non-negative integer K) if we can swap the positions of two letters in A exactly K times so that the resulting string equals B.

--- Strings/KSimilarStrings.java
+++ Strings/KSimilarStrings.java
@@ -3,33 +3,33 @@ import java.io.*;
 
   public class KSimilarStrings {
 	  
-	public static int kSimilarity(String A, String B) {
-	  char[] arrayA = A.toCharArray();
-	  char[] arrayB = B.toCharArray();
-	  int count = 0;  
-	  for (int i = 0; i < arrayA.length; i++) {
-	    if (arrayA[i] == arrayB[i]) {
-	      continue;
-	    }
-	    for (int j = i + 1; j < arrayA.length; j++) {
-	      if (arrayA[i] == arrayB[j] && arrayB[i] == arrayA[j]) {
-	        swap(arrayA, i, j);
-	        count++;
-	        break;
-	      }
-	    }
+    public static int kSimilarity(String A, String B) {
+      char[] arrayA = A.toCharArray();
+      char[] arrayB = B.toCharArray();
+      int count = 0;  
+      for (int i = 0; i < arrayA.length; i++) {
+	if (arrayA[i] == arrayB[i]) {
+	  continue;
+	}
+	for (int j = i + 1; j < arrayA.length; j++) {
+	  if (arrayA[i] == arrayB[j] && arrayB[i] == arrayA[j]) {
+	    swap(arrayA, i, j);
+	    count++;
+	    break;
 	  }
-	  count += backtrack(arrayA, arrayB, 0);
+	}
+      }
+      count += backtrack(arrayA, arrayB, 0);
 	  
-	  return count;
+      return count;
     }	  
 	
-	private static int backtrack(char[] A, char[] B, int start) {
+    private static int backtrack(char[] A, char[] B, int start) {
       if (start == A.length) {
-		return 0;
+	return 0;
       }
       if (A[start] == B[start]) {
-		return backtrack(A, B, start + 1);
+	return backtrack(A, B, start + 1);
       }
       int result = Integer.MAX_VALUE;
       for (int i = start + 1; i < A.length; i++) {
@@ -40,17 +40,17 @@ import java.io.*;
         }
       }
 	
-	  return result;
-	}
+      return result;
+    }
 	
-	public static void swap(char[] T, int i, int j) {
-	  char temp = T[i];
-	  T[i] = T[j];
-	  T[j] = temp;
-	}
+    public static void swap(char[] T, int i, int j) {
+      char temp = T[i];
+      T[i] = T[j];
+      T[j] = temp;
+    }
 
-	public static void main(String[] args) {  
-	  String A = new String();	  
+    public static void main(String[] args) {  
+      String A = new String();	  
       Scanner input = new Scanner(System.in);
       System.out.print("Enter the first string: ");
       A = input.nextLine(); 
@@ -63,5 +63,5 @@ import java.io.*;
       int result = kSimilarity(A, B);
       System.out.println();
       System.out.println("The smallest K for which " + A  + " and " + B + " are K-similar is: " + result);
-	}
+    }
   }
