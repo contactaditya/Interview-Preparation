d75d6894dfe7888980183d464df614d84370b175
contactaditya <contactadityag@gmail.com>
2018-07-31 21:34:21 -0400

    This is a progam to output the length L and the width W of the web page you designed in sequence.
    So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
    
    1. The area of the rectangular web page you designed must equal to the given target area.
    
    2. The width W should not be larger than the length L, which means L >= W.
    
    3. The difference between length L and width W should be as small as possible.

---
+++ Arrays/ConstructTheRectangle.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+  public class ConstructTheRectangle {
+	
+	public static int[] constructRectangle(int area) {
+	  int width = (int) Math.sqrt(area);
+      while(area % width != 0) {
+    	width--;
+      }
+      return new int[]{area/width, width};
+	}
+
+	public static void main(String[] args) {   
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the number: ");
+	  int area = input.nextInt();   
+		    	   
+	  int result[] = constructRectangle(area); 
+		   
+	  System.out.println();
+	  System.out.print("The dimensions of the rectangular web page, whose length L and width W satisfy the given requirements are: ");
+	  System.out.print("[");
+	  for (int i = 0; i < result.length; i++) {
+		System.out.print(" " + result[i] + " ");
+	  }		   
+	  System.out.print("]");
+	}
+  }
