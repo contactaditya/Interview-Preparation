60ccac75372392ae9769d0c236cea7025190cf8f
Aditya Gupta <contactadityag@gmail.com>
2018-08-01 23:38:21 -0400

    This is a program to populate each next pointer to point to its next right node in a binary tree.

--- Binary Search Trees/PopulatingNextRightPointersInEachNode.java
+++ Binary Search Trees/PopulatingNextRightPointersInEachNode.java
@@ -13,27 +13,27 @@ import java.io.*;
   }
 
   public class PopulatingNextRightPointersInEachNode {
-	TreeLinkNode root;
+    TreeLinkNode root;
 	  
-	public void connect(TreeLinkNode root) {
-	  if(root == null) {
-	    return;
+    public void connect(TreeLinkNode root) {
+      if(root == null) {
+	return;
       }
 	        
-	  if(root.left != null) {
-	    root.left.next = root.right;
-	    if(root.next != null) {
-	      root.right.next = root.next.left;
-	    }
-	  }
+      if(root.left != null) {
+	root.left.next = root.right;
+	if(root.next != null) {
+	  root.right.next = root.next.left;
+	}
+      }
 	    
-	  connect(root.left);
-	  connect(root.right);      
+      connect(root.left);
+      connect(root.right);      
     }
 	
     public void printPreorderTraversal(TreeLinkNode node) {	 
       if (node == null) {
-  	    return;
+  	return;
       }
       
       System.out.print(node.data + " ");		  
@@ -41,18 +41,18 @@ import java.io.*;
       printPreorderTraversal(node.right);
     }
 
-	public static void main(String[] args) {  
-	  PopulatingNextRightPointersInEachNode tree = new PopulatingNextRightPointersInEachNode();
-	  tree.root = new TreeLinkNode(1);
-	  tree.root.left = new TreeLinkNode(2);
-	  tree.root.right = new TreeLinkNode(3);
-	  tree.root.left.left = new TreeLinkNode(4);
-	  tree.root.left.right = new TreeLinkNode(5);   
-	  tree.root.right.left = new TreeLinkNode(6);
-	  tree.root.right.right = new TreeLinkNode(7);
+    public static void main(String[] args) {  
+      PopulatingNextRightPointersInEachNode tree = new PopulatingNextRightPointersInEachNode();
+      tree.root = new TreeLinkNode(1);
+      tree.root.left = new TreeLinkNode(2);
+      tree.root.right = new TreeLinkNode(3);
+      tree.root.left.left = new TreeLinkNode(4);
+      tree.root.left.right = new TreeLinkNode(5);   
+      tree.root.right.left = new TreeLinkNode(6);
+      tree.root.right.right = new TreeLinkNode(7);
 	  
-	  tree.connect(tree.root);
-	  System.out.print("The tree after its pointer points to its next right node is: ");		  
-	  tree.printPreorderTraversal(tree.root);		
-	}
+      tree.connect(tree.root);
+      System.out.print("The tree after its pointer points to its next right node is: ");		  
+      tree.printPreorderTraversal(tree.root);		
+    }
   }
