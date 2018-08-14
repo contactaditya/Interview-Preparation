f9c6c49523f3173edb0b2e2701d65b94deec38db
contactaditya <contactadityag@gmail.com>
2018-08-14 05:00:20 -0400

    This is a program to find the node in the binary tree with the largest depth such that it contains all the deepest nodes in its subtree.
    The depth of each node is the shortest distance to the root.
    
    A node is deepest if it has the largest depth possible among any node in the entire tree.
    
    The subtree of a node is that node, plus the set of all descendants of that node.

---
+++ Binary Search Trees/SmallestSubtreeWithAllTheDeepestNodes.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+  public class SmallestSubtreeWithAllTheDeepestNodes { 	
+	Map<Node, Integer> depth;
+	int max_depth;
+	Node root;
+	  
+	public Node subtreeWithAllDeepest(Node root) {
+      depth = new HashMap<Node, Integer>();
+      depth.put(null, -1);
+      dfs(root, null);
+      max_depth = -1;	
+      for (Integer d : depth.values()) {
+        max_depth = Math.max(max_depth, d);
+      }
+	
+	  return answer(root);       
+	}
+
+	private Node answer(Node node) {
+	  if (node == null || depth.get(node) == max_depth) {
+	    return node;
+	  }
+	  Node L = answer(node.left), R = answer(node.right);
+	  if (L != null && R != null) {
+		return node;
+	  }
+      if (L != null) {
+    	return L;
+      }
+      if (R != null) {
+    	return R;
+      }
+		
+	  return null;
+	}
+
+	private void dfs(Node node, Node parent) {
+	  if (node != null) {
+	    depth.put(node, depth.get(parent) + 1);
+	    dfs(node.left, node);
+	    dfs(node.right, node);
+	  }
+	}
+	
+    public void printPreorderTraversal(Node node) {	 
+      if (node == null) {
+  	    return;
+      }
+      
+      System.out.print(node.data + " ");		  
+      printPreorderTraversal(node.left);
+      printPreorderTraversal(node.right);
+    }	
+
+	public static void main(String[] args) { 	 
+	  SmallestSubtreeWithAllTheDeepestNodes tree = new SmallestSubtreeWithAllTheDeepestNodes();
+	  tree.root = new Node(3);
+	  tree.root.left = new Node(5);
+	  tree.root.left.left = new Node(6);
+	  tree.root.left.right = new Node(2);
+	  tree.root.left.right.left = new Node(7);
+	  tree.root.left.right.right = new Node(4);
+	  tree.root.right = new Node(1);
+      tree.root.right.right = new Node(8);
+      tree.root.right.left = new Node(0);
+      
+      Node result = tree.subtreeWithAllDeepest(tree.root);
+      
+      System.out.print("The node with the largest depth such that it contains all the deepest nodes in its subtree is: ");
+      tree.printPreorderTraversal(result);
+	}
+  }
