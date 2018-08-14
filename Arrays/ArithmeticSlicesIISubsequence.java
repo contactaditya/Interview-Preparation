9e7fcc41a29a9082156ba1b13b5364e1468daba2
Aditya Gupta <contactadityag@gmail.com>
2018-08-14 06:01:48 -0400

    This is a program to calculate the number of arithmetic subsequence slices in the array A.
    A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.

--- Arrays/ArithmeticSlicesIISubsequence.java
+++ Arrays/ArithmeticSlicesIISubsequence.java
@@ -3,11 +3,11 @@ import java.io.*;
 
   public class ArithmeticSlicesIISubsequence {
 	
-	public static int numberOfArithmeticSlices(int[] array) {
-	  int n = array.length;
-	  long numberOfArithmeticSlices = 0;
-	  Map<Integer, Integer> count[] = new Map[n];
-	  for (int i = 0; i < n; i++) {
+    public static int numberOfArithmeticSlices(int[] array) {
+      int n = array.length;
+      long numberOfArithmeticSlices = 0;
+      Map<Integer, Integer> count[] = new Map[n];
+      for (int i = 0; i < n; i++) {
         count[i] = new HashMap<>(i);
         for (int j = 0; j < i; j++) {
           long delta = (long)array[i] - (long)array[j];
@@ -22,23 +22,23 @@ import java.io.*;
         }
       }
 		
-	  return (int)numberOfArithmeticSlices;     
-	}
+      return (int)numberOfArithmeticSlices;     
+    }
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();  
+      int array[] = new int[number];
 		
-	  System.out.println();	
-	  System.out.print("Please enter the elements of the array: ");
-	  for (int i = 0; i < array.length; i++) {
-	    array[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the elements of the array: ");
+      for (int i = 0; i < array.length; i++) {
+	array[i] = input.nextInt();
+      }
 	  
-	  int result = numberOfArithmeticSlices(array);
-	  System.out.println();	
-	  System.out.print("The number of arithmetic subsequence slices in the array are: " + result);
-	}
+      int result = numberOfArithmeticSlices(array);
+      System.out.println();	
+      System.out.print("The number of arithmetic subsequence slices in the array are: " + result);
+    }
   }
