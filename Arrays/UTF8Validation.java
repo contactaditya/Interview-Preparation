4a2b25ca83c641abe58a425ee999d40026a610ab
Aditya Gupta <contactadityag@gmail.com>
2018-08-15 10:09:02 -0400

    This is a program to find out if whether an array of integers representing the data is a valid utf-8 encoding.

--- Arrays/UTF8Validation.java
+++ Arrays/UTF8Validation.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
   public class UTF8Validation {
-	private static int[] masks = {128, 64, 32, 16, 8};
+    private static int[] masks = {128, 64, 32, 16, 8};
 	  
-	public static boolean validUtf8(int[] data) {
-	  int length = data.length;
-	  for (int i = 0; i < length; i++) {
-	    int current = data[i];
+    public static boolean validUtf8(int[] data) {
+      int length = data.length;
+      for (int i = 0; i < length; i++) {
+	int current = data[i];
         int type = getType(current);
         if (type == 0) {
           continue;
@@ -18,14 +18,14 @@ import java.io.*;
             }
           }
         }
-	    else {
-		  return false;  
-	    }
-	  }
-	  return true;      
+	else {
+	  return false;  
 	}
+      }
+      return true;      
+    }
 	
-	public static int getType(int number) {
+    public static int getType(int number) {
       for (int i = 0; i < 5; i ++) {
         if ((masks[i] & number) == 0) {
           return i;
@@ -34,25 +34,25 @@ import java.io.*;
       return -1;
     }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int data[] = new int[number];
+    public static void main(String[] args) {    
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();  
+      int data[] = new int[number];
 	
-	  System.out.println();	
-	  System.out.print("Please enter the elements in the array: ");
-	  for (int i = 0; i < data.length; i++) {
-		data[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the elements in the array: ");
+      for (int i = 0; i < data.length; i++) {
+	data[i] = input.nextInt();
+      }
 		  
-	  System.out.println();
-	  boolean result = validUtf8(data);
-	  if(result) {
-		System.out.println("It is a valid utf-8 encoding.");  
-	  }
-	  else {
-		System.out.println("It is not an valid utf-8 encoding.");  
-	  }
-	}
+      System.out.println();
+      boolean result = validUtf8(data);
+      if(result) {
+	System.out.println("It is a valid utf-8 encoding.");  
+      }
+      else {
+	System.out.println("It is not an valid utf-8 encoding.");  
+      }
+    }
   }
