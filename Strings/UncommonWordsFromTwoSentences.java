467a6223816790f1c7e538a04822161a40089f75
Aditya Gupta <contactadityag@gmail.com>
2018-08-15 09:58:48 -0400

    This is a program to return a list of all uncommon words in the two given sentences A and B.
    A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.

--- Strings/UncommonWordsFromTwoSentences.java
+++ Strings/UncommonWordsFromTwoSentences.java
@@ -3,7 +3,7 @@ import java.io.*;
 
   public class UncommonWordsFromTwoSentences {
 	  
-	public static String[] uncommonFromSentences(String A, String B) {
+    public static String[] uncommonFromSentences(String A, String B) {
       Map<String, Integer> count = new HashMap<String, Integer>();
       for (String word: A.split(" ")) {
         count.put(word, count.getOrDefault(word, 0) + 1);
@@ -11,32 +11,32 @@ import java.io.*;
       for (String word: B.split(" ")) {
         count.put(word, count.getOrDefault(word, 0) + 1);
       }
-	  List<String> answer = new LinkedList<String>();
-	  for (String word: count.keySet()) {
+      List<String> answer = new LinkedList<String>();
+      for (String word: count.keySet()) {
         if (count.get(word) == 1) {
           answer.add(word);
         }
-	  }
-		
-	  return answer.toArray(new String[answer.size()]);
-	}
-
-	public static void main(String[] args) { 	 
-	  String A = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the first sentence: ");
-	  A = input.nextLine();  
-	  System.out.println();	
-	  String B = new String();	  
-	  System.out.print("Enter the second sentence: ");
-	  B = input.nextLine();
-	  String result[] = uncommonFromSentences(A, B);
-	  System.out.println();	
-	  System.out.print("A list of all uncommon words is: ");
-	  System.out.print("[");	
-	  for (String word : result) {
-		System.out.print(word + " ");	   
       }
-	  System.out.print("]");
-	}
+		
+      return answer.toArray(new String[answer.size()]);
+    }
+
+    public static void main(String[] args) { 	 
+      String A = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the first sentence: ");
+      A = input.nextLine();  
+      System.out.println();	
+      String B = new String();	  
+      System.out.print("Enter the second sentence: ");
+      B = input.nextLine();
+      String result[] = uncommonFromSentences(A, B);
+      System.out.println();	
+      System.out.print("A list of all uncommon words is: ");
+      System.out.print("[");	
+      for (String word : result) {
+	System.out.print(word + " ");	   
+      }
+      System.out.print("]");
+    }
   }
