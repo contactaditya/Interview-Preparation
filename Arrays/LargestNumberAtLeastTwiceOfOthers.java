c1866fef796fc6d6904a6c8f7f1cd94d9e829407
Aditya Gupta <contactadityag@gmail.com>
2018-09-03 03:15:31 -0400

    This is a program to find whether the largest element in the array is at least twice as much as every other number in the array.

--- Arrays/LargestNumberAtLeastTwiceOfOthers.java
+++ Arrays/LargestNumberAtLeastTwiceOfOthers.java
@@ -3,37 +3,36 @@ import java.io.*;
 
   public class LargestNumberAtLeastTwiceOfOthers {
 	  
-	public static int dominantIndex(int[] array) {
-	  int maxIndex = 0;
-	  // Find the index of the largest number
-	  for (int i = 0; i < array.length; i++) {
-	    if (array[i] > array[maxIndex]) {
-	       maxIndex = i;
-	    }
-	  }
-	  for (int i = 0; i < array.length; ++i) {
-	    if (maxIndex != i && array[maxIndex] < 2 * array[i]) {
-	       return -1;
-	    }
-	  }
-	  return maxIndex;
+    public static int dominantIndex(int[] array) {
+      int maxIndex = 0;
+      // Find the index of the largest number
+      for (int i = 0; i < array.length; i++) {
+        if (array[i] > array[maxIndex]) {
+          maxIndex = i;
+	}
+      }
+      for (int i = 0; i < array.length; ++i) {
+        if (maxIndex != i && array[maxIndex] < 2 * array[i]) {
+	  return -1;
+	}
+      } 
+      return maxIndex;
     }	  
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 		    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
       for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
-		   
-	  
-	  int largestElement = dominantIndex(array);
-	  System.out.println();
-	  System.out.println("The index of the largest element that is at least twice as much as every other number in the array is: " + largestElement);
-	}
+	array[i] = input.nextInt(); 
+      }
+		     
+      int largestElement = dominantIndex(array);
+      System.out.println();
+      System.out.println("The index of the largest element that is at least twice as much as every other number in the array is: " + largestElement);
+    }
   }
