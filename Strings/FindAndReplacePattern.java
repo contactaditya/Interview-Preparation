e22817b4f2883f559bd1782f0df68b0229dc2e46
Aditya Gupta <contactadityag@gmail.com>
2018-09-03 03:26:17 -0400

    This is a program to find out if you a given a list of words and a pattern to know which words in the given list match the pattern.
    A word matches the pattern if there exists a permutation of letters p so that after replacing every letter x in the pattern with p(x), we get the desired word.

--- Strings/FindAndReplacePattern.java
+++ Strings/FindAndReplacePattern.java
@@ -3,21 +3,21 @@ import java.io.*;
 
   public class FindAndReplacePattern {
 	
-	public static List<String> findAndReplacePattern(String[] words, String pattern) {
-	  List<String> result = new ArrayList<String>();
-	  for (String word : words) {
+    public static List<String> findAndReplacePattern(String[] words, String pattern) {
+      List<String> result = new ArrayList<String>();
+      for (String word : words) {
         if (match(word, pattern)) {
           result.add(word);
         }
-	  }
+      }
        
-	  return result;
-	}
+      return result;
+    }
 
-	private static boolean match(String word, String pattern) {
-	  Map<Character, Character> map = new HashMap<Character, Character>();
-	  for (int i = 0; i < word.length(); i++) {
-		char w = word.charAt(i);
+    private static boolean match(String word, String pattern) {
+      Map<Character, Character> map = new HashMap<Character, Character>();
+      for (int i = 0; i < word.length(); i++) {
+	char w = word.charAt(i);
         char p = pattern.charAt(i);  
         if (!map.containsKey(w)) {
           map.put(w, p);  
@@ -25,41 +25,41 @@ import java.io.*;
         if (map.get(w) != p) {
           return false;
         }
-	  }
+      }
 	  
-	  boolean[] seen = new boolean[26];
-	  for (char p : map.values()) {
+      boolean[] seen = new boolean[26];
+      for (char p : map.values()) {
         if (seen[p - 'a']) {
           return false;
         }
         seen[p - 'a'] = true;
       }
 		
-	  return true;
-	}
+      return true;
+    }
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of words: ");
-	  int numbers = input.nextInt();   
-	  String words[] = new String[numbers];
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of words: ");
+      int numbers = input.nextInt();   
+      String words[] = new String[numbers];
 		    
-	  System.out.println();
-	  input.nextLine();
-	  System.out.print("Please enter the actual words: ");
-	  System.out.println();
-	  for (int i = 0; i < numbers; i++) {
-		words[i] = input.nextLine(); 
-	  }
-	  String pattern = new String();	  	
-	  System.out.println();
-	  System.out.print("Enter the pattern: ");
-	  pattern = input.nextLine(); 
+      System.out.println();
+      input.nextLine();
+      System.out.print("Please enter the actual words: ");
+      System.out.println();
+      for (int i = 0; i < numbers; i++) {
+	words[i] = input.nextLine(); 
+      }
+      String pattern = new String();	  	
+      System.out.println();
+      System.out.print("Enter the pattern: ");
+      pattern = input.nextLine(); 
 	  
-	  System.out.println();
+      System.out.println();
 	  
-	  List<String> result = findAndReplacePattern(words, pattern);
-	  System.out.println();
-	  System.out.println("A list of the words in words that match the given pattern are: " + result);
-	}
+      List<String> result = findAndReplacePattern(words, pattern);
+      System.out.println();
+      System.out.println("A list of the words in words that match the given pattern are: " + result);
+    }
   }
