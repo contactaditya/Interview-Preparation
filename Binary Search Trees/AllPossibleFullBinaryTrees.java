94d34c4ee547e31d54deabaa4dcfaf81205a682f
Aditya Gupta <contactadityag@gmail.com>
2018-09-03 03:34:23 -0400

    This is a program to return a list of all the possible full binary trees with N nodes. Each element of the answer is the root node of one possible tree.
    A full binary tree is a binary tree where each node has exactly 0 or 2 children.
    
    Each node of each tree in the answer must have node.val = 0.

--- Binary Search Trees/AllPossibleFullBinaryTrees.java
+++ Binary Search Trees/AllPossibleFullBinaryTrees.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
   public class AllPossibleFullBinaryTrees {
-	static Map<Integer,List<Node>> memo = new HashMap<>();
+    static Map<Integer,List<Node>> memo = new HashMap<>();
 	  
-	public static List<Node> allPossibleFBT(int N) {
-	  if (!memo.containsKey(N)) {	   
-		List<Node> answer = new LinkedList<Node>();   
-		if (N == 1) {
+    public static List<Node> allPossibleFBT(int N) {
+      if (!memo.containsKey(N)) {	   
+	List<Node> answer = new LinkedList<Node>();   
+	if (N == 1) {
           answer.add(new Node(0));
         } else if (N % 2 == 1) {
           for (int x = 0; x < N; x++) {
@@ -22,36 +22,34 @@ import java.io.*;
             }	 
           }
         }
-	    memo.put(N, answer); 
-	  }
+	memo.put(N, answer); 
+      }
 	
-	  return memo.get(N);     
-	}
+      return memo.get(N);     
+    }
 
-	public static void printInorderTraversal(Node node) {	 
-	  if (node == null) {
-		return;
-	  }
+    public static void printInorderTraversal(Node node) {	 
+      if (node == null) {
+	return;
+      }
 		 
       printInorderTraversal(node.left);
-	  System.out.print(node.data + " ");	
-	  printInorderTraversal(node.right);
+      System.out.print(node.data + " ");	
+      printInorderTraversal(node.right);
     }     
 	  
-	public static void main(String[] args) {  
+    public static void main(String[] args) {  
       Scanner input = new Scanner(System.in);
       System.out.print("Enter the number of nodes in the full binary tree: ");
       int N = input.nextInt();
-      
-      
-      
+    
       List<Node> result = new ArrayList<Node>();
       result = allPossibleFBT(N);
       System.out.println();
-	  System.out.println("A list of all possible full binary trees with " + N + " nodes is: ");
-	  for(int i = 0; i < result.size(); i++) {
-		printInorderTraversal(result.get(i));	
-		System.out.println("");
-	  }
-	}
+      System.out.println("A list of all possible full binary trees with " + N + " nodes is: ");
+      for(int i = 0; i < result.size(); i++) {
+	printInorderTraversal(result.get(i));	
+	System.out.println("");
+      }
+    }
   }
