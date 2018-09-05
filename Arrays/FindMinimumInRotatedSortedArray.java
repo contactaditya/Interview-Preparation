a859a8ce0a2c3393a05e2d6a1ab2fa197c245b09
Aditya Gupta <contactadityag@gmail.com>
2018-09-04 22:51:46 -0400

    This is a program to find minimum element in a sorted array which has been rotated at some pivot unknown to you beforehand.

--- Arrays/FindMinimumInRotatedSortedArray.java
+++ Arrays/FindMinimumInRotatedSortedArray.java
@@ -21,49 +21,48 @@ import java.io.*;
     }
 	
     public static void rotate(int[] array, int k) { 
-       if(k > array.length) {
-	 k = k % array.length;
-       }
-       int[] result = new int[array.length];   
+      if(k > array.length) {
+	k = k % array.length;
+      }
+      int[] result = new int[array.length];   
 			    
-       for(int i=0; i<array.length; i++) {
-         result[(i+k)%array.length] = array[i];  
-       }
+      for(int i = 0; i < array.length; i++) {
+        result[(i + k) % array.length] = array[i];  
+      }
     }	
 
     public static void main(String[] args) {   
-       ShuffleAnArray arrayshuffle  = new ShuffleAnArray();	
-       Scanner input = new Scanner(System.in);
-       System.out.print("Enter number of elements in the array: ");
-       int lengthOfArray = input.nextInt();  
-       int array[] = new int[lengthOfArray];
-       System.out.println();	
-       System.out.print("Please enter the elements in the array: ");
-       for(int i=0; i<lengthOfArray; i++) {  
-         array[i] = input.nextInt();
-       }
-       Arrays.sort(array);
-       System.out.println();	
-       System.out.print("Enter the number of rotations in the array: ");
-       int k = input.nextInt();
-       if(k > array.length) {
- 	 k = k % array.length;
-       }
-       int[] result = new int[array.length];   
+      ShuffleAnArray arrayshuffle  = new ShuffleAnArray();	
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int lengthOfArray = input.nextInt();  
+      int array[] = new int[lengthOfArray];
+      System.out.println();	
+      System.out.print("Please enter the elements in the array: ");
+      for(int i=0; i<lengthOfArray; i++) {  
+        array[i] = input.nextInt();
+      }
+      Arrays.sort(array);
+      System.out.println();	
+      System.out.print("Enter the number of rotations in the array: ");
+      int k = input.nextInt();
+      if(k > array.length) {
+ 	k = k % array.length;
+      }
+      int[] result = new int[array.length];   
  		    
-       for(int i=0; i<array.length; i++) {
- 	 result[(i+array.length-k)%array.length] = array[i];  
-       }
-       System.out.println();	
-       System.out.print("The contents of the array after rotation are: ");
+      for(int i=0; i<array.length; i++) {
+ 	result[(i+array.length-k)%array.length] = array[i];  
+      }
+      System.out.println();	
+      System.out.print("The contents of the array after rotation are: ");
 	    
-       for(int i=0; i<array.length; i++) {
-         System.out.print(result[i] + " "); 
-       } 
-       System.out.println();	
-       int findMinimum = findMinimum(result);
-       System.out.println();	
-       System.out.print("The minimum element in the array sorted in ascending order and rotated at some unknown pivot is: " + findMinimum);
-	  
+      for(int i=0; i<array.length; i++) {
+        System.out.print(result[i] + " "); 
+      } 
+      System.out.println();	
+      int findMinimum = findMinimum(result);
+      System.out.println();	
+      System.out.print("The minimum element in the array sorted in ascending order and rotated at some unknown pivot is: " + findMinimum);  
     }
   }
