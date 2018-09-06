5ff90b03e2cb8db9443f6281bc81f22b42b7741c
Aditya Gupta <contactadityag@gmail.com>
2018-09-06 03:37:17 -0400

    This is a program to find the number of possible bitwise ORs of subarrays of all the elements in the array.

--- Bit Manipulation/BitwiseORsOfSubarrays.java
+++ Bit Manipulation/BitwiseORsOfSubarrays.java
@@ -3,8 +3,8 @@ import java.io.*;
 
   public class BitwiseORsOfSubarrays {
 	  
-	public static int subarrayBitwiseORs(int[] array) {
-	  Set<Integer> answer = new HashSet<Integer>();
+    public static int subarrayBitwiseORs(int[] array) {
+      Set<Integer> answer = new HashSet<Integer>();
       Set<Integer> current = new HashSet<Integer>();
       current.add(0);
       for (int x : array) {
@@ -17,22 +17,22 @@ import java.io.*;
         answer.addAll(current);
       }
       
-	  return answer.size();      
-	}
+      return answer.size();      
+    }
 
-	public static void main(String[] args) { 	   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+    public static void main(String[] args) { 	   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();  
+      int array[] = new int[number];
 		
-	  System.out.println();	
-	  System.out.print("Please enter the elements of the array: ");
-	  for (int i = 0; i < array.length; i++) {
-		array[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the elements of the array: ");
+      for (int i = 0; i < array.length; i++) {
+	array[i] = input.nextInt();
+      }
 	  
-	  int result = subarrayBitwiseORs(array); 
-	  System.out.println("The number of possible Bitwise ORs of the Subarrays is: " + result);  
-	}
+      int result = subarrayBitwiseORs(array); 
+      System.out.println("The number of possible Bitwise ORs of the Subarrays is: " + result);  
+    }
   }
