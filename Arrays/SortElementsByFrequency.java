3c3ed934a9263e6f95a6bfb72e78ce6b967f56e3
Aditya Gupta <contactadityag@gmail.com>
2018-09-06 09:48:18 -0400

    This is a program to sort the elements of an array by number of times they are repeated in descending order.

--- Arrays/SortElementsByFrequency.java
+++ Arrays/SortElementsByFrequency.java
@@ -2,11 +2,11 @@ import java.util.*;
 import java.util.Map.Entry;
 import java.io.*;
 
- public class SortElementsByFrequency {  
+  public class SortElementsByFrequency {  
 	
-	public static void sortElementsByFrequency(String[] array) {
-	  Map<String, Integer> map = new HashMap<String, Integer>();	
-	  /* Logic to place the elements to Map */
+    public static void sortElementsByFrequency(String[] array) {
+      Map<String, Integer> map = new HashMap<String, Integer>();	
+      /* Logic to place the elements to Map */
       for(int i = 0; i < array.length; i++) {
         if(map.get(array[i]) == null) {
           map.put(array[i], 1);
@@ -21,10 +21,10 @@ import java.io.*;
        
       /* Sort the list elements based on frequency */
       Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-		@Override
-		public int compare(Entry<String, Integer> object1, Entry<String, Integer> object2) {
-		  return object2.getValue().compareTo(object1.getValue());
-		}
+	@Override
+	public int compare(Entry<String, Integer> object1, Entry<String, Integer> object2) {
+	  return object2.getValue().compareTo(object1.getValue());
+	}
       });
        
       int count = 0;
@@ -32,45 +32,45 @@ import java.io.*;
       /* Place the elements in to the array based on frequency */
       for (Entry<String, Integer> entry : list) {
         String key = entry.getKey();
-  	    int value = entry.getValue();
+  	int value = entry.getValue();
   	            
-  	    for(int i = 0; i < value; i++) {
-  	      array[count] = key;
-  	      count++;
-  	    }
+  	for(int i = 0; i < value; i++) {
+  	  array[count] = key;
+  	  count++;
+  	}
       }
-	}	 
+    }	 
 	 
     public static void printArray(String[] array) {
- 	  for (int i = 0; i < array.length; i++) {
- 		System.out.print(array[i] + " ");
- 	  }  
+      for (int i = 0; i < array.length; i++) {
+ 	System.out.print(array[i] + " ");
+      }  
     }	
 
-	public static void main(String[] args) { 	   	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  String array[] = new String[numbers];
+    public static void main(String[] args) { 	   	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      String array[] = new String[numbers];
 		    
-	  System.out.println();
-	  input.nextLine();
-	  System.out.print("Please enter the actual values in the array: ");
-	  System.out.println();
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextLine(); 
-	  }
+      System.out.println();
+      input.nextLine();
+      System.out.print("Please enter the actual values in the array: ");
+      System.out.println();
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextLine(); 
+      }
 	 
-	  System.out.println();
-	  System.out.print("Array before sorting elements by frequency are: ");
-	  printArray(array);
+      System.out.println();
+      System.out.print("Array before sorting elements by frequency are: ");
+      printArray(array);
 	   
-	  sortElementsByFrequency(array);
+      sortElementsByFrequency(array);
 	   
-	  System.out.println();
-	  System.out.println();
+      System.out.println();
+      System.out.println();
 		
-	  System.out.print("Array after sorting elements by frequency are: ");		
-	  printArray(array);
-	}
+      System.out.print("Array after sorting elements by frequency are: ");		
+      printArray(array);
+    }
   }
