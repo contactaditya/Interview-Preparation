70afd9b43d028cb38c1a78d12e4435c6e62cf4cb
Aditya Gupta <contactadityag@gmail.com>
2018-09-12 00:04:06 -0400

    This is the code for implementing a stack in java using Linked List.

--- Stacks/Stack.java
+++ Stacks/Stack.java
@@ -1,15 +1,13 @@
 import java.io.*;
 import java.util.*;
 
- public class Stack {
-	
+  public class Stack {
     private static LinkedList<Object> stack = new LinkedList<Object>();	
 	
     public static void push(Object object) {
      /**
       * Add an item to back of stack.
-      */
-		   
+      */  
       stack.addFirst(object);
     }
 
@@ -61,7 +59,6 @@ import java.util.*;
      /**
       * Returns the front item from the stack without popping it.
       */	 
-		  
       if(stack.isEmpty()) {  
         System.out.println("The stack is empty so we can't see the last item of the stack.");  
         return -1;
@@ -74,41 +71,40 @@ import java.util.*;
     }
 
     public static void main(String[] args) throws NumberFormatException, IOException {
-		  
-       int choice = 0;
+      int choice = 0;
 	    
-       do{	
-           System.out.println("1. For inserting an element in the stack.");
-           System.out.println("2. For deleting an element from the stack.");
-       	   System.out.println("3. For displaying the contents of the stack.");
-           System.out.println("4. For checking the contents of the stack are empty or not");
-           System.out.println("5. For checking the size of the stack");
-           System.out.println("6. For returning the last element from the stack without popping it.");
-           System.out.println("7. For exiting from the program.");
+      do{	
+          System.out.println("1. For inserting an element in the stack.");
+          System.out.println("2. For deleting an element from the stack.");
+       	  System.out.println("3. For displaying the contents of the stack.");
+          System.out.println("4. For checking the contents of the stack are empty or not");
+          System.out.println("5. For checking the size of the stack");
+          System.out.println("6. For returning the last element from the stack without popping it.");
+          System.out.println("7. For exiting from the program.");
 	       
-	   System.out.println(); 	
-           System.out.print("Enter your choice: ");
-           BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
-           choice = Integer.parseInt(br.readLine());
-           switch (choice) {
-             case 1:  System.out.println(); 
-		      System.out.print("Enter a element to insert in the stack: ");
-	              BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));  	
-	              String element = br1.readLine();
-	     	      push(element);
-	              break;
-    	     case 2:  pop();
-	     	      break;
-    	     case 3:  display();
-	     	      break;
-    	     case 4:  isEmpty();
-	     	      break;
-             case 5:  size();
-	              break;  
-             case 6:  peek();
-	              break;     
-           }
-           System.out.print("\n");
-         } while(choice!=7);
-    }
- }
+	  System.out.println(); 	
+          System.out.print("Enter your choice: ");
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+          choice = Integer.parseInt(br.readLine());
+          switch (choice) {
+            case 1:  System.out.println(); 
+		     System.out.print("Enter a element to insert in the stack: ");
+	             BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));  	
+	             String element = br1.readLine();
+	     	     push(element);
+	             break;
+    	    case 2:  pop();
+	     	     break;
+    	    case 3:  display();
+	     	     break;
+    	    case 4:  isEmpty();
+	     	     break;
+            case 5:  size();
+	             break;  
+            case 6:  peek();
+	             break;     
+          }
+          System.out.println();
+        } while(choice!=7);
+     }
+  }
