7e5cb38d4e2651f70d6c09eb7b3e54519c9d0cb0
contactaditya <contactadityag@gmail.com>
2018-09-21 04:12:45 -0400

    This is a program to find the length of maximum consecutive repeating character in the string.

---
+++ Strings/LongestUniformSubstring.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+  public class LongestUniformSubstring {
+	  
+	public static int lengthOfUniformSubstring(String string) {
+      char result[] = string.toCharArray();
+      int count = 1;
+      int maxCount = 0;
+      
+      for(int i = 0; i < result.length - 1; i++) {
+    	if(result[i] == result[i+1]) {
+    	  count++;	
+    	}
+    	else {
+    	  count = 1;	
+    	}
+    	if(count > maxCount) {
+    	  maxCount = count;	
+    	}
+      }
+
+	  return maxCount;
+	}
+	
+	public static void main(String[] args) {   
+	  String string = new String();	  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the string: ");
+	  string = input.next(); 
+	  int length = lengthOfUniformSubstring(string);
+	  System.out.println();
+	  System.out.print("The length of the maximum consecutive repeating character in string is: " + length);
+	}
+  }
