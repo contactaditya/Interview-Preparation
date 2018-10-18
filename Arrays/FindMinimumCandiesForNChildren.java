d42731b9532d6f5cee58b1a9f5cce66ad234139a
Aditya Gupta <contactadityag@gmail.com>
2018-10-18 02:59:49 -0400

    This is a program to find the minimum candies you must give in a line containing N children standing in a line. Each child is assigned a rating value.

--- Arrays/FindMinimumCandiesForNChildren.java
+++ Arrays/FindMinimumCandiesForNChildren.java
@@ -3,27 +3,27 @@ import java.io.*;
 
   public class FindMinimumCandiesForNChildren {
 	  
-	public static int candy(ArrayList<Integer> ratings) { 	 
-	  int[] candies = new int[ratings.size()];
-	  Arrays.fill(candies, 1);
-	  for (int i = 1; i < ratings.size(); i++) {
-	    if(ratings.get(i) > ratings.get(i - 1)) {
-	      candies[i] = candies[i - 1] + 1;	
-	    }
-	  }
-	  int sum = candies[ratings.size() - 1];
-	  for (int i = ratings.size() - 2; i >= 0; i--) {
-	    if(ratings.get(i) > ratings.get(i + 1)) {
-		  candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-		}
-	    sum += candies[i];
-	  }
+    public static int candy(ArrayList<Integer> ratings) { 	 
+      int[] candies = new int[ratings.size()];
+      Arrays.fill(candies, 1);
+      for (int i = 1; i < ratings.size(); i++) {
+        if(ratings.get(i) > ratings.get(i - 1)) {
+	  candies[i] = candies[i - 1] + 1;	
+	}
+      }
+      int sum = candies[ratings.size() - 1];
+      for (int i = ratings.size() - 2; i >= 0; i--) {
+        if(ratings.get(i) > ratings.get(i + 1)) {
+	  candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+	}
+	sum += candies[i];
+      }
 				
-	  return sum;
+      return sum;
     }  
 
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	 
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of children: ");
       int number = input.nextInt();  
       ArrayList<Integer> ratings = new ArrayList<Integer>();	
@@ -34,9 +34,9 @@ import java.io.*;
     	ratings.add(input.nextInt());
       }
 		  
-	  int minimumNumberOfCandies = candy(ratings); 
-	  System.out.println();	
-	  System.out.print("The minimum candies that must be given to children in the line is: " + minimumNumberOfCandies);
-	  input.close();
-	}
+      int minimumNumberOfCandies = candy(ratings); 
+      System.out.println();	
+      System.out.print("The minimum candies that must be given to children in the line is: " + minimumNumberOfCandies);
+      input.close();
+    }
   }
