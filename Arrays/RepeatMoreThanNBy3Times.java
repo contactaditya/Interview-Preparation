1ce764a108b9f803bee9c243b149757d9f40a0e1
Aditya Gupta <contactadityag@gmail.com>
2018-10-18 02:56:48 -0400

    This is a progam to find if any integer occurs more than n/3 times in the array in linear time and constant additional space.

--- Arrays/RepeatMoreThanNBy3Times.java
+++ Arrays/RepeatMoreThanNBy3Times.java
@@ -3,9 +3,9 @@ import java.util.*;
 
   public class RepeatMoreThanNBy3Times {
 	
-	public static int repeatedNumber(final List<Integer> list) {
-	  int count1 = 0, count2 = 0;
-	  int first = Integer.MAX_VALUE; 
+    public static int repeatedNumber(final List<Integer> list) {
+      int count1 = 0, count2 = 0;
+      int first = Integer.MAX_VALUE; 
       int second = Integer.MAX_VALUE; 
       
       for (int i = 0; i < list.size(); i++) {
@@ -41,16 +41,15 @@ import java.util.*;
       if (count1 > list.size() / 3) { 
         return first; 
       }
-    
       if (count2 > list.size() / 3) { 
         return second; 
       }
 	  
-	  return -1;
-	}
+      return -1;
+    }
 
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	 
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the list: ");
       int number = input.nextInt();  
       ArrayList<Integer> list = new ArrayList<Integer>();	
@@ -58,12 +57,12 @@ import java.util.*;
       System.out.println();	
       System.out.print("Please enter the elements of the list: ");
       for (int i = 0; i < number; i++) {
-	    list.add(input.nextInt());
+	list.add(input.nextInt());
       }
 		  
-	  int repeatedNumber = repeatedNumber(list); 
-	  System.out.println();	
-	  System.out.print("The integer that occurs more than n/3 times in the array is: " + repeatedNumber);
-	  input.close();
-	}
+      int repeatedNumber = repeatedNumber(list); 
+      System.out.println();	
+      System.out.print("The integer that occurs more than n/3 times in the array is: " + repeatedNumber);
+      input.close();
+    }
   }
