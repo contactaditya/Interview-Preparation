603274bea2fb23e6d1aa8c3f8898f98aaf5ee8cc
Aditya Gupta <contactadityag@gmail.com>
2018-10-20 21:43:07 -0400

    This is a program to return the minimum number of parentheses we must add to make the resulting string valid.
    Formally, a parentheses string is valid if and only if:
    
    It is the empty string, or
    
    It can be written as AB (A concatenated with B), where A and B are valid strings, or
    
    It can be written as (A), where A is a valid string.

--- Strings/MinimumAddToMakeParenthesesValid.java
+++ Strings/MinimumAddToMakeParenthesesValid.java
@@ -3,11 +3,11 @@ import java.util.*;
 
   public class MinimumAddToMakeParenthesesValid {
 	  
-	public static int minAddToMakeValid(String string) {
-	  if (string == null || string.length() == 0) {
+    public static int minAddToMakeValid(String string) {
+      if (string == null || string.length() == 0) {
         return 0;
-	  }
-	  int changes = 0;
+      }
+      int changes = 0;
       int count = 0;
       for (int i = 0; i < string.length(); i++) {
         char c = string.charAt(i);
@@ -15,24 +15,24 @@ import java.util.*;
           count++;
         } else {
           if (count > 0) {
-        	count--;
+            count--;
           } else {
             changes++;
           }	
         }
       }
       return changes + count;
-	}
-
-	public static void main(String[] args) { 
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine();
+    }
+ 
+    public static void main(String[] args) { 
+      String string = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the string: ");
+      string = input.nextLine();
 		  
-	  int result = minAddToMakeValid(string);
-	  System.out.println();
-	  System.out.println("The minimum number of parentheses we must add to make the resulting string valid are: " + result);
-	  input.close();
-	}
+      int result = minAddToMakeValid(string);
+      System.out.println();
+      System.out.println("The minimum number of parentheses we must add to make the resulting string valid are: " + result);
+      input.close();
+    }
   }
