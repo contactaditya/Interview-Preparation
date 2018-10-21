753bcff43926ee1750c7dbdc5e9edc9d8a92134b
contactaditya <contactadityag@gmail.com>
2018-10-20 21:22:21 -0400

    This is a program to return the minimum number of parentheses we must add to make the resulting string valid.
    Formally, a parentheses string is valid if and only if:
    
    It is the empty string, or
    
    It can be written as AB (A concatenated with B), where A and B are valid strings, or
    
    It can be written as (A), where A is a valid string.

---
+++ Strings/MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+  public class MinimumAddToMakeParenthesesValid {
+	  
+	public static int minAddToMakeValid(String string) {
+	  if (string == null || string.length() == 0) {
+        return 0;
+	  }
+	  int changes = 0;
+      int count = 0;
+      for (int i = 0; i < string.length(); i++) {
+        char c = string.charAt(i);
+        if (c == '(') {
+          count++;
+        } else {
+          if (count > 0) {
+        	count--;
+          } else {
+            changes++;
+          }	
+        }
+      }
+      return changes + count;
+	}
+
+	public static void main(String[] args) { 
+	  String string = new String();	  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the string: ");
+	  string = input.nextLine();
+		  
+	  int result = minAddToMakeValid(string);
+	  System.out.println();
+	  System.out.println("The minimum number of parentheses we must add to make the resulting string valid are: " + result);
+	  input.close();
+	}
+  }
