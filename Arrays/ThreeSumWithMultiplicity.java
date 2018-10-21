3dd1bf08754fa6518c9507b6ca8a48a69c1d3a6c
Aditya Gupta <contactadityag@gmail.com>
2018-10-20 21:46:28 -0400

    This is a program to return the number of tuples i, j, k such that i < j < k and A[i] + A[j] + A[k] == target.

--- Arrays/ThreeSumWithMultiplicity.java
+++ Arrays/ThreeSumWithMultiplicity.java
@@ -3,50 +3,50 @@ import java.util.*;
 
   public class ThreeSumWithMultiplicity {
 	
-	public static int threeSumMulti(int[] array, int target) {
-	  int MOD = 1_000_000_007;
-	  long[] count = new long[101];
-	  for (int x : array) {
-		count[x]++;
-	  }
-	  long result = 0;
+    public static int threeSumMulti(int[] array, int target) {
+      int MOD = 1_000_000_007;
+      long[] count = new long[101];
+      for (int x : array) {
+	count[x]++;
+      }
+      long result = 0;
 	  
-	  for (int i = 0; i <= 100; i++) {
-	    for (int j = i; j <= 100; j++) {	  
-	      int k = target - i - j;
-	      if (k > 100 || k < 0) {
-	    	continue;
-	      }
-	      if (i == j && j == k) {
-	    	result += count[i] * (count[i] - 1) * (count[i] - 2) / 6;
+      for (int i = 0; i <= 100; i++) {
+	for (int j = i; j <= 100; j++) {	  
+	  int k = target - i - j;
+	  if (k > 100 || k < 0) {
+	    continue;
+	  }
+	  if (i == j && j == k) {
+	    result += count[i] * (count[i] - 1) * (count[i] - 2) / 6;
           } else if (i == j && j != k) {
-        	result += count[i] * (count[i] - 1) / 2 * count[k];  
+            result += count[i] * (count[i] - 1) / 2 * count[k];  
           } else if (j < k) {
-        	result += count[i] * count[j] * count[k];
+            result += count[i] * count[j] * count[k];
           }
-		}  
-	  }
+	}  
+      }
 
-	  return (int) (result % MOD);     
-	}
+      return (int) (result % MOD);     
+    }
 
-	public static void main(String[] args) { 	
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 		    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-	    array[i] = input.nextInt(); 
-	  }
-	  System.out.println();
-	  System.out.print("Enter the target sum: ");
-	  int target = input.nextInt();   
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt(); 
+      } 
+      System.out.println();
+      System.out.print("Enter the target sum: ");
+      int target = input.nextInt();   
 	   
-	  int result = threeSumMulti(array, target);
-	  System.out.println();
-	  System.out.print("The number of tuples i, j, k such that i < j < k and array[i] + array[j] + array[k] == target are: " + result);
-	}
+      int result = threeSumMulti(array, target);
+      System.out.println();
+      System.out.print("The number of tuples i, j, k such that i < j < k and array[i] + array[j] + array[k] == target are: " + result);
+    }
   }
