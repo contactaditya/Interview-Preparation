b4dffa8d25d41445475f2e942b461952a0bba7e9
Aditya Gupta <contactadityag@gmail.com>
2018-10-22 04:40:36 -0400

    This is a program to return a list of numbers less than a number which are product of distinct prime numbers.

--- Numbers/ProductsOfDistinctPrimeNumbers.java
+++ Numbers/ProductsOfDistinctPrimeNumbers.java
@@ -3,45 +3,45 @@ import java.util.*;
 
   public class ProductsOfDistinctPrimeNumbers {
 	   
-	public static List<Integer> primeProducts(int number) {
-	  List<Integer> result = new ArrayList<Integer>();
-	  List<Integer> primeNumbers = new ArrayList<Integer>();
-	  for(int i = 0; i < number; i++) {
-		if(isPrime(i)) {
-		  primeNumbers.add(i);    
-		}
+    public static List<Integer> primeProducts(int number) {
+      List<Integer> result = new ArrayList<Integer>();
+      List<Integer> primeNumbers = new ArrayList<Integer>();
+      for(int i = 0; i < number; i++) {
+	if(isPrime(i)) {
+	  primeNumbers.add(i);    
+	}
       }
-	  for(int i = 0; i < primeNumbers.size(); i++) {
-		for (int j = i + 1; j < primeNumbers.size(); j++) { 
-		  if(primeNumbers.get(i) * primeNumbers.get(j) < number) {
-		    result.add(primeNumbers.get(i) * primeNumbers.get(j));    
-		  }	
-	    }
-	  }
+      for(int i = 0; i < primeNumbers.size(); i++) {
+	for (int j = i + 1; j < primeNumbers.size(); j++) { 
+	  if(primeNumbers.get(i) * primeNumbers.get(j) < number) {
+	    result.add(primeNumbers.get(i) * primeNumbers.get(j));    
+	  }	
+	}
+      }
 	  
-	  Collections.sort(result); 
-	  return result;
-	}
+      Collections.sort(result); 
+      return result;
+    }
 	
-	public static boolean isPrime(int number) { 	
-	  if(number < 2) {
-	    return false;  
-	  }
-	  for(int i = 2; i <= Math.sqrt(number); i++) {
-	    if(number % i == 0) {
-	      return false;
-	    }
-	  }
-	  return true;
+    public static boolean isPrime(int number) { 	
+      if(number < 2) {
+	return false;  
+      }
+      for(int i = 2; i <= Math.sqrt(number); i++) {
+        if(number % i == 0) {
+	  return false;
 	}
+      }
+      return true;
+    }
 
-	public static void main(String[] args) { 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the number: ");
-	  int number = input.nextInt();   
-	  List<Integer> result = new ArrayList<Integer>();
-	  result = primeProducts(number);
-	  System.out.println();
-	  System.out.println("The list of numbers less than " + number + " which are a product of distinct prime numbers is: " + result);
-	} 
+    public static void main(String[] args) { 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int number = input.nextInt();   
+      List<Integer> result = new ArrayList<Integer>();
+      result = primeProducts(number);
+      System.out.println();
+      System.out.println("The list of numbers less than " + number + " which are a product of distinct prime numbers is: " + result);
+    } 
   }
