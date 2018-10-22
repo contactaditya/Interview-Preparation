e41f0cbb0d387e89f79a1be5e862a82487877579
Aditya Gupta <contactadityag@gmail.com>
2018-10-21 20:06:19 -0400

    his is a program to check if a singly linked list is palindrome.

--- Linked List/IsLinkedListAPalindrome.java
+++ Linked List/IsLinkedListAPalindrome.java
@@ -1,14 +1,14 @@
 import java.util.*;
 import java.io.*;
 
-  public class IsLinkedListAPalindrome { 
+   public class IsLinkedListAPalindrome { 
      private static LinkedListNode head;
      private static int size;  
 
      public IsLinkedListAPalindrome() {
-        // this is an empty list, so the reference to the head node is set to a new node with no data				  
-        head = new LinkedListNode(null);
-        size = 0;
+       // this is an empty list, so the reference to the head node is set to a new node with no data				  
+       head = new LinkedListNode(null);
+       size = 0;
      }
 	
      public static boolean isPalindrome(LinkedListNode node) {
@@ -18,26 +18,24 @@ import java.io.*;
        java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
 	   
        while(fast != null && fast.next != null) {
-	  stack.push((Integer) slow.data);
-          slow = slow.next;
-          fast = fast.next.next;		   
+	 stack.push((Integer) slow.data);
+         slow = slow.next;
+         fast = fast.next.next;		   
        }
 	   
-       // Has odd numbers of elements, so skip the middle elements
-	   
+       // Has odd numbers of elements, so skip the middle elements  
        if(fast != null) {
-          slow = slow.next;
+         slow = slow.next;
        }
 	   
        while(slow != null) {
-	  int top = stack.pop().intValue(); 
+	 int top = stack.pop().intValue(); 
 		
-	  // If values are different, then it's not a palindrome
-		 
-	  if(top != (int)slow.data) {
-	     return false;
-	  }
-	  slow = slow.next;  
+	 // If values are different, then it's not a palindrome 
+	 if(top != (int)slow.data) {
+	   return false;
+	 }
+	 slow = slow.next;  
        }
 	   
        return true;   
@@ -51,8 +49,7 @@ import java.io.*;
        list.head.next.next.next = new LinkedListNode(1);
        list.head.next.next.next.next = new LinkedListNode(0);
 		     	     
-       boolean IsAPalindrome = list.isPalindrome(head);
-		  
+       boolean IsAPalindrome = list.isPalindrome(head);	  
        if(IsAPalindrome) {
 	 System.out.println("The linkedlist is a palindrome.");  
        }
@@ -60,4 +57,4 @@ import java.io.*;
 	 System.out.println("The linkedlist is not a palindrome.");    
        }	
      }
-  }
+   }
