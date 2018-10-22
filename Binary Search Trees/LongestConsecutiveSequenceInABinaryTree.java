1ac43b0c6e2e6653e7c1c583f64177ebf8ba2fe2
Aditya Gupta <contactadityag@gmail.com>
2018-10-22 04:43:51 -0400

    This is a program to find the length of the longest path in a Binary Tree which comprises of nodes with consecutive values in increasing order.  Every node is considered as a path of length 1.

--- Binary Search Trees/LongestConsecutiveSequenceInABinaryTree.java
+++ Binary Search Trees/LongestConsecutiveSequenceInABinaryTree.java
@@ -2,44 +2,44 @@ import java.io.*;
 import java.util.*;
 
   public class LongestConsecutiveSequenceInABinaryTree {
-	Node root;	    
-	Node previous;
+    Node root;	    
+    Node previous;
 	
-	public int longestConsecutiveUtil(Node node, int currentLength, int target) {
-	  if (node == null) {
-		return 0;
-	  }
-	  // If root data has one more than its parent then increase current length
-	  if (node.data == target) {
-		currentLength++;  
-	  }
-	  else {
-		currentLength = 1; 
-	  }
+    public int longestConsecutiveUtil(Node node, int currentLength, int target) {
+      if (node == null) {
+	return 0;
+      }
+      // If root data has one more than its parent then increase current length
+      if (node.data == target) {
+	currentLength++;  
+      }
+      else {
+	currentLength = 1; 
+      }
 	   
-	  int left = longestConsecutiveUtil(node.left, currentLength, node.data + 1);
+      int left = longestConsecutiveUtil(node.left, currentLength, node.data + 1);
       int right = longestConsecutiveUtil(node.right, currentLength, node.data + 1);
        
       return Math.max(currentLength, Math.max(left, right));
-	}
+    }
 
-	public int longestConsecutiveSequence(Node node) {
-	  if (node == null) {
-		return 0;
-	  }
+    public int longestConsecutiveSequence(Node node) {
+      if (node == null) {
+	return 0;
+      }
 	   
-	  return longestConsecutiveUtil(node, 0, node.data + 1);
-	}
+      return longestConsecutiveUtil(node, 0, node.data + 1);
+    }
 
-	public static void main(String[] args) {
-	  LongestConsecutiveSequenceInABinaryTree tree = new LongestConsecutiveSequenceInABinaryTree();
-	  tree.root = new Node(6);
-	  tree.root.left = new Node(7);
-	  tree.root.right = new Node(9);
-	  tree.root.right.right = new Node(10);
-	  tree.root.right.right.right = new Node(11);
+    public static void main(String[] args) {
+      LongestConsecutiveSequenceInABinaryTree tree = new LongestConsecutiveSequenceInABinaryTree();
+      tree.root = new Node(6);
+      tree.root.left = new Node(7);
+      tree.root.right = new Node(9);
+      tree.root.right.right = new Node(10);
+      tree.root.right.right.right = new Node(11);
 	  
-	  int length = tree.longestConsecutiveSequence(tree.root);  
-	  System.out.print("The length of the longest path which comprises of nodes with consecutive values in increasing order is: " + length);
-	}
+      int length = tree.longestConsecutiveSequence(tree.root);  
+      System.out.print("The length of the longest path which comprises of nodes with consecutive values in increasing order is: " + length);
+    }
   }
