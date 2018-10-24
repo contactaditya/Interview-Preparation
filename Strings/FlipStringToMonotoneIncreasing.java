9283d5f28535105fc098a76c7409920491ebbe29
Aditya Gupta <contactadityag@gmail.com>
2018-10-24 19:54:30 -0400

    This is a program to return the minimum number of flips to make the string S monotone increasing.
    A string of '0's and '1's is monotone increasing if it consists of some number of '0's (possibly 0), followed by some number of '1's (also possibly 0).
    
    We are given a string S of '0's and '1's, and we may flip any '0' to a '1' or a '1' to a '0'.

--- Strings/FlipStringToMonotoneIncreasing.java
+++ Strings/FlipStringToMonotoneIncreasing.java
@@ -3,35 +3,35 @@ import java.io.*;
 
   public class FlipStringToMonotoneIncreasing {
 	  
-	public static int minFlipsMonoIncreasing(String string) {
-	  int ldp[] = new int[string.length()], rdp[] = new int[string.length()];
-	  char s[] = string.toCharArray();
-	  int l = 0, r = 0;
-	  for (int i = 0, j = s.length - 1; i < s.length; i++, j--) {
-	    ldp[i] = l;
+    public static int minFlipsMonoIncreasing(String string) {
+      int ldp[] = new int[string.length()], rdp[] = new int[string.length()];
+      char s[] = string.toCharArray();
+      int l = 0, r = 0;
+      for (int i = 0, j = s.length - 1; i < s.length; i++, j--) {
+	ldp[i] = l;
         rdp[j] = r;
-	    if (s[i] == '1') {
-	      l++;
-	    }
+	if (s[i] == '1') {
+	  l++;
+	}
         if (s[j] == '0') {
           r++;
         }
-	  }
-	  int result = Integer.MAX_VALUE;
-	  for (int i = 0; i < s.length; i++) {
+      }
+      int result = Integer.MAX_VALUE;
+      for (int i = 0; i < s.length; i++) {
         result = Math.min(result, ldp[i] + rdp[i]);
       }
-	  return result;
-	}
+      return result;
+    }
 
-	public static void main(String[] args) {   
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.next(); 
-	  int length = minFlipsMonoIncreasing(string);
-	  System.out.println();
-	  System.out.print("The minimum number of flips to make " + string + " monotone increasing are: " + length);
-	  input.close();
-	}
+    public static void main(String[] args) {   
+      String string = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the string: ");
+      string = input.next(); 
+      int length = minFlipsMonoIncreasing(string);
+      System.out.println();
+      System.out.print("The minimum number of flips to make " + string + " monotone increasing are: " + length);
+      input.close();
+    }
   } 
