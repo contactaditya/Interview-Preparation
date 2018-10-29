8971d77a6b65ac4742fdd8cbd3a37427859d75cb
Aditya Gupta <contactadityag@gmail.com>
2018-10-29 17:42:59 -0400

    This is a program to split the array in two subarrays (without reordering the elements), such that the sum of the two subarrays is the same.

--- Arrays/SplitAnArrayIntoTwoEqualSumSubarrays.java
+++ Arrays/SplitAnArrayIntoTwoEqualSumSubarrays.java
@@ -3,58 +3,58 @@ import java.io.*;
 
   public class SplitAnArrayIntoTwoEqualSumSubarrays { 
 	  
-	private static int findSplitPoint(int[] array, int n) {
-	  int leftSum = 0; 
+    private static int findSplitPoint(int[] array, int n) {
+      int leftSum = 0; 
 	      
-	  for (int i = 0; i < n; i++) { 
-	    leftSum += array[i]; 
-	  }
+      for (int i = 0; i < n; i++) { 
+	leftSum += array[i]; 
+      }
 	  
-	  int rightSum = 0;
-	  for (int i = n-1; i >= 0; i--) { 
-	    // Add current element to the right sum 
-	    rightSum += array[i]; 
+      int rightSum = 0;
+      for (int i = n-1; i >= 0; i--) { 
+	// Add current element to the right sum 
+	rightSum += array[i]; 
 	  
-	    // Exclude current element to the left sum 
-	    leftSum -= array[i] ; 
+	// Exclude current element to the left sum 
+	leftSum -= array[i] ; 
 	  
-	    if (rightSum == leftSum) {
-	      return i; 
-	    }
-	  } 
+	if (rightSum == leftSum) {
+	  return i; 
+        }
+      } 
 	    
-	  return -1;
+      return -1;
     }
 	  
-	private static void printTwoParts(int[] array, int n) {
-	  int splitPoint = findSplitPoint(array, array.length); 	
-	  if (splitPoint == -1 || splitPoint == n) { 
+    private static void printTwoParts(int[] array, int n) { 
+      int splitPoint = findSplitPoint(array, array.length); 	
+      if (splitPoint == -1 || splitPoint == n) { 
         System.out.println("Not Possible"); 
         return; 
       }
-	  System.out.println();
-	  for (int i = 0; i < n; i++) {
+      System.out.println();
+      for (int i = 0; i < n; i++) {
         if(splitPoint == i) { 
           System.out.println();
           System.out.println();
         }        
         System.out.print(array[i] + " "); 
       } 
-	}
+    }
 
-	public static void main(String[] args) { 	
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 	    
-	  System.out.println();	
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+      System.out.println();	 
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt(); 
+      }
 	  	  
-	  printTwoParts(array, array.length);
+      printTwoParts(array, array.length);
       input.close();
-	}
+    }
   }
