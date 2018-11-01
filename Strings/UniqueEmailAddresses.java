2a346adff910517191bae09abac21d9dfd4993ea
Aditya Gupta <contactadityag@gmail.com>
2018-10-31 20:03:57 -0400

    This is a program to find the number of different email addresses that actually receive mails.

--- Strings/UniqueEmailAddresses.java
+++ Strings/UniqueEmailAddresses.java
@@ -3,16 +3,16 @@ import java.io.*;
 
   public class UniqueEmailAddresses {
 	  
-	public static int numUniqueEmails(String[] emails) {
-	  Set<String> set = new HashSet<String>();
-	  for (String email : emails) {
-	    set.add(transform(email));
-	  }
-	  return set.size();      
-	}
+    public static int numUniqueEmails(String[] emails) {
+      Set<String> set = new HashSet<String>();
+      for (String email : emails) {
+	set.add(transform(email));
+      }
+      return set.size();      
+    }
 
-	private static String transform(String email) {  
-	  int a = email.indexOf('@');
+    private static String transform(String email) {  
+      int a = email.indexOf('@');
       StringBuilder sb = new StringBuilder();
       for (int i = 0; i < a; i++) {
         char c = email.charAt(i);  
@@ -21,16 +21,16 @@ import java.io.*;
             sb.append(email.substring(a));
             break;  
           } else {
-        	sb.append(c);  
+            sb.append(c);  
           }
         }
       }
 
-	  return sb.toString();
-	}
+      return sb.toString();
+    }
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of email addresses: ");
       int numbers = input.nextInt();   
       String emails[] = new String[numbers];
@@ -40,12 +40,12 @@ import java.io.*;
       System.out.print("Please enter the actual email addresses: ");
       System.out.println();
       for (int i = 0; i < numbers; i++) {
-	    emails[i] = input.nextLine(); 
+	emails[i] = input.nextLine(); 
       }
     
       int result = numUniqueEmails(emails);
       System.out.println();
       System.out.println("The number of different email addresses that actually receive mails are: " + result);
       input.close();
-	}
+    }
   }
