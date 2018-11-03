ea12db2d5f72b8018dd2104cfe49f6a6b1b27554
Aditya Gupta <contactadityag@gmail.com>
2018-11-03 19:48:27 -0400

    This is a program to find the length of the longest path where each node in the path has the same value in a Binary Tree.
    The length of path between two nodes is represented by the number of edges between them.

--- Binary Search Trees/LongestUnivaluePath.java
+++ Binary Search Trees/LongestUnivaluePath.java
@@ -7,37 +7,37 @@ import java.io.*;
    int length = 0; 
  
    public int longestUnivaluePath(Node root) {
-      if (root == null) {
-	 return 0;
-      }
-      length = 0;
-      getLength(root, root.data);
-      return length;	        
+     if (root == null) {
+       return 0;
+     }
+     length = 0;
+     getLength(root, root.data);
+     return length;	        
    }	 
 
    private int getLength(Node node, int value) {
-      if (node == null) {
-	return 0;
-      }
-      int left = getLength(node.left, node.data);
-      int right = getLength(node.right, node.data);
-      length = Math.max(length, left + right);
-      if (value == node.data) {
-	return Math.max(left, right) + 1;
-      }
-      return 0;	
+     if (node == null) {
+       return 0;
+     }
+     int left = getLength(node.left, node.data);
+     int right = getLength(node.right, node.data);
+     length = Math.max(length, left + right);
+     if (value == node.data) {
+       return Math.max(left, right) + 1;
+     }
+     return 0;	
    }
 
    public static void main(String[] args) { 
-      LongestUnivaluePath tree = new LongestUnivaluePath();
-      tree.root = new Node(5);
-      tree.root.left = new Node(4);
-      tree.root.right = new Node(5);
-      tree.root.left.left = new Node(1);
-      tree.root.right.right = new Node(5);
-      tree.root.left.right = new Node(1);
+     LongestUnivaluePath tree = new LongestUnivaluePath();
+     tree.root = new Node(5);
+     tree.root.left = new Node(4);
+     tree.root.right = new Node(5);
+     tree.root.left.left = new Node(1);
+     tree.root.right.right = new Node(5);
+     tree.root.left.right = new Node(1);
 
-      int lengthOfLongestPath = tree.longestUnivaluePath(tree.root);
-      System.out.print("The length of the longest path where each node in the path has the same value is: " + lengthOfLongestPath);
+     int lengthOfLongestPath = tree.longestUnivaluePath(tree.root);
+     System.out.print("The length of the longest path where each node in the path has the same value is: " + lengthOfLongestPath);
    }
  }
