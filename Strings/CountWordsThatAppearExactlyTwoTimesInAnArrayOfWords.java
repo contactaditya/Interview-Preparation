b7f1b7ad0ba755a37f84ea03ee6e80bed2612ff6
Aditya Gupta <contactadityag@gmail.com>
2018-11-03 20:34:46 -0400

    This is a program to count words that appear exactly two times in an array of words.

--- Strings/CountWordsThatAppearExactlyTwoTimesInAnArrayOfWords.java
+++ Strings/CountWordsThatAppearExactlyTwoTimesInAnArrayOfWords.java
@@ -3,43 +3,43 @@ import java.io.*;
 
   public class CountWordsThatAppearExactlyTwoTimesInAnArrayOfWords {
 	  
-	public static int countWords(String string[], int n) { 
-	  HashMap<String, Integer> map = new HashMap<String, Integer>(); 
-	  for (int i = 0; i < n; i++) {
-	    if(map.containsKey(string[i])) { 
+    public static int countWords(String string[], int n) { 
+      HashMap<String, Integer> map = new HashMap<String, Integer>(); 
+      for (int i = 0; i < n; i++) {
+	if(map.containsKey(string[i])) { 
           int get = map.get(string[i]); 
           map.put(string[i], get + 1); 
         } else {
           map.put(string[i], 1); 
         }
-	  }
-	  int result = 0;
-	  for (Map.Entry<String, Integer> it : map.entrySet()) { 
+      }
+      int result = 0;
+      for (Map.Entry<String, Integer> it : map.entrySet()) { 
         if(it.getValue() == 2) { 
           result++; 
         }
       } 
 	  
-	  return result;  
+      return result;  
     }
 
-	public static void main(String[] args) {  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of words: ");
-	  int numbers = input.nextInt();   
-	  String words[] = new String[numbers];
+    public static void main(String[] args) {  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of words: ");
+      int numbers = input.nextInt();   
+      String words[] = new String[numbers];
 		    
-	  System.out.println();
-	  input.nextLine();
-	  System.out.print("Please enter the actual words: ");
-	  System.out.println();
-	  for (int i = 0; i < numbers; i++) {
-		words[i] = input.nextLine(); 
-	  }
+      System.out.println();
+      input.nextLine();
+      System.out.print("Please enter the actual words: ");
+      System.out.println();
+      for (int i = 0; i < numbers; i++) {
+	words[i] = input.nextLine(); 
+      }
 	  			  
-	  int result = countWords(words, words.length);
-	  System.out.println();
-	  System.out.println("The words that are repeated exactly twice are: " + result);
-	  input.close();
-	}
+      int result = countWords(words, words.length);
+      System.out.println();
+      System.out.println("The words that are repeated exactly twice are: " + result);
+      input.close();
+    }
   }
