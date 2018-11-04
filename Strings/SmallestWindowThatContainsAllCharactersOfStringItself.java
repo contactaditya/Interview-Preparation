da86f5c56abd1651de5962ce7bfe830e3c573c1f
Aditya Gupta <contactadityag@gmail.com>
2018-11-03 20:31:18 -0400

    This is a program to find the smallest window length with all distinct characters of the given string.

--- Strings/SmallestWindowThatContainsAllCharactersOfStringItself.java
+++ Strings/SmallestWindowThatContainsAllCharactersOfStringItself.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
   public class SmallestWindowThatContainsAllCharactersOfStringItself { 	  
-	static final int MAX_CHARS = 256; 
+    static final int MAX_CHARS = 256; 
 	  
-	public static String findSubString(String string) {  
-	  int n = string.length(); 
-	  int distinctCount = 0; 
-	  boolean[] visited = new boolean[MAX_CHARS]; 
+    public static String findSubString(String string) {  
+      int n = string.length(); 
+      int distinctCount = 0; 
+      boolean[] visited = new boolean[MAX_CHARS]; 
       Arrays.fill(visited, false); 
       for(int i = 0; i < n; i++) { 
         if (visited[string.charAt(i)] == false) { 
@@ -30,31 +30,31 @@ import java.io.*;
         if (count == distinctCount) {
           // If any character is occurring more number of times than it's occurence in the pattern then remove the character
           while (currentCount[string.charAt(start)] > 1) {
-        	if (currentCount[string.charAt(start)] > 1) { 
-        	  currentCount[string.charAt(start)]--; 
-        	}
+            if (currentCount[string.charAt(start)] > 1) { 
+              currentCount[string.charAt(start)]--; 
+            }
             start++;   
           }
           // Update window size 
           int lengthWindow = j - start + 1; 
           if (minLength > lengthWindow) { 
-        	minLength = lengthWindow; 
+            minLength = lengthWindow; 
             startIndex = start; 
           } 
         }
       }
       
-	  return string.substring(startIndex, startIndex + minLength);  
-	}
+      return string.substring(startIndex, startIndex + minLength);   
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  String string = new String();	
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine();  
-	  String smallestWindow = findSubString(string); 
-	  System.out.println();
-	  System.out.println("Smallest window containing all distinct characters is: " + smallestWindow);
-	  input.close();
-	}
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      String string = new String();	
+      System.out.print("Enter the string: ");
+      string = input.nextLine();  
+      String smallestWindow = findSubString(string); 
+      System.out.println();
+      System.out.println("Smallest window containing all distinct characters is: " + smallestWindow);
+      input.close();
+    }
   }
