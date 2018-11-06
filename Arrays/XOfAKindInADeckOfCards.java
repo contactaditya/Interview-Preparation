90b8aa75e4e9606f2d583c04114280059dad017b
Aditya Gupta <contactadityag@gmail.com>
2018-11-06 04:26:21 -0500

    This is a program to return if and only if you can choose X >= 2 such that it is possible to split the entire deck into 1 or more groups of cards, where: Each group has exactly X cards. All the cards in each group have the same integer.

--- Arrays/XOfAKindInADeckOfCards.java
+++ Arrays/XOfAKindInADeckOfCards.java
@@ -3,8 +3,8 @@ import java.io.*;
 
   public class XOfAKindInADeckOfCards {
 	  
-	public static boolean hasGroupsSizeX(int[] deck) {
-	  Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+    public static boolean hasGroupsSizeX(int[] deck) {
+      Map<Integer, Integer> count = new HashMap<Integer, Integer>();
       int result = 0;
       for (int i : deck) {
     	count.put(i, count.getOrDefault(i, 0) + 1);
@@ -13,31 +13,31 @@ import java.io.*;
     	result = gcd(i, result);
       }
       return result > 1;  
-	}
+    }
 	
-	public static int gcd(int x, int y) {
-	  return y > 0 ? gcd(y, x % y) : x;  
-	}
+    public static int gcd(int x, int y) {
+      return y > 0 ? gcd(y, x % y) : x;  
+    }
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int deck[] = new int[numbers];
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int deck[] = new int[numbers];
 					    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		deck[i] = input.nextInt(); 
-	  }
-	  boolean result = hasGroupsSizeX(deck);
-	  System.out.println();
-	  if(result) {
-	    System.out.println("It is possible to split the entire deck.");
-	  }
-	  else {
-	 	System.out.println("It is not possible to split the entire deck.");  
-	  }
-	  input.close();
-	}
+      System.out.println();
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	deck[i] = input.nextInt(); 
+      }
+      boolean result = hasGroupsSizeX(deck);
+      System.out.println();
+      if(result) {
+	System.out.println("It is possible to split the entire deck.");
+      }
+      else {
+	System.out.println("It is not possible to split the entire deck.");  
+      }
+      input.close();
+    }
   }
