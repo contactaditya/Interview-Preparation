8cc618a8322bb3c74579510bdc41a9a30842b288
Aditya Gupta <contactadityag@gmail.com>
2018-11-05 23:41:34 -0500

    This is a program to check if a sentence is palindrome or not. You can ignore white spaces and other characters to consider sentence as a palindrome.

--- Strings/IsTheSentenceAPalindromeOrNot.java
+++ Strings/IsTheSentenceAPalindromeOrNot.java
@@ -3,13 +3,13 @@ import java.io.*;
 
   public class IsTheSentenceAPalindromeOrNot {
 	  
-	public static boolean sentencePalindrome(String string) {
-	  int low = 0; 
-	  int high = string.length() - 1;
-	  string = string.toLowerCase(); 
-	  // Compares character until they are equal 
-	  while(low <= high) {
-		char getAtLow = string.charAt(low); 
+    public static boolean sentencePalindrome(String string) {
+      int low = 0; 
+      int high = string.length() - 1;
+      string = string.toLowerCase(); 
+      // Compares character until they are equal 
+      while(low <= high) {
+	char getAtLow = string.charAt(low); 
         char getAtHigh = string.charAt(high);  
         // If there is another symbol in left of sentence 
         if (!(getAtLow >= 'a' && getAtLow <= 'z')) {
@@ -27,24 +27,24 @@ import java.io.*;
         else {
           return false;
         }
-	  }
+      }
 		
-	  return true;  
-	}
+      return true;  
+    }
 
-	public static void main(String[] args) { 	
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the string: ");
       String string = input.nextLine();  
 			  
       boolean result = sentencePalindrome(string); 
       System.out.println();
       if(result) {
- 		System.out.println("Yes this sentence is a palindrome.");
- 	  }
- 	  else {
- 		System.out.println("No this sentence is not a palindrome.");  
- 	  }
+ 	System.out.println("Yes this sentence is a palindrome.");
+      }
+      else {
+ 	System.out.println("No this sentence is not a palindrome.");  
+      }
       input.close();
-	}
+    }
   }
