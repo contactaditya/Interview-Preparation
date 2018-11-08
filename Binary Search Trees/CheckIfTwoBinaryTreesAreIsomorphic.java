c2f86449330286a66df724f7416293ff6a5133e6
Aditya Gupta <contactadityag@gmail.com>
2018-11-08 04:21:06 -0500

    This is a program to detect if two trees are isomorphic.
    Two trees are called isomorphic if one of them can be obtained from other by a series of flips, i.e. by swapping left and right children of a number of nodes. Any number of nodes at any level can have their children swapped. Two empty trees are isomorphic.

--- Binary Search Trees/CheckIfTwoBinaryTreesAreIsomorphic.java
+++ Binary Search Trees/CheckIfTwoBinaryTreesAreIsomorphic.java
@@ -2,28 +2,28 @@ import java.util.*;
 import java.io.*;
 
   public class CheckIfTwoBinaryTreesAreIsomorphic {    
-	private Node root1;	
-	private Node root2;		    
+    private Node root1;	
+    private Node root2;		    
 	
     public boolean isIsomorphic(Node node1, Node node2) {
       // Both roots are NULL, trees isomorphic by definition 	
-	  if (node1 == null && node2 == null) { 
-		return true;
-	  }
-	  // Exactly one of the n1 and n2 is NULL, trees are not isomorphic 
-	  if (node1 == null || node2 == null) {
-		return false;
-	  }
+      if (node1 == null && node2 == null) { 
+	return true;
+      }
+      // Exactly one of the n1 and n2 is NULL, trees are not isomorphic 
+      if (node1 == null || node2 == null) {
+	return false;
+      }
 	  
-	  if (node1.data != node2.data) {
-	    return false;
-	  }
+      if (node1.data != node2.data) {
+	return false;
+      }
 
-	  return (isIsomorphic(node1.left, node2.left) && isIsomorphic(node1.right, node2.right)) || (isIsomorphic(node1.left, node2.right) && isIsomorphic(node1.right, node2.left));
-	}
+      return (isIsomorphic(node1.left, node2.left) && isIsomorphic(node1.right, node2.right)) || (isIsomorphic(node1.left, node2.right) && isIsomorphic(node1.right, node2.left));
+    }
 
-	public static void main(String[] args) {   
-	  CheckIfTwoBinaryTreesAreIsomorphic tree = new CheckIfTwoBinaryTreesAreIsomorphic();
+    public static void main(String[] args) {   
+      CheckIfTwoBinaryTreesAreIsomorphic tree = new CheckIfTwoBinaryTreesAreIsomorphic();
       tree.root1 = new Node(1); 
       tree.root1.left = new Node(2); 
       tree.root1.right = new Node(3); 
@@ -42,11 +42,11 @@ import java.io.*;
       tree.root2.right.right.left = new Node(8); 
       tree.root2.right.right.right = new Node(7); 
 	  
-	  if (tree.isIsomorphic(tree.root1, tree.root2) == true) {
-		System.out.println("The two trees are isomorphic.");
-	  }
-	  else {
-		System.out.println("TThe two trees are not isomorphic.");
-	  }
-	}
+      if (tree.isIsomorphic(tree.root1, tree.root2) == true) {
+	System.out.println("The two trees are isomorphic.");
+      }
+      else {
+	System.out.println("TThe two trees are not isomorphic.");
+      }
+    }
   }
