d8f3b3b54a537e687319ab1882e1e7bdd394865d
Aditya Gupta <contactadityag@gmail.com>
2018-11-12 04:11:27 -0500

    This is a program to find the sum of all the leaf nodes which are at maximum depth from root node.

--- Binary Search Trees/SumOfNodesAtMaximumDepthOfABinaryTree.java
+++ Binary Search Trees/SumOfNodesAtMaximumDepthOfABinaryTree.java
@@ -5,18 +5,18 @@ import java.io.*;
     Node root;	    
     Node previous;
     
-	public static int maximumDepth(Node root) {
-	  if (root == null) {
-		return 0;
-	  }
+    public static int maximumDepth(Node root) {
+      if (root == null) {
+	return 0;
+      }
 		    
-	  return Math.max(maximumDepth(root.left), maximumDepth(root.right)) + 1;     	
-	}  
+      return Math.max(maximumDepth(root.left), maximumDepth(root.right)) + 1;     	
+    }  
     
-	private static int sumMaxLevel(Node root) {
-	  int maxDepth = maximumDepth(root);    
-	  return sumMaxLevelRec(root, maxDepth); 
-	}
+    private static int sumMaxLevel(Node root) {
+      int maxDepth = maximumDepth(root);    
+      return sumMaxLevelRec(root, maxDepth); 
+    }
 
     private static int sumMaxLevelRec(Node node, int maxDepth) {
       if (node == null)  {
@@ -27,9 +27,9 @@ import java.io.*;
       }
 		
       return sumMaxLevelRec(node.left, maxDepth - 1) + sumMaxLevelRec(node.right, maxDepth - 1); 
-	}
+    }
 
-	public void printInorderTraversal(Node node) {	 
+    public void printInorderTraversal(Node node) {	 
       if (node == null) {
         return;
       }
@@ -39,9 +39,9 @@ import java.io.*;
       printInorderTraversal(node.right);
     }
 
-	public static void main(String[] args) {  
-	  SumOfNodesAtMaximumDepthOfABinaryTree tree = new SumOfNodesAtMaximumDepthOfABinaryTree();
-	  tree.root = new Node(1);
+    public static void main(String[] args) {  
+      SumOfNodesAtMaximumDepthOfABinaryTree tree = new SumOfNodesAtMaximumDepthOfABinaryTree();
+      tree.root = new Node(1);
       tree.root.left = new Node(2);
       tree.root.right = new Node(3);
       tree.root.left.right = new Node(5);
@@ -50,6 +50,6 @@ import java.io.*;
       tree.root.right.right = new Node(7);
       
       int sum = sumMaxLevel(tree.root);
-	  System.out.print("The sum of all the leaf nodes which are at maximum depth from root node is: " + sum);
-	}
+      System.out.print("The sum of all the leaf nodes which are at maximum depth from root node is: " + sum);
+    }
   }
