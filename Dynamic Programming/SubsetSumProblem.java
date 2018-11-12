e4eb3e0ebc893f581be73f7db7e060c8f27e9989
Aditya Gupta <contactadityag@gmail.com>
2018-11-12 03:43:36 -0500

    This is a program to print all subsets of given array with sum equal to given sum.

--- Dynamic Programming/SubsetSumProblem.java
+++ Dynamic Programming/SubsetSumProblem.java
@@ -2,13 +2,13 @@ import java.util.*;
 import java.io.*;
 
   public class SubsetSumProblem {
-	static boolean dp[][]; 
+    static boolean dp[][]; 
 	  
-	private static void printAllSubsets(int[] array, int sum) {
-	  if (array.length == 0 || sum < 0) {
-	    return; 
-	  }
-	  // Sum 0 can always be achieved with 0 elements 
+    private static void printAllSubsets(int[] array, int sum) {
+      if (array.length == 0 || sum < 0) {
+	return; 
+      }
+      // Sum 0 can always be achieved with 0 elements 
       dp = new boolean[array.length][sum + 1]; 
       for(int i = 0; i < array.length; i++) { 
         dp[i][0] = true;   
@@ -28,40 +28,40 @@ import java.io.*;
         return; 
       } 
       System.out.println();
-	  System.out.println("All subsets of given array with sum equal to given " + sum + " are: "); 
+      System.out.println("All subsets of given array with sum equal to given " + sum + " are: "); 
       ArrayList<Integer> p = new ArrayList<Integer>(); 
       printSubsetsRec(array, array.length - 1, sum, p); 
-	}
+    }
 
-	private static void printSubsetsRec(int[] array, int i, int sum, ArrayList<Integer> p) {
+    private static void printSubsetsRec(int[] array, int i, int sum, ArrayList<Integer> p) {
       // We print p[] only if array[0] is equal to sum OR dp[0][sum] is true. 
-	  if (i == 0 && sum != 0 && dp[0][sum]) { 
-	    p.add(array[i]); 
-	    System.out.println(p); 
-	    p.clear(); 
-	    return; 
-	  } 
-	  // If sum becomes 0 
-	  if (i == 0 && sum == 0) { 
-		System.out.println(p); 
+      if (i == 0 && sum != 0 && dp[0][sum]) { 
+        p.add(array[i]); 
+	System.out.println(p); 
+	p.clear(); 
+	return; 
+      } 
+      // If sum becomes 0 
+      if (i == 0 && sum == 0) { 
+	System.out.println(p); 
         p.clear(); 
         return; 
       } 
-	  // If given sum can be achieved after ignoring current element. 
-	  if (dp[i-1][sum]) {
-		ArrayList<Integer> b = new ArrayList<Integer>(); 
+      // If given sum can be achieved after ignoring current element. 
+      if (dp[i-1][sum]) {
+	ArrayList<Integer> b = new ArrayList<Integer>(); 
         b.addAll(p); 
         printSubsetsRec(array, i-1, sum, b);    
-	  }
-	  // If given sum can be achieved after considering current element. 
-	  if (sum >= array[i] && dp[i - 1][sum - array[i]]) {
-		p.add(array[i]); 
-        printSubsetsRec(array, i-1, sum - array[i], p); 
-	  }
-	}
+      }
+      // If given sum can be achieved after considering current element. 
+      if (sum >= array[i] && dp[i - 1][sum - array[i]]) {
+	p.add(array[i]); 
+        printSubsetsRec(array, i-1, sum - array[i], p);  
+      }
+    }
 	
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int number = input.nextInt();  
       int array[] = new int[number];
@@ -78,5 +78,5 @@ import java.io.*;
       
       printAllSubsets(array, sum); 
       input.close();
-	}
+    }
   }
