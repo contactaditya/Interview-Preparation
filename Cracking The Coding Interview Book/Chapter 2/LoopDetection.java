d9e50127cececfe115f2ab701c40038f9584acab
Aditya Gupta <contactadityag@gmail.com>
2018-11-16 03:08:45 -0500

    This is the solution to the Loop Detection Problem.

--- Cracking The Coding Interview Book/Chapter 2/LoopDetection.java
+++ Cracking The Coding Interview Book/Chapter 2/LoopDetection.java
@@ -5,88 +5,86 @@ import java.util.*;
    public LinkedListNode next;
 
    //Node constructor
-		    
    public LinkedListNode(Object data) {
-      this.data = data;
-      next = null;
+     this.data = data;
+     next = null;
    }
 		    
    public LinkedListNode(Object data, LinkedListNode next) {
-      this.data = data;
-      this.next = next;
+     this.data = data;
+     this.next = next;
    }
 	 
    public Object getData() {
-      return data;
+     return data;
    }
 			
    public void setData(Object data) {
-      this.data = data; 
+     this.data = data; 
    }
 			
    public LinkedListNode getNext() {
-      return next;
+     return next;
    }
 			
    public void setNext(LinkedListNode next) {
-      this.next = next;
+     this.next = next;
    }
  }
 
  public class LoopDetection { 
-    private static LinkedListNode head;
-    private static int size;  
+   private static LinkedListNode head;
+   private static int size;  
 	
-    public LoopDetection() {
-       // this is an empty list, so the reference to the head node is set to a new node with no data				  
-       head = new LinkedListNode(null);
-       size = 0;
-    }
+   public LoopDetection() {
+     // this is an empty list, so the reference to the head node is set to a new node with no data				  
+     head = new LinkedListNode(null);
+     size = 0;
+   }
 
-    public static LinkedListNode findBeginning(LinkedListNode head) {
-       LinkedListNode slow = head;
-       LinkedListNode fast = head;
+   public static LinkedListNode findBeginning(LinkedListNode head) {
+     LinkedListNode slow = head;
+     LinkedListNode fast = head;
 		     
-       while(fast != null && fast.next != null) {    
-	 slow = slow.next;
-	 fast = fast.next.next;
-	 if(slow == fast) {
-	   break; 
-	 }
+     while(fast != null && fast.next != null) {    
+       slow = slow.next;
+       fast = fast.next.next;
+       if(slow == fast) {
+	 break; 
        }
+     }
 	   
-       // If there is no meeting point then there is no loop
+     // If there is no meeting point then there is no loop   
+     if(fast == null || fast.next == null) {
+       return null;
+     }
 	   
-       if(fast == null || fast.next == null) {
-	 return null;
-       }
+     slow = head;
 	   
-      slow = head;
-	   
-      while(slow != fast) {
-	slow = slow.next;
-	fast = fast.next;
-      }
+     while(slow != fast) {
+       slow = slow.next;
+       fast = fast.next;
+     }
 	  
-      return fast;
-    }
+     return fast;
+   }
 	  
-    public static void main(String[] args) {  
-      LoopDetection list = new LoopDetection();
-      list.head = new LinkedListNode(50);
-      list.head.next = new LinkedListNode(20);
-      list.head.next.next = new LinkedListNode(15); 
-      list.head.next.next.next = new LinkedListNode(4);
-      list.head.next.next.next.next = new LinkedListNode(10);
-      list.head.next.next.next.next.next = list.head.next;
+   public static void main(String[] args) {  
+     LoopDetection list = new LoopDetection();
+     list.head = new LinkedListNode(50);
+     list.head.next = new LinkedListNode(20);
+     list.head.next.next = new LinkedListNode(15); 
+     list.head.next.next.next = new LinkedListNode(4);
+     list.head.next.next.next.next = new LinkedListNode(10);
+     list.head.next.next.next.next.next = list.head.next;
 	     	     
-      LinkedListNode IsALoop = list.findBeginning(head);
+     LinkedListNode IsALoop = list.findBeginning(head);
 	  
-      if(IsALoop == null) {
-        System.out.println("There is no loop in the linkedlist.");  
-      }
-      else {
-        System.out.println("There is a loop in the linkedlist and data where the loop is present is: " + IsALoop.data);    
-      }
-    }
+     if(IsALoop == null) {
+       System.out.println("There is no loop in the linkedlist.");  
+     }
+     else {
+       System.out.println("There is a loop in the linkedlist and data where the loop is present is: " + IsALoop.data);    
+     }
+   }
  }
