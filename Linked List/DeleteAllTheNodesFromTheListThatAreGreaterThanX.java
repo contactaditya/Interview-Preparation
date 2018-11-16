b5ab15ed9fc264f7240c5c14f5a4912810efbc26
Aditya Gupta <contactadityag@gmail.com>
2018-11-15 23:36:53 -0500

    This is a program to delete all the nodes from the list that are greater than the specified value x in a linked list.

--- Linked List/DeleteAllTheNodesFromTheListThatAreGreaterThanX.java
+++ Linked List/DeleteAllTheNodesFromTheListThatAreGreaterThanX.java
@@ -2,53 +2,53 @@ import java.util.*;
 import java.io.*;
 
   public class DeleteAllTheNodesFromTheListThatAreGreaterThanX { 
-	private LinkedListNode head;
-	private static int size;  
+    private LinkedListNode head;
+    private static int size;  
 	
-	public LinkedListNode deleteGreaterNodes(LinkedListNode head, int value) {     
-	  while (head != null && (int)head.data > value) {
-	    head = head.next;
-	  }
-	  LinkedListNode current = head; 		 
-	  while(current != null && current.getNext() != null) {
-	    if((int)current.next.data > value) {
-	      current.next = current.next.next; 
-	    }
-	    else {
-	      current = current.next; 	
-	    }
-	  }
-	  return head;
+    public LinkedListNode deleteGreaterNodes(LinkedListNode head, int value) {     
+      while (head != null && (int)head.data > value) {
+	head = head.next;
+      }
+      LinkedListNode current = head; 		 
+      while(current != null && current.getNext() != null) {
+        if((int)current.next.data > value) {
+	  current.next = current.next.next; 
 	}
+	else {
+	  current = current.next; 	
+	}
+      }
+      return head;
+    }
 	
-	public void PrintElementsOfALinkedList(LinkedListNode head) {	 
-	  LinkedListNode current = head; 		    
-	  while(current != null) {	   
-		System.out.print(current.getData() + " ");	 	
-		current = current.getNext();
-	  }
-	}  			
+    public void PrintElementsOfALinkedList(LinkedListNode head) {	  
+      LinkedListNode current = head; 		    
+      while(current != null) {	   
+	System.out.print(current.getData() + " ");	 	
+	current = current.getNext();
+      }
+    }  			
 
-	public static void main(String[] args) { 	 
-	  DeleteAllTheNodesFromTheListThatAreGreaterThanX list = new DeleteAllTheNodesFromTheListThatAreGreaterThanX();
-	  list.head = new LinkedListNode(7);
-	  list.head.next = new LinkedListNode(3);
-	  list.head.next.next = new LinkedListNode(4);
-	  list.head.next.next.next = new LinkedListNode(8);
-	  list.head.next.next.next.next = new LinkedListNode(5);
-	  list.head.next.next.next.next.next = new LinkedListNode(1);
+    public static void main(String[] args) { 	 
+      DeleteAllTheNodesFromTheListThatAreGreaterThanX list = new DeleteAllTheNodesFromTheListThatAreGreaterThanX();
+      list.head = new LinkedListNode(7);
+      list.head.next = new LinkedListNode(3);
+      list.head.next.next = new LinkedListNode(4); 
+      list.head.next.next.next = new LinkedListNode(8);
+      list.head.next.next.next.next = new LinkedListNode(5);
+      list.head.next.next.next.next.next = new LinkedListNode(1);
 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the value: ");
-	  int value = input.nextInt();   
-	  System.out.println();
-	  System.out.print("Contents of the linked list before deleting are: ");	 
-	  list.PrintElementsOfALinkedList(list.head);
-	  LinkedListNode current = list.deleteGreaterNodes(list.head, value); 
-	  System.out.println();
-	  System.out.println();
-	  System.out.print("Contents of the linked list after deleting are: ");	 
-	  list.PrintElementsOfALinkedList(current);
-	  input.close();
-	}
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the value: ");
+      int value = input.nextInt();   
+      System.out.println();
+      System.out.print("Contents of the linked list before deleting are: ");	 
+      list.PrintElementsOfALinkedList(list.head);
+      LinkedListNode current = list.deleteGreaterNodes(list.head, value); 
+      System.out.println();
+      System.out.println();
+      System.out.print("Contents of the linked list after deleting are: ");	 
+      list.PrintElementsOfALinkedList(current);
+      input.close();
+    }
   }
