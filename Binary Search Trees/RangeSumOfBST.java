401db2ed82d4aef4a13477025634ac11f5a91c0f
Aditya Gupta <contactadityag@gmail.com>
2018-11-16 05:30:24 -0500

    This is a program to return the sum of values of all nodes with value between L and R in a Binary Search Tree.

--- Binary Search Trees/RangeSumOfBST.java
+++ Binary Search Trees/RangeSumOfBST.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.io.*;
 
   public class RangeSumOfBST {
-	Node root;	    
-	int answer;
+    Node root;	    
+    int answer;
 	  
-	public int rangeSumBST(Node root, int L, int R) {
-	  answer = 0;
-	  dfs(root, L, R);
-	  return answer;      
-	}
+    public int rangeSumBST(Node root, int L, int R) {
+      answer = 0;
+      dfs(root, L, R);
+      return answer;      
+    }
 
     private void dfs(Node node, int L, int R) {
       if (node != null) {  
@@ -23,10 +23,10 @@ import java.io.*;
           dfs(node.right, L, R);
         }
       }
-	}
+    }
 
-	public static void main(String[] args) {      
-	  RangeSumOfBST tree = new RangeSumOfBST();
+    public static void main(String[] args) {      
+      RangeSumOfBST tree = new RangeSumOfBST();
       tree.root = new Node(10);
       tree.root.left = new Node(5);
       tree.root.right = new Node(50);
@@ -34,16 +34,16 @@ import java.io.*;
       tree.root.right.left = new Node(40);
       tree.root.right.right = new Node(100);
       
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the lower value for the range: ");
-	  int left = input.nextInt();  
-	  System.out.println();
-	  System.out.print("Enter the upper value for the range: ");
-	  int right = input.nextInt();  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the lower value for the range: ");
+      int left = input.nextInt();  
+      System.out.println();
+      System.out.print("Enter the upper value for the range: ");
+      int right = input.nextInt();  
   	     	  
       int rangeSum = tree.rangeSumBST(tree.root, left, right);
       System.out.println();	
       System.out.print("The sum of values of all nodes with value between " + left + " and " + right + " is: " + rangeSum);
       input.close();
-	}
+    }
   }
