256e580a91d62e39aa47eb2310f421b502f5c3c5
Aditya Gupta <contactadityag@gmail.com>
2018-11-16 00:48:08 -0500

    This is a program to delete all the nodes from the list that are lesser than the key K in a Linked List

--- Linked List/DeleteAllTheNodesFromTheListThatAreLessThanX.java
+++ Linked List/DeleteAllTheNodesFromTheListThatAreLessThanX.java
@@ -2,53 +2,53 @@ import java.util.*;
 import java.io.*;
 
   public class DeleteAllTheNodesFromTheListThatAreLessThanX { 
-	private LinkedListNode head;
-	private static int size;  
+    private LinkedListNode head;
+    private static int size;  
 		
-	public LinkedListNode deleteLesserNodes(LinkedListNode head, int value) {     
-	  while (head != null && (int)head.data < value) {
-	    head = head.next;
-	  }
-	  LinkedListNode current = head; 		 
-	  while(current != null && current.getNext() != null) {
-	    if((int)current.next.data < value) {
-	      current.next = current.next.next; 
-		}
-	    else {
-		  current = current.next; 	
-		}
-	  }
-	  return head;
+    public LinkedListNode deleteLesserNodes(LinkedListNode head, int value) {     
+      while (head != null && (int)head.data < value) {
+	head = head.next;
+      }
+      LinkedListNode current = head; 		 
+      while(current != null && current.getNext() != null) {
+	if((int)current.next.data < value) {
+	  current.next = current.next.next; 
 	}
+        else {
+	  current = current.next; 	
+        }
+      }
+      return head;
+    }
 		
-	public void PrintElementsOfALinkedList(LinkedListNode head) {	 
-	  LinkedListNode current = head; 		    
-	  while(current != null) {	   
-		System.out.print(current.getData() + " ");	 	
-		current = current.getNext();
-	  }
-	}  			
+    public void PrintElementsOfALinkedList(LinkedListNode head) {	 
+      LinkedListNode current = head; 		    
+      while(current != null) {	   
+	System.out.print(current.getData() + " ");	 	
+	current = current.getNext();
+      }
+    }  			
 
-	public static void main(String[] args) {   
-	  DeleteAllTheNodesFromTheListThatAreLessThanX list = new DeleteAllTheNodesFromTheListThatAreLessThanX();
-	  list.head = new LinkedListNode(12);
-	  list.head.next = new LinkedListNode(15);
-	  list.head.next.next = new LinkedListNode(9);
-	  list.head.next.next.next = new LinkedListNode(11);
-	  list.head.next.next.next.next = new LinkedListNode(5);
-	  list.head.next.next.next.next.next = new LinkedListNode(6);
+    public static void main(String[] args) {   
+      DeleteAllTheNodesFromTheListThatAreLessThanX list = new DeleteAllTheNodesFromTheListThatAreLessThanX();
+      list.head = new LinkedListNode(12);
+      list.head.next = new LinkedListNode(15);
+      list.head.next.next = new LinkedListNode(9);
+      list.head.next.next.next = new LinkedListNode(11);
+      list.head.next.next.next.next = new LinkedListNode(5);
+      list.head.next.next.next.next.next = new LinkedListNode(6);
 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the value: ");
-	  int value = input.nextInt();   
-	  System.out.println();
-	  System.out.print("Contents of the linked list before deleting are: ");	 
-	  list.PrintElementsOfALinkedList(list.head);
-	  LinkedListNode current = list.deleteLesserNodes(list.head, value); 
-	  System.out.println();
-	  System.out.println();
-	  System.out.print("Contents of the linked list after deleting are: ");	 
-	  list.PrintElementsOfALinkedList(current);
-	  input.close();
-	}
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the value: ");
+      int value = input.nextInt();   
+      System.out.println();
+      System.out.print("Contents of the linked list before deleting are: ");	 
+      list.PrintElementsOfALinkedList(list.head);
+      LinkedListNode current = list.deleteLesserNodes(list.head, value); 
+      System.out.println();
+      System.out.println();
+      System.out.print("Contents of the linked list after deleting are: ");	 
+      list.PrintElementsOfALinkedList(current);
+      input.close();
+    }
   }
