ab73963a0fa59dae3651e9ad1c057299cf7c12ca
Aditya Gupta <contactadityag@gmail.com>
2018-11-22 07:00:43 -0500

    This is a program to check if an array is a valid mountain array.
    Array is a mountain array if and only if :
    
    A.length >= 3
    
    There exists some i with 0 < i < A.length - 1 such that:
    A[0] < A[1] < ... A[i-1] < A[i]
    A[i] > A[i+1] > ... > A[B.length - 1]

--- Arrays/ValidMountainArray.java
+++ Arrays/ValidMountainArray.java
@@ -3,42 +3,42 @@ import java.io.*;
 
   public class ValidMountainArray {
 	  
-	public static boolean validMountainArray(int[] array) {
-	  if (array == null || array.length < 3) {
-		return false;
-	  }	
-	  int i = 0;
-	  while (i < array.length - 1 && array[i + 1] > array[i]) {
+    public static boolean validMountainArray(int[] array) {
+      if (array == null || array.length < 3) {
+	return false;
+      }	
+      int i = 0;
+      while (i < array.length - 1 && array[i + 1] > array[i]) {
         i++;
       }
-	  int j = array.length - 1;
-	  while (j > 0 && array[j - 1] > array[j]) {
+      int j = array.length - 1;
+      while (j > 0 && array[j - 1] > array[j]) {
         j--;
       }
 	  
-	  return i > 0 && j < array.length - 1 && i == j;      
+      return i > 0 && j < array.length - 1 && i == j;      
     }	  
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();  
+      int array[] = new int[number];
 	
-	  System.out.println();	
-	  System.out.print("Please enter the elements in the array: ");
-	  for (int i = 0; i < array.length; i++) {
-		array[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the elements in the array: ");
+      for (int i = 0; i < array.length; i++) {
+	array[i] = input.nextInt();
+      }
 	
-	  System.out.println();	
-	  boolean isAValidMountainArray = validMountainArray(array);
-	  if(isAValidMountainArray) {
-		System.out.println("This array is a valid mountain array.");  
-	  }
+      System.out.println();	
+      boolean isAValidMountainArray = validMountainArray(array);
+      if(isAValidMountainArray) {
+	System.out.println("This array is a valid mountain array.");  
+      }
       else {
-		System.out.println("This array is not a valid mountain array.");  
-	  }
+	System.out.println("This array is not a valid mountain array.");  
+      }
       input.close();
-	}
+    }
   }
