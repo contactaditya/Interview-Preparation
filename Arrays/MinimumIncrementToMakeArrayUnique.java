f2b08c250fc5f190deb0c4a12e741bfb8338ca0a
contactaditya <contactadityag@gmail.com>
2018-11-25 02:49:33 -0500

    This is a program to return the least number of moves to make every value in the array unique.

---
+++ Arrays/MinimumIncrementToMakeArrayUnique.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+  public class MinimumIncrementToMakeArrayUnique {
+	  
+	public static int minIncrementForUnique(int[] array) {
+	  Arrays.sort(array);
+	  int result = 0, need = 0;	
+	  for (int a : array) {
+        result += Math.max(need - a, 0);
+        need = Math.max(a, need) + 1;
+      }
+		
+      return result;       
+	}
+
+	public static void main(String[] args) {  
+	  Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
+
+      System.out.println();
+	  System.out.print("Please enter the actual values in the array: ");
+	  for (int i = 0; i < numbers; i++) {
+	    array[i] = input.nextInt();
+	  }
+	  
+	  int minimumNumberOfMoves = minIncrementForUnique(array);
+	  System.out.println();
+	  System.out.print("The least number of moves to make every value in array unique are: " + minimumNumberOfMoves);
+	}
+  }
