ad511336d6752e7ef23896a81f6a96b872fb4320
Aditya Gupta <contactadityag@gmail.com>
2018-11-25 07:23:28 -0500

    This is a program to return the largest number of points we can have after playing any number of tokens.
    Each token can be used at most once, has a value token[i], and has potentially two ways to use it.
    
    If we have at least token[i] power, we may play the token face up, losing token[i] power, and gaining 1 point.
    
    If we have at least 1 point, we may play the token face down, gaining token[i] power, and losing 1 point.

--- Arrays/BagOfTokens.java
+++ Arrays/BagOfTokens.java
@@ -3,8 +3,8 @@ import java.io.*;
 
   public class BagOfTokens {
 	  
-	public static int bagOfTokensScore(int[] tokens, int P) { 
-	  Arrays.sort(tokens);
+    public static int bagOfTokensScore(int[] tokens, int P) { 
+      Arrays.sort(tokens);
       int result = 0, points = 0, i = 0, j = tokens.length - 1;
       while (i <= j) {
         if (P >= tokens[i]) {
@@ -19,27 +19,27 @@ import java.io.*;
       }
 		
       return result;      
-	}
+    }
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of tokens: ");
-	  int number = input.nextInt();  
-	  int tokens[] = new int[number];
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of tokens: ");
+      int number = input.nextInt();  
+      int tokens[] = new int[number];
 	
-	  System.out.println();	
-	  System.out.print("Please enter the value of the tokens: ");
-	  for (int i = 0; i < tokens.length; i++) {
-		tokens[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the value of the tokens: ");
+      for (int i = 0; i < tokens.length; i++) {
+	tokens[i] = input.nextInt();
+      }
 	  
-	  System.out.println();
-	  System.out.print("Enter the number of points: ");
-	  int P = input.nextInt();  
+      System.out.println();
+      System.out.print("Enter the number of points: ");
+      int P = input.nextInt();  
 	
-	  System.out.println();	
-	  int largestNumberOfPoints = bagOfTokensScore(tokens, P);
-	  System.out.println("The largest number of points we can have after playing any number of tokens are: " + largestNumberOfPoints);  
+      System.out.println();	
+      int largestNumberOfPoints = bagOfTokensScore(tokens, P);
+      System.out.println("The largest number of points we can have after playing any number of tokens are: " + largestNumberOfPoints);  
       input.close(); 
-	}
+    }
   }
