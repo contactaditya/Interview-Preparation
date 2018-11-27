8905291d5161b508514d67e1cfe6ac34cb452fb4
Aditya Gupta <contactadityag@gmail.com>
2018-11-27 08:35:16 -0500

    This is a program to generate the nth term of the count-and-say sequence.

--- Numbers/CountAndSay.java
+++ Numbers/CountAndSay.java
@@ -3,32 +3,32 @@ import java.io.*;
 
   public class CountAndSay {
 	  
-	public static String countAndSay(int number) {
-	  if (number == 1) {
-	    return "1";
-	  }
-	  String string = countAndSay(number-1);
-	  StringBuilder sb = new StringBuilder();
-	  int count = 0;
-	  for (int i = 0; i <= string.length(); i++) {
+    public static String countAndSay(int number) {
+      if (number == 1) {
+	return "1";
+      }
+      String string = countAndSay(number-1);
+      StringBuilder sb = new StringBuilder();
+      int count = 0;
+      for (int i = 0; i <= string.length(); i++) {
         if (i == string.length() || (i - 1) >= 0 && string.charAt(i - 1) != string.charAt(i)) {
           sb.append(count);
           sb.append(string.charAt(i - 1));
           count = 0;
         }
         count++;
-	  }
+      }
 		
-	  return sb.toString();      
-	}	  
+      return sb.toString();      
+    }	  
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int number = input.nextInt();  
 		 		
       String nthTerm = countAndSay(number);
       System.out.println();	
       System.out.print("The nth term of the count-and-say sequence is: " + nthTerm);
-	}
+    }
   }
