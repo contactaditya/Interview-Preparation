1c91f8eaf0a640e258db954beb4237ec49c7ae3e
Aditya Gupta <contactadityag@gmail.com>
2018-12-01 23:22:06 -0500

    This is a program to find the maximum difference between the successive elements in its sorted form in an unsorted array.

--- Arrays/MaximumGap.java
+++ Arrays/MaximumGap.java
@@ -3,17 +3,17 @@ import java.io.*;
 
   public class MaximumGap {
 	  
-	public static int maximumGap(int[] array) {
-	  if (array == null || array.length < 2) {
-		return 0;  
-	  }
+    public static int maximumGap(int[] array) {
+      if (array == null || array.length < 2) {
+	return 0;  
+      }
       // Get the max and min value of the array
       int min = array[0];
-	  int max = array[0];
+      int max = array[0];
       for (int i : array) {
-		min = Math.min(min, i);
-		max = Math.max(max, i);
-	  }	
+	min = Math.min(min, i);
+	max = Math.max(max, i);
+      }	
       // The minimum possible gap, ceiling of the integer division
       int gap = (int) Math.ceil((double)(max - min)/(array.length - 1));
       int[] bucketsMIN = new int[array.length - 1]; // store the min value in that bucket
@@ -43,24 +43,24 @@ import java.io.*;
       }
       
       maxGap = Math.max(maxGap, max - previous); // update the final max value gap
-	  return maxGap;      
+      return maxGap;      
     }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int number = input.nextInt();  
-	  int array[] = new int[number];
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int number = input.nextInt();  
+      int array[] = new int[number];
 		
-	  System.out.println();	
-	  System.out.print("Please enter the elements of the array: ");
-	  for (int i = 0; i < array.length; i++) {
-	    array[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the elements of the array: ");
+      for (int i = 0; i < array.length; i++) {
+	array[i] = input.nextInt();
+      }
 			
       int getMaximumGap = maximumGap(array);
       System.out.println();	
-	  System.out.println("The maximum difference between the successive elements in its sorted form is: " + getMaximumGap);
+      System.out.println("The maximum difference between the successive elements in its sorted form is: " + getMaximumGap);
       input.close();
-	}
+    }
   }
