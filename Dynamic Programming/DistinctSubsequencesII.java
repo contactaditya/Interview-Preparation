1e421ccb38a80f488974bb964f39f24445bd8e54
Aditya Gupta <contactadityag@gmail.com>
2018-12-01 19:44:08 -0500

    This is a program to count the number of distinct, non-empty subsequences of the given string.
    Since the result may be large, return the answer modulo 10^9 + 7.

--- Dynamic Programming/DistinctSubsequencesII.java
+++ Dynamic Programming/DistinctSubsequencesII.java
@@ -3,7 +3,7 @@ import java.util.*;
 
   public class DistinctSubsequencesII {
 	  
-	public static int distinctSubseqII(String string) {	
+    public static int distinctSubseqII(String string) {	
       int end[] = new int[26]; // Represents the count of subsequence ends with a particular character
       int result = 0, added = 0, mod = (int)1e9 + 7;
       for (char c : string.toCharArray()) {
@@ -12,19 +12,19 @@ import java.util.*;
         end[c - 'a'] = (end[c - 'a'] + added) % mod;
       }
 		
-	  return (result + mod) % mod;      
-	}
+      return (result + mod) % mod;      
+    }
 
-	public static int distinctSubsequenceII(String string) {	
-	  int MOD = 1_000_000_007;
-	  int N = string.length();
-	  int[] dp = new int[N + 1];
-	  dp[0] = 1; // Empty substring has only one subsequence 
+    public static int distinctSubsequenceII(String string) {	
+      int MOD = 1_000_000_007;
+      int N = string.length();
+      int[] dp = new int[N + 1];
+      dp[0] = 1; // Empty substring has only one subsequence 
 
-	  int[] last = new int[26];
-	  Arrays.fill(last, -1);
+      int[] last = new int[26];
+      Arrays.fill(last, -1);
 	  
-	  for (int i = 0; i < N; i++) {
+      for (int i = 0; i < N; i++) {
         int x = string.charAt(i) - 'a';
         dp[i+1] = dp[i] * 2 % MOD;
         // If current character has appeared before, then remove all subsequences ending with previous occurrence. 
@@ -40,17 +40,17 @@ import java.util.*;
     	dp[N] += MOD; 
       }
       
-	  return dp[N];
-	}
+      return dp[N];
+    }
 	
-	public static void main(String[] args) { 	  
-	  String string = new String();	  
+    public static void main(String[] args) { 	  
+      String string = new String();	  
       Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine();  
+      System.out.print("Enter the string: ");
+      string = input.nextLine();  
       int numDistinct = distinctSubsequenceII(string);
-	  System.out.println();
-	  System.out.println("The number of distinct non-empty subsequences of " + string + " are: " + numDistinct); 
-	  input.close();
-	}
+      System.out.println();
+      System.out.println("The number of distinct non-empty subsequences of " + string + " are: " + numDistinct); 
+      input.close();
+    }
   }
