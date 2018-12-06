a9ff0bb265b02e7da6472c937c51d307de6e62f2
Aditya Gupta <contactadityag@gmail.com>
2018-12-06 21:51:25 +0000

    This is a program to find the minimum number of platforms required for the railway station so that no train waits given arrival and departure times of all trains that reach a railway station.

--- Arrays/MinimumNumberOfPlatformsRequired.java
+++ Arrays/MinimumNumberOfPlatformsRequired.java
@@ -3,31 +3,31 @@ import java.io.*;
 
   public class MinimumNumberOfPlatformsRequired {
 	  
-	private static int minimumNumberOfPlatforms(int[] arrival, int[] departure) {
-	  Arrays.sort(arrival); 
-	  Arrays.sort(departure); 
-	  int numberOfPlatformsNeeded = 1, result = 1; 
-	  int i = 1, j = 0; 
-	  int n = arrival.length;
-	  while (i < n && j < n) {
-	    // If next event in sorted order is arrival, increment count of platforms needed   
-	    if (arrival[i] <= departure[j]) {    
-	      numberOfPlatformsNeeded++; 
-	      i++;  
-	      if (numberOfPlatformsNeeded > result) { 
+    private static int minimumNumberOfPlatforms(int[] arrival, int[] departure) {
+      Arrays.sort(arrival); 
+      Arrays.sort(departure); 
+      int numberOfPlatformsNeeded = 1, result = 1; 
+      int i = 1, j = 0; 
+      int n = arrival.length;
+      while (i < n && j < n) {
+        // If next event in sorted order is arrival, increment count of platforms needed   
+        if (arrival[i] <= departure[j]) {    
+	  numberOfPlatformsNeeded++; 
+	  i++;  
+	  if (numberOfPlatformsNeeded > result) { 
             result = numberOfPlatformsNeeded; 
-	      } 
-	    } else { 
-	      numberOfPlatformsNeeded--; 
-	      j++; 
-	    } 
-	  }   
+	  } 
+        } else { 
+	  numberOfPlatformsNeeded--; 
+	  j++; 
+	} 
+      }   
 	  
-	  return result;
+      return result;
     }	  
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of trains: ");
       int number = input.nextInt();  
       int arrival[] = new int[number];
@@ -35,7 +35,7 @@ import java.io.*;
       System.out.println();	
       System.out.print("Please enter the arrival times of the trains: ");
       for (int i = 0; i < arrival.length; i++) {
-	    arrival[i] = input.nextInt();
+	arrival[i] = input.nextInt();
       }
 		    
       int departure[] = new int[number];
@@ -49,5 +49,5 @@ import java.io.*;
       System.out.println();	
       System.out.print("The minimum number of platforms required for the railway station so that no train waits are: " + minimumNumberOfPlatforms);
       input.close();
-	}
+    }
   }
