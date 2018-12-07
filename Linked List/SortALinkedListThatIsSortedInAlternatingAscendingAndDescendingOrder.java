0f2d4511f1aaf5442d6a86dee407a275efd58b57
Aditya Gupta <contactadityag@gmail.com>
2018-12-07 03:06:29 +0000

    This is a program to sort a linked list that is sorted in alternating ascending and descending orders.

--- Linked List/SortALinkedListThatIsSortedInAlternatingAscendingAndDescendingOrder.java
+++ Linked List/SortALinkedListThatIsSortedInAlternatingAscendingAndDescendingOrder.java
@@ -2,20 +2,20 @@ import java.util.*;
 import java.io.*;
 
   public class SortALinkedListThatIsSortedInAlternatingAscendingAndDescendingOrder { 	
-	private LinkedListNode head;
-	private static int size;  
+    private LinkedListNode head;
+    private static int size;  
 
-	public SortALinkedListThatIsSortedInAlternatingAscendingAndDescendingOrder() {
+    public SortALinkedListThatIsSortedInAlternatingAscendingAndDescendingOrder() {
       // this is an empty list, so the reference to the head node is set to a new node with no data				  
       head = new LinkedListNode(null);
       size = 0;
-	}
+    }
 	
-	public void sortLinkedList() {	 
-	  LinkedListNode Ahead = new LinkedListNode(0); 
-	  LinkedListNode Dhead = new LinkedListNode(0); 
+    public void sortLinkedList() {	 
+      LinkedListNode Ahead = new LinkedListNode(0); 
+      LinkedListNode Dhead = new LinkedListNode(0); 
 	  
-	  // Split the list into lists 
+      // Split the list into lists 
       splitList(Ahead, Dhead); 
       
       Ahead = Ahead.next; 
@@ -26,47 +26,47 @@ import java.io.*;
 
       // merge the 2 linked lists 
       head = MergeLists(Ahead, Dhead); 
-	} 
+    } 
 	
     private LinkedListNode reverseLL(LinkedListNode head) {
      /**
-	  * Reverses a linked list.
-	  */ 
-	  LinkedListNode current = head;	  
-	  LinkedListNode previous = null;
+      * Reverses a linked list.
+      */ 
+      LinkedListNode current = head;	  
+      LinkedListNode previous = null;
 			 	   
-	  while(current!=null) {
-		LinkedListNode next = current.next;
-		current.next = previous;
-		previous = current;
-		current = next;
-	  }	
+      while(current!=null) {
+	LinkedListNode next = current.next;
+	current.next = previous;
+	previous = current;
+	current = next;
+      }	
 			   
-	  head = previous;
+      head = previous;
 
       return head;
-	}
+    }
     
     public LinkedListNode MergeLists(LinkedListNode headA, LinkedListNode headB) { 
       LinkedListNode list1 = headA;
       LinkedListNode list2 = headB;
   	       
       if (list1 == null && list2 == null) {
-  	    return null; 
+  	return null; 
       } 
       if (list1 == null) {
-  	    return list2; 
+  	return list2; 
       }   
       if (list2 == null) {
-  	    return list1;
+  	return list1;
       }
 
       if ((int)list1.data < (int)list2.data) {
-  	    list1.next = MergeLists(list1.next, list2);
-  	    return list1;
+  	list1.next = MergeLists(list1.next, list2);
+  	return list1;
       } else {
-  	    list2.next = MergeLists(list1, list2.next);
-  	    return list2;
+  	list2.next = MergeLists(list1, list2.next);
+  	return list2;
       }
     }
     
@@ -93,30 +93,30 @@ import java.io.*;
       descending.next = null; 
     }     
 
-	public void PrintElementsOfALinkedList(LinkedListNode head) {	 
-	  LinkedListNode current = head; 		    
-	  while(current != null) {	   
-		System.out.print(current.getData() + " ");	 	
-		current = current.getNext();
-	  }
-	} 
+    public void PrintElementsOfALinkedList(LinkedListNode head) {	 
+      LinkedListNode current = head; 		    
+      while(current != null) {	   
+	System.out.print(current.getData() + " ");	 	
+	current = current.getNext();
+      }
+    } 
 
-	public static void main(String[] args) { 	  
-	  SortALinkedListThatIsSortedInAlternatingAscendingAndDescendingOrder list = new SortALinkedListThatIsSortedInAlternatingAscendingAndDescendingOrder();
-	  list.head = new LinkedListNode(10);
-	  list.head.next = new LinkedListNode(40);
-	  list.head.next.next = new LinkedListNode(53);
-	  list.head.next.next.next = new LinkedListNode(30);
-	  list.head.next.next.next.next = new LinkedListNode(67);	  
-	  list.head.next.next.next.next.next = new LinkedListNode(12);
-	  list.head.next.next.next.next.next.next = new LinkedListNode(89);
+    public static void main(String[] args) { 	  
+      SortALinkedListThatIsSortedInAlternatingAscendingAndDescendingOrder list = new SortALinkedListThatIsSortedInAlternatingAscendingAndDescendingOrder();
+      list.head = new LinkedListNode(10);
+      list.head.next = new LinkedListNode(40);
+      list.head.next.next = new LinkedListNode(53);
+      list.head.next.next.next = new LinkedListNode(30);
+      list.head.next.next.next.next = new LinkedListNode(67);	  
+      list.head.next.next.next.next.next = new LinkedListNode(12);
+      list.head.next.next.next.next.next.next = new LinkedListNode(89);
 
-	  System.out.print("Contents of the linked list before sorting are: ");	 
-	  list.PrintElementsOfALinkedList(list.head);
+      System.out.print("Contents of the linked list before sorting are: ");	 
+      list.PrintElementsOfALinkedList(list.head);
       list.sortLinkedList(); 
       System.out.println();
       System.out.println();
       System.out.print("Contents of the linked list after sorting are: ");	 
       list.PrintElementsOfALinkedList(list.head);
-	}
+    }
   }
