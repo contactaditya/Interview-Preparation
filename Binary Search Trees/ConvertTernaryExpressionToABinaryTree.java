47b036932c5415c345cc4e8104204a37de5235f7
Aditya Gupta <contactadityag@gmail.com>
2018-12-07 19:04:08 +0000

    This is a program to convert a string that contains ternary expressions to a Binary Tree.

--- Binary Search Trees/ConvertTernaryExpressionToABinaryTree.java
+++ Binary Search Trees/ConvertTernaryExpressionToABinaryTree.java
@@ -16,38 +16,38 @@ import java.io.*;
     Node1 root;	    
     Node1 previous;
     
-	private Node1 convertExpression(char[] expression, int i) {
-	  if (i >= expression.length) {
-		return null;
-	  }
-	  // Store current character of expression string['a' to 'z'] 
-	  Node1 root = new Node1(expression[i]); 
-	  ++i;
-	  // If current character of ternary expression is '?' then we add next character as a left child of current node 
-	  if (i < expression.length && expression[i] == '?') { 
+    private Node1 convertExpression(char[] expression, int i) {
+      if (i >= expression.length) {
+	return null;
+      }
+      // Store current character of expression string['a' to 'z'] 
+      Node1 root = new Node1(expression[i]); 
+      ++i;
+      // If current character of ternary expression is '?' then we add next character as a left child of current node 
+      if (i < expression.length && expression[i] == '?') { 
         root.left = convertExpression(expression, i+1); 
-	  } else if(i < expression.length) {
-		root.right = convertExpression(expression, i+1);  
-	  }
+      } else if(i < expression.length) {
+	root.right = convertExpression(expression, i+1);  
+      }
 	  
-	  return root;
-	}    
+      return root;
+    }    
 	  
-	public void printPreorderTraversal(Node1 node) {	 
+    public void printPreorderTraversal(Node1 node) {	 
       if (node == null) {
-		return;
-	  }
+	return;
+      }
       
-	  System.out.print(node.data + " ");
-	  printPreorderTraversal(node.left);
-	  printPreorderTraversal(node.right);
+      System.out.print(node.data + " ");
+      printPreorderTraversal(node.left);
+      printPreorderTraversal(node.right);
     }
 
-	public static void main(String[] args) {
-	  String string = "a?b?c:d:e"; 
-	  ConvertTernaryExpressionToABinaryTree tree = new ConvertTernaryExpressionToABinaryTree(); 
-	  char[] expression = string.toCharArray();  
-	  Node1 root = tree.convertExpression(expression, 0); 
-	  tree.printPreorderTraversal(root); 
-	}
+    public static void main(String[] args) {
+      String string = "a?b?c:d:e"; 
+      ConvertTernaryExpressionToABinaryTree tree = new ConvertTernaryExpressionToABinaryTree(); 
+      char[] expression = string.toCharArray();  
+      Node1 root = tree.convertExpression(expression, 0); 
+      tree.printPreorderTraversal(root); 
+    }
   }
