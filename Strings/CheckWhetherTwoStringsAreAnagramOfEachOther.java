22e8e2ad5e9406419e733a89012d2d8770c206ff
Aditya Gupta <contactadityag@gmail.com>
2018-12-10 01:24:21 +0000

    This is a program to check whether two given strings are anagram of each other or not.
    An anagram of a string is another string that contains same characters, only the order of characters can be different.

--- Strings/CheckWhetherTwoStringsAreAnagramOfEachOther.java
+++ Strings/CheckWhetherTwoStringsAreAnagramOfEachOther.java
@@ -2,16 +2,16 @@ import java.util.*;
 import java.io.*;
 
   public class CheckWhetherTwoStringsAreAnagramOfEachOther {
-	static int NO_OF_CHARS = 256; 
+    static int NO_OF_CHARS = 256; 
 	  
-	private static boolean areAnagram(String string1, String string2) {
+    private static boolean areAnagram(String string1, String string2) {
       int count1[] = new int [NO_OF_CHARS]; 
-	  Arrays.fill(count1, 0); 
-	  int count2[] = new int [NO_OF_CHARS]; 
-	  Arrays.fill(count2, 0); 
-	  int i; 
+      Arrays.fill(count1, 0); 
+      int count2[] = new int [NO_OF_CHARS]; 
+      Arrays.fill(count2, 0); 
+      int i; 
 	  
-	  char str1[] = string1.toCharArray(); 
+      char str1[] = string1.toCharArray(); 
       char str2[] = string2.toCharArray(); 
       
       for (i = 0; i < str1.length && i < str2.length; i++) { 
@@ -20,37 +20,37 @@ import java.io.*;
       } 
 
       // If both strings are of different length. Removing this condition will make the program fail for strings like "aaca" and "aca" 
-	  if (string1.length() != string2.length()) {
-		return false;   
-	  }
-	  // Compare count arrays 
-	  for (i = 0; i < NO_OF_CHARS; i++) {
-	    if (count1[i] != count2[i]) { 
-	      return false; 
-	    }
-	  }
+      if (string1.length() != string2.length()) {
+	return false;   
+      }
+      // Compare count arrays 
+      for (i = 0; i < NO_OF_CHARS; i++) {
+	if (count1[i] != count2[i]) { 
+	  return false; 
+        }
+      }
 	   
-	  return true;
-	}	  
+      return true;
+    }	  
 
-	public static void main(String[] args) { 	 
-	  String string1 = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the string: ");
-	  string1 = input.nextLine(); 
+    public static void main(String[] args) { 	 
+      String string1 = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the string: ");
+      string1 = input.nextLine(); 
 	  
-	  System.out.println();
+      System.out.println();
 	  
-	  String string2 = new String();	  
-	  System.out.print("Enter the string: ");
-	  string2 = input.nextLine(); 
+      String string2 = new String();	  
+      System.out.print("Enter the string: ");
+      string2 = input.nextLine(); 
 	  	  
-	  System.out.println();
-	  if(areAnagram(string1, string2)) {
-	    System.out.println("The two strings are anagram of each other.");
-	  } else {
-		System.out.println("The two strings are not anagram of each other.");  
-	  }
-	  input.close();
-	}
+      System.out.println();
+      if(areAnagram(string1, string2)) {
+	System.out.println("The two strings are anagram of each other.");
+      } else {
+	System.out.println("The two strings are not anagram of each other.");  
+      }
+      input.close();
+    }
   }
