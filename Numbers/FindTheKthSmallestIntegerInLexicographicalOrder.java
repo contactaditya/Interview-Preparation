1beedaa28f7be06449e5e9f2eeee2c1be4045687
contactaditya <contactadityag@gmail.com>
2020-03-12 12:56:18 -0400

    This is a program to find the lexicographically k-th smallest integer in the range from 1 to n.
    Example:
    
    Input:
    n: 13   k: 2
    
    Output:
    10
    
    Explanation:
    The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest number is 10.

---
+++ Numbers/FindTheKthSmallestIntegerInLexicographicalOrder.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+  public class FindTheKthSmallestIntegerInLexicographicalOrder {
+	  
+	public static int findKthNumber(int n, int k) {
+	  int current = 1;
+	  k = k - 1;	
+	  
+	  while (k > 0) {
+	    int steps = calcuateSteps(n, current, current + 1);
+	    if (steps <= k) {
+	      current += 1;
+	      k -= steps;
+	    } else {
+	      current *= 10;
+	      k -= 1;
+	    }
+	  }
+		
+	  return current;     
+	}
+
+	private static int calcuateSteps(int n, long current, long next) {
+	  int steps = 0;
+	  while (current <= n) {
+		steps += Math.min(n + 1, next) - current;
+		next *= 10;
+		current *= 10;
+      }
+	  return steps;
+	}
+
+	public static void main(String[] args) {  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Please enter an integer: ");
+	  int n = input.nextInt();  
+
+	  System.out.println();	
+	  System.out.print("Please enter an integer in the range from 1 to n: ");
+	  int k = input.nextInt(); 
+	  		  
+	  int result = findKthNumber(n, k);
+		    
+	  System.out.println();	
+	  System.out.print("The lexicographically " + k + "-th smallest integer in the range from 1 to " + n + " is: " + result);
+      input.close();
+	}
+  }
