23dcdfd50f22b2e2b412e5fac74c19e178b07d8d
Aditya Gupta <contactadityag@gmail.com>
2020-03-22 22:42:43 +0530

    This is a program to return the final string after removing all adjacent duplicates from the given string.
    Input: "abbaca"
    Output: "ca"
    Explanation:
    For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".

--- Strings/RemoveAllAdjacentDuplicatesInString.java
+++ Strings/RemoveAllAdjacentDuplicatesInString.java
@@ -3,7 +3,7 @@ import java.io.*;
 
   public class RemoveAllAdjacentDuplicatesInString {
 	  
-	public static String removeDuplicates(String string) {        
+    public static String removeDuplicates(String string) {        
       int i = 0, n = string.length();
       char[] result = string.toCharArray();
       for (int j = 0; j < n; ++j, ++i) {
@@ -14,10 +14,10 @@ import java.io.*;
         }
       }
       return new String(result, 0, i);
-	}	  
+    }	  
 	  
-	public static String removeDuplicates1(String string) {        
-	  java.util.Stack<Character> stack = new java.util.Stack<Character>();
+    public static String removeDuplicates1(String string) {        
+      java.util.Stack<Character> stack = new java.util.Stack<Character>();
       for(char s : string.toCharArray()) {
         if(!stack.isEmpty() && stack.peek() == s) {
            stack.pop();
@@ -32,16 +32,16 @@ import java.io.*;
       }
      
       return sb.toString(); 
-	}
+    }
 
-	public static void main(String[] args) { 	  
-	  String string = new String();	  
+    public static void main(String[] args) { 	  
+      String string = new String();	  
       Scanner input = new Scanner(System.in);
       System.out.print("Enter the string: ");
       string = input.nextLine();  
       String result = removeDuplicates(string);
-	  System.out.println();
-	  System.out.println("The new string after removing all adjacent duplicates is: " + result);  
-	  input.close();
-	}
+      System.out.println();
+      System.out.println("The new string after removing all adjacent duplicates is: " + result);  
+      input.close();
+    }
   }
