5b14ff30d02361a51a88ca36a72f4979b04ea434
Aditya Gupta <contactadityag@gmail.com>
2020-03-22 21:01:20 +0530

    This is a program to find the element that is repeated exactly N times in an array of size 2N elements containing exactly N+1 unique elements.

--- Arrays/NRepeatedElementInSize2NArray.java
+++ Arrays/NRepeatedElementInSize2NArray.java
@@ -3,37 +3,37 @@ import java.io.*;
 
   public class NRepeatedElementInSize2NArray {
 	  
-	public static int repeatedNTimes(int[] array) {
-	  Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+    public static int repeatedNTimes(int[] array) {
+      Map<Integer, Integer> count = new HashMap<Integer, Integer>();
 
-	  for (int x : array) {
+      for (int x : array) {
         count.put(x, count.getOrDefault(x, 0) + 1);
       }
 	  
-	  for (int k : count.keySet()) {
+      for (int k : count.keySet()) {
         if (count.get(k) > 1) {
           return k;
         }
-	  }
+      }
 	  
-	  throw null;
+      throw null;
     }
 
-	public static void main(String[] args) { 	 
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+    public static void main(String[] args) { 	 
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the array: ");
+      int numbers = input.nextInt();   
+      int array[] = new int[numbers];
 
-	  System.out.println();	
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt();
-	  }
+      System.out.println();	
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < numbers; i++) {
+	array[i] = input.nextInt();
+      }
 	
-	  int result = repeatedNTimes(array); 
-	  System.out.println();	
-	  System.out.println("The element repeated " + numbers/2 + " times is: " + result); 
-	  input.close();
-	}
+      int result = repeatedNTimes(array); 
+      System.out.println();	
+      System.out.println("The element repeated " + numbers/2 + " times is: " + result); 
+      input.close();
+    }
   }
