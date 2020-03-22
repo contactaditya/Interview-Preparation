a8965fbbb59c949a3f98e889dd14271c979852c5
contactaditya <contactadityag@gmail.com>
2020-03-22 21:31:50 +0530

    This is a program to find the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1.
    F(0) = 0,   F(1) = 1
    
    F(N) = F(N - 1) + F(N - 2), for N > 1.

---
+++ Numbers/FibonacciNumber.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+ public class FibonacciNumber {
+	 
+	public static int fibonacci(int number) {
+	  if(number == 0) {
+	    return 0; 
+	  } 
+	  else if(number <= 2) {
+	    return 1; 
+	  } 
+	  int current = 0;
+	  int previous1 = 1;
+	  int previous2 = 1;
+	         
+	  for (int i = 3; i <= number; i++) {
+	    current = previous1 + previous2;
+	    previous2 = previous1;
+	    previous1 = current;
+	  }
+	         
+	  return current; 
+	} 
+
+	public static void main(String[] args) { 
+	  Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int number = input.nextInt();  
+ 
+      int result = fibonacci(number);
+      System.out.println();	
+      System.out.print("The fibonacci sequence for " + number + " such that each number is the sum of the two preceding ones starting from 0 and 1 is: " + result);
+	}
+ }
