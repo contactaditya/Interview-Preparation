b259baa42e7d057bc185681cee35e4ac3a207b10
Aditya Gupta <contactadityag@gmail.com>
2020-04-03 03:27:04 +0530

    This is a program to return the minimum number of steps to make the given string empty.
    Given a string s consisting only of letters 'a' and 'b'. In a single step you can remove one palindromic subsequence from s.
    
    A string is a subsequence of a given string, if it is generated by deleting some characters of a given string without changing its order.

--- Strings/RemovePalindromicSubsequences.java
+++ Strings/RemovePalindromicSubsequences.java
@@ -3,40 +3,40 @@ import java.io.*;
 
  public class RemovePalindromicSubsequences {
 	 
-	public static int removePalindromeSubsequences(String string) { 	  
-	  if (string.isEmpty()) {
-		return 0;
+    public static int removePalindromeSubsequences(String string) { 	  
+      if (string.isEmpty()) {
+	return 0;
       }
 	  
-	  if(isPalindrome(string)) {
-		return 1;
-	  }
+      if(isPalindrome(string)) {
+	return 1;
+      }
 	  
-	  return 2;      
+      return 2;      
+    }
+
+    private static boolean isPalindrome(String string) {
+      int left = 0, right = string.length() - 1;
+      while(left < right) {
+        if(string.charAt(left) != string.charAt(right)) {
+	  return false;
 	}
+	left++;
+	right--;
+      }
+      return true;
+    }
 
-	private static boolean isPalindrome(String string) {
-	  int left = 0, right = string.length() - 1;
-	  while(left < right) {
-	    if(string.charAt(left) != string.charAt(right)) {
-	      return false;
-	    }
-	    left++;
-	    right--;
-	  }
-	  return true;
-	}
+    public static void main(String[] args) { 	  
+      String string = new String();	  
+      Scanner input = new Scanner(System.in);
 
-	public static void main(String[] args) { 	  
-	  String string = new String();	  
-	  Scanner input = new Scanner(System.in);
-
-	  System.out.print("Enter the string: ");
-	  string = input.nextLine();
+      System.out.print("Enter the string: ");
+      string = input.nextLine();
 		  
-	  int minimumNumberOfSteps = removePalindromeSubsequences(string);
-	  System.out.println();
-	  System.out.println("The minimum number of steps to make the given string empty is: " + minimumNumberOfSteps);  
+      int minimumNumberOfSteps = removePalindromeSubsequences(string);
+      System.out.println();
+      System.out.println("The minimum number of steps to make the given string empty is: " + minimumNumberOfSteps);  
       input.close();
-	}
+    }
  }
