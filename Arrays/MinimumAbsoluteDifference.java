ceae22b8305ce2d597b34341ef6b618aff67e8b8
Aditya Gupta <contactadityag@gmail.com>
2020-04-03 03:43:07 +0530

    This is a program to find all pairs of elements with the minimum absolute difference of any two elements.
    Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows:
    
    a, b are from arr
    
    a < b
    
    b - a equals to the minimum absolute difference of any two elements in arr

--- Arrays/MinimumAbsoluteDifference.java
+++ Arrays/MinimumAbsoluteDifference.java
@@ -3,53 +3,53 @@ import java.io.*;
 
  public class MinimumAbsoluteDifference {
 	
-    public static List<List<Integer>> minimumAbsDifference(int[] array) {
-      List<List<Integer>> result = new ArrayList<List<Integer>>();	
-      if(array.length == 0){
-        return result;
-      }
-      List<Integer> list = new ArrayList<Integer>();
+   public static List<List<Integer>> minimumAbsDifference(int[] array) {
+     List<List<Integer>> result = new ArrayList<List<Integer>>();	
+     if(array.length == 0) {
+       return result;
+     }
+     List<Integer> list = new ArrayList<Integer>();
       
-      Arrays.sort(array);
-      int minimumDifference = Integer.MAX_VALUE;
-      int difference = 0;
+     Arrays.sort(array);
+     int minimumDifference = Integer.MAX_VALUE;
+     int difference = 0;
       
-      // Find the value of the minimum difference in the array
-      for(int i = 0; i < array.length - 1; i++) { 
-        minimumDifference = Math.min(array[i + 1] - array[i], minimumDifference); 
-      }
+     // Find the value of the minimum difference in the array
+     for(int i = 0; i < array.length - 1; i++) { 
+       minimumDifference = Math.min(array[i + 1] - array[i], minimumDifference); 
+     }
       
-      // If the differene between any two array elements is equal to the minimum difference then they are added to the array
-      for(int i = 0;i < array.length-1; i++) {
-        if(array[i + 1] - array[i] == minimumDifference) {
-          result.add(Arrays.asList(array[i], array[i + 1]));
-        }
-      }
+     // If the differene between any two array elements is equal to the minimum difference then they are added to the array
+     for(int i = 0;i < array.length-1; i++) {
+       if(array[i + 1] - array[i] == minimumDifference) {
+         result.add(Arrays.asList(array[i], array[i + 1]));
+       }
+     }
      
-      return result; 
-    }
+     return result; 
+   }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of elements in the array: ");
-	  int numbers = input.nextInt();   
-	  int array[] = new int[numbers];
+   public static void main(String[] args) {   
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter number of elements in the array: ");
+     int numbers = input.nextInt();   
+     int array[] = new int[numbers];
 			    
-	  System.out.println();
-	  System.out.print("Please enter the actual values in the array: ");
-	  for (int i = 0; i < numbers; i++) {
-		array[i] = input.nextInt(); 
-	  }
+     System.out.println();
+     System.out.print("Please enter the actual values in the array: ");
+     for (int i = 0; i < numbers; i++) {
+       array[i] = input.nextInt(); 
+     }
 		  
-	  List<List<Integer>> result = new ArrayList<List<Integer>>();	
-	  result = minimumAbsDifference(array); 
-	  System.out.println();
-	  System.out.print("The solution set is: ");
-	  System.out.print('[');
-	  for(List<Integer> innerList : result) {
-		System.out.print(innerList + ",");
-	  }
-	  System.out.println(']');
-	  input.close();
-	}
+     List<List<Integer>> result = new ArrayList<List<Integer>>();	
+     result = minimumAbsDifference(array); 
+     System.out.println();
+     System.out.print("The solution set is: ");
+     System.out.print('[');
+     for(List<Integer> innerList : result) {
+       System.out.print(innerList + ",");
+     }
+     System.out.println(']');
+     input.close();
+   }
  }
