379bcf131f3ba8710cc9351627a44ad7eb901864
Aditya Gupta <contactadityag@gmail.com>
2020-04-03 03:07:55 +0530

    This is a program to find all the different possible increasing subsequences in the given integer array.  The length of an increasing subsequence should be at least 2.

--- Arrays/IncreasingSubsequences.java
+++ Arrays/IncreasingSubsequences.java
@@ -3,32 +3,32 @@ import java.io.*;
 
   public class IncreasingSubsequences {
 	  
-	public static List<List<Integer>> findSubsequences(int[] array) {
-	  List<List<Integer>> result = new ArrayList<List<Integer>>();
+    public static List<List<Integer>> findSubsequences(int[] array) {
+      List<List<Integer>> result = new ArrayList<List<Integer>>();
       List<Integer> list = new ArrayList<Integer>();
       findSequence(array, 0, result, list);
       return result;    
-	}	  
+    }	  
 
-	private static void findSequence(int[] array, int index, List<List<Integer>> result, List<Integer> list) {
-	  if (index == array.length) {
+    private static void findSequence(int[] array, int index, List<List<Integer>> result, List<Integer> list) {
+      if (index == array.length) {
         if (list.size() > 1) {
           result.add(new ArrayList<>(list));
         }
         return ;
       }
-	  if (list.isEmpty() || list.get(list.size() - 1) <= array[index]) {
+      if (list.isEmpty() || list.get(list.size() - 1) <= array[index]) {
         list.add(array[index]);
         findSequence(array, index + 1, result, list);
         list.remove(list.size() - 1);
       }
-	  if (list.isEmpty() || list.get(list.size() - 1) != array[index]) {
-		findSequence(array, index + 1, result, list);
+      if (list.isEmpty() || list.get(list.size() - 1) != array[index]) {
+	findSequence(array, index + 1, result, list);
       }
-	}
+    }
 	
-	public static void main(String[] args) {    
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {    
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int number = input.nextInt();  
       int array[] = new int[number];
@@ -42,10 +42,10 @@ import java.io.*;
       result = findSubsequences(array);  
       System.out.println();	 
       System.out.print("All the different possible increasing subsequences of the given array are: ");
-	  System.out.print('[');
-	  for(List<Integer> innerList : result) {
-		System.out.print(innerList + ", ");
-	  }
-	  System.out.print(']');
-	}
+      System.out.print('[');
+      for(List<Integer> innerList : result) {
+	System.out.print(innerList + ", ");
+      }
+      System.out.print(']');
+    }
   }
