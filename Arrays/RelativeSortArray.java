565805d4bf4b9e82459b5926cbbf2eaf91e3ca2d
contactaditya <contactadityag@gmail.com>
2020-04-12 02:51:50 +0530

    This is a program to sort the elements of array1 such that the relative ordering of items in array1 are the same as in array2.
    We are given two arrays array1 and array2, the elements of array2 are distinct, and all elements in array2 are also in array1.
    
    Elements that don't appear in array2 should be placed at the end of array1 in ascending order.

---
+++ Arrays/RelativeSortArray.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+  public class RelativeSortArray {
+	  
+	public static int[] relativeSortArray(int[] array1, int[] array2) {
+      int count[] = new int[1001];
+      int k = 0;
+      int result[] = new int[array1.length];
+      // Count each number in array1
+      for(int i : array1) {
+    	count[i]++;
+      }
+      // Sort the common numbers in both arrays by the order of array2
+      for(int i : array2) {
+        while(count[i]-- > 0) {
+          result[k++] = i;
+        }
+      }
+      // Sort the numbers only in array1
+      for(int i = 0; i < 1001; i++) {
+        while(count[i]-- > 0) {
+          result[k++] = i;
+        }
+      }
+     
+	  return result;
+    }	  
+
+	public static void main(String[] args) { 
+	  Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of elements in the first array: ");
+      int number = input.nextInt();  
+      int array1[] = new int[number];
+		  
+      System.out.println();	
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < array1.length; i++) {
+	    array1[i] = input.nextInt();
+      }
+		    
+      System.out.println();	
+      System.out.print("Enter number of elements in the second array: ");
+      int number1 = input.nextInt();  
+      int array2[] = new int[number1];
+		  
+      System.out.println();	
+      System.out.print("Please enter the actual values in the array: ");
+      for (int i = 0; i < array2.length; i++) {
+	    array2[i] = input.nextInt();
+      }
+		  
+      int result[] = new int[array1.length];
+		  
+      result = relativeSortArray(array1, array2);
+      System.out.println();	
+      System.out.print("The relative ordering of the items in array1 according to the items in the array2 is: ");
+      for(int i = 0; i < result.length; i++) {
+	    System.out.print(result[i] + " ");
+      }
+      input.close();
+	}
+  }
