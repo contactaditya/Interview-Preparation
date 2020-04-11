e3e73fb91ec1efff41d8c4825660ec1564a0acd2
Aditya Gupta <contactadityag@gmail.com>
2020-04-12 03:03:54 +0530

    This is a program to find the sum of lengths of all good strings in words.
    A string is good if it can be formed by characters from chars (each character can only be used once).

--- Strings/FindWordsThatCanBeFormedByCharacters.java
+++ Strings/FindWordsThatCanBeFormedByCharacters.java
@@ -18,40 +18,40 @@ import java.io.*;
       return result;  
     }
 
-	private static boolean wordExist(String word, int[] copySet) {
-	  for(int i = 0; i < word.length(); i++) {
-		if(copySet[(word.charAt(i) - 'a')] == 0) {
+    private static boolean wordExist(String word, int[] copySet) {
+      for(int i = 0; i < word.length(); i++) {
+	if(copySet[(word.charAt(i) - 'a')] == 0) {
           return false;
-		}
+	}
         else {
           copySet[(word.charAt(i) - 'a')]--;
         }  
-	  }
-	  return true;
-	}
+      }
+      return true;
+    }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter number of words: ");
-	  int numbers = input.nextInt();   
-	  String words[] = new String[numbers];		    
-	  System.out.println();
-	  input.nextLine();
-	  System.out.print("Please enter the actual words: ");
-	  System.out.println();
-	  for (int i = 0; i < numbers; i++) {
-		words[i] = input.nextLine(); 
-	  }
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter number of words: ");
+      int numbers = input.nextInt();   
+      String words[] = new String[numbers];		    
+      System.out.println();
+      input.nextLine();
+      System.out.print("Please enter the actual words: ");
+      System.out.println();
+      for (int i = 0; i < numbers; i++) {
+	words[i] = input.nextLine(); 
+      }
 	  
-	  System.out.println();	
-	  String chars = new String();	  
-	  System.out.print("Enter the string containing the characters: ");
-	  chars = input.next();
+      System.out.println();	
+      String chars = new String();	   
+      System.out.print("Enter the string containing the characters: ");
+      chars = input.next();
 	  
-	  int countCharacters = countCharacters(words, chars);
-	  System.out.println();	
-	  System.out.println("The sum of lengths of all good strings in words is: " + countCharacters);
+      int countCharacters = countCharacters(words, chars);
+      System.out.println();	
+      System.out.println("The sum of lengths of all good strings in words is: " + countCharacters);
 	  
-	  input.close();
-	}
+      input.close();
+    }
   }
