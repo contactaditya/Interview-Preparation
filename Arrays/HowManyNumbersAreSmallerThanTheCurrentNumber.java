058d803bcccae26cfa446f4d1d2bc727d5add38c
Aditya Gupta <contactadityag@gmail.com>
2020-04-12 03:14:57 +0530

    This is a program to find out for each element in the array how many numbers in the array are smaller than it.
    That is, for each array[i] you have to count the number of valid j's such that j != i and array[j] < array[i].

--- Arrays/HowManyNumbersAreSmallerThanTheCurrentNumber.java
+++ Arrays/HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -3,32 +3,32 @@ import java.io.*;
 
   public class HowManyNumbersAreSmallerThanTheCurrentNumber {
 	
-	public static int[] smallerNumbersThanCurrent(int[] array) {
-	  int count[] = new int[101];
-	  int result[] = new int[array.length];
+    public static int[] smallerNumbersThanCurrent(int[] array) {
+      int count[] = new int[101];
+      int result[] = new int[array.length];
 
-	  // We are doing the frequency count here
-	  for(int i = 0; i < array.length; i++) {
-	    count[array[i]]++;
-	  }
+      // We are doing the frequency count here
+      for(int i = 0; i < array.length; i++) {
+	count[array[i]]++;
+      }
 	     
-	  // Here we are counting the numbers in the array less than or equal to the number 
-	  for (int i = 1 ; i <= 100; i++) {
-	    count[i] += count[i-1];    
-	  }
+      // Here we are counting the numbers in the array less than or equal to the number 
+      for (int i = 1 ; i <= 100; i++) {
+	count[i] += count[i-1];    
+      }
 	      
-	  for (int i = 0; i < array.length; i++) {
+      for (int i = 0; i < array.length; i++) {
         if(array[i] == 0) {
-		  result[i] = 0;	
-		} else {
-		  result[i] = count[array[i] - 1];
-		}
-	  }
-	  return result;    
+          result[i] = 0;	
+	} else {
+	  result[i] = count[array[i] - 1];
+	}
+      }
+      return result;    
     }
 
-	public static void main(String[] args) {      
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {      
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int numbers = input.nextInt();   
       int array[] = new int[numbers];
@@ -45,5 +45,5 @@ import java.io.*;
         System.out.print(result[i] + " ");
       }
       input.close();
-	}
+    }
   }
