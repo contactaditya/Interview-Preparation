912b3fccaf66e875b82b4060c4e218185a1ff2b1
Aditya Gupta <contactadityag@gmail.com>
2020-04-12 04:01:48 +0530

    This is a program to convert a BST to to a Greater Tree.

--- Binary Search Trees/ConvertBSTToGreaterTree.java
+++ Binary Search Trees/ConvertBSTToGreaterTree.java
@@ -2,11 +2,11 @@ import java.util.*;
 import java.io.*;
 
   public class ConvertBSTToGreaterTree {
-	Node root;	    
-	Node previous;
-	int sum = 0;
+    Node root;	    
+    Node previous;
+    int sum = 0;
 
-	public Node convertBST(Node root) {
+    public Node convertBST(Node root) {
       if (root != null) {
         convertBST(root.right);
         sum += root.data;
@@ -14,27 +14,27 @@ import java.io.*;
         convertBST(root.left);     
       }
       return root;
-	}	
+    }	
 	  
-	public void printInorderTraversal(Node node) {
-	  if (node == null) {
-		return;
-	  }
+    public void printInorderTraversal(Node node) {
+      if (node == null) {
+	return;
+      }
 		 
-	  printInorderTraversal(node.left);
-	  System.out.print(node.data + " ");	
-	  printInorderTraversal(node.right);
-	}  
+      printInorderTraversal(node.left);
+      System.out.print(node.data + " ");	
+      printInorderTraversal(node.right);
+    }  
 
-	public static void main(String[] args) {
-	  ConvertBSTToGreaterTree tree = new ConvertBSTToGreaterTree();
-	  tree.root = new Node(5);
-	  tree.root.left = new Node(2);
-	  tree.root.right = new Node(13);
+    public static void main(String[] args) {
+      ConvertBSTToGreaterTree tree = new ConvertBSTToGreaterTree();
+      tree.root = new Node(5);
+      tree.root.left = new Node(2);
+      tree.root.right = new Node(13);
 	      
-	  Node node = tree.convertBST(tree.root);
+      Node node = tree.convertBST(tree.root);
 		     
-	  System.out.print("Inorder Traversal of the resultant tree is: ");
-	  tree.printInorderTraversal(node);	
+      System.out.print("Inorder Traversal of the resultant tree is: ");
+      tree.printInorderTraversal(node);	
     }
   }
