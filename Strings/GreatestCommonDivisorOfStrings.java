4e5059e99fc384fee3c32968611e04050b565ca6
contactaditya <contactadityag@gmail.com>
2020-04-12 02:57:07 +0530

    This is a program to find the largest string X such that X divides string1 and X divides string2.
    For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T concatenated with itself 1 or more times).

---
+++ Strings/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+  public class GreatestCommonDivisorOfStrings {
+	
+	public static String gcdOfStrings(String string1, String string2) {
+      if(string2.length() == 0) {
+    	return string1;
+      }
+		
+	  if(string1.length() < string2.length()) {
+	    return gcdOfStrings(string2, string1);
+	  } 
+	  
+	  if(string1.startsWith(string2)) {
+		string1 = string1.substring(string2.length());
+		return gcdOfStrings(string1, string2);
+	  }
+	  return "";
+	}
+
+	public static void main(String[] args) { 	 
+	  String string1 = new String();	  
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("Enter the first string: ");
+	  string1 = input.nextLine();  
+	  System.out.println();	
+	  String string2 = new String();	  
+	  System.out.print("Enter the second string: ");
+	  string2 = input.nextLine();
+		  
+	  String result = gcdOfStrings(string1, string2);
+	  System.out.println();
+	  System.out.print("The greatest common divisor of strings " + string1 + " and " + string2 + " is: " + result);
+	  input.close();
+	}
+  }
