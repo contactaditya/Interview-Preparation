55337f42605364bf8b36ed09ca4afa8766500c16
Aditya Gupta <contactadityag@gmail.com>
2020-04-12 05:51:53 +0530

    This is a program to find the level with the maximum sum in a Binary Tree.

--- Binary Search Trees/MaximumLevelSumOfABinaryTree.java
+++ Binary Search Trees/MaximumLevelSumOfABinaryTree.java
@@ -2,26 +2,26 @@ import java.util.*;
 import java.io.*;
 
   public class MaximumLevelSumOfABinaryTree { 	
-	Node root;	    
-	Node previous;
-	int maxDepth = 0;
+    Node root;	    
+    Node previous; 
+    int maxDepth = 0;
 	
-	public int maxLevelSum(Node root) {
-	  int[] sum = new int[10001];
-	  calculateLevelSum(root, 0, sum);
-	  int depth = 0, max = root.data;
+    public int maxLevelSum(Node root) {
+      int[] sum = new int[10001];
+      calculateLevelSum(root, 0, sum);
+      int depth = 0, max = root.data;
 	  
-	  for (int i = 1; i < maxDepth; i++) {
+      for (int i = 1; i < maxDepth; i++) {
         if (sum[i] > max) {
           max = sum[i];
           depth = i;
         }
       }
      
-	  return depth + 1;      
-	}
+      return depth + 1;      
+    }
 	
-	private void calculateLevelSum(Node root, int depth, int sum[]) {    
+    private void calculateLevelSum(Node root, int depth, int sum[]) {    
       if(root == null) {
         return;
       }
@@ -36,15 +36,15 @@ import java.io.*;
       calculateLevelSum(root.right, depth + 1, sum);
     }
 
-	public static void main(String[] args) {   
-	  MaximumLevelSumOfABinaryTree tree = new MaximumLevelSumOfABinaryTree();
-	  tree.root = new Node(1);
-	  tree.root.left = new Node(7);
-	  tree.root.right = new Node(0);
-	  tree.root.left.left = new Node(7);
+    public static void main(String[] args) {   
+      MaximumLevelSumOfABinaryTree tree = new MaximumLevelSumOfABinaryTree();
+      tree.root = new Node(1);
+      tree.root.left = new Node(7);
+      tree.root.right = new Node(0);
+      tree.root.left.left = new Node(7);
       tree.root.left.right = new Node(-8);
         
-	  int maxLevelSum = tree.maxLevelSum(tree.root);		
+      int maxLevelSum = tree.maxLevelSum(tree.root);		
       System.out.print("The level with the maximum sum is: " + maxLevelSum);
-	}
+    }
   }
