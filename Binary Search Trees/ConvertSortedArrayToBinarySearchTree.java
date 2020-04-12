cc8905435b50940e6e277e01da13532f524ce9fa
contactaditya <contactadityag@gmail.com>
2020-04-13 04:35:14 +0530

    This is a program to convert an array where elements are sorted in ascending order to a height balanced BST.
    For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.

--- Binary Search Trees/ConvertSortedArrayToBinarySearchTree.java
+++ Binary Search Trees/ConvertSortedArrayToBinarySearchTree.java
@@ -2,57 +2,59 @@ import java.util.*;
 import java.io.*;
 
   public class ConvertSortedArrayToBinarySearchTree {
-    Node root;	    
-    Node previous;
+	static Node root;	    
+	static Node previous;
 	  
-    public static Node sortedArrayToBST(int[] array) {
-      if (array.length == 0) {
-	return null;
-      }	
+	public Node sortedArrayToBST(int[] array) {
+	  if (array.length == 0) {
+	    return null;
+	  }	
 		
-      return sortedArrayToBST(array, 0, array.length - 1);    
-    }	  
+	  return sortedArrayToBST(array, 0, array.length - 1);    
+	}	  
  
-    private static Node sortedArrayToBST(int[] array, int start, int end) {
-      if (start > end) {
-        return null;
-      }
+	private Node sortedArrayToBST(int[] array, int start, int end) {
+	  if (start > end) {
+	    return null;
+	  }
  
-      int mid = (start + end) / 2;
-      Node root = new Node(array[mid]);
-      root.left = sortedArrayToBST(array, start, mid - 1);
-      root.right = sortedArrayToBST(array, mid + 1, end);
+	  int mid = (start + end) / 2;
+	  Node root = new Node(array[mid]);
+	  root.left = sortedArrayToBST(array, start, mid - 1);
+	  root.right = sortedArrayToBST(array, mid + 1, end);
  
-      return root;
-    }
+	  return root;
+	}
 	
-    public static void printInorderTraversal(Node node) { 
-      if (node == null) {
-	return;
-      }
+	public void printInorderTraversal(Node node) { 
+	  if (node == null) {
+		return;
+	  }
 			 
-      printInorderTraversal(node.left);
-      System.out.print(node.data + " ");	
-      printInorderTraversal(node.right);
-    }
+	  printInorderTraversal(node.left);
+	  System.out.print(node.data + " ");	
+	  printInorderTraversal(node.right);
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+	  ConvertSortedArrayToBinarySearchTree tree = new ConvertSortedArrayToBinarySearchTree();	
       Scanner input = new Scanner(System.in);
-      System.out.print("Enter number of elements in the array: ");
-      int number = input.nextInt();  
-      int array[] = new int[number];
+	  System.out.print("Enter number of elements in the array: ");
+	  int number = input.nextInt();  
+	  int array[] = new int[number];
 			
-      System.out.println();	
-      System.out.print("Please enter the elements of the array: ");
-      for (int i = 0; i < array.length; i++) {
-	array[i] = input.nextInt();
-      }
+	  System.out.println();	
+	  System.out.print("Please enter the elements of the array: ");
+	  for (int i = 0; i < array.length; i++) {
+		array[i] = input.nextInt();
+	  }
 	   
-      Arrays.sort(array);
+	  Arrays.sort(array);
 			  
-      Node node = sortedArrayToBST(array);
-      System.out.println();	
-      System.out.print("Inorder traversal of constructed tree is: ");
-      printInorderTraversal(node);		
-    }
+	  root = tree.sortedArrayToBST(array);
+	  System.out.println();	
+	  System.out.print("Inorder traversal of constructed tree is: ");
+	  tree.printInorderTraversal(root);		
+	  input.close();
+	}
   }
