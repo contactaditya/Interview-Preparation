c22cc81994621e048c10e9f14f94d1744d70b438
Aditya Gupta <contactadityag@gmail.com>
2020-04-13 04:55:18 +0530

    This is a program to return a balanced binary search tree with the same node values if you are given a binary search tree.
    A binary search tree is balanced if and only if the depth of the two subtrees of every node never differ by more than 1.

--- Binary Search Trees/BalanceABinarySearchTree.java
+++ Binary Search Trees/BalanceABinarySearchTree.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.io.*;
 
   public class BalanceABinarySearchTree { 	
-	Node root;	    
-	Node previous;
+    Node root;	    
+    Node previous;
 	  
-	public Node balanceBST(Node root) {
-	  List<Integer> list = new ArrayList<Integer>();
+    public Node balanceBST(Node root) {
+      List<Integer> list = new ArrayList<Integer>();
       inorderTraversal(root, list);
       return generateBST(list, 0, list.size() - 1); 
-	}
+    }
 	
     private Node generateBST(List<Integer> list, int left, int right) {
       if(right < left) {
@@ -21,21 +21,21 @@ import java.io.*;
       root.right = generateBST(list, middle + 1, right);
 		
       return root;
-	}
+    }
 
-	private void inorderTraversal(Node root, List<Integer> list) {	 
-	  if (root == null) {
-	    return;
-	  }
+    private void inorderTraversal(Node root, List<Integer> list) {	 
+      if (root == null) {
+	return;
+      }
 		 
-	  inorderTraversal(root.left, list);
-	  list.add(root.data);	
-	  inorderTraversal(root.right, list);
-	}
+      inorderTraversal(root.left, list);
+      list.add(root.data);	
+      inorderTraversal(root.right, list);
+    }
 	
-	private void printPreorderTraversal(Node node) {	 
+    private void printPreorderTraversal(Node node) {	 
       if (node == null) {
-  	    return;
+  	return;
       }
       
       System.out.print(node.data + " ");		  
@@ -43,16 +43,16 @@ import java.io.*;
       printPreorderTraversal(node.right);
     }	
 
-	public static void main(String[] args) { 
-	  BalanceABinarySearchTree tree = new BalanceABinarySearchTree();
-	  tree.root = new Node(1);
-	  tree.root.right = new Node(2);
-	  tree.root.right.right = new Node(3);
-	  tree.root.right.right.right = new Node(4);
+    public static void main(String[] args) { 
+      BalanceABinarySearchTree tree = new BalanceABinarySearchTree();
+      tree.root = new Node(1);
+      tree.root.right = new Node(2);
+      tree.root.right.right = new Node(3);
+      tree.root.right.right.right = new Node(4);
 	  	  
-	  tree.root = tree.balanceBST(tree.root);
+      tree.root = tree.balanceBST(tree.root);
 	  
-	  System.out.print("Preorder traversal of binary tree is: ");
-	  tree.printPreorderTraversal(tree.root);		
-	}
+      System.out.print("Preorder traversal of binary tree is: ");
+      tree.printPreorderTraversal(tree.root);		
+    }
   }
