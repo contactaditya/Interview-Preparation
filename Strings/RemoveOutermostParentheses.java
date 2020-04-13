21c9fa6c7153a0e6d93c8424cb4b17618f066eb2
Aditya Gupta <contactadityag@gmail.com>
2020-04-13 07:02:59 +0530

    This is a program to return a string S after removing the outermost parentheses of every primitive string in the primitive decomposition of the string S.
    A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.

--- Strings/RemoveOutermostParentheses.java
+++ Strings/RemoveOutermostParentheses.java
@@ -3,25 +3,25 @@ import java.io.*;
 
   public class RemoveOutermostParentheses {     
 	
-	public static String removeOuterParentheses(String string) {
-	  StringBuilder result = new StringBuilder();
-	  int open = 0, close = 0, start = 0;
-	  for(int i = 0; i < string.length(); i++) {
-		if(string.charAt(i) == '(') {
-		  open++;	
-		} else if (string.charAt(i) == ')') {
-		  close++;	
-		}
-		if(open == close) {
-		  result.append(string.substring(start + 1, i));
+    public static String removeOuterParentheses(String string) {
+      StringBuilder result = new StringBuilder();
+      int open = 0, close = 0, start = 0;
+      for(int i = 0; i < string.length(); i++) {
+	if(string.charAt(i) == '(') {
+	  open++;	
+	} else if (string.charAt(i) == ')') {
+	  close++;	
+	}
+	if(open == close) {
+          result.append(string.substring(start + 1, i));
           start = i + 1;	
-		}
-	  }
-	  return result.toString();
+	}
+      }
+      return result.toString();
     }
 
-	public static void main(String[] args) {     
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {     
+      Scanner input = new Scanner(System.in);
       String string = new String();	
       System.out.print("Please enter an string with only these characters (): ");
       string = input.next();  
@@ -29,5 +29,5 @@ import java.io.*;
       String result = removeOuterParentheses(string);
       System.out.println();
       System.out.println("The final string after removing the outermost parentheses of every primitive string in the primitive decomposition of the string is: " + result);
-	}
+    }
   }
