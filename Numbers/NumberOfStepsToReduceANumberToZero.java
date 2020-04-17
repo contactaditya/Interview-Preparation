247736c57cc563b572e1900ed60267e41120b0a0
Aditya Gupta <contactadityag@gmail.com>
2020-04-18 03:22:56 +0530

    This is a program to return the number of steps to reduce a non negative integer to zero.
    If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.

--- Numbers/NumberOfStepsToReduceANumberToZero.java
+++ Numbers/NumberOfStepsToReduceANumberToZero.java
@@ -3,22 +3,22 @@ import java.io.*;
 
   public class NumberOfStepsToReduceANumberToZero {
 	
-    public static int numberOfSteps(int number) {
-	  int numberOfSteps = 0;
-	  while(number != 0) {
-	    if(number % 2 == 0) {
-		 number = number/2;
-		 numberOfSteps++;
-	    } else {
-		 number = number - 1;
-		 numberOfSteps++;
-	    }
-	  }
+    public static int numberOfSteps(int number) { 
+      int numberOfSteps = 0;
+      while(number != 0) {
+        if(number % 2 == 0) {
+          number = number/2;
+          numberOfSteps++;
+	} else {
+          number = number - 1;
+          numberOfSteps++;
+	}
+      }
       return numberOfSteps;  
     }
 
-	public static void main(String[] args) {      
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {      
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int number = input.nextInt();  
 			
@@ -27,5 +27,5 @@ import java.io.*;
       System.out.println();	
       System.out.print("The number of steps needed to reduce a number to zero are: " + numberOfSteps);
       input.close();
-	}
+    }
   }
