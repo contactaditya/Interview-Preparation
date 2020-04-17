951cd23406b5a0a224d97dd42617c9342938162d
Aditya Gupta <contactadityag@gmail.com>
2020-04-18 03:28:27 +0530

    This is a program to return all strings from an array of strings called words which is a substring of another word in any order.
    String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].

--- Strings/StringMatchingInAnArray.java
+++ Strings/StringMatchingInAnArray.java
@@ -3,8 +3,8 @@ import java.io.*;
 
   public class StringMatchingInAnArray {
 	  
-	public static List<String> stringMatching(String[] words) {
-	  HashSet<String> set = new HashSet<String>();
+    public static List<String> stringMatching(String[] words) {
+      HashSet<String> set = new HashSet<String>();
       
       for(int i = 0; i < words.length - 1; i++) {
     	String currentWord = words[i];
@@ -20,11 +20,11 @@ import java.io.*;
       }
 		
       List<String> result = new ArrayList<String>(set); 	
-	  return result;      
-	}	  
+      return result;      
+    }	  
 
-	public static void main(String[] args) {      
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {      
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of words: ");
       int numbers = input.nextInt();   
       String words[] = new String[numbers];
@@ -34,7 +34,7 @@ import java.io.*;
       System.out.print("Please enter the actual words: ");
       System.out.println();
       for (int i = 0; i < numbers; i++) {
-	    words[i] = input.nextLine(); 
+        words[i] = input.nextLine(); 
       }
 	  
       List<String> result = new ArrayList<String>(); 
@@ -42,5 +42,5 @@ import java.io.*;
       System.out.println();
       System.out.println("All strings in words which are substring of another word in any order are: " + result);	
       input.close();
-	}
+    }
   }
