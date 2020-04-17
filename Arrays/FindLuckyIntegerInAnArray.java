859fc8271cf6c33b114c9bc9b4ec3170cb3ce46a
Aditya Gupta <contactadityag@gmail.com>
2020-04-18 05:21:01 +0530

    This is a program to return a lucky integer in the array.
    A lucky integer is an integer which has a frequency in the array equal to its value.
    
    If there are multiple lucky integers return the largest of them. If there is no lucky integer return -1.

--- Arrays/FindLuckyIntegerInAnArray.java
+++ Arrays/FindLuckyIntegerInAnArray.java
@@ -3,30 +3,30 @@ import java.io.*;
 
   public class FindLuckyIntegerInAnArray {
 	  
-	public static int findLucky1(int[] array) {
-	  Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    public static int findLucky1(int[] array) {
+      Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
-	  for(int i : array) {
-	    if(!map.containsKey(i)) {
+      for(int i : array) {
+        if(!map.containsKey(i)) {
           map.put(i, 1);
-		}
+	}
         else {
           map.replace(i, map.get(i) + 1);
         }
-	  }
+      }
 	  
-	  int luckyInteger = -1;
-	  for(int i = 0; i < array.length; i++) {
+      int luckyInteger = -1;
+      for(int i = 0; i < array.length; i++) {
         if((map.get(array[i])) == array[i]) {
           luckyInteger = Math.max(array[i], luckyInteger);  
         } 
       }
 	  
-	  return luckyInteger;      
-	}	
+      return luckyInteger;      
+    }	
 	
-	public static int findLucky(int[] array) {
-	  int count[] = new int[501];
+    public static int findLucky(int[] array) {
+      int count[] = new int[501];
       for(int i = 0; i < array.length; i++) {
         count[array[i]]++;
       }
@@ -36,10 +36,10 @@ import java.io.*;
         }
       }
       return -1;  
-	}		
- 
-	public static void main(String[] args) {     
-	  Scanner input = new Scanner(System.in);
+    }		
+  
+    public static void main(String[] args) {     
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of elements in the array: ");
       int numbers = input.nextInt();   
       int array[] = new int[numbers];
@@ -47,11 +47,11 @@ import java.io.*;
       System.out.println();
       System.out.print("Please enter the actual values in the array: ");
       for (int i = 0; i < numbers; i++) {
-	    array[i] = input.nextInt(); 
+        array[i] = input.nextInt(); 
       }
       int luckyNumber = findLucky(array);
       System.out.println();
       System.out.print("The lucky integer in the array is: " + luckyNumber);
       input.close();
-	}
+    }
   }
