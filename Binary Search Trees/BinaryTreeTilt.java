f8c7768bb8a86480ef5f648c02c47cc60fe2c597
Aditya Gupta <contactadityag@gmail.com>
2020-04-18 05:59:36 +0530

    This is a program to find the tilt of the whole binary tree.

--- Binary Search Trees/BinaryTreeTilt.java
+++ Binary Search Trees/BinaryTreeTilt.java
@@ -7,28 +7,27 @@ import java.io.*;
     static int tilt = 0;
 	  
     public static int findTilt(Node root) {
-       traverse(root);
-       return tilt;     
+      traverse(root);
+      return tilt;     
     }  
  
     private static int traverse(Node root) {
-       if(root==null) {
-         return 0;
-       }
-       int left = traverse(root.left);
-       int right = traverse(root.right);
-       tilt += Math.abs(left-right);
-       return left + right + root.data;
+      if(root == null) {
+        return 0;
+      }
+      int left = traverse(root.left);
+      int right = traverse(root.right);
+      tilt += Math.abs(left-right);
+      return left + right + root.data;
     }
 
     public static void main(String[] args) {   
-       BinaryTreeTilt tree = new BinaryTreeTilt();
-       tree.root = new Node(1);
-       tree.root.left = new Node(2);
-       tree.root.right = new Node(3);
+      BinaryTreeTilt tree = new BinaryTreeTilt();
+      tree.root = new Node(1);
+      tree.root.left = new Node(2);
+      tree.root.right = new Node(3);
 	
-       int tilt = findTilt(tree.root); 
-       System.out.println("The tilt of the whole binary tree is: " + tilt);
-	  
+      int tilt = findTilt(tree.root); 
+      System.out.println("The tilt of the whole binary tree is: " + tilt);  
     }
   }
