da0097384bcbb1a84cc7e6d8294b6330dc1a4056
Aditya Gupta <contactadityag@gmail.com>
2020-04-18 06:04:13 +0530

    This is a program to return the zigzag level order traversal of a binary tree.

--- Binary Search Trees/BinaryTreeZigzagLevelOrderTraversal.java
+++ Binary Search Trees/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,54 +1,53 @@
 import java.util.*;
 import java.io.*;
 
- public class BinaryTreeZigzagLevelOrderTraversal {    
+  public class BinaryTreeZigzagLevelOrderTraversal {    
     Node root;	    
     Node previous;
 
     public List<List<Integer>> levelOrder(Node root) { 
-       if (root == null) {
-	  return null;
-       }
+      if (root == null) {
+	return null;
+      }
 	   
-       List<List<Integer>> result = new ArrayList<List<Integer>>();
-       zigzagLevelOrder(root, result, 1); 
-       return result; 
+      List<List<Integer>> result = new ArrayList<List<Integer>>();
+      zigzagLevelOrder(root, result, 1); 
+      return result; 
     }
 
     private void zigzagLevelOrder(Node root, List<List<Integer>> result, int level) {
-       if (root == null) {
-	  return;
-       }
+      if (root == null) {
+	return;
+      }
 	   
-       if (result.size() < level) {
-         ArrayList<Integer> levelList = new ArrayList<Integer>();
-         if ((level % 2) == 1) {
-           levelList.add(root.data);
-         } else {
-           levelList.add(0, root.data);
-         }
-         result.add(levelList);
-       } else {
-    	 if ((level % 2) == 1) {
-           result.get(level - 1).add(root.data);
-         } else {
-           result.get(level - 1).add(0, root.data);
-         }	
-       }
-       zigzagLevelOrder(root.left, result, level+1); 
-       zigzagLevelOrder(root.right, result, level+1); 
+      if (result.size() < level) {
+        ArrayList<Integer> levelList = new ArrayList<Integer>();
+        if ((level % 2) == 1) {
+          levelList.add(root.data);
+        } else {
+          levelList.add(0, root.data);
+        }
+        result.add(levelList);
+      } else {
+    	if ((level % 2) == 1) {
+          result.get(level - 1).add(root.data);
+        } else {
+          result.get(level - 1).add(0, root.data);
+        }	
+      }
+      zigzagLevelOrder(root.left, result, level+1); 
+      zigzagLevelOrder(root.right, result, level+1); 
     }
 
     public static void main(String[] args) { 	 
-       BinaryTreeZigzagLevelOrderTraversal tree = new BinaryTreeZigzagLevelOrderTraversal();
-       tree.root = new Node(3);
-       tree.root.left = new Node(9);
-       tree.root.right = new Node(20);
-       tree.root.right.left = new Node(15);
-       tree.root.right.right = new Node(7);
-       List<List<Integer>> result = new ArrayList<List<Integer>>();
-       result = tree.levelOrder(tree.root);
-       System.out.print("The zigzag level order traversal of the Binary Tree is: " + result);
-	  		
+      BinaryTreeZigzagLevelOrderTraversal tree = new BinaryTreeZigzagLevelOrderTraversal();
+      tree.root = new Node(3);
+      tree.root.left = new Node(9);
+      tree.root.right = new Node(20);
+      tree.root.right.left = new Node(15);
+      tree.root.right.right = new Node(7);
+      List<List<Integer>> result = new ArrayList<List<Integer>>();
+      result = tree.levelOrder(tree.root);
+      System.out.print("The zigzag level order traversal of the Binary Tree is: " + result);  		
     }
- }
+  }
