5b1f75c034365681c30d24178265fd35dd72bd07
Aditya Gupta <contactadityag@gmail.com>
2020-04-19 08:20:18 +0530

    This is a program to return the sum of values of the binary tree's deepest leaves.

--- Binary Search Trees/DeepestLeavesSum.java
+++ Binary Search Trees/DeepestLeavesSum.java
@@ -2,35 +2,35 @@ import java.util.*;
 import java.io.*;
 
   public class DeepestLeavesSum {
-	private Node root;
-	private int maxLevel = 0;
+    private Node root;
+    private int maxLevel = 0;
     private int sum = 0;
 	  
-	public int deepestLeavesSum(Node root) {
-	  if (root == null) {
-		return 0;
-	  }
-	  calculateSumAtLevel(root, 0);
-	  return sum;
-	}
+    public int deepestLeavesSum(Node root) {
+      if (root == null) {
+	return 0;
+      }
+      calculateSumAtLevel(root, 0);
+      return sum;
+    }
 
-	private void calculateSumAtLevel(Node root, int level) {
-	  if (root == null) {
-		return;
-	  }
-	  if(level > maxLevel) {
+    private void calculateSumAtLevel(Node root, int level) {
+      if (root == null) {
+	return;
+      }
+      if(level > maxLevel) {
         sum = 0;
         maxLevel = level;
       }
-	  if(level == maxLevel) {
-	    sum = sum + root.data;
-	  }
+      if(level == maxLevel) {
+	sum = sum + root.data;
+      }
       calculateSumAtLevel(root.left, level + 1);
       calculateSumAtLevel(root.right, level + 1);
-	}
+    }
 
-	public static void main(String[] args) {   
-	  DeepestLeavesSum tree = new DeepestLeavesSum();
+    public static void main(String[] args) {   
+      DeepestLeavesSum tree = new DeepestLeavesSum();
       tree.root = new Node(1);
       tree.root.left = new Node(2);
       tree.root.right = new Node(3);
@@ -41,6 +41,6 @@ import java.io.*;
       tree.root.left.left.left = new Node(7);
       
       int deepestLeavesSum = tree.deepestLeavesSum(tree.root);
-	  System.out.println("The sum values of the deepest leaves of a binary tree is: " + deepestLeavesSum);
-	}
+      System.out.println("The sum values of the deepest leaves of a binary tree is: " + deepestLeavesSum);
+    }
   }
