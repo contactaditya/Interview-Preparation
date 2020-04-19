f02e853b2ced92a8d56ac823b3b7715aefe9a0af
contactaditya <contactadityag@gmail.com>
2020-04-19 08:17:12 +0530

    This is a program to find the sum of the numbers represented by the path from the root to the leaf for all the leaves in the binary tree.
    Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.

---
+++ Binary Search Trees/SumOfRootToLeafBinaryNumbers.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+  public class SumOfRootToLeafBinaryNumbers {
+	private Node root;	
+	  
+	public static int sumRootToLeaf(Node root) {
+	  return dfs(root, 0);  
+	}	  
+
+	private static int dfs(Node root, int value) {
+	  if (root == null) {
+	    return 0;
+	  }
+	  value = value * 2 + root.data;
+	  return root.left == root.right ? value : dfs(root.left, value) + dfs(root.right, value);
+	}
+
+	public static void main(String[] args) {   
+	  SumOfRootToLeafBinaryNumbers tree = new SumOfRootToLeafBinaryNumbers();
+	  tree.root = new Node(1);
+	  tree.root.left = new Node(0);
+	  tree.root.right = new Node(1);
+	  tree.root.left.left = new Node(0);
+	  tree.root.left.right = new Node(1);
+	  tree.root.right.right = new Node(1);
+	  tree.root.right.left = new Node(0);
+	    
+	  int sumRootToLeaf = sumRootToLeaf(tree.root);
+	  System.out.println("The sum of the numbers represented by the path from the root to that leaf is: " + sumRootToLeaf);
+	}
+  }
