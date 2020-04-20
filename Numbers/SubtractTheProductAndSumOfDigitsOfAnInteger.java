5f4d28ddade27c0cd798b8e452fa94e666d29842
Aditya Gupta <contactadityag@gmail.com>
2020-04-20 06:59:38 +0530

    This is a program to find the difference between the product of its digits and the sum of its digits of the given integer number.

--- Numbers/SubtractTheProductAndSumOfDigitsOfAnInteger.java
+++ Numbers/SubtractTheProductAndSumOfDigitsOfAnInteger.java
@@ -3,40 +3,40 @@ import java.io.*;
 
   public class SubtractTheProductAndSumOfDigitsOfAnInteger {
 	  
-	public static int subtractProductAndSum(int number) {
-	  int result = 0;
-	  int product = getProduct(number);
-	  int sum = getSum(number);
+    public static int subtractProductAndSum(int number) {
+      int result = 0;
+      int product = getProduct(number);
+      int sum = getSum(number);
 	  
-	  result = product - sum;
+      result = product - sum;
 		
-	  return result;      
+      return result;      
     }	
 	
-	private static int getSum(int number) {     
-	  int sum = 0; 
+    private static int getSum(int number) {     
+      int sum = 0; 
 	          
-	  while (number != 0) { 
-	    sum = sum + number % 10; 
-	    number = number/10; 
-	  } 
+      while (number != 0) { 
+	sum = sum + number % 10; 
+	number = number/10; 
+      } 
 	      
-	  return sum; 
+      return sum; 
     } 	
 	
-	private static int getProduct(int number) {     
+    private static int getProduct(int number) {     
       int product = 1; 
 		          
       while (number != 0) { 
-	    product = product * (number % 10); 
-		number = number/10; 
-	  } 
+        product = product * (number % 10); 
+	number = number/10; 
+      } 
 		      
       return product; 
-	} 		
+    } 		
 
-	public static void main(String[] args) {      
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {      
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int number = input.nextInt();
 		  
@@ -44,5 +44,5 @@ import java.io.*;
       System.out.println();
       System.out.println("The difference between product of it's digits and the sum of it's digits is: " + result);
       input.close();
-	}
+    }
   }
