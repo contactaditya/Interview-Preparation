b20c9ef94a84f3f3c14ccf52844ffe6a2e609166
Aditya Gupta <contactadityag@gmail.com>
2020-05-02 08:11:38 +0530

    This is a program to return the length of the longest common subsequence given two strings string1 and string2.
    A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
    
    If there is no common subsequence, return 0.

--- Dynamic Programming/LengthOfLongestCommonSubsequence.java
+++ Dynamic Programming/LengthOfLongestCommonSubsequence.java
@@ -3,45 +3,45 @@ import java.io.*;
 
   public class LengthOfLongestCommonSubsequence {
 	  
-	private static int LongestCommonSubsequence(String string1, String string2) {
-	  int m = string1.length();
-	  int n = string2.length();
-	  int LCS[][] = new int[m + 1][n + 1];
+    private static int LongestCommonSubsequence(String string1, String string2) {
+      int m = string1.length();
+      int n = string2.length();
+      int LCS[][] = new int[m + 1][n + 1];
 	
-	  for (int i = 0; i <= m; i++) { 
-	    for (int j = 0; j <= n; j++) {
-	      if (i == 0 || j == 0) { 
-	        LCS[i][j] = 0; 
-	      } 
-	      // If the current character of the string1 and string2 matches
-	      else if (string1.charAt(i - 1) == string2.charAt(j - 1)) {
-	    	LCS[i][j] = LCS[i - 1][j - 1] + 1;  
-	      } 
-	      // If the current characters of the string1 and string2 don't match
-	      else {
-	    	LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);   
-	      }
-	    }
+      for (int i = 0; i <= m; i++) { 
+	for (int j = 0; j <= n; j++) {
+	  if (i == 0 || j == 0) { 
+	    LCS[i][j] = 0; 
+	  } 
+	  // If the current character of the string1 and string2 matches
+	  else if (string1.charAt(i - 1) == string2.charAt(j - 1)) {
+	    LCS[i][j] = LCS[i - 1][j - 1] + 1;  
+	  } 
+	  // If the current characters of the string1 and string2 don't match
+	  else {
+	    LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);   
 	  }
-		  
-	  return LCS[m][n];
-	}	  
-
-	public static void main(String[] args) {
-      String string1 = new String();	  
-	  Scanner input = new Scanner(System.in);
-	  System.out.print("Enter the first string: ");
-	  string1 = input.next();  
-		  
-	  System.out.println();	
-	  String string2 = new String();	  
-	  System.out.print("Enter the second string: ");
-	  string2 = input.next();
-		  
-	  int lengthOfLongestSubsequence = LongestCommonSubsequence(string1, string2);
-		  
-	  System.out.println();	
-	  System.out.println("The length of the longest common subsequence of the two strings is: " + lengthOfLongestSubsequence);	
-      input.close();
 	}
+      }
+		  
+      return LCS[m][n];
+    }	  
+
+    public static void main(String[] args) {
+      String string1 = new String();	  
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the first string: ");
+      string1 = input.next();  
+		  
+      System.out.println();	
+      String string2 = new String();	  
+      System.out.print("Enter the second string: ");
+      string2 = input.next();
+		  
+      int lengthOfLongestSubsequence = LongestCommonSubsequence(string1, string2);
+		  
+      System.out.println();	
+      System.out.println("The length of the longest common subsequence of the two strings is: " + lengthOfLongestSubsequence);	
+      input.close();
+    }
   }
