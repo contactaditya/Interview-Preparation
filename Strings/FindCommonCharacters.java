0acb615a60f411ab7b5f5a9898029b9fb02f2438
Aditya Gupta <contactadityag@gmail.com>
2020-05-02 07:48:17 +0530

    This is a program to return a list of all characters that show up in all strings within the list (including duplicates).
    For example, if a character occurs 3 times in all strings but not 4 times, you need to include that character three times in the final answer.
    
    You may return the answer in any order.

--- Strings/FindCommonCharacters.java
+++ Strings/FindCommonCharacters.java
@@ -1,34 +1,34 @@
 import java.util.*;
 import java.io.*;
 
-public class FindCommonCharacters {
+  public class FindCommonCharacters {
 
-	public static List<String> commonChars(String[] array) {
-	  List<String> result = new ArrayList<String>(); 
-	  int[] count = new int[26], temp = new int[26];
-	  Arrays.fill(count, Integer.MAX_VALUE);
-	  for (int i = 0; i < array.length; i++) {
-	    for (char c : array[i].toCharArray()) {
-		  temp[c - 'a']++;    
-	    }
-	    // store the minimum character frequency in the count array
-	    for(int j = 0; j < 26; j++) {
-	      count[j] = Math.min(count[j], temp[j]);
-	    }
+    public static List<String> commonChars(String[] array) {
+      List<String> result = new ArrayList<String>(); 
+      int[] count = new int[26], temp = new int[26];
+      Arrays.fill(count, Integer.MAX_VALUE);
+      for (int i = 0; i < array.length; i++) {
+        for (char c : array[i].toCharArray()) {
+	  temp[c - 'a']++;    
+        }
+        // store the minimum character frequency in the count array
+        for(int j = 0; j < 26; j++) {
+	  count[j] = Math.min(count[j], temp[j]);
+        }
         Arrays.fill(temp, 0);
-	  }
+      }
 	  
-	  for (char c = 'a'; c <= 'z'; ++c) {
-	    while (count[c - 'a']-- > 0) {
-	      result.add("" + c);
-	    } 
-	  }
+      for (char c = 'a'; c <= 'z'; ++c) {
+        while (count[c - 'a']-- > 0) {
+	  result.add("" + c);
+	} 
+      }
 		
-	  return result;  
+      return result;  
     }
 
-	public static void main(String[] args) { 	  
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { 	  
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter number of words: ");
       int numbers = input.nextInt();   
       String words[] = new String[numbers];
@@ -38,7 +38,7 @@ public class FindCommonCharacters {
       System.out.print("Please enter the actual words: ");
       System.out.println();
       for (int i = 0; i < numbers; i++) {
-	    words[i] = input.nextLine(); 
+	words[i] = input.nextLine(); 
       }
 	  
       List<String> result = new ArrayList<String>(); 
@@ -46,5 +46,5 @@ public class FindCommonCharacters {
       System.out.println();
       System.out.println("All strings in words which are substring of another word in any order are: " + result);	
       input.close();
-	}
+    }
   }
