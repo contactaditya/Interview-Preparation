6591c2b2ef4b678e0d5c61b1c354e165f7dc91fd
Aditya Gupta <contactadityag@gmail.com>
2020-05-02 07:37:55 +0530

    This is a program to find who is the winner of the divisor game on Leetcode.
    Alice and Bob take turns playing a game, with Alice starting first.
    
    Initially, there is a number N on the chalkboard.  On each player's turn, that player makes a move consisting of:
    
    Choosing any x with 0 < x < N and N % x == 0.
    Replacing the number N on the chalkboard with N - x.
    Also, if a player cannot make a move, they lose the game.
    
    Return True if and only if Alice wins the game, assuming both players play optimally.

--- Numbers/DivisorGame.java
+++ Numbers/DivisorGame.java
@@ -3,13 +3,13 @@ import java.io.*;
 
   public class DivisorGame {
 	  
-	public static boolean divisorGame(int number) {
-	  // According to the rules of the problems even numbers will always win. Odd numbers will give back an even number to your opponent, so odd numbers always lose.
-	  return number % 2 == 0;
+    public static boolean divisorGame(int number) {
+      // According to the rules of the problems even numbers will always win. Odd numbers will give back an even number to your opponent, so odd numbers always lose.
+      return number % 2 == 0;
     }
 
-	public static void main(String[] args) {   
-	  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {   
+      Scanner input = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int number = input.nextInt();   
 		    	   
@@ -23,5 +23,5 @@ import java.io.*;
     	System.out.println("Bob has won the game.");    
       }
       input.close();
-	}
+    }
   }
